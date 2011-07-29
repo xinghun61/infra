5ac83037d7b8146d1c72d52d13080468ba1e1f81
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-29 19:32:24 +0000

    Update tools/run_local_server.sh to be a tad more useful.
    R=bradnelson@chromium.org
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7523062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/chromium-status@94734 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/run_local_server.sh
+++ tools/run_local_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-../google_appengine/dev_appserver.py . --debug --require_indexes \
-   --datastore_path=./tmp.db $@
+python2.5 ../google_appengine/dev_appserver.py . --require_indexes \
+   --debug -a 0.0.0.0 --disable_static_caching $@
