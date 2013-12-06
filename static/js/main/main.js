5595010ee9a3ff2b2458873a93149019c88338ca
vapier@chromium.org <vapier@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-06 20:59:15 +0000

    chromium-status: localize timestamps
    While UTC is great for robots, it sucks for humans.  So let's translate
    things to the user's local timezone.
    
    Unfortunately, appengine does not make this easy (which is partially due
    to HTTP clients not communicating it easily), so instead, let's do it all
    on the fly client side.
    
    The browser knows the exact timezone that is active and javascript makes
    it easy to convert from UTC to it.  There is a slight refresh delay as
    the code kicks in, but it's not a big deal.
    
    BUG=chromium:314501
    TEST=ran an appengine instance and checked the timestamps get converted correctly
    TEST=`./tests/main_test.py` passes
    
    Review URL: https://codereview.chromium.org/63603008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/chromium-status@239256 0039d316-1c4b-4281-b951-d872f2087c98

--- static/js/main/main.js
+++ static/js/main/main.js
@@ -9,6 +9,60 @@
 window.onload = function() {
   document.add_new_message.message.focus();
   help_init();
+  localize_times();
+}
+
+/*
+ * Code for dealing with localization of timestamps.
+ */
+
+function localize_times() {
+  // Localize all the UTC timestamps coming from the server to whatever
+  // the user has set in their browser.
+
+  require(["dojo/date/locale"], function(locale) {
+    function format(date, datePattern, timePattern) {
+      // The dojo guys like to add a sep between the date and the time
+      // fields for us (based on locale).  Since we want a standards
+      // format, that sep is pure noise, so kill it with {...}.
+      // https://bugs.dojotoolkit.org/ticket/17544
+      return locale.format(new Date(date), {
+          formatLength: 'short',
+          datePattern: datePattern + '{',
+          timePattern: '}' + timePattern
+        }).replace(/{.*}/, ' ');
+    }
+
+    function long_date(date) { // RFC2822
+      return format(date, 'EEE, dd MMM yyyy', 'HH:mm:ss z');
+    }
+
+    function short_date(date) {
+      return format(date, 'EEE, dd MMM', 'HH:mm');
+    }
+
+    var tzname = locale.format(new Date(), {
+        selector: 'time',
+        timePattern: 'z'
+      });
+
+    var i, elements;
+
+    // Convert all the fields that have a timezone already.
+    elements = document.getElementsByName('date.datetz');
+    for (i = 0; i < elements.length; ++i)
+      elements[i].innerText = long_date(elements[i].innerText);
+
+    // Convert all the fields that lack a timezone (which we know is UTC).
+    elements = document.getElementsByName('date.date');
+    for (i = 0; i < elements.length; ++i)
+      elements[i].innerText = short_date(elements[i].innerText + ' UTC');
+
+    // Convert all the fields that are just a timezone.
+    elements = document.getElementsByName('date.tz');
+    for (i = 0; i < elements.length; ++i)
+      elements[i].innerText = tzname;
+  });
 }
 
 /*
