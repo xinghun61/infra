2602937c93b3bc0b43650490917ae45629323af7
vadimsh <vadimsh@chromium.org>
2014-07-28 21:05:30 -0700

    Fix pytz custom build script.
    pytz preprocesses src/__init__.py as a separate step (ran during 'make build').
    Processed copy of the package is placed in build/dist. It should be put in
    the wheelhouse instead of original src/.
    
    R=iannucci@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/423053002

--- bootstrap/custom_builds/pytz.py
+++ bootstrap/custom_builds/pytz.py
@@ -1,11 +1,16 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 import os
 import subprocess
 
+
 def Build(source_path, wheelhouse_path):
   subprocess.check_call(
-      ['make', 'CFLAGS=-DSTD_INSPIRED', '.stamp-tzinfo'], cwd=source_path)
+      ['make', 'CFLAGS=-DSTD_INSPIRED', 'build'], cwd=source_path)
 
-  path = os.path.join(source_path, 'src')
+  path = os.path.join(source_path, 'build', 'dist')
   subprocess.check_call(
       ['python', 'setup.py', 'bdist_wheel', '--dist-dir', wheelhouse_path],
       cwd=path)
