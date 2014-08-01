3c9676e9755c405cfe0a3a8ccb0cd40b00e83959
dstockwell <dstockwell@chromium.org>
2014-08-01 06:56:35 -0700

    [trooper-o-matic] Add scatter chart of offending builds
    BUG=396294
    
    Review URL: https://codereview.chromium.org/428783006

--- appengine/trooper_o_matic/tree.py
+++ appengine/trooper_o_matic/tree.py
@@ -21,7 +21,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 
 class TreeHandler(webapp2.RequestHandler):
-  """Displays counts of builds that did not meet SLO for the given Tree."""
+  """Displays summary of builds that did not meet SLO for the given Tree."""
 
   @staticmethod
   def _FormatGoogleChartsDateTime(dt):
@@ -34,6 +34,7 @@ class TreeHandler(webapp2.RequestHandler):
     data = models.BuildTimeStat.query(ancestor=tree_key).order(
         -models.BuildTimeStat.timestamp).fetch(limit=30)
     data.reverse()
+
     builds_cols = [
         {'id': 'timestamp', 'label': 'Time', 'type': 'datetime'},
         {'id': 'num_builds', 'label': 'Number of Builds', 'type': 'number'},
@@ -51,8 +52,18 @@ class TreeHandler(webapp2.RequestHandler):
             ]} for stat in data]
     builds_graph = {'cols': builds_cols, 'rows': builds_rows}
 
+    offenders = [
+      # TODO: Ideally this timestamp would be from the build rather
+      # than the stat, but that's not in the model yet.
+      [stat.timestamp.isoformat(), offender.buildtime,
+       offender.master, offender.builder, offender.buildnumber]
+          for stat in data for offender in stat.slo_offenders
+    ]
+
     template = JINJA_ENVIRONMENT.get_template('tree.html')
     self.response.write(template.render({
+        'offenders': json.dumps(offenders),
         'builds': json.dumps(builds_graph),
         'tree': tree.title(),
+        'slo_buildtime_max': models.SLO_BUILDTIME_MAX,
     }))
