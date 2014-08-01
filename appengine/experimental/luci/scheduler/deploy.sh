3d950f6b006b3e17ef1962f020eed8e4dcab1602
vadimsh <vadimsh@chromium.org>
2014-07-31 17:25:48 -0700

    Switch scheduler app to use Swarming GAE tools.
    R=iannucci@chromium.org,nbharadwaj@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/432223002

--- appengine/experimental/luci/scheduler/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# TODO(nbharadwaj) merge deploy and serve_dev
-
-appcfg.py --oauth2 update api.yaml log.yaml
-appcfg.py --oauth2 update_indexes -A unnamed-ci .
--- appengine/experimental/luci/scheduler/serve_dev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-dev_appserver.py --host 0.0.0.0 api.yaml log.yaml
