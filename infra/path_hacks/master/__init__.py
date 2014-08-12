ccbd96024160667836360ed6abb6d6fb64e79464
agable <agable@chromium.org>
2014-08-11 17:00:40 -0700

    Add path hacks to infra package.
    R=iannucci@chromium.org
    
    Review URL: https://codereview.chromium.org/430383006

---
+++ infra/path_hacks/master/__init__.py
@@ -0,0 +1,13 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os as _os
+
+import infra as _infra
+
+_build_scripts_master = _os.path.abspath(_os.path.join(
+    _infra.__file__, _os.pardir, _os.pardir, _os.pardir,
+    'build', 'scripts', 'master'))
+
+__path__ = [_build_scripts_master]
