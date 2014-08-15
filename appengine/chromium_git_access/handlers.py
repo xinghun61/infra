5b9411cc4aa58378568d4316eb7158bfeffd1aa7
vadimsh <vadimsh@chromium.org>
2014-08-15 16:15:06 -0700

    Entity structure and API handler for git access check reports.
    R=iannucci@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/476033002

--- appengine/chromium_git_access/handlers.py
+++ appengine/chromium_git_access/handlers.py
@@ -2,15 +2,98 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import hashlib
+import logging
 import webapp2
 
+from google.appengine.api import datastore_errors
+from google.appengine.ext import ndb
+
 from components import auth
+from components import datastore_utils
+
+
+class AccessCheckEntry(ndb.Model):
+  """Information about some attempt to check write access to Chromium git repo.
+
+  Submitted by clients from gclient hooks in chromium/src.
+  """
+  # Class name of a parent key (identifying a shard).
+  SHARD_ENTITY_CLASS = 'AccessCheckEntryShard_v1'
+
+  # How many letters of entity ID (hex) to use to identify a shard.
+  SHARD_PREFIX_LEN = 2
+
+  # Properties used to construct entity ID.
+  FINGERPRINT_PROPERTIES = frozenset([
+    'checker_version',
+    'chrome_internal_netrc_email',
+    'chromium_netrc_email',
+    'git_user_email',
+    'git_user_name',
+    'is_git',
+    'is_home_set',
+    'is_using_netrc',
+    'netrc_file_mode',
+    'push_works',
+    'username',
+    'platform',
+  ])
+
+  # Properties settable via HTTP POST from client.
+  EXTERNAL_PROPERTIES = frozenset(list(FINGERPRINT_PROPERTIES) + [
+    'push_duration_ms',
+    'push_log',
+    'push_works',
+  ])
+
+  # When the entry was submitted.
+  timestamp = ndb.DateTimeProperty(auto_now_add=True)
+  # Client version that submitted this entry.
+  checker_version = ndb.IntegerProperty()
+
+  # True if chromium git checkout is used, False if svn.
+  is_git = ndb.BooleanProperty()
+  # True if HOME env var is set.
+  is_home_set = ndb.BooleanProperty()
+  # True if ~/.netrc file exists.
+  is_using_netrc = ndb.BooleanProperty()
+  # netrc file access mode.
+  netrc_file_mode = ndb.IntegerProperty()
+
+  # System platform, as reported by sys.platform.
+  platform = ndb.StringProperty()
+  # User name in the system, as returned by getpass.getuser().
+  username = ndb.StringProperty()
+  # git email as seen by chromium/src repo.
+  git_user_email = ndb.StringProperty()
+  # git user name as seen by chromium/src repo.
+  git_user_name = ndb.StringProperty()
+
+  # email in ~/.netrc entry for chromium host, or None if missing.
+  chromium_netrc_email = ndb.StringProperty()
+  # email in ~/.netrc entry for chrome-internal host, or None if missing.
+  chrome_internal_netrc_email = ndb.StringProperty()
+
+  # True if push attempt was successful.
+  push_works = ndb.BooleanProperty()
+  # Log of the push operation.
+  push_log = ndb.TextProperty()
+  # How long push took (ms), successful or not.
+  push_duration_ms = ndb.IntegerProperty()
+
+
+# Typo safety measures.
+assert AccessCheckEntry.EXTERNAL_PROPERTIES.issubset(
+    AccessCheckEntry._properties)  # pylint: disable=W0212
 
 
 def get_routes():
   return [
     webapp2.Route(r'/', MainHandler),
     webapp2.Route(r'/_ah/warmup', WarmupHandler),
+    webapp2.Route(
+        r'/git_access/api/v1/reports/access_check', AccessCheckHandler),
   ]
 
 
@@ -23,3 +106,51 @@ class WarmupHandler(webapp2.RequestHandler):
   def get(self):
     auth.warmup()
     self.response.write('ok')
+
+
+class AccessCheckHandler(auth.ApiHandler):
+  # We don't care about XSRF here since there's no authentication anyway.
+  xsrf_token_enforce_on = ()
+
+  @auth.public
+  def post(self):
+    body = self.parse_body()
+    if not isinstance(body.get('access_check'), dict):
+      self.abort_with_error(400, text='Missing access_check dict')
+    access_check = body['access_check']
+
+    # All EXTERNAL_PROPERTIES are required.
+    if not AccessCheckEntry.EXTERNAL_PROPERTIES.issubset(access_check):
+      self.abort_with_error(400, text='Incomplete access_check dict')
+
+    # Read entity properties from the request body.
+    entity = AccessCheckEntry()
+    for key, value in body['access_check'].iteritems():
+      if key in AccessCheckEntry.EXTERNAL_PROPERTIES:
+        try:
+          setattr(entity, key, value)
+        except (datastore_errors.BadValueError, ValueError):
+          self.abort_with_error(400, text='Key %s has invalid type' % key)
+      else:
+        logging.warning('Skipping unknown key %s', key)
+
+    # Use important parts of the entity to derive its key. It's a simple way
+    # to avoid having duplicate entities in db in case client submits
+    # same report multiple times.
+    fingerprint = []
+    for key in sorted(list(AccessCheckEntry.FINGERPRINT_PROPERTIES)):
+      fingerprint.append(str(getattr(entity, key, None)))
+    fingerprint = hashlib.sha1('\0'.join(fingerprint)).hexdigest()
+
+    # Shard across 256 shards to avoid creating too many entity groups, queries
+    # are faster that way. Entity key will be mostly meaningless.
+    entity.key = ndb.Key(
+        AccessCheckEntry,
+        fingerprint,
+        parent=datastore_utils.shard_key(
+            fingerprint,
+            AccessCheckEntry.SHARD_PREFIX_LEN,
+            AccessCheckEntry.SHARD_ENTITY_CLASS))
+    entity.put()
+
+    self.send_response({'ok': True})
--- appengine/chromium_git_access/test/handlers_test.py
+++ appengine/chromium_git_access/test/handlers_test.py
@@ -5,21 +5,134 @@
 # Hack sys.path.
 import setup_test_env  # pylint: disable=W0611
 
+import datetime
 import webapp2
+
+from google.appengine.ext import ndb
 
 from appengine.utils import testing
 from appengine.chromium_git_access import handlers
 
 
-class TestHandlers(testing.AppengineTestCase):
+class TestBasicHandlers(testing.AppengineTestCase):
   app_module = webapp2.WSGIApplication(handlers.get_routes(), debug=True)
 
   def test_main_page(self):
     """Test that the root page renders."""
     response = self.test_app.get('/')
-    self.assertEquals(response.status_int, 200)
+    self.assertEquals(200, response.status_int)
 
   def test_warmup(self):
     """Test that warmup works."""
     response = self.test_app.get('/_ah/warmup')
-    self.assertEquals(response.status_int, 200)
+    self.assertEquals(200, response.status_int)
+
+
+class TestAccessCheckHandler(testing.AppengineTestCase):
+  app_module = webapp2.WSGIApplication(handlers.get_routes(), debug=True)
+
+  canned_access_check = {
+    'checker_version': 0,
+    'chrome_internal_netrc_email': None,
+    'chromium_netrc_email': 'blah@chromium.org',
+    'git_user_email': 'blah@chromium.org',
+    'git_user_name': 'Blah Blahovic',
+    'is_git': True,
+    'is_home_set': True,
+    'is_using_netrc': True,
+    'netrc_file_mode': 0600,
+    'platform': 'linux2',
+    'push_duration_ms': 5000,
+    'push_log': 'It just worked!!!111',
+    'push_works': True,
+    'username': 'blah',
+  }
+
+  def post_check(self, **access_check):
+    """Helper to post JSON to the handler."""
+    return self.test_app.post_json(
+        '/git_access/api/v1/reports/access_check',
+        {'access_check': access_check},
+        expect_errors=True)
+
+  @staticmethod
+  def fetch_entries():
+    """Fetches all AccessCheckEntry from datastore."""
+    return list(handlers.AccessCheckEntry.query())
+
+  def test_submit_works(self):
+    """Submitting reports add it to datastore."""
+    mocked_now = datetime.datetime(1963, 11, 22)
+    self.mock_now(mocked_now)
+
+    self.assertEquals(0, len(self.fetch_entries()))
+    response = self.post_check(**self.canned_access_check)
+    self.assertEquals(200, response.status_int)
+    self.assertEquals({'ok': True}, response.json_body)
+    # Read it back from datastore.
+    entries = self.fetch_entries()
+    self.assertEquals(1, len(entries))
+    self.assertEquals(
+        ndb.Key(
+            'AccessCheckEntryShard_v1',
+            '52',
+            'AccessCheckEntry',
+            '523e0b836ceaf0583017c23e8cab77908e8fc932'),
+        entries[0].key)
+    self.assertEquals(
+        {
+          'checker_version': 0,
+          'chrome_internal_netrc_email': None,
+          'chromium_netrc_email': u'blah@chromium.org',
+          'git_user_email': u'blah@chromium.org',
+          'git_user_name': u'Blah Blahovic',
+          'is_git': True,
+          'is_home_set': True,
+          'is_using_netrc': True,
+          'netrc_file_mode': 384,
+          'platform': u'linux2',
+          'push_duration_ms': 5000,
+          'push_log': u'It just worked!!!111',
+          'push_works': True,
+          'timestamp': datetime.datetime(1963, 11, 22, 0, 0),
+          'username': u'blah',
+      }, entries[0].to_dict())
+
+  def test_no_access_check_dict(self):
+    """'access_check' dict is required."""
+    response = self.test_app.post_json(
+        '/git_access/api/v1/reports/access_check',
+        {'blah': {}},
+        expect_errors=True)
+    self.assertEquals(400, response.status_int)
+    self.assertEquals({'text': 'Missing access_check dict'}, response.json_body)
+
+  def test_missing_some_properties(self):
+    """All known properties are required."""
+    request = self.canned_access_check.copy()
+    request.pop('username')
+    response = self.post_check(**request)
+    self.assertEquals(400, response.status_int)
+    self.assertEquals(
+        {'text': 'Incomplete access_check dict'}, response.json_body)
+
+  def test_wrong_type(self):
+    request = self.canned_access_check.copy()
+    request['checker_version'] = 'I am totally an integer'
+    response = self.post_check(**request)
+    self.assertEquals(400, response.status_int)
+    self.assertEquals(
+        {'text': 'Key checker_version has invalid type'}, response.json_body)
+
+  def test_extra_properties(self):
+    """Extra properties are ignored."""
+    response = self.post_check(extra=1, **self.canned_access_check)
+    self.assertEquals(200, response.status_int)
+    self.assertEquals({'ok': True}, response.json_body)
+
+  def test_resubmit_skipped(self):
+    """Submitting same report twice doesn't create second entity."""
+    self.assertEquals(0, len(self.fetch_entries()))
+    self.post_check(**self.canned_access_check)
+    self.post_check(**self.canned_access_check)
+    self.assertEquals(1, len(self.fetch_entries()))
