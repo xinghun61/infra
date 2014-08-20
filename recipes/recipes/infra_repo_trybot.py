0990c014951d400b41fea360d31ad20732bb4875
eseidel@chromium.org <eseidel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-20 15:31:38 +0000

    Revert of Teach the Infra recipe how to run presubmit (patchset #1 of https://codereview.chromium.org/483023003/)
    Reason for revert:
    This appears to be failing on all presubmits.  I must have wired something wrong.
    
    Original issue's description:
    > Teach the Infra recipe how to run presubmit
    > 
    > Once this is done, we can make the presbumit
    > not actually run tests.py, so that we don't run
    > it twice.
    > 
    > BUG=402776
    > 
    > Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=290765
    
    TBR=iannucci@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=402776
    
    Review URL: https://codereview.chromium.org/494733003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@290843 0039d316-1c4b-4281-b951-d872f2087c98

--- recipes/recipes/infra_repo_trybot.py
+++ recipes/recipes/infra_repo_trybot.py
@@ -5,7 +5,6 @@
 DEPS = [
   'gclient',
   'path',
-  'properties',
   'python',
 ]
 
@@ -15,22 +14,6 @@ def GenSteps(api):
   api.gclient.runhooks()
   api.python('test.py', api.path['checkout'].join('test.py'))
 
-  # FIXME: This is copied from the run_presubmit.py recipe.
-  # We should instead share code!
-  api.python('presubmit',
-    api.path['depot_tools'].join('presubmit_support.py'),
-    ['--commit',
-    '--verbose', '--verbose',
-    '--issue', api.properties['issue'],
-    '--patchset', api.properties['patchset'],
-    '--skip_canned', 'CheckRietveldTryJobExecution',
-    '--skip_canned', 'CheckTreeIsOpen',
-    '--skip_canned', 'CheckBuildbotPendingBuilds',
-    '--rietveld_url', api.properties['rietveld'],
-    '--rietveld_email', '',  # activates anonymous mode
-    '--rietveld_fetch'])
-
 
 def GenTests(api):
-  yield api.test('basic') + api.properties.tryserver()
-
+  yield api.test('basic')
