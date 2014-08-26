67256440196ae15a0b514a31914a3ce8fcb3997e
agable@chromium.org <agable@chromium.org>
2014-08-26 01:45:03 +0000

    Get infra trybot recipe to run presubmit in right directory.
    TBR=eseidel@chromium.org
    BUG=402776
    
    Review URL: https://codereview.chromium.org/502303002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@291624 0039d316-1c4b-4281-b951-d872f2087c98

--- recipes/recipes/infra_repo_trybot.py
+++ recipes/recipes/infra_repo_trybot.py
@@ -21,17 +21,18 @@ def GenSteps(api):
   # FIXME: This is copied from the run_presubmit.py recipe.
   # We should instead share code!
   api.python('presubmit',
-    api.path['depot_tools'].join('presubmit_support.py'),
-    ['--commit',
-    '--verbose', '--verbose',
-    '--issue', api.properties['issue'],
-    '--patchset', api.properties['patchset'],
-    '--skip_canned', 'CheckRietveldTryJobExecution',
-    '--skip_canned', 'CheckTreeIsOpen',
-    '--skip_canned', 'CheckBuildbotPendingBuilds',
-    '--rietveld_url', api.properties['rietveld'],
-    '--rietveld_email', '',  # activates anonymous mode
-    '--rietveld_fetch'])
+      api.path['depot_tools'].join('presubmit_support.py'),
+      ['--root', api.path['checkout'],
+      '--commit',
+      '--verbose', '--verbose',
+      '--issue', api.properties['issue'],
+      '--patchset', api.properties['patchset'],
+      '--skip_canned', 'CheckRietveldTryJobExecution',
+      '--skip_canned', 'CheckTreeIsOpen',
+      '--skip_canned', 'CheckBuildbotPendingBuilds',
+      '--rietveld_url', api.properties['rietveld'],
+      '--rietveld_email', '',  # activates anonymous mode
+      '--rietveld_fetch'])
 
 
 def GenTests(api):
