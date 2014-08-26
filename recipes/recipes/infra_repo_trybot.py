90eaaccea3499648641398922cb4de21a8c5f3de
agable@chromium.org <agable@chromium.org>
2014-08-26 01:12:54 +0000

    Infra recipe how to run presubmit
    Once this is done, we can make the presbumit
    not actually run tests.py, so that we don't run
    it twice.
    
    Originally landed as:
    https://codereview.chromium.org/483023003
    But then reverted as:
    https://codereview.chromium.org/494733003/
    
    The underlying issue of the patch not being applied was
    caused by the infra tryjob being run on the
    chromium.linux master and defaulting to
    root='src' causing all patches to fail to apply.
    
    That issue is in the process of being fixed.
    
    BUG=402776
    TBR=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/506573004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@291622 0039d316-1c4b-4281-b951-d872f2087c98

--- recipes/recipes/infra_repo_trybot.py
+++ recipes/recipes/infra_repo_trybot.py
@@ -6,9 +6,11 @@ DEPS = [
   'bot_update',
   'gclient',
   'path',
+  'properties',
   'python',
   'properties',
 ]
+
 
 def GenSteps(api):
   api.gclient.set_config('infra')
@@ -16,10 +18,21 @@ def GenSteps(api):
   api.gclient.runhooks()
   api.python('test.py', api.path['checkout'].join('test.py'))
 
+  # FIXME: This is copied from the run_presubmit.py recipe.
+  # We should instead share code!
+  api.python('presubmit',
+    api.path['depot_tools'].join('presubmit_support.py'),
+    ['--commit',
+    '--verbose', '--verbose',
+    '--issue', api.properties['issue'],
+    '--patchset', api.properties['patchset'],
+    '--skip_canned', 'CheckRietveldTryJobExecution',
+    '--skip_canned', 'CheckTreeIsOpen',
+    '--skip_canned', 'CheckBuildbotPendingBuilds',
+    '--rietveld_url', api.properties['rietveld'],
+    '--rietveld_email', '',  # activates anonymous mode
+    '--rietveld_fetch'])
+
 
 def GenTests(api):
-  yield api.test('basic') + api.properties(
-    mastername='fake master',
-    buildername='fake builder',
-    slavename='fake slave',
-  )
+  yield api.test('basic') + api.properties.tryserver()
