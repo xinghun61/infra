2410952b6c245ffb8de335211d8cfb98e51a8827
stip <stip@chromium.org>
2014-08-29 11:47:53 -0700

    Remove apptrace.
    This pleases pylint greatly.
    
    Review URL: https://codereview.chromium.org/523603003

--- appengine/chrome_infra_stats/appengine_config.py
+++ appengine/chrome_infra_stats/appengine_config.py
@@ -9,9 +9,4 @@ apptrace_TRACE_MODULES = ['models.py', 'app.py', 'views.py']
 def webapp_add_wsgi_middleware(app):
   from google.appengine.ext.appstats import recording
   app = recording.appstats_wsgi_middleware(app)
-  try:
-    from apptrace.middleware import apptrace_middleware
-    app = apptrace_middleware(app)
-  except ImportError:
-    pass
   return app
