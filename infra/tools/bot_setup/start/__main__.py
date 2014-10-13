485005723d5b97dd0b0b12c10fe3758d5d4b60df
hinoka <hinoka@chromium.org>
2014-10-13 10:16:58 -0700

    Add GCE bot userland setup scripts to infra/
    These are meant to be used to bootstrap GCE slaves.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/642933003

---
+++ infra/tools/bot_setup/start/__main__.py
@@ -0,0 +1,15 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Sets up and starts a bot."""
+
+import sys
+
+
+def main():
+  print 'This is a stub.  Currently this does nothing.'
+
+
+if __name__ == '__main__':
+  sys.exit(main())
---
+++ infra/tools/bot_setup/version/__main__.py
@@ -0,0 +1,13 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Returns the canonical version of bot setup scripts for hostname and image."""
+
+import sys
+
+def main():
+  print 'origin/master'
+
+if __name__ == '__main__':
+  sys.exit(main())
