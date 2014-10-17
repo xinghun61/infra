b2c3f53a62de2d79c59d42377c79db20896cbce9
hinoka <hinoka@chromium.org>
2014-10-17 14:51:53 -0700

    Add disabled slave code path for bot_setup.version
    This adds a way to turn off a GCE slave by committing a 1 line change to a file.
    
    BUG=406073
    
    Review URL: https://codereview.chromium.org/668433002

--- infra/tools/bot_setup/version/__main__.py
+++ infra/tools/bot_setup/version/__main__.py
@@ -4,9 +4,35 @@
 
 """Returns the canonical version of bot setup scripts for hostname and image."""
 
+import argparse
 import sys
 
+
+DISABLED_BUILDERS = [
+    'test_disabled_slave'
+]
+
+
+class BuilderDisabled(Exception):
+  """This is raised when a builder should be disabled.
+
+  By raising an exeception, the startup sequence becomes interrupted.
+  """
+  pass
+
+
+def parse_args():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--slave_name')
+  parser.add_argument('--image_name')
+
+  return parser.parse_args()
+
+
 def main():
+  args = parse_args()
+  if args.slave_name and args.slave_name in DISABLED_BUILDERS:
+    raise BuilderDisabled()
   print 'origin/master'
 
 if __name__ == '__main__':
