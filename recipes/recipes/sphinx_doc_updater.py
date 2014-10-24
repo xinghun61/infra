18ed53ec8dc311c543ab053e149a3c5a65d52840
pgervais@chromium.org <pgervais@chromium.org>
2014-10-24 23:33:21 +0000

    Infra doc generation recipe
    This is used by the doc generation and deployment builder.
    
    BUG=362735
    
    Review URL: https://codereview.chromium.org/670313003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@292669 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ recipes/recipes/sphinx_doc_updater.py
@@ -0,0 +1,43 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Compile and deploy infra.git documentation."""
+
+DEPS = [
+  'bot_update',
+  'gclient',
+  'gsutil',
+  'path',
+  'properties',
+  'python',
+]
+
+
+def GenSteps(api):
+  api.gclient.set_config('infra')
+  api.bot_update.ensure_checkout(force=True)
+  api.gclient.runhooks()
+
+  # Update documentation
+  api.python('Clean documentation',
+             api.path['slave_build'].join('infra', 'docgen.py'),
+             ['clean'])
+  api.python('Generate documentation',
+             api.path['slave_build'].join('infra', 'docgen.py'))
+
+  # Upload generated documentation
+  api.gsutil.upload(
+    api.path['slave_build'].join('infra', 'doc', 'html'),
+    'chromium-infra-docs', 'infra',
+    args=['-R'],
+    link_name='Deploy documentation to GCS',
+    use_retry_wrapper=False)
+
+
+def GenTests(api):
+  yield (
+      api.test('basic') +
+      api.properties(mastername='fake', buildername='fake',
+                     slavename='fake')
+  )
