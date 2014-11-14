43d8b3817c4673152b956c630e4d45578f8a9a59
Aaron Gable <agable@chromium.org>
2014-11-14 13:31:54 -0800

    Fixes for sheriff-o-matic to live in infra.
    This commit includes symlinks to the necessary npm_modules and
    bower_components directories, cleanup to make pylint happy, and
    various other small fixes.
    
    Review URL: https://codereview.chromium.org/730543003
    
    BUG=433568
    R=ojan@chromium.org,pgervais@chromium.org

--- appengine/sheriff_o_matic/internal_alerts.py
+++ appengine/sheriff_o_matic/internal_alerts.py
@@ -4,43 +4,42 @@
 
 import datetime
 import alerts
-import json
 import webapp2
 
 from google.appengine.api import users
 
 
 class InternalAlertsHandler(alerts.AlertsHandler):
-    INTERNAL_ALERTS_TYPE = 'internal-alerts'
+  INTERNAL_ALERTS_TYPE = 'internal-alerts'
 
-    # Has no 'request' member.
-    # Has no 'response' member.
-    # Use of super on an old style class.
-    # pylint: disable=E1002,E1101
-    def get(self):
-        # Require users to be logged to see builder alerts from private/internal
-        # trees.
-        user = users.get_current_user()
-        if not user:
-            alerts = {}
-            alerts.update({
-                'date': datetime.datetime.utcnow(),
-                'redirect-url': users.create_login_url(self.request.uri)})
-            uncompressed = super(InternalAlertsHandler,
-                                 self).generate_json_dump(alerts)
-            super(InternalAlertsHandler, self).send_json_data(uncompressed)
-            return
+  # Has no 'request' member.
+  # Has no 'response' member.
+  # Use of super on an old style class.
+  # pylint: disable=E1002,E1101
+  def get(self):
+    # Require users to be logged to see builder alerts from private/internal
+    # trees.
+    user = users.get_current_user()
+    if not user:
+      ret = {}
+      ret.update({
+          'date': datetime.datetime.utcnow(),
+          'redirect-url': users.create_login_url(self.request.uri)})
+      uncompressed = super(InternalAlertsHandler,
+                           self).generate_json_dump(ret)
+      super(InternalAlertsHandler, self).send_json_data(uncompressed)
+      return
 
-        email = user.email()
-        if not email.endswith('@google.com'):
-            self.response.set_status(403, 'invalid user')
-            return
+    email = user.email()
+    if not email.endswith('@google.com'):
+      self.response.set_status(403, 'invalid user')
+      return
 
-        super(InternalAlertsHandler, self).get_from_memcache(
-            InternalAlertsHandler.INTERNAL_ALERTS_TYPE)
+    super(InternalAlertsHandler, self).get_from_memcache(
+        InternalAlertsHandler.INTERNAL_ALERTS_TYPE)
 
-    def post(self):
-        self.update_alerts(InternalAlertsHandler.INTERNAL_ALERTS_TYPE)
+  def post(self):
+    self.update_alerts(InternalAlertsHandler.INTERNAL_ALERTS_TYPE)
 
 
 app = webapp2.WSGIApplication([
--- appengine/sheriff_o_matic/tests.py
+++ appengine/sheriff_o_matic/tests.py
@@ -21,25 +21,25 @@ _webtest_not_installed_message = '''
 
 
 def main():
-    try:
-        import dev_appserver
-    except ImportError:
-        print >> sys.stderr, _gae_sdk_not_on_python_path_message
-        raise
+  try:
+    import dev_appserver
+  except ImportError:
+    print >> sys.stderr, _gae_sdk_not_on_python_path_message
+    raise
 
-    dev_appserver.fix_sys_path()
+  dev_appserver.fix_sys_path()
 
-    try:
-        import webtest
-    except ImportError:
-        print >> sys.stderr, _webtest_not_installed_message
-        raise
+  try:
+    import webtest  # pylint: disable=W0612
+  except ImportError:
+    print >> sys.stderr, _webtest_not_installed_message
+    raise
 
-    tests_path = os.path.dirname(sys.modules[__name__].__file__)
-    suite = unittest.loader.TestLoader().discover(tests_path,
-                                                  pattern='*_test.py')
-    unittest.TextTestRunner(verbosity=2).run(suite)
+  tests_path = os.path.dirname(sys.modules[__name__].__file__)
+  suite = unittest.loader.TestLoader().discover(tests_path,
+                                                pattern='*_test.py')
+  unittest.TextTestRunner(verbosity=2).run(suite)
 
 
 if __name__ == '__main__':
-    main()
+  main()
