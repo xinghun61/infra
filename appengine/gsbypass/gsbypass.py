a202bddbd3f36636d080c2f7b381dda1a6ff7f44
dnj <dnj@chromium.org>
2014-12-18 09:38:41 -0800

    gsbypass: Cleanup and updates.
    Updated gsbypass code:
    - Added some more logging to help diagnose problems.
    - Updated block size to 1MB (from 16KB).
    - Exceptions are handled during open/stat operations.
    - Users that are not logged in will be redirected to a log-in page.
    
    BUG=chromium:443278
    
    Review URL: https://codereview.chromium.org/810583003

--- appengine/gsbypass/gsbypass.py
+++ appengine/gsbypass/gsbypass.py
@@ -4,10 +4,12 @@
 
 """App to access protected Google Storage URLs via their object names."""
 
-import cloudstorage as gcs
+import httplib
 import logging
+import urllib
 import webapp2
 
+import cloudstorage as gcs
 from google.appengine.api import users
 
 
@@ -15,32 +17,48 @@ def google_login_required(fn):
   """Return 403 unless the user is logged in from a @google.com domain"""
   def wrapper(self, *args, **kwargs):
     user = users.get_current_user()
+    if not user:
+      self.redirect(users.create_login_url(self.request.uri))
     _, _, domain = user.email().partition('@')
     if domain == 'google.com':
       return fn(self, *args, **kwargs)
-    self.error(403)  # Unrecognized email or unauthorized domain.
-    self.response.out.write('unauthorized email %s' % user.user_id())
+    logging.error('Unauthorized e-mail (%s)', user.user_id())
+    self.response.out.write('Unauthorized e-mail')
+    self.abort(httplib.FORBIDDEN)
   return wrapper
 
 
 class ProxyPage(webapp2.RequestHandler):
+  BLOCK_SIZE = 1024 * 1024 # 1MB at a time.
+  RETRY_PARAMS = gcs.RetryParams(max_retries=3)
+
   @google_login_required
   def get(self, bucket, obj):
-    BLOCK_SIZE = 16 * 1024
-    filename = '/%s/%s' % (bucket, obj)
-    gcs_file = gcs.open(filename)
-    gcs_stat = gcs.stat(filename)
-    logging.info('Opening %s using BLOCK_SIZE=%d' % (filename, BLOCK_SIZE))
+    gcs_path = '/%s/%s' % (bucket, urllib.quote(obj))
+    logging.info('Opening %s using BLOCK_SIZE=%d', gcs_path, self.BLOCK_SIZE)
+    try:
+      gcs_file = gcs.open(gcs_path, retry_params=self.RETRY_PARAMS)
+      gcs_stat = gcs.stat(gcs_path, retry_params=self.RETRY_PARAMS)
+    except gcs.ForbiddenError:
+      logging.exception("ForbiddenError accessing path %s", gcs_path)
+      self.abort(httplib.FORBIDDEN)
+    except gcs.AuthorizationError:
+      logging.exception("AuthorizationError accessing path %s", gcs_path)
+      self.abort(httplib.UNAUTHORIZED)
+
     self.response.headers["Content-Type"] = gcs_stat.content_type
 
+    content_size = 0L
     block_num = 0
     while True:
-      block = gcs_file.read(BLOCK_SIZE)
+      block = gcs_file.read(self.BLOCK_SIZE)
       if not block:
-        logging.info('Finished reading %d blocks' % block_num)
-        return
-      block_num += 1
+        break
       self.response.write(block)
+      content_size += len(block)
+      block_num += 1
+    logging.info("Wrote content from [%s]: %s blocks, %s bytes",
+                 gcs_path, block_num, content_size)
 
 
 app = webapp2.WSGIApplication([
