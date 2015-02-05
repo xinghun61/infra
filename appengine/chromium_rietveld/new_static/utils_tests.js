ccc18916656765dcaf78cccb0a97356206a49e59
esprehn <esprehn@chromium.org>
2015-02-05 15:49:03 -0800

    Refactor the email quoting to remove code duplication.
    The email quoting logic was duplicated in <cr-patch-file-message> and in
    <cr-issue-message-reply>. Instead add it to a helper function and
    also add some tests.
    
    Review URL: https://codereview.chromium.org/903883004

---
+++ appengine/chromium_rietveld/new_static/utils_tests.js
@@ -0,0 +1,15 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+"use strict";
+
+describe("Utils", function() {
+describe("emailQuote", function() {
+    it("should quote in email format", function() {
+        var date = Date.create("2011-07-05 12:24:55.528Z");
+        var text = emailQuote("This is a\n> quoted message", "Elliott Sprehn", date);
+        expect(text).toBe("On 2011/07/05 at 12:24:55, Elliott Sprehn wrote:\n> This is a\n> > quoted message\n\n");
+    });
+});
+});
