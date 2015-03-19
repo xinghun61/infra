2583bea824cfab5044624bd4273c9ec30799b2fa
pgervais <pgervais@chromium.org>
2015-03-19 14:58:56 -0700

    Created event_mon.get_revinfo()
    This function runs gclient to get the revisions at which code is
    checked out.
    
    BUG=468453
    
    Review URL: https://codereview.chromium.org/1023563002

--- infra/libs/event_mon/__init__.py
+++ infra/libs/event_mon/__init__.py
@@ -1,6 +1,8 @@
 # Copyright 2015 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+from infra.libs.event_mon.checkouts import get_revinfo
+
 from infra.libs.event_mon.config import add_argparse_options
 from infra.libs.event_mon.config import close
 from infra.libs.event_mon.config import process_argparse_options
---
+++ infra/libs/event_mon/checkouts.py
@@ -0,0 +1,60 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Functions in this file relies on depot_tools been checked-out as a sibling
+# of infra.git.
+
+import logging
+import os
+import re
+import subprocess
+
+
+BASE_DIR = os.path.dirname(
+  os.path.dirname(
+    os.path.dirname(
+      os.path.dirname(
+        os.path.dirname(os.path.abspath(__file__))))))
+
+
+def parse_revinfo(revinfo):
+  """Parse the output of "gclient revinfo -a"
+
+  Args:
+    revinfo (str): string containing gclient stdout.
+
+  Returns:
+    revinfo_d (dict): <directory>: (URL, revision)
+  """
+  revision_expr = re.compile('(.*)@([^@]*)')
+
+  revinfo_d = {}
+  for line in revinfo.splitlines():
+    if ':' not in line:
+      continue
+
+    # TODO: this fails when the file name contains a colon.
+    path, line = line.split(':', 1)
+    if '@' in line:
+      url, revision = revision_expr.match(line).groups()
+      revision = revision.strip()
+    else:
+      # Split at the last @
+      url, revision = line.strip(), None
+
+    path = path.strip()
+    url = url.strip()
+    revinfo_d[path] = {'source_url': url, 'revision': revision}
+  return revinfo_d
+
+
+def get_revinfo(cwd=None):  # pragma: no cover
+  # Call gclient to get the list of all revisions actually checked out on disk.
+
+  cmd = [os.path.join(BASE_DIR, 'depot_tools', 'gclient'), 'revinfo', '-a']
+  try:
+    revinfo = subprocess.check_output(cmd, cwd=cwd)
+  except subprocess.CalledProcessError:
+    logging.error('Command failed to run: %s', ' '.join(cmd))
+  return parse_revinfo(revinfo)
