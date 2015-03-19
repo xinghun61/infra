ba39d9a3c727e36112eb18d79fbde259c4d2b6a0
pgervais <pgervais@chromium.org>
2015-03-19 16:42:10 -0700

    Code version can be sent as part of a ServiceEvent
    Added a keyword argument to send_service_event in the event_mon package
    to provide information about the currently running code.
    
    Also refactored for testing, and wrote tests.
    
    BUG=444091
    
    Review URL: https://codereview.chromium.org/1009033003

--- infra/libs/event_mon/monitoring.py
+++ infra/libs/event_mon/monitoring.py
@@ -12,16 +12,31 @@ from infra.libs.event_mon import config, router
 EVENT_TYPES = ('START', 'STOP', 'UPDATE', 'CURRENT_VERSION')
 TIMESTAMP_KINDS = (None, 'UNKNOWN', 'POINT', 'BEGIN', 'END')
 
-def send_service_event(event_type,
+
+def _get_service_event(event_type,
                        timestamp_kind='POINT',
                        event_timestamp=None,
-                     ):
-  """Send service event.
+                       code_version=None):
+  """Compute a ChromeInfraEvent filled with a ServiceEvent.
+
+  Args:
+    event_type (string): any name of enum ServiceEvent.ServiceEventType.
+      ('START', 'STOP', 'UPDATE', 'CURRENT_VERSION')
+    timestamp_kind (string): any of ('POINT', 'BEGIN', 'END').
+    event_timestamp (int or float): timestamp of when the event happened
+      as a number of milliseconds since the epoch. If not provided, the
+      current time is used.
+    code_version (list/tuple of dict or None): required keys are
+        'source_url' -> full url to the repository
+        'revision' -> (string) git sha1 or svn revision number.
+      optional keys are
+        'dirty' -> boolean. True if the local source tree has local
+            modification.
+        'version' -> manually-set version number (like 'v2.6.0')
 
   Returns:
-    success (bool): False if some error happened.
+    event (log_request_lite_pb2.LogRequestLite.LogEventLite):
   """
-
   if event_type not in EVENT_TYPES:
     logging.error('Invalid value for event_type: %s' % str(event_type))
     return False
@@ -38,10 +53,72 @@ def send_service_event(event_type,
   event.CopyFrom(config.cache['default_event'])
   event.service_event.type = getattr(ServiceEvent, event_type)
 
+  if code_version is None:
+    code_version = ()
+  if not isinstance(code_version, (tuple, list)):
+    logging.error('Invalid type provided to code_version argument in'
+                  '_get_service_event. Please fix the calling code.'
+                  'Type provided: %s, expected list, tuple or None.'
+                  % str(type(code_version)))
+    code_version = ()
+
+  for version_d in code_version:
+    try:
+      if 'source_url' not in version_d:
+        logging.error('source_url missing in %s', version_d)
+        continue
+
+      version = event.service_event.code_version.add()
+      version.source_url = version_d['source_url']
+      if 'revision' in version_d:
+        # Rely on the url to switch between svn and git because an
+        # abbreviated sha1 can sometime be confused with an int.
+        if version.source_url.startswith('svn://'):
+          version.svn_revision = int(version_d['revision'])
+        else:
+          version.git_hash = version_d['revision']
+
+      if 'version' in version_d:
+        version.version = version_d['version']
+      if 'dirty' in version_d:
+        version.dirty = version_d['dirty']
+
+    except TypeError:
+      logging.exception('Invalid type provided to code_version argument in '
+                        '_get_service_event. Please fix the calling code.')
+      continue
+
   if timestamp_kind:
     event.timestamp_kind = getattr(ChromeInfraEvent, timestamp_kind)
 
   log_event = LogRequestLite.LogEventLite()
   log_event.event_time_ms = event_timestamp or router.time_ms()
   log_event.source_extension = event.SerializeToString()
+
+  return log_event
+
+
+def send_service_event(event_type,
+                       timestamp_kind='POINT',
+                       event_timestamp=None,
+                       code_version=(),
+                     ):
+  """Send service event.
+
+  Args:
+    event_type (string): any name of enum ServiceEvent.ServiceEventType.
+      ('START', 'STOP', 'UPDATE', 'CURRENT_VERSION')
+    timestamp_kind (string): any of ('POINT', 'BEGIN', 'END').
+    event_timestamp (int or float): timestamp of when the event happened
+      as a number of milliseconds since the epoch. If not provided, the
+      current time is used.
+
+  Returns:
+    success (bool): False if some error happened.
+  """
+  log_event = _get_service_event(event_type,
+                                 timestamp_kind=timestamp_kind,
+                                 event_timestamp=event_timestamp,
+                                 code_version=code_version)
+
   return config._router.push_event(log_event)
--- infra/libs/event_mon/test/monitoring_test.py
+++ infra/libs/event_mon/test/monitoring_test.py
@@ -7,23 +7,106 @@ import unittest
 
 from infra.libs import event_mon
 from infra.libs.event_mon import config, router
+from infra.libs.event_mon import monitoring
+from infra.libs.event_mon.chrome_infra_log_pb2 import ChromeInfraEvent
+from infra.libs.event_mon.chrome_infra_log_pb2 import ServiceEvent
+from infra.libs.event_mon.log_request_lite_pb2 import LogRequestLite
 
 
 class MonitoringTest(unittest.TestCase):
 
-  def _set_up_args(self, args=None):
-    parser = argparse.ArgumentParser()
-    event_mon.add_argparse_options(parser)
-    args = parser.parse_args((args or []))
-    self.assertEquals(args.event_mon_run_type, 'dry')
-    event_mon.process_argparse_options(args)
+  def setUpClass(self): # pragma: no cover
+    # Use setUpClass here because it is better to initialize event_mon once.
+    event_mon.setup_monitoring(run_type='dry')
+
+  def test_get_service_event_default(self):
     self.assertIsInstance(config._router, router._Router)
+    self.assertIsInstance(config.cache.get('default_event'), ChromeInfraEvent)
 
-  def tearDown(self):
-    event_mon.close()
+    log_event = monitoring._get_service_event('START')
+    self.assertIsInstance(log_event, LogRequestLite.LogEventLite)
+    self.assertTrue(log_event.HasField('event_time_ms'))
+    self.assertTrue(log_event.HasField('source_extension'))
 
-  def test_send_service_smoke(self):
-    self._set_up_args()
+    # Check that source_extension deserializes to the right thing.
+    event = ChromeInfraEvent.FromString(log_event.source_extension)
+    self.assertTrue(event.HasField('service_event'))
+    self.assertTrue(event.service_event.HasField('type'))
+    self.assertEquals(event.service_event.type, ServiceEvent.START)
+
+  def test_get_service_event_correct_versions(self):
+    self.assertIsInstance(config._router, router._Router)
+    self.assertIsInstance(config.cache.get('default_event'), ChromeInfraEvent)
+
+    code_version = [
+      {'source_url': 'https://fake.url/thing',
+       'revision': '708329c2aeece8aac33af6a5a772ffb14b55903f'},
+      {'source_url': 'svn://fake_svn.url/other_thing',
+       'revision': '123456'},
+      {'source_url': 'https://other_fake.url/yet_another_thing',
+       'version': 'v2.0'},
+      {'source_url': 'https://other_fake2.url/yet_another_thing2',
+       'dirty': True},
+      ]
+
+    log_event = monitoring._get_service_event('START',
+                                              code_version=code_version)
+    event = ChromeInfraEvent.FromString(log_event.source_extension)
+    code_version_p = event.service_event.code_version
+    self.assertEquals(len(code_version_p), len(code_version))
+
+    self.assertEqual(code_version_p[0].source_url,
+                     code_version[0]['source_url'])
+    self.assertEqual(code_version_p[0].git_hash, code_version[0]['revision'])
+    self.assertFalse(code_version_p[0].HasField('svn_revision'))
+
+    self.assertEqual(code_version_p[1].source_url,
+                     code_version[1]['source_url'])
+    self.assertEqual(code_version_p[1].svn_revision,
+                     int(code_version[1]['revision']))
+    self.assertFalse(code_version_p[1].HasField('git_hash'))
+
+    self.assertEqual(code_version_p[2].source_url,
+                     code_version[2]['source_url'])
+    self.assertFalse(code_version_p[2].HasField('svn_revision'))
+    self.assertEqual(code_version_p[2].version,
+                     code_version[2]['version'])
+
+    self.assertEqual(code_version_p[3].source_url,
+                     code_version[3]['source_url'])
+    self.assertEqual(code_version_p[3].dirty, True)
+
+  def test_send_service_event_bad_versions(self):
+    # Check that an invalid version does not cause any exception.
+    self.assertIsInstance(config._router, router._Router)
+    self.assertIsInstance(config.cache.get('default_event'), ChromeInfraEvent)
+
+    code_version = [{}, {'revision': 'https://fake.url'}]
+    log_event = monitoring._get_service_event('START',
+                                              code_version=code_version)
+    event = ChromeInfraEvent.FromString(log_event.source_extension)
+    self.assertTrue(event.HasField('service_event'))
+    self.assertTrue(event.service_event.HasField('type'))
+    self.assertEqual(len(event.service_event.code_version), 0)
+
+  def test_send_service_event_bad_type(self):
+    # Check that an invalid type for code_version does not raise
+    # any exception.
+
+    code_versions = [None, 123, 'string',
+                     [None], [123], ['string'], [['list']]]
+    for code_version in code_versions:
+      log_event = monitoring._get_service_event('START',
+                                                code_version=code_version)
+      event = ChromeInfraEvent.FromString(log_event.source_extension)
+      self.assertTrue(event.HasField('service_event'))
+      self.assertTrue(event.service_event.HasField('type'))
+      self.assertEqual(len(event.service_event.code_version), 0)
+
+  def test_send_service_event_smoke(self):
+    self.assertIsInstance(config._router, router._Router)
+    self.assertIsInstance(config.cache.get('default_event'), ChromeInfraEvent)
+
     self.assertTrue(event_mon.send_service_event('START'))
     self.assertTrue(event_mon.send_service_event('START',
                                                  timestamp_kind=None))
@@ -34,7 +117,9 @@ class MonitoringTest(unittest.TestCase):
                                                   event_timestamp=1234))
 
   def test_send_service_errors(self):
-    self._set_up_args()
+    self.assertIsInstance(config._router, router._Router)
+    self.assertIsInstance(config.cache.get('default_event'), ChromeInfraEvent)
+
     self.assertFalse(event_mon.send_service_event('invalid'))
     self.assertFalse(event_mon.send_service_event('START',
                                                    timestamp_kind='invalid'))
