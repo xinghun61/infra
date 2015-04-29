4e22fbab5f302fca17c4c5e82a3e590218c2e244
agable@chromium.org <agable@chromium.org>
2015-04-29 20:38:33 +0000

    Revert of Split infra presubmit into a separate recipe (patchset #9 id:130010 of https://codereview.chromium.org/1104083002/)
    Reason for revert:
    See https://code.google.com/p/chromium/issues/detail?id=482150
    
    Original issue's description:
    > Separate infra presubmit into a separate builder
    > 
    > R=phajdan@chromium.org
    > BUG=478651
    > 
    > Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=295005
    
    TBR=phajdan@chromium.org,sergiyb@chromium.org,phajdan.jr@chromium.org,sergiyb@google.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=478651
    
    Review URL: https://codereview.chromium.org/1116613004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@295068 0039d316-1c4b-4281-b951-d872f2087c98

--- recipes/recipes/infra_repo_trybot.py
+++ recipes/recipes/infra_repo_trybot.py
@@ -17,11 +17,13 @@ DEPS = [
 
 def GenSteps(api):
   project = api.properties['patch_project'] or api.properties['project']
-  internal = 'internal' in project
+  internal = (project == 'infra_internal')
 
   api.gclient.set_config(project)
-  api.bot_update.ensure_checkout(force=True, patch_root=project,
-                                 patch_oauth2=internal)
+  res = api.bot_update.ensure_checkout(force=True, patch_root=project,
+                                       patch_oauth2=internal)
+  upstream = res.json.output['properties'].get('got_revision')
+  api.presubmit.commit_patch_locally()
   api.gclient.runhooks()
 
   # Grab a list of changed files.
@@ -41,6 +43,8 @@ def GenSteps(api):
       api.python(
           'go test.py', api.path['checkout'].join('go', 'env.py'),
           ['python', api.path['checkout'].join('go', 'test.py')])
+
+  api.presubmit(upstream=upstream, use_rietveld_credentials=internal)
 
 
 def GenTests(api):
