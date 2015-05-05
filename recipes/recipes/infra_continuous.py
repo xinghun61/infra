f52eccba0d84a3791d423956d980eb310c8d373e
vadimsh@chromium.org <vadimsh@chromium.org>
2015-05-05 01:09:17 +0000

    Show CIPD package instance IDs on the waterfall page as links.
    Later the links will point to some UI page on chrome-infra-packages.appspot.com.
    
    R=iannucci@chromium.org,nodir@chromium.org
    BUG=481270
    
    Review URL: https://codereview.chromium.org/1118373004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@295131 0039d316-1c4b-4281-b951-d872f2087c98

--- recipes/recipes/infra_continuous.py
+++ recipes/recipes/infra_continuous.py
@@ -5,12 +5,10 @@
 DEPS = [
   'bot_update',
   'gclient',
-  'git',
   'json',
   'path',
   'properties',
   'python',
-  'raw_io',
   'step',
 ]
 
@@ -18,6 +16,26 @@ DEPS = [
 # Path to a service account credentials to use to talk to CIPD backend.
 # Deployed by Puppet.
 CIPD_BUILDER_CREDS = '/creds/service_accounts/service-account-cipd-builder.json'
+
+
+def build_cipd_packages(api):
+  try:
+    return api.python(
+        'build cipd packages',
+        api.path['checkout'].join('build', 'build.py'),
+        [
+          '--upload',
+          '--service-account-json', CIPD_BUILDER_CREDS,
+          '--json-output', api.json.output(),
+        ])
+  finally:
+    step_result = api.step.active_result
+    output = step_result.json.output or {}
+    p = step_result.presentation
+    for pkg in output.get('succeeded', []):
+      info = pkg['info']
+      title = '%s %s' % (info['package'], info['instance_id'])
+      p.links[title] = info.get('url', 'http://example.com/not-implemented-yet')
 
 
 def GenSteps(api):
@@ -41,20 +59,34 @@ def GenSteps(api):
     api.python('infra go tests', api.path['checkout'].join('go', 'env.py'),
                ['python', api.path['checkout'].join('go', 'test.py')])
 
-  api.python('build cipd packages',
-             api.path['checkout'].join('build', 'build.py'),
-             ['--upload', '--service-account-json', CIPD_BUILDER_CREDS])
+  build_cipd_packages(api)
 
 
 def GenTests(api):
+  cipd_json_output = {
+    'succeeded': [
+      {
+        'info': {
+          'instance_id': 'abcdefabcdef63ad814cd1dfffe2fcfc9f81299c',
+          'package': 'infra/tools/some_tool/linux-bitness',
+        },
+        'pkg_def_name': 'some_tool',
+      },
+    ],
+    'failed': [],
+  }
+
   yield (
     api.test('infra') +
     api.properties.tryserver(
-        buildername='infra-continuous')
+        buildername='infra-continuous') +
+    api.override_step_data(
+        'build cipd packages', api.json.output(cipd_json_output))
   )
   yield (
     api.test('infra_internal') +
     api.properties.tryserver(
-        buildername='infra-internal-continuous')
-
+        buildername='infra-internal-continuous') +
+    api.override_step_data(
+        'build cipd packages', api.json.output(cipd_json_output))
   )
