86a9eb4259feb64fe1eacf9a5ecafcf1e290ca0e
maruel@chromium.org <maruel@chromium.org>
2015-05-12 13:11:16 +0000

    Add luci-go builders to chromium.infra to build official releases.
    It's not doing anything useful (yet).
    
    R=vadimsh@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1139753004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@295228 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ recipes/recipes/luci_go.py
@@ -0,0 +1,36 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+DEPS = [
+  'bot_update',
+  'gclient',
+  'path',
+  'properties',
+  'python',
+]
+
+
+def GenSteps(api):
+  api.gclient.set_config('infra')
+  api.bot_update.ensure_checkout(force=True)
+  api.gclient.runhooks()
+  api.python(
+      'go build',
+      api.path['checkout'].join('go', 'env.py'),
+      ['go', 'build', 'github.com/luci/luci-go/cmd/...'])
+  api.python(
+      'go test',
+      api.path['checkout'].join('go', 'env.py'),
+      ['go', 'test', 'github.com/luci/luci-go/...'])
+
+
+def GenTests(api):
+  yield (
+    api.test('luci_go') +
+    api.properties.git_scheduled(
+        buildername='luci-go-linux64',
+        buildnumber=123,
+        mastername='chromium.infra',
+        repository='https://chromium.googlesource.com/external/github.com/luci/luci-go',
+    ))
