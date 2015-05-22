56286c1c85e874d43dd8c59a5d9d8f31d3558f76
vadimsh@chromium.org <vadimsh@chromium.org>
2015-05-22 18:37:44 +0000

    Fixing various nits in LUCI related recipes.
    In particular:
      1) Stop building LUCI on internal CI builders.
      2) Make "got_revision" match luci-go.git revision, not infra.git one.
      3) Move Go bootstrap in CI builder into separate step (as in luci-go builder).
      4) Correctly grap git revision and repo when CI build is triggered manually.
    
    R=iannucci@chromium.org,maruel@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1154703003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@295384 0039d316-1c4b-4281-b951-d872f2087c98

--- recipes/recipes/infra_continuous.py
+++ recipes/recipes/infra_continuous.py
@@ -20,14 +20,14 @@ DEPS = [
 CIPD_BUILDER_CREDS = '/creds/service_accounts/service-account-cipd-builder.json'
 
 
-def build_cipd_packages(api):
+def build_cipd_packages(api, repo, rev):
   tags = [
     'buildbot_build:%s/%s/%s' % (
         api.properties['mastername'],
         api.properties['buildername'],
         api.properties['buildnumber']),
-    'git_repository:%s' % api.properties['repository'],
-    'git_revision:%s' % api.properties['revision'],
+    'git_repository:%s' % repo,
+    'git_revision:%s' % rev,
   ]
   try:
     return api.python(
@@ -48,56 +48,76 @@ def build_cipd_packages(api):
       p.links[title] = info.get('url', 'http://example.com/not-implemented-yet')
 
 
+def build_luci(api):
+  go_bin = api.path['checkout'].join('go', 'bin')
+  go_env = api.path['checkout'].join('go', 'env.py')
+  api.file.rmcontents('clean go bin', go_bin)
+
+  api.python(
+      'build luci-go', go_env,
+      ['go', 'install', 'github.com/luci/luci-go/client/cmd/...'])
+
+  files = api.file.listdir('listing go bin', go_bin)
+  absfiles = [api.path.join(go_bin, i) for i in files]
+  api.python(
+      'upload go bin',
+      api.path['depot_tools'].join('upload_to_google_storage.py'),
+      ['-b', 'chromium-luci'] + absfiles)
+  for name, abspath in zip(files, absfiles):
+    sha1 = api.file.read(
+        '%s sha1' % str(name), abspath + '.sha1',
+        test_data='0123456789abcdeffedcba987654321012345678')
+    api.step.active_result.presentation.step_text = sha1
+
+
 def GenSteps(api):
   builder_name = api.properties.get('buildername')
   if builder_name.startswith('infra-internal-continuous'):
     project_name = 'infra_internal'
+    repo_name = 'https://chrome-internal.googlesource.com/infra/infra_internal'
   elif builder_name.startswith('infra-continuous'):
     project_name = 'infra'
-  else:  #pragma: no cover
-    raise ValueError('This recipe is not intended for builder %s. '
-                     % builder_name)
-
-  # Only build luci-go executables on 64 bits.
-  build_luci = builder_name.endswith('-64')
+    repo_name = 'https://chromium.googlesource.com/infra/infra'
+  else:  # pragma: no cover
+    raise ValueError(
+        'This recipe is not intended for builder %s. ' % builder_name)
 
   api.gclient.set_config(project_name)
-  api.bot_update.ensure_checkout(force=True)
+  bot_update_step = api.bot_update.ensure_checkout(force=True)
   api.gclient.runhooks()
+
+  # Whatever is checked out by bot_update. It is usually equal to
+  # api.properties['revision'] except when the build was triggered manually
+  # ('revision' property is missing in that case).
+  rev = bot_update_step.presentation.properties['got_revision']
 
   with api.step.defer_results():
     # TODO(crbug.com/487485): expect_test + venv is broken on Windows.
     if not api.platform.is_win:
-      api.python('infra python tests',
-                 'test.py', ['test'], cwd=api.path['checkout'])
+      api.python(
+          'infra python tests',
+          'test.py',
+          ['test'],
+          cwd=api.path['checkout'])
+    # This downloads Go third parties, so that the next step doesn't have junk
+    # output in it.
+    api.python(
+        'go third parties',
+        api.path['checkout'].join('go', 'env.py'),
+        ['go', 'version'])
     # Note: env.py knows how to expand 'python' into sys.executable.
-    api.python('infra go tests', api.path['checkout'].join('go', 'env.py'),
-               ['python', api.path['checkout'].join('go', 'test.py')])
+    api.python(
+        'infra go tests',
+        api.path['checkout'].join('go', 'env.py'),
+        ['python', api.path['checkout'].join('go', 'test.py')])
 
   # TODO(crbug.com/481661): CIPD client doesn't support Windows yet.
   if not api.platform.is_win:
-    build_cipd_packages(api)
+    build_cipd_packages(api, repo_name, rev)
 
-  if build_luci:
-    go_bin = api.path['checkout'].join('go', 'bin')
-    go_env = api.path['checkout'].join('go', 'env.py')
-    api.file.rmcontents('clean go bin', go_bin)
-
-    api.python(
-        'build luci-go', go_env,
-        ['go', 'install', 'github.com/luci/luci-go/client/cmd/...'])
-
-    files = api.file.listdir('listing go bin', go_bin)
-    absfiles = [api.path.join(go_bin, i) for i in files]
-    api.python(
-        'upload go bin',
-        api.path['depot_tools'].join('upload_to_google_storage.py'),
-        ['-b', 'chromium-luci'] + absfiles)
-    for name, abspath in zip(files, absfiles):
-      sha1 = api.file.read(
-          '%s sha1' % str(name), abspath + '.sha1',
-          test_data='0123456789abcdeffedcba987654321012345678')
-      api.step.active_result.presentation.step_text = sha1
+  # Only build luci-go executables on 64 bits, public CI.
+  if project_name == 'infra' and builder_name.endswith('-64'):
+    build_luci(api)
 
 
 def GenTests(api):
