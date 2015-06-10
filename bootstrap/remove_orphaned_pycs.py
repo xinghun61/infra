7910d8e8a8f2dde3f676c2a64bcab81d3de9a124
dnj <dnj@chromium.org>
2015-06-10 12:09:11 -0700

    Enable docgen to build other repo documentation.
    Specifically, this enables the central 'docgen' script to build
    documentation for the other "infra*" repositories with flags.
    
    Also updated "remove_orphaned_pycs.py" to allow it to operate on named
    directories when provided.
    
    BUG=chromium:498374
    TEST=local,pylint
    
    Review URL: https://codereview.chromium.org/1172953002

--- bootstrap/remove_orphaned_pycs.py
+++ bootstrap/remove_orphaned_pycs.py
@@ -8,22 +8,26 @@ import os
 import sys
 
 
-def main():
+def main(argv):
   logging.basicConfig(level=logging.DEBUG)
+  if len(argv) == 0:
+    bootstrap_dir = os.path.dirname(os.path.abspath(__file__))
+    infra_dir = os.path.dirname(bootstrap_dir)
+    argv = [infra_dir]
 
-  # This could take an argument, except gclient DEPS has no good way to pass
-  # us an argument, and gclient getcwd() is ../ from our .gclient file. :(
-  bootstrap_dir = os.path.dirname(os.path.abspath(__file__))
-  infra_dir = os.path.dirname(bootstrap_dir)
-  logging.debug("Cleaning orphaned *.pyc files from: %s" % infra_dir)
+  for root in argv:
+    # This could take an argument, except gclient DEPS has no good way to pass
+    # us an argument, and gclient getcwd() is ../ from our .gclient file. :(
+    logging.debug("Cleaning orphaned *.pyc files from: %s" % root)
 
-  for (dirpath, _, filenames) in os.walk(infra_dir):
-    for filename in filenames:
-      path = os.path.join(infra_dir, dirpath, filename)
-      if filename.endswith(".pyc") and filename[:-1] not in filenames:
-        logging.info("Deleting orphan *.pyc file: %s" % path)
-        os.remove(path)
+    for (dirpath, _, filenames) in os.walk(root):
+      fnset = set(filenames)
+      for filename in filenames:
+        if filename.endswith(".pyc") and filename[:-1] not in fnset:
+          path = os.path.join(dirpath, filename)
+          logging.info("Deleting orphan *.pyc file: %s" % path)
+          os.remove(path)
 
 
 if __name__ == '__main__':
-  sys.exit(main())
+  sys.exit(main(sys.argv[1:]))
--- infra/tools/docgen/test/__main___test.py
+++ infra/tools/docgen/test/__main___test.py
@@ -6,24 +6,42 @@ import unittest
 
 import mock
 
+# NOTE: Do not use "from ... import __main__", else __name__ == "__main__" will
+# be True and the script will execute on import.
 import infra.tools.docgen.__main__ as docgen_main
 
 
 class DocgenTest(unittest.TestCase):
   @mock.patch('subprocess.check_call')
   @mock.patch('shutil.rmtree')
-  def test_docgen_default_smoke(self, _check_call, _rmtree):
+  def test_docgen_default_smoke(self, _rmtree, _check_call):
     argv = []
     self.assertIn(docgen_main.main(argv), (0, None))
 
   @mock.patch('subprocess.check_call')
   @mock.patch('shutil.rmtree')
-  def test_docgen_clean_smoke(self, _check_call, _rmtree):
+  def test_docgen_clean_smoke(self, _rmtree, _check_call):
     argv = ['clean']
     self.assertIn(docgen_main.main(argv), (0, None))
 
   @mock.patch('subprocess.check_call')
   @mock.patch('shutil.rmtree')
-  def test_docgen_run_smoke(self, _check_call, _rmtree):
+  def test_docgen_run_smoke(self, _rmtree, _check_call):
     argv = ['run']
+    self.assertIn(docgen_main.main(argv), (0, None))
+
+  @mock.patch('subprocess.check_call')
+  @mock.patch('shutil.rmtree')
+  @mock.patch('glob.glob')
+  def test_docgen_hooks(self, glob, _rmtree, _check_call):
+    glob.return_value = ['testhook.py']
+
+    argv = ['run']
+    self.assertIn(docgen_main.main(argv), (0, None))
+    self.assertTrue(glob.called)
+
+  @mock.patch('subprocess.check_call')
+  @mock.patch('shutil.rmtree')
+  def test_docgen_external(self, _check_call, _rmtree):
+    argv = ['--root', '/path/to/root', '--base', 'testbase', 'run']
     self.assertIn(docgen_main.main(argv), (0, None))
