b2f4b7f0fd11e71a2e4d687e7909d5e5dbc667cf
pgervais <pgervais@chromium.org>
2015-06-10 17:38:10 -0700

    Created temporary_directory context manager
    BUG=499082
    
    Review URL: https://codereview.chromium.org/1179683003

--- infra_libs/__init__.py
+++ infra_libs/__init__.py
@@ -2,4 +2,4 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from infra_libs.utils import read_json_as_utf8
+from infra_libs.utils import read_json_as_utf8, temporary_directory
--- infra_libs/utils.py
+++ infra_libs/utils.py
@@ -2,7 +2,14 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+"""Miscellaneous utility functions."""
+
+
+import contextlib
 import json
+import shutil
+import sys
+import tempfile
 
 
 def read_json_as_utf8(filename=None, text=None):
@@ -42,3 +49,42 @@ def read_json_as_utf8(filename=None, text=None):
     obj = json.loads(text)
 
   return to_utf8(obj)
+
+
+# We're trying to be compatible with Python3 tempfile.TemporaryDirectory
+# context manager here. And they used 'dir' as a keyword argument.
+# pylint: disable=redefined-builtin
+@contextlib.contextmanager
+def temporary_directory(suffix="", prefix="tmp", dir=None,
+                        keep_directory=False):
+  """Create and return a temporary directory.  This has the same
+  behavior as mkdtemp but can be used as a context manager.  For
+  example:
+
+    with temporary_directory() as tmpdir:
+      ...
+
+  Upon exiting the context, the directory and everything contained
+  in it are removed.
+
+  Args:
+    suffix, prefix, dir: same arguments as for tempfile.mkdtemp.
+    keep_directory (bool): if True, do not delete the temporary directory
+      when exiting. Useful for debugging.
+
+  Returns:
+    tempdir (str): full path to the temporary directory.
+  """
+  tempdir = None  # Handle mkdtemp raising an exception
+  try:
+    tempdir = tempfile.mkdtemp(suffix, prefix, dir)
+    yield tempdir
+
+  finally:
+    if tempdir and not keep_directory:  # pragma: no branch
+      try:
+        # TODO(pgervais,496347) Make this work reliably on Windows.
+        shutil.rmtree(tempdir, ignore_errors=True)
+      except OSError as ex:  # pragma: no cover
+        print >> sys.stderr, (
+          "ERROR: {!r} while cleaning up {!r}".format(ex, tempdir))
