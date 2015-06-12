9572ae7d7c6eb405eef672d7b1af68b27583a232
phajdan.jr@chromium.org <phajdan.jr@chromium.org>
2015-06-12 19:02:16 +0000

    publish_tarball: export lite tarball
    BUG=433338
    
    Review URL: https://codereview.chromium.org/1181093003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@295661 0039d316-1c4b-4281-b951-d872f2087c98

--- recipes/recipes/publish_tarball.py
+++ recipes/recipes/publish_tarball.py
@@ -57,6 +57,72 @@ def export_tarball(api, args, source, destination):
     api.file.rmtree('temp dir', temp_dir)
 
 
+@recipe_api.composite_step
+def export_lite_tarball(api, version):
+  try:
+    temp_dir = api.path.mkdtemp('lite_tarball')
+    dest_dir = api.path.join(temp_dir, 'src')
+
+    # Make destructive file operations on the copy of the checkout.
+    api.file.copytree('copytree', api.path['checkout'], dest_dir)
+
+    directories = [
+      'native_client',
+      'native_client_sdk',
+      'third_party/WebKit/ManualTests',
+      'third_party/WebKit/PerformanceTests',
+      'third_party/android_platform',
+      'third_party/chromite',
+      'third_party/freetype2',
+      'third_party/gles2_book',
+      'third_party/gold',
+      'third_party/icu',
+      'third_party/launchpad_translations',
+      'third_party/libc++',
+      'third_party/libevent',
+      'third_party/libjpeg_turbo',
+      'third_party/libmtp',
+      'third_party/libxml/src',
+      'third_party/openssl',
+      'third_party/snappy',
+      'third_party/webgl',
+      'third_party/webgl_conformance',
+      'third_party/yasm',
+      'tools/win',
+      'v8/third_party/icu',
+    ]
+    for directory in directories:
+      try:
+        api.step('prune %s' % directory, [
+            'find', api.path.join(dest_dir, directory), '-type', 'f',
+            '!', '-iname', '*.gyp*',
+            '!', '-iname', '*.isolate*',
+            '-delete'])
+      except api.step.StepFailure:  # pragma: no cover
+        # Ignore failures to delete these directories - they can be inspected
+        # later to see whether they have moved to a different location
+        # or deleted in different versions of the codebase.
+        pass
+
+    # Empty directories take up space in the tarball.
+    api.step('prune empty directories', [
+        'find', dest_dir, '-depth', '-type', 'd', '-empty', '-delete'])
+
+    export_tarball(
+        api,
+        # Verbose output helps avoid a buildbot timeout when no output
+        # is produced for a long time.
+        ['--remove-nonessential-files',
+         'chromium-%s' % version,
+         '--verbose',
+         '--progress',
+         '--src-dir', dest_dir],
+        'chromium-%s.tar.xz' % version,
+        'chromium-%s-lite.tar.xz' % version)
+  finally:
+    api.file.rmtree('temp dir', temp_dir)
+
+
 def GenSteps(api):
   if 'version' not in api.properties:
     ls_result = api.gsutil(['ls', 'gs://chromium-browser-official/'],
@@ -107,6 +173,8 @@ def GenSteps(api):
          '--src-dir', api.path['checkout']],
         'chromium-%s.tar.xz' % version,
         'chromium-%s-testdata.tar.xz' % version)
+
+    export_lite_tarball(api, version)
 
 
 def GenTests(api):
