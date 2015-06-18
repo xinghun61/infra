00ee4dc1c534f15712002d05825ac42234ba8467
seanmccullough <seanmccullough@chromium.org>
2015-06-17 17:59:05 -0700

    dispatcher: cipd package
    BUG=
    
    Review URL: https://codereview.chromium.org/1187763004

---
+++ build/tests/dispatcher.py
@@ -0,0 +1,22 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Test dispatcher.cipd package by running 'dispatcher -h'."""
+
+import subprocess
+import sys
+import os
+
+
+# .exe on Windows.
+EXE_SUFFIX = '.exe' if sys.platform == 'win32' else ''
+
+
+def main():
+  dispatcher = os.path.join(os.getcwd(), 'dispatcher' + EXE_SUFFIX)
+  return subprocess.call([dispatcher, '-h'], executable=dispatcher)
+
+
+if __name__ == '__main__':
+  sys.exit(main())
