b1d5237a958ecd994a19863a12f0b08da2543d6b
norulez <norulez@google.com>
2015-06-19 10:25:34 -0700

    Cloud Endpoint API back-end for Chrome Infra loadtest appengine app
    R=sergeyberezin@chromium.org
    BUG=Simple GET in cloud endpoint for loadtest appengine app
    
    Review URL: https://codereview.chromium.org/1187903002

--- appengine/chrome_infra_console_loadtest/main.py
+++ appengine/chrome_infra_console_loadtest/main.py
@@ -2,20 +2,77 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
 
+import endpoints
 import webapp2
 
 from components import auth
+from protorpc import messages
+from protorpc import message_types
+from protorpc import remote
 
 
-class MainHandler(auth.AuthenticatingHandler):
-  @auth.public
-  def get(self):
-    self.response.write('Hello world!')
+class Field(messages.Message):
+  key = messages.StringField(1)
+  value = messages.StringField(2)
 
 
-main_handlers = [
-      (r'/', MainHandler),
-]
+class Point(messages.Message):
+  time = messages.FloatField(1)
+  value = messages.FloatField(2)
 
-app = webapp2.WSGIApplication(main_handlers, debug=True)
+
+class FieldParams(messages.Message):
+  key = messages.StringField(1)
+  values = messages.StringField(2, repeated = True)
+
+
+class Params(messages.Message):
+  time = messages.FloatField(1)
+  freq = messages.FloatField(2)
+  params = messages.MessageField(FieldParams, 3, repeated = True)
+
+
+class TimeSeries(messages.Message):
+  points = messages.MessageField(Point, 1, repeated = True)
+  fields = messages.MessageField(Field, 2, repeated = True)
+  metric = messages.StringField(3)
+
+
+class DataPacket(messages.Message):
+  timeseries = messages.MessageField(TimeSeries, 1, repeated = True)
+
+
+@auth.endpoints_api(name='loadtestapp', version='v1')
+class LoadTestApi(remote.Service):
+  """A testing endpoint that receives timeseries data."""
+
+  @auth.endpoints_method(DataPacket, message_types.VoidMessage,
+                         name='loadtest.timeseries')
+  @auth.require(lambda: auth.is_group_member('metric-generators'))
+  def loadtest_timeseries(self, _request):
+    return message_types.VoidMessage()
+
+
+@auth.endpoints_api(name='ui', version='v1')
+class UIApi(remote.Service):
+  """API for the loadtest configuration UI."""
+
+  @auth.endpoints_method(message_types.VoidMessage, Params,
+                         name='UI.get')
+  @auth.require(lambda: auth.is_group_member('metric-generators'))
+  def UI_get(self, _request):
+    param_response = Params(time=0.0, freq=10.0, params=[
+        FieldParams(key='project_id', values=['chromium', 'blink'])])
+    return param_response
+
+  @auth.endpoints_method(Params, message_types.VoidMessage,
+                         name='UI.set')
+  @auth.require(lambda: auth.is_group_member('metric-generators'))
+  def UI_set(self, request):
+    logging.debug('Got %s', request)
+    return message_types.VoidMessage()
+
+
+APPLICATION = endpoints.api_server([LoadTestApi, UIApi])
--- appengine/chrome_infra_console_loadtest/test/main_test.py
+++ appengine/chrome_infra_console_loadtest/test/main_test.py
@@ -2,20 +2,48 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from components import auth
 
-import logging
-
+from main import LoadTestApi
+from main import UIApi
+from protorpc import messages
+from protorpc import message_types
 from testing_utils import testing
 
-import main
+
+class LoadTestApiTest(testing.EndpointsTestCase):
+
+  api_service_cls = LoadTestApi
+
+  def testLoadTestSet(self):
+    point = {'time': 0.0, 
+             'value': 10.0}
+    field = {'key': 'project_id',
+             'value': 'chromium'}
+    request = {'timeseries': [{'points': [point],
+               'fields': [field],
+               'metric': 'disk_used'}]}
+    self.mock(auth, 'is_group_member', lambda _: True)
+    response = self.call_api('loadtest_timeseries', request).json_body
+    self.assertEquals(response, {})
 
 
-class MainTest(testing.AppengineTestCase):
-  @property
-  def app_module(self):
-    return main.app
+class UIApiTest(testing.EndpointsTestCase):
 
-  def test_get(self):
-    response = self.test_app.get('/')
-    logging.info('response = %s', response)
-    self.assertEquals(200, response.status_int)
+  api_service_cls = UIApi
+
+  def testUIGet(self):
+    request = {}
+    self.mock(auth, 'is_group_member', lambda _: True)
+    response = self.call_api('UI_get', request).json_body
+    self.assertEquals(response['time'], 0.0)
+
+  def testUISet(self):
+    field = {'key': 'project_id',
+             'values': ['chromium', 'blink', 'v8']}
+    request = {'time': 5.0,
+               'freq': 3.0,
+               'params': [field]}
+    self.mock(auth, 'is_group_member', lambda _: True)
+    response = self.call_api('UI_set', request).json_body
+    self.assertEquals(response, {})
