2709e8f2ff8f228b30a01a2f30fa153451f25a89
hinoka@chromium.org <hinoka@chromium.org>
2015-07-06 22:17:03 +0000

    Add antibody recipe
    Builder CL at https://codereview.chromium.org/1207483005/
    
    BUG=503642
    
    Review URL: https://codereview.chromium.org/1208453002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@295926 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ recipes/recipes/antibody.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Runs a pipeline to detect suspicious commits in Chromium."""
+
+
+DEPS = [
+  'bot_update',
+  'gclient',
+  'gsutil',
+  'path',
+  'properties',
+  'python',
+]
+
+
+def RunSteps(api):
+  api.gclient.set_config('infra_with_chromium')
+  api.bot_update.ensure_checkout(force=True)
+  api.gclient.runhooks()
+  filename = api.path.mkdtemp(api.path._temp_dir)
+
+  cmd = ['infra.tools.antibody']
+  cmd.extend(['--sql-password-file', '/home/chrome-bot/.antibody_password'])
+  cmd.extend(['--git-checkout-path', api.m.path['root'].join('src')])
+  cmd.extend(['--html-output-file', filename])
+  cmd.extend(['--since', '2015.01.01'])
+  cmd.extend(['--run-antibody'])
+
+  api.python('Antibody', 'run.py', cmd)
+  api.gsutil(['cp', filename, 'gs://chromium-antibody/report.html'])
+
+
+def GenTests(api):
+  yield (api.test('antibody') +
+         api.properties(mastername='chromium.infra.cron',
+                        buildername='antibody',
+                        slavename='fake-slave'))
+
