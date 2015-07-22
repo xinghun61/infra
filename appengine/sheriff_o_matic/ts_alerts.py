c111a571e398f0042cda393e3d7905174d2e15f5
jshu <jshu@google.com>
2015-07-22 16:07:34 -0700

    Add a /ts-alerts/PUT endpoint to update alerts
    Get specific ts-alerts by key
    
    Create /ts-alerts/DELETE/ endpoint
    
    Create front end for time-series alerts
    
    Add link-bug and delete buttons on time-series alerts
    
    Improve UI
    
    Add support to time-zone conversions
    
    Fix crash on bad input to link-bug
    
    Review URL: https://codereview.chromium.org/1229403002

--- appengine/sheriff_o_matic/ts_alerts.py
+++ appengine/sheriff_o_matic/ts_alerts.py
@@ -41,7 +41,64 @@ class TimeSeriesAlertsHandler(alerts.AlertsHandler):
     self.send_json_headers()
     self.response.write(self.generate_json_dump(data))
 
+  def put(self, ind):
+    old_alerts = self.get_from_memcache() or self.get_from_datastore() or []
+
+    try:
+      changed_alert = old_alerts[int(ind)]
+    except IndexError:
+      self.response.write('This alert does not exist or is no longer active.')
+      self.response.set_status(404, 'Alert does not exist')
+      return
+    except ValueError:
+      self.response.write('Invalid key format; should be int.')
+      self.response.set_status(400, 'Invalid Key Format')
+      return
+    try:
+      data = json.loads(self.request.body)
+    except ValueError:
+      warning = ('Content %s was not valid JSON string.', self.request.body)
+      self.response.set_status(400, warning)
+      logging.warning(warning)
+      return
+    for k in data:
+      changed_alert[k] = data[k]
+    old_alerts[int(ind)] = changed_alert
+    self.update_db(old_alerts)
+    self.response.write("Updated ts-alerts.")
+
+  def secs_from_now(self, time_string):
+    time_sent = datetime.datetime.strptime(time_string, '%Y-%m-%d %H:%M:%S %Z')
+    time_now = datetime.datetime.utcnow()
+    latency = int(time_now.strftime('%s')) - int(time_sent.strftime('%s'))
+    return latency
+
+  def convert_to_secs(self, duration_str):
+    duration_str = duration_str.strip()
+    if duration_str[-1] == 's':
+      return int(duration_str[:-1])
+    elif duration_str[-1] == 'm':
+      return 60 * int(duration_str[:-1])
+    elif duration_str[-1] == 'h':
+      return 3600 * int(duration_str[:-1])
+    elif duration_str[-1] == 'd':
+      return 24 * 3600 * int(duration_str[:-1])
+    elif duration_str[-1] == 'w':
+      return 7 * 24 * 3600 * int(duration_str[:-1])
+    else:
+      raise Exception('Invalid duration_str ' + duration_str[-1])
+
+  def remove_expired_alerts(self, alerts_list):
+    active_alerts = []
+    for alert in alerts_list:
+      trigger_dur = self.convert_to_secs(alert['condition_name'].split('_')[-1])
+      alert_age = self.secs_from_now(alert['alert_sent_utc'])
+      if alert_age <= (trigger_dur + 100):
+        active_alerts.append(alert)
+    return active_alerts
+
   def update_db(self, new_alerts):
+    new_alerts = self.remove_expired_alerts(new_alerts)
     json_data = self.generate_json_dump(new_alerts)
 
     compression_level = 9
@@ -63,6 +120,10 @@ class TimeSeriesAlertsHandler(alerts.AlertsHandler):
   def delete(self, ind):
     old_alerts = self.get_from_memcache() or self.get_from_datastore() or []
 
+    if ind == 'all':
+      self.update_db([])
+      self.response.write('Cleared ts-alerts')
+      return
     try:
       del old_alerts[int(ind)]
       self.update_db(old_alerts)
@@ -75,15 +136,16 @@ class TimeSeriesAlertsHandler(alerts.AlertsHandler):
 
     def find_alert(old_alerts, new_alert):
       for ind, old_alert in enumerate(old_alerts):
-        if (old_alert['condition_name'] == new_alert['condition_name'] and
-           old_alert['active_since'] == new_alert['active_since']):
+        if old_alert['mash_expression'] == new_alert['mash_expression']:
           return ind
       return -1
 
-    if find_alert(old_alerts, alert) == -1:
-      old_alerts.append(alert)
-      logging.info('storing a new ts-alert')
-      self.update_db(old_alerts)
+    ind = find_alert(old_alerts, alert)
+    if ind > -1:
+      old_alerts.pop(ind)
+    old_alerts.insert(0, alert)
+    logging.info('storing a new ts-alert')
+    self.update_db(old_alerts)
 
   def get_from_datastore(self):
     db = alerts.AlertsJSON
@@ -96,15 +158,24 @@ class TimeSeriesAlertsHandler(alerts.AlertsHandler):
       return data
     return False
 
-  def get(self):
+  def get(self, key=None): # pylint: disable=arguments-differ
     data = self.get_from_memcache() or self.get_from_datastore() or []
-    self.send_json_data(data)
+    if key:
+      try:
+        self.send_json_data(data[int(key)])
+      except IndexError:
+        self.response.set_status(404, 'Alert does not exist')
+      except ValueError:
+        self.response.set_status(400, 'Invalid Key Format')
+    else:
+      self.update_db(data)
+      self.send_json_data(data)
 
 
 class TimeSeriesInternalAlertsHandler(TimeSeriesAlertsHandler):
   ALERT_TYPE = 'ts-internal-alerts'
 
-  def get(self):
+  def get(self, key=None):
     user = users.get_current_user()
     if not user:
       ret = {}
@@ -121,7 +192,7 @@ class TimeSeriesInternalAlertsHandler(TimeSeriesAlertsHandler):
       self.response.set_status(403, 'Permission denied.')
       return
 
-    super(TimeSeriesInternalAlertsHandler, self).get()
+    super(TimeSeriesInternalAlertsHandler, self).get(key)
 
 class TimeSeriesAlertsHistory(alerts_history.AlertsHistory):
   PUBLIC_TYPE = TimeSeriesAlertsHandler.ALERT_TYPE
@@ -148,7 +219,7 @@ class TimeSeriesAlertsHistory(alerts_history.AlertsHistory):
 
 app = webapp2.WSGIApplication([
     ('/ts-alerts', TimeSeriesAlertsHandler),
-    ('/ts-alerts/(\d+)', TimeSeriesAlertsHandler),
+    ('/ts-alerts/(.*)', TimeSeriesAlertsHandler),
     ('/ts-internal-alerts', TimeSeriesInternalAlertsHandler),
     ('/ts-alerts-history', TimeSeriesAlertsHistory),
     ('/ts-alerts-history/(.*)', TimeSeriesAlertsHistory)])
