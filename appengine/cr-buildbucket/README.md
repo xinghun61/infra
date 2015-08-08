dc9f41ac8b51f26b12821570e76bae656912e3ef
nodir <nodir@chromium.org>
2015-08-07 18:32:37 -0700

    Moved Buildbucket documentation to markdown
    * Converted all html documentation to Markdown
    * Made it one flat .md file
    * Removed outdated info about buildbot integration
    * Removed all in /static, in particular polymer
    * Main handler redirects to README.md
    
    R=vadimsh@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1279613004

---
+++ appengine/cr-buildbucket/README.md
@@ -0,0 +1,15 @@
+# Buildbucket
+
+Buildbucket is a generic build queue. A build requester can schedule a build
+and wait for a result. A building system, such as Buildbot, can lease it, build
+it and report a result back.
+
+*   [Documentation](doc/index.md)
+*   Design doc: [go/buildbucket-design]
+*   Deployments:
+    *   Prod: [cr-buildbucket.appspot.com](https://cr-buildbucket.appspot.com)
+    *   Test: [cr-buildbucket.appspot.com](https://cr-buildbucket-test.appspot.com)
+    *   Dev: [cr-buildbucket-dev.appspot.com](https://cr-buildbucket-test.appspot.com)
+*   Owner: nodir@
+
+[go/buildbucket-design]: http://go/buildbucket-design
--- appengine/cr-buildbucket/handlers.py
+++ appengine/cr-buildbucket/handlers.py
@@ -9,9 +9,19 @@ import config
 import metrics
 import service
 
+README_MD = (
+    'https://chromium.googlesource.com/infra/infra/+/master/'
+    'appengine/cr-buildbucket/README.md')
+
 
 def create_service():
   return service.BuildBucketService()
+
+
+class MainHandler(webapp2.RequestHandler):  # pragma: no cover
+  """Redirects to README.md."""
+  def get(self):
+    return self.redirect(README_MD)
 
 
 class CronResetExpiredBuilds(webapp2.RequestHandler):
@@ -44,6 +54,7 @@ class BuildHandler(webapp2.RequestHandler):  # pragma: no cover
 
 def get_frontend_routes():  # pragma: no cover
   return [
+      webapp2.Route(r'/', MainHandler),
       webapp2.Route(r'/b/<build_id:\d+>', BuildHandler),
   ]
 
