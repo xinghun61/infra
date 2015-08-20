d593abddb4790ed1f10bf66a392ad0507552b6d8
tandrii <tandrii@chromium.org>
2015-08-20 08:35:25 -0700

    Use cq.cfg for default git cl try.
    R=sergiyb@chromium.org,akuegel@chromium.org
    BUG=403494
    
    Review URL: https://codereview.chromium.org/1302923002

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -347,8 +347,24 @@ def CheckChangeOnCommit(input_api, output_api):  # pragma: no cover
 
 # Unused argument - pylint: disable=W0613
 def GetPreferredTryMasters(project, change):  # pragma: no cover
-  return {
-    'tryserver.chromium.linux': {
-      'infra_tester': set(['defaulttests']),
-    }
-  }
+  import json
+  import os.path
+  import platform
+  import subprocess
+  cq_config_path = os.path.join(
+      change.RepositoryRoot(), 'config', 'cq.cfg')
+  # commit_queue.py below is a script in depot_tools directory, which has a
+  # 'builders' command to retrieve a list of CQ builders from the CQ config.
+  is_win = platform.system() == 'Windows'
+  masters = json.loads(subprocess.check_output(
+      ['commit_queue', 'builders', cq_config_path], shell=is_win))
+  try_config = {}
+  for master in masters:
+      try_config.setdefault(master, {})
+      for builder in masters[master]:
+          # Do not trigger presubmit builders, since they're likely to fail
+          # (e.g. OWNERS checks before finished code review), and we're
+          # running local presubmit anyway.
+          if 'presubmit' not in builder:
+              try_config[master][builder] = ['defaulttests']
+  return try_config
