ff66f507f22b577d9449bfc308be996f54a009ba
phajdan.jr <phajdan.jr@chromium.org>
2015-08-25 04:05:54 -0700

    mastermon: report buildbot metrics about success rate and cycle time
    See https://codereview.chromium.org/1309163002 for master-side change.
    
    BUG=524013
    
    Review URL: https://codereview.chromium.org/1307203002

--- infra/services/mastermon/pollers.py
+++ infra/services/mastermon/pollers.py
@@ -3,6 +3,7 @@
 # found in the LICENSE file.
 
 import collections
+import copy
 import logging
 
 import requests
@@ -11,6 +12,21 @@ from infra_libs import instrumented_requests
 from infra_libs import ts_mon
 
 LOGGER = logging.getLogger(__name__)
+
+
+# These values are buildbot constants used for Build and BuildStep.
+# This line was copied from master/buildbot/status/builder.py.
+SUCCESS, WARNINGS, FAILURE, SKIPPED, EXCEPTION, RETRY = range(6)
+
+
+STATUS_TO_STRING = {
+  str(SUCCESS): 'success',
+  str(WARNINGS): 'warnings',
+  str(FAILURE): 'failure',
+  str(SKIPPED): 'skipped',
+  str(EXCEPTION): 'exception',
+  str(RETRY): 'retry',
+}
 
 
 class Poller(object):
@@ -68,6 +84,14 @@ class VarzPoller(Poller):
   state = ts_mon.StringMetric('buildbot/master/builders/state')
   total = ts_mon.GaugeMetric('buildbot/master/builders/total_slaves')
 
+  recent_builds = ts_mon.GaugeMetric(
+      'buildbot/master/builders/recent_builds')
+
+  recent_successful_build_times = ts_mon.NonCumulativeDistributionMetric(
+      'buildbot/master/builders/recent_successful_build_times')
+  recent_finished_build_times = ts_mon.NonCumulativeDistributionMetric(
+      'buildbot/master/builders/recent_finished_build_times')
+
   def handle_response(self, data):
     self.uptime.set(data['server_uptime'], fields=self.fields())
     self.accepting_builds.set(data['accepting_builds'], self.fields())
@@ -82,3 +106,20 @@ class VarzPoller(Poller):
           builder_info.get('pending_builds', 0), fields=fields)
       self.state.set(builder_info.get('state', 'unknown'), fields=fields)
       self.total.set(builder_info.get('total_slaves', 0), fields=fields)
+
+      for status, builds in builder_info.get(
+          'recent_builds_by_status', {}).iteritems():
+        recent_builds_fields = copy.copy(fields)
+        recent_builds_fields['status'] = STATUS_TO_STRING.get(
+            str(status), str(status))
+        self.recent_builds.set(builds, fields=recent_builds_fields)
+
+      successful_dist = ts_mon.Distribution(ts_mon.GeometricBucketer())
+      for duration in builder_info.get('recent_successful_build_times', []):
+        successful_dist.add(duration)
+      self.recent_successful_build_times.set(successful_dist, fields=fields)
+
+      finished_dist = ts_mon.Distribution(ts_mon.GeometricBucketer())
+      for duration in builder_info.get('recent_finished_build_times', []):
+        finished_dist.add(duration)
+      self.recent_finished_build_times.set(finished_dist, fields=fields)
