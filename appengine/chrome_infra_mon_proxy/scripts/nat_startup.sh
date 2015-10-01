c26e0921c5fffc62a0be0ed0f744fc47ec8c9ebb
sergeyberezin <sergeyberezin@chromium.org>
2015-10-01 09:50:43 -0700

    Disable monitoring proxy on AppEngine.
    R=pgervais@chromium.org
    BUG=531584
    
    Review URL: https://codereview.chromium.org/1382813002

--- appengine/chrome_infra_mon_proxy/scripts/nat_startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
-sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-
-# Setup a health check endpoint.
-cd
-echo 'OK' > index.html
-python -m SimpleHTTPServer 8000 &
