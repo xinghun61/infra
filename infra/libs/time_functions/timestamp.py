d2f058413d89a99c0fef53ab2ee3118df3144768
sergiyb <sergiyb@chromium.org>
2015-11-02 07:09:21 -0800

    Moved time_functions into infra_libs to make it more useful for AE apps
    R=tandrii@chromium.org
    BUG=544873
    
    Review URL: https://codereview.chromium.org/1409193006

--- infra/libs/time_functions/timestamp.py
+++ infra/libs/time_functions/timestamp.py
@@ -2,26 +2,4 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-
-"""Provides common timestamp functions."""
-
-import datetime
-import pytz
-
-
-def utctimestamp(dt):
-  """Converts a datetime object into a floating point timestamp since the epoch.
-
-  dt is the datetime to convert. If dt is a naive (non-tz-aware) object, it
-     will implicitly be treated as UTC.
-  """
-  epoch = datetime.datetime.utcfromtimestamp(0).replace(tzinfo=pytz.UTC)
-  # This check is from http://stackoverflow.com/a/27596917/3984761.
-  if dt.tzinfo is None or dt.tzinfo.utcoffset(dt) is None:
-    dt = dt.replace(tzinfo=pytz.UTC)
-  return (dt - epoch).total_seconds()
-
-
-def utcnow_ts():  # pragma: no cover
-  """Returns the floating point number of seconds since the UTC epoch."""
-  return utctimestamp(datetime.datetime.utcnow())
+from infra_libs.time_functions.timestamp import *  # pylint: disable=W0614,W0401
