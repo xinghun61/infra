938a96cf52bcbd0883421a0b42e722090be536f9
stgao <stgao@chromium.org>
2015-11-17 15:33:22 -0800

    [Findit] Update code and enforce no local change before deployment to prod.
    BUG=475623
    
    Review URL: https://codereview.chromium.org/1454893002

--- appengine/findit/util_scripts/run.sh
+++ appengine/findit/util_scripts/run.sh
@@ -48,18 +48,44 @@ deploy_findit() {
   local app_id="findit-for-waterfall"
   if [[ "$1" == "prod" ]]; then
     app_id="findit-for-me"
+    local update_log="$TMP_DIR/update.log"
+    echo "Syncing code to tip of tree, logging in $update_log ..."
+    local update="$(cd $INFRA_DIR && git pull >>$update_log 2>>$update_log && gclient sync >>$update_log 2>>$update_log && echo $?)"
+    if [[ "$update" != "0" ]]; then
+      echo "Failed to run 'git pull && gclient sync'!"
+      echo "Please check log at $update_log"
+      return
+    fi
+    echo "Code was synced successfully."
   fi
 
   local current_version=`curl -s https://${app_id}.appspot.com/version`
   if ! [[ $current_version =~ ^[0-9a-fA-F]+$ ]]; then
-    echo $current_version
     echo "Failed to retrieve current version of Findit from the live app."
     echo "Please input the current version, followed by [ENTER]:"
     read current_version
-    echo
   fi
+  echo "Current deployed version is $current_version"
 
   local new_version="$(git rev-parse --short HEAD)"
+  if [[ "$1" == "prod" ]]; then
+    # Check uncommitted local changes.
+    local changed_file_number="$(git status --porcelain | wc -l)"
+    if [[ "$changed_file_number" != "0" ]]; then
+      echo "You have uncommitted local changes!"
+      echo "Please run 'git status' to check local changes."
+      return
+    fi
+
+    # Check committed local changes.
+    local tot_version="$(git rev-parse --short origin/master)"
+    if [[ "$new_version" != "$tot_version" ]]; then
+      echo "You have local commits!"
+      echo "Please run 'git reset $tot_version' to reset the local changes."
+      return
+    fi
+  fi
+
   local app_cfg="${GOOGLE_APP_ENGINE_DIR}/appcfg.py"
 
   echo "Current deployed version is '$current_version'."
@@ -78,13 +104,13 @@ deploy_findit() {
 
   local dashboard_url="https://${new_version}-dot-${app_id}.appspot.com/list-analyses"
   echo "Please force a re-run of a recent build failure on dashboard ${dashboard_url},"
-  echo "ensure that the analysis is run in the new version and gives correct results,"
-  echo "and then set the new version ${new_version} as default for all modules."
+  echo "ensure that the analysis is run in the new-version frontend & backend and gives correct results,"
+  echo "and then set the new version ${new_version} as default for both frontend and backend."
   echo
 
   local change_log_url="https://chromium.googlesource.com/infra/infra/+log/${current_version}..${new_version}/appengine/findit"
-  echo "Please also email chrome-findit@ with the following:"
-  echo "Subject: 'Release: findit-for-me was updated to ${new_version}.'"
+  echo "If the release is for findit-for-me, please email chrome-findit with the following:"
+  echo "Subject: 'Release: findit-for-me was update to ${new_version}.'"
   echo "Hi all,"
   echo
   echo "The app findit-for-me was updated from ${current_version} to ${new_version}."
