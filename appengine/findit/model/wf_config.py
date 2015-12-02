adbb7124f333ca27c7d47f6774ddb5098619518f
lijeffrey <lijeffrey@chromium.org>
2015-12-01 17:07:00 -0800

    [Findit] Temporary fix for config import break.
    For some reason, the line
    
    from components.datastore_utils import config
    
    causes import errors in other parts of the code. This change removes the dependency on config.py found in infra/appengine/luci/, though with many shared pieces. Eventually this CL should be either revisited or reverted when a fix for the import error is implemented in order to reuse as much of luci's config.py as possible.
    
    BUG=564356
    
    Review URL: https://codereview.chromium.org/1490983002

--- appengine/findit/model/wf_config.py
+++ appengine/findit/model/wf_config.py
@@ -7,11 +7,15 @@
 from google.appengine.ext import ndb
 
 from components import datastore_utils
-from components.datastore_utils import config
 
 
-class FinditConfig(config.GlobalConfig):
+# TODO(lijeffrey): It seems importing config from luci causes import breakages
+# in other parts of the code. Need to debug and fix the import error and
+# subclass FinditConfig from config.GlobalConfig if possible.
+class FinditConfig(ndb.Model):
   """Singleton entity with the global configuration of findit."""
+  _config_fetcher = None
+
   # A Dict mapping supported masters to lists of unsupported steps.
   masters_to_blacklisted_steps = ndb.JsonProperty(indexed=False, default={})
 
@@ -23,10 +27,58 @@ class FinditConfig(config.GlobalConfig):
   def VersionNumber(self):  # pragma: no cover
     return datastore_utils.HIGH_KEY_ID - self.key.integer_id()
 
+  @classmethod
+  def _get_root_model(cls):  # pragma: no cover
+    return datastore_utils.get_versioned_root_model('%sRoot' % cls.__name__)
 
-def Settings(fresh=False):  # pragma: no cover
-  if fresh:
-    FinditConfig.clear_cache()
+  @classmethod
+  def _get_root_key(cls):  # pragma: no cover
+    return ndb.Key(cls._get_root_model(), 1)
+
+  @classmethod
+  def fetch(cls):  # pragma: no cover
+    """Returns the current up-to-date version of the config entity.
+
+    Always fetches it from datastore. May return None if missing.
+    """
+    return datastore_utils.get_versioned_most_recent(cls, cls._get_root_key())
+
+  @classmethod
+  def cached(cls):  # pragma: no cover
+    if not cls._config_fetcher:
+      def config_fetcher():
+        conf = cls.fetch()
+        if not conf:
+          conf = cls()
+          conf.store()
+        return conf
+      cls._config_fetcher = staticmethod(config_fetcher)
+    return cls._config_fetcher()
+
+  def store(self):  # pragma: no cover
+    """Stores a new version of the config entity."""
+    # Create an incomplete key, to be completed by 'store_new_version'.
+    self.key = ndb.Key(self.__class__, None, parent=self._get_root_key())
+    return datastore_utils.store_new_version(self, self._get_root_model())
+
+  def modify(self, **kwargs):  # pragma: no cover
+    """Applies |kwargs| dict to the entity and stores the entity if changed."""
+    dirty = False
+    for k, v in kwargs.iteritems():
+      assert k in self._properties, k
+      if getattr(self, k) != v:
+        setattr(self, k, v)
+        dirty = True
+    if dirty:
+      self.store()
+    return dirty
+
+  # Mapping of waterfall builders to try-server trybots, which are used to
+  # re-run compile to identify culprits for compile failures.
+  builders_to_trybots = ndb.JsonProperty(indexed=False, default={})
+
+
+def Settings():  # pragma: no cover
   return FinditConfig.cached()
 
 
