efe2765c313fbac9265f179a149f1ebfe6ad5aad
nodir <nodir@chromium.org>
2015-12-03 13:53:31 -0800

    buildbucket: a better fix for the protobuf import problem
    TBR=vadimsh@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1492353003

--- appengine/cr-buildbucket/config.py
+++ appengine/cr-buildbucket/config.py
@@ -11,16 +11,15 @@ project repositories: `projects/<project_id>:<buildbucket-app-id>.cfg`.
 import hashlib
 import logging
 
-# import components.auth first because it modifies sys.path
-# and makes google.protobuf importable.
-from components import auth
+from components import utils
+utils.fix_protobuf_package()
 
 from google import protobuf
 from google.appengine.api import app_identity
 from google.appengine.ext import ndb
 
+from components import auth
 from components import config
-from components import utils
 from components.config import validation
 from proto import project_config_pb2
 import errors
