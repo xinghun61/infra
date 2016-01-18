b46aaf14409af8906051be9a16adb5c2f3ad7676
tandrii@chromium.org <tandrii@chromium.org>
2016-01-18 03:52:32 +0000

    Add infra/gerrit_cq_test recipe to tryserver.infra.
    This is a naive builder to test gerrit CQ for real.
    
    TBR=machenbach@chromium.org,phajdan.jr@chromium.org
    BUG=551403
    
    Review URL: https://codereview.chromium.org/1599043003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@298295 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ recipes/recipes/gerrit_cq_test.py
@@ -0,0 +1,37 @@
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+DEPS = [
+  'bot_update',
+  'gclient',
+  'recipe_engine/properties',
+]
+
+REPO = 'https://chromium.googlesource.com/playground/gerrit-cq/normal'
+
+
+def RunSteps(api):
+  api.gclient.set_config('gerrit_test_cq_normal')
+  api.bot_update.ensure_checkout(patch=True, force=True);
+
+
+def GenTests(api):
+  gerrit_kwargs={
+    'event.change.id':
+      'playground/gerrit-cq/normal'
+      '~master~Iff9c127b16841bc27728304a5ba2caff49ff11b5',
+    'event.change.number': 322360,
+    'event.change.url': 'https://chromium-review.googlesource.com/#/c/322360',
+    'event.patchSet.ref': 'refs/changes/60/322360/2',
+  }
+  yield (
+    api.test('try') +
+    api.properties.generic(
+        buildername='gerrit-test-cq-normal',
+        buildnumber=123,
+        mastername='tryserver.infra',
+        repository=REPO,
+        **gerrit_kwargs
+    )
+  )
