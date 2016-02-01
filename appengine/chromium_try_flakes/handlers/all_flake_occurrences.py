dfe6753e16b8f4465ce300da41f47c78b22f69da
sergiyb <sergiyb@chromium.org>
2016-02-01 05:52:21 -0800

    Display corresponding issue_id next to the flaky run
    R=tandrii@chromium.org
    BUG=579589
    
    Review URL: https://codereview.chromium.org/1644223008

--- appengine/chromium_try_flakes/handlers/all_flake_occurrences.py
+++ appengine/chromium_try_flakes/handlers/all_flake_occurrences.py
@@ -28,25 +28,47 @@ def show_all_flakes(flake, bug_friendly):  # pragma: no cover
 
   failure_runs_keys = []
   patchsets_keys = []
+  flakes = []
   for o in occurrences:
     failure_runs_keys.append(o.failure_run)
     patchsets_keys.append(o.failure_run.parent())
+    flakes.append(f for f in o.flakes if f.failure == flake.name)
 
   failure_runs = ndb.get_multi(failure_runs_keys)
   patchsets = ndb.get_multi(patchsets_keys)
 
-  for index, f in enumerate(failure_runs):
-    f.patchset_url = patchsets[index].getURL()
-    f.builder = patchsets[index].builder
-    f.formatted_time = f.time_finished.strftime('%Y-%m-%d %H:%M:%S UTC')
+  class FailureRunExtended:
+    def __init__(self, url, patchset_url, builder, formatted_time, issue_ids,
+                 time_finished):
+      self.url = url
+      self.patchset_url = patchset_url
+      self.builder = builder
+      self.formatted_time = formatted_time
+      self.issue_ids = issue_ids
+      self.time_finished = time_finished
+
+  failure_runs_extended = []
+  for index, fr in enumerate(failure_runs):
+    failure_runs_extended.append(FailureRunExtended(
+      fr.getURL(),
+      patchsets[index].getURL(),
+      patchsets[index].builder,
+      fr.time_finished.strftime('%Y-%m-%d %H:%M:%S UTC'),
+      set([f.issue_id for f in flakes[index] if f.issue_id > 0]),
+      fr.time_finished,
+    ))
+    logging.debug('flakes: %s', list(flakes[index]))
+    logging.debug('fr: %s', failure_runs_extended[-1].issue_ids)
 
   # Do simple sorting to make reading easier.
-  failure_runs = sorted(failure_runs, key=RunsSortFunction, reverse=True)
+  failure_runs_extended = sorted(
+      failure_runs_extended, key=RunsSortFunction, reverse=True)
 
+  # Group flaky runs into periods separated by at least 3 days.
   grouped_runs = []
-  if failure_runs:
-    current_group = [failure_runs[0]]
-    for f in failure_runs[1:]:
+  if failure_runs_extended:
+    current_group = [failure_runs_extended[0]]
+    for f in failure_runs_extended[1:]:
       if current_group[-1].time_finished - f.time_finished < MAX_GROUP_DISTANCE:
         current_group.append(f)
       else:
