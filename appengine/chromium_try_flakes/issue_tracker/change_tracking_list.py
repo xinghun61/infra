573fa4d2a3012356ac191be04130dfd981edbe91
sergiyb <sergiyb@chromium.org>
2016-02-10 17:25:28 -0800

    Updated comment and added license header
    TBR=tandrii@chromium.org
    BUG=545408
    
    Review URL: https://codereview.chromium.org/1673303006

--- appengine/chromium_try_flakes/issue_tracker/change_tracking_list.py
+++ appengine/chromium_try_flakes/issue_tracker/change_tracking_list.py
@@ -1,7 +1,8 @@
-"""
-A wrapper around list that caches additions or removals until they are
-performed
-"""
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""A wrapper around list that keep track of changes to it."""
 
 
 class ChangeTrackingList(list):  # pragma: no cover
