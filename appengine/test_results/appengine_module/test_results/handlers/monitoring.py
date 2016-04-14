3c23215806027e22bc0c14a5fb183344cdd0af48
sergiyb <sergiyb@chromium.org>
2016-04-14 07:17:10 -0700

    Report all JSON files reported to test-results app also to Dremel
    R=estaab@chromium.org, phajdan.jr@chromium.org, pgervais@chromium.org
    BUG=595509
    
    Review URL: https://codereview.chromium.org/1858243002

--- appengine/test_results/appengine_module/test_results/handlers/monitoring.py
+++ appengine/test_results/appengine_module/test_results/handlers/monitoring.py
@@ -9,6 +9,8 @@ import webapp2
 from google.appengine.api import taskqueue
 
 from appengine_module.test_results.handlers import util
+from appengine_module.test_results.model.jsonresults import JsonResults
+from appengine_module.test_results.model.testfile import TestFile
 from infra_libs import ts_mon
 from infra_libs import event_mon
 
@@ -25,9 +27,62 @@ class EventMonUploader(webapp2.RequestHandler):
       description='Sum of all characters in all test names')
 
   def post(self):
-    # TODO(sergiyb): Retrieve test json from datastore based on task parameters.
-    # TODO(sergiyb): Create a proto event and send it to event_mon.
-    pass
+    # Retrieve test json from datastore based on task parameters.
+    master = self.request.get('master')
+    builder = self.request.get('builder')
+    build_number = self.request.get('build_number')
+    test_type = self.request.get('test_type')
+    if not master or not builder or not build_number or not test_type:
+      logging.error(
+          'Missing required parameters: (master=%s, builder=%s, '
+          'build_number=%s, test_type=%s)' %
+          (master, builder, build_number, test_type))
+      self.response.set_status(400)
+      return
+
+    files = TestFile.get_files(
+        master, builder, test_type, build_number, 'full_results.json',
+        load_data=True, limit=1)
+    if not files:
+      logging.error('Failed to find full_results.json for (%s, %s, %s, %s)' % (
+                    master, builder, build_number, test_type))
+      self.response.set_status(404)
+      return
+    file_json = JsonResults.load_json(files[0].data)
+
+    # Create a proto event and send it to event_mon.
+    event = event_mon.Event('POINT')
+    test_results = event.proto.test_results
+    test_results.master_name = master
+    test_results.builder_name = builder
+    test_results.build_number = int(build_number)
+    test_results.test_type = test_type
+    if 'interrupted' in file_json:
+      test_results.interrupted = file_json['interrupted']
+    if 'version' in file_json:
+      test_results.version = file_json['version']
+    if 'seconds_since_epoch' in file_json:
+      test_results.usec_since_epoch = long(
+          float(file_json['seconds_since_epoch']) * 1000 * 1000)
+
+    def convert_test_result_type(json_val):
+      try:
+        return (event_mon.protos.chrome_infra_log_pb2.TestResultsEvent.
+                TestResultType.Value(json_val.upper().replace('+', '_')))
+      except ValueError:
+        return event_mon.protos.chrome_infra_log_pb2.TestResultsEvent.UNKNOWN
+
+    tests = util.flatten_tests_trie(
+        file_json.get('tests', {}), file_json.get('path_delimiter', '/'))
+    for name, test in tests.iteritems():
+      test_result = test_results.tests.add()
+      test_result.test_name = name
+      test_result.actual.extend(
+          convert_test_result_type(res) for res in test['actual'])
+      test_result.expected.extend(
+          convert_test_result_type(res) for res in test['expected'])
+
+    event.send()
 
   @classmethod
   def upload(cls, master, builder, build_number, test_type, file_json):
--- appengine/test_results/appengine_module/test_results/handlers/test/monitoring_test.py
+++ appengine/test_results/appengine_module/test_results/handlers/test/monitoring_test.py
@@ -3,19 +3,22 @@
 # found in the LICENSE file.
 
 import json
+import os
+import tempfile
 
 from appengine_module.testing_utils import testing
 
 from appengine_module.test_results import main
 from appengine_module.test_results.handlers.monitoring import EventMonUploader
 from appengine_module.test_results.model.testfile import TestFile
+from infra_libs import event_mon
 
 
 TEST_JSON = {
     'tests': {
       'web-animations-api': {
-        'animation-state-changes-negative-playback-rate.html': {
-          'expected': 'PASS',
+        'animation-state-changes.html': {
+          'expected': 'PASS IMAGE+TEXT FOOBAR',
           'actual': 'PASS',
           'has_stderr': True,
           'time': 0.1
@@ -24,12 +27,39 @@ TEST_JSON = {
     },
     'interrupted': False,
     'version': 3,
-    'seconds_since_epoch': 1457612314,
+    'seconds_since_epoch': 1457612314.123,
+    'path_delimiter': '\\',
+}
+
+REQ_PARAMS = {
+  'master': 'master',
+  'builder': 'builder',
+  'build_number': '123',
+  'test_type': 'ui_tests',
 }
 
 
 class EventMonUploaderTest(testing.AppengineTestCase):
-  app_module = main.app
+  app_module = main.app  # for testing.AppengineTestCase
+
+  def setUp(self):
+    super(EventMonUploaderTest, self).setUp()
+    tmp_handle, self.event_mon_file = tempfile.mkstemp()
+    os.close(tmp_handle)
+    event_mon.close()
+    event_mon.setup_monitoring(
+        run_type='file', hostname='test_host', output_file=self.event_mon_file)
+
+  def tearDown(self):
+    event_mon.close()
+    os.remove(self.event_mon_file)
+    super(EventMonUploaderTest, self).tearDown()
+
+  def read_event_mon_file(self):
+    with open(self.event_mon_file, 'rb') as f:
+      log_proto = event_mon.LogRequestLite.FromString(f.read())
+    return [event_mon.ChromeInfraEvent.FromString(ev.source_extension)
+            for ev in log_proto.log_event]
 
   def test_creates_task_for_upload(self):
     EventMonUploader.upload('master', 'builder', 123, 'ui_tests', TEST_JSON)
@@ -47,15 +77,52 @@ class EventMonUploaderTest(testing.AppengineTestCase):
     TestFile.add_file(
         'master', 'builder', 'ui_tests', 123, 'full_results.json',
         json.dumps(TEST_JSON))
-    response = self.test_app.post('/internal/monitoring/upload', {
-      'master': 'master',
-      'builder': 'builder',
-      'build_number': '123',
-      'test_type': 'ui_tests',
-    })
+    response = self.test_app.post('/internal/monitoring/upload', REQ_PARAMS)
 
     self.assertEqual(200, response.status_int)
-    # TODO(sergiyb): Check that event is correctly created.
+    events = self.read_event_mon_file()
+    self.assertEqual(1, len(events))
+    self.assertEqual(events[0].test_results.master_name, 'master')
+    self.assertEqual(events[0].test_results.builder_name, 'builder')
+    self.assertEqual(events[0].test_results.build_number, 123)
+    self.assertEqual(events[0].test_results.test_type, 'ui_tests')
+    self.assertEqual(events[0].test_results.interrupted, False)
+    self.assertEqual(events[0].test_results.version, 3)
+    self.assertEqual(events[0].test_results.usec_since_epoch, 1457612314123000)
+    self.assertEqual(1, len(events[0].test_results.tests))
+    self.assertEqual(
+        events[0].test_results.tests[0].test_name,
+        'web-animations-api\\animation-state-changes.html')
+    self.assertEqual(
+        events[0].test_results.tests[0].expected,
+        [
+          event_mon.protos.chrome_infra_log_pb2.TestResultsEvent.PASS,
+          event_mon.protos.chrome_infra_log_pb2.TestResultsEvent.IMAGE_TEXT,
+          event_mon.protos.chrome_infra_log_pb2.TestResultsEvent.UNKNOWN,
+        ])
+    self.assertEqual(
+        events[0].test_results.tests[0].actual,
+        [event_mon.protos.chrome_infra_log_pb2.TestResultsEvent.PASS])
 
   def test_does_not_crash_on_incorrect_tests_structure(self):
     EventMonUploader.upload('', '', '', '', {'tests': ['foo', 'bar']})
+
+  def test_returns_400_on_missing_request_params(self):
+    self.test_app.post('/internal/monitoring/upload', {}, status=400)
+    self.assertEqual(0, len(self.read_event_mon_file()))
+
+  def test_returns_404_on_missing_file(self):
+    self.test_app.post('/internal/monitoring/upload', REQ_PARAMS, status=404)
+    self.assertEqual(0, len(self.read_event_mon_file()))
+
+  def test_does_not_crash_on_missing_required_fields_in_json(self):
+    TestFile.add_file(
+        'master', 'builder', 'ui_tests', 123, 'full_results.json', '{}')
+    response = self.test_app.post('/internal/monitoring/upload', REQ_PARAMS)
+    self.assertEqual(200, response.status_int)
+    events = self.read_event_mon_file()
+    self.assertEqual(1, len(events))
+    self.assertFalse(events[0].test_results.HasField('interrupted'))
+    self.assertFalse(events[0].test_results.HasField('version'))
+    self.assertFalse(events[0].test_results.HasField('usec_since_epoch'))
+    self.assertEqual(0, len(events[0].test_results.tests))
