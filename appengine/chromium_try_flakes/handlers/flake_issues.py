4dc6071bf0fd8d2012a5fd6c4177119add888a86
sergiyb <sergiyb@chromium.org>
2016-05-04 10:00:31 -0700

    Mark taskkill as infra step
    R=phajdan.jr@chromium.org
    BUG=607807
    
    Review-Url: https://codereview.chromium.org/1934403002

--- appengine/chromium_try_flakes/handlers/flake_issues.py
+++ appengine/chromium_try_flakes/handlers/flake_issues.py
@@ -80,7 +80,7 @@ MAX_GAP_FOR_FLAKINESS_PERIOD = datetime.timedelta(days=3)
 KNOWN_TROOPER_FLAKE_NAMES = [
     'analyze', 'bot_update', 'compile (with patch)', 'compile',
     'device_status_check', 'gclient runhooks (with patch)', 'Patch',
-    'process_dumps', 'provision_devices', 'update_scripts']
+    'process_dumps', 'provision_devices', 'update_scripts', 'taskkill']
 
 # Flakes in these steps are always ignored:
 #  - steps: always red when any other step is red (duplicates failure)
