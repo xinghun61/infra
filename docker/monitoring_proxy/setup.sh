2753f0a748d1d2da51df224c6b6444ae7acc378f
dsansome <dsansome@chromium.org>
2016-05-05 10:22:04 -0700

    Add missing copyright headers
    BUG=
    
    Review-Url: https://codereview.chromium.org/1952253002

--- docker/monitoring_proxy/setup.sh
+++ docker/monitoring_proxy/setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 PROJECT_DEV="chrome-infra-mon-proxy-dev"
 PROJECT_PROD="chrome-infra-mon-proxy"
