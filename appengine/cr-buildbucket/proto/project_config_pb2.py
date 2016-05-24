986a76fb6ccc093df9cd387899ac055ce5e0db18
nodir <nodir@chromium.org>
2016-05-24 09:34:54 -0700

    buildbucket: recompile proto
    This should have been done in
    https://codereview.chromium.org/1998083004/
    
    TBR=estaab@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2007963002

--- appengine/cr-buildbucket/proto/project_config_pb2.py
+++ appengine/cr-buildbucket/proto/project_config_pb2.py
@@ -13,7 +13,7 @@ from google.protobuf import descriptor_pb2
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='project_config.proto',
   package='buildbucket',
-  serialized_pb='\n\x14project_config.proto\x12\x0b\x62uildbucket\"z\n\x03\x41\x63l\x12#\n\x04role\x18\x01 \x01(\x0e\x32\x15.buildbucket.Acl.Role\x12\r\n\x05group\x18\x02 \x01(\t\x12\x10\n\x08identity\x18\x03 \x01(\t\"-\n\x04Role\x12\n\n\x06READER\x10\x00\x12\r\n\tSCHEDULER\x10\x01\x12\n\n\x06WRITER\x10\x02\"\xdf\x02\n\x08Swarming\x12\x10\n\x08hostname\x18\x01 \x01(\t\x12\x12\n\nurl_format\x18\x02 \x01(\t\x12\x1c\n\x14\x63ommon_swarming_tags\x18\x03 \x03(\t\x12\x19\n\x11\x63ommon_dimensions\x18\x04 \x03(\t\x12/\n\x08\x62uilders\x18\x05 \x03(\x0b\x32\x1d.buildbucket.Swarming.Builder\x1a>\n\x06Recipe\x12\x12\n\nrepository\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x12\n\nproperties\x18\x03 \x03(\t\x1a\x82\x01\n\x07\x42uilder\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x15\n\rswarming_tags\x18\x02 \x03(\t\x12\x12\n\ndimensions\x18\x03 \x03(\t\x12,\n\x06recipe\x18\x04 \x01(\x0b\x32\x1c.buildbucket.Swarming.Recipe\x12\x10\n\x08priority\x18\x05 \x01(\x05\"_\n\x06\x42ucket\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x1e\n\x04\x61\x63ls\x18\x02 \x03(\x0b\x32\x10.buildbucket.Acl\x12\'\n\x08swarming\x18\x03 \x01(\x0b\x32\x15.buildbucket.Swarming\"6\n\x0e\x42uildbucketCfg\x12$\n\x07\x62uckets\x18\x01 \x03(\x0b\x32\x13.buildbucket.Bucket')
+  serialized_pb='\n\x14project_config.proto\x12\x0b\x62uildbucket\"z\n\x03\x41\x63l\x12#\n\x04role\x18\x01 \x01(\x0e\x32\x15.buildbucket.Acl.Role\x12\r\n\x05group\x18\x02 \x01(\t\x12\x10\n\x08identity\x18\x03 \x01(\t\"-\n\x04Role\x12\n\n\x06READER\x10\x00\x12\r\n\tSCHEDULER\x10\x01\x12\n\n\x06WRITER\x10\x02\"\xf1\x02\n\x08Swarming\x12\x10\n\x08hostname\x18\x01 \x01(\t\x12\x12\n\nurl_format\x18\x02 \x01(\t\x12\x1c\n\x14\x63ommon_swarming_tags\x18\x03 \x03(\t\x12\x19\n\x11\x63ommon_dimensions\x18\x04 \x03(\t\x12/\n\x08\x62uilders\x18\x05 \x03(\x0b\x32\x1d.buildbucket.Swarming.Builder\x1a>\n\x06Recipe\x12\x12\n\nrepository\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x12\n\nproperties\x18\x03 \x03(\t\x1a\x94\x01\n\x07\x42uilder\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x10\n\x08\x63\x61tegory\x18\x06 \x01(\t\x12\x15\n\rswarming_tags\x18\x02 \x03(\t\x12\x12\n\ndimensions\x18\x03 \x03(\t\x12,\n\x06recipe\x18\x04 \x01(\x0b\x32\x1c.buildbucket.Swarming.Recipe\x12\x10\n\x08priority\x18\x05 \x01(\x05\"_\n\x06\x42ucket\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x1e\n\x04\x61\x63ls\x18\x02 \x03(\x0b\x32\x10.buildbucket.Acl\x12\'\n\x08swarming\x18\x03 \x01(\x0b\x32\x15.buildbucket.Swarming\"6\n\x0e\x42uildbucketCfg\x12$\n\x07\x62uckets\x18\x01 \x03(\x0b\x32\x13.buildbucket.Bucket')
 
 
 
@@ -142,28 +142,35 @@ _SWARMING_BUILDER = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='swarming_tags', full_name='buildbucket.Swarming.Builder.swarming_tags', index=1,
+      name='category', full_name='buildbucket.Swarming.Builder.category', index=1,
+      number=6, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=unicode("", "utf-8"),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='swarming_tags', full_name='buildbucket.Swarming.Builder.swarming_tags', index=2,
       number=2, type=9, cpp_type=9, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='dimensions', full_name='buildbucket.Swarming.Builder.dimensions', index=2,
+      name='dimensions', full_name='buildbucket.Swarming.Builder.dimensions', index=3,
       number=3, type=9, cpp_type=9, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='recipe', full_name='buildbucket.Swarming.Builder.recipe', index=3,
+      name='recipe', full_name='buildbucket.Swarming.Builder.recipe', index=4,
       number=4, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='priority', full_name='buildbucket.Swarming.Builder.priority', index=4,
+      name='priority', full_name='buildbucket.Swarming.Builder.priority', index=5,
       number=5, type=5, cpp_type=1, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
@@ -179,7 +186,7 @@ _SWARMING_BUILDER = _descriptor.Descriptor(
   is_extendable=False,
   extension_ranges=[],
   serialized_start=383,
-  serialized_end=513,
+  serialized_end=531,
 )
 
 _SWARMING = _descriptor.Descriptor(
@@ -234,7 +241,7 @@ _SWARMING = _descriptor.Descriptor(
   is_extendable=False,
   extension_ranges=[],
   serialized_start=162,
-  serialized_end=513,
+  serialized_end=531,
 )
 
 
@@ -275,8 +282,8 @@ _BUCKET = _descriptor.Descriptor(
   options=None,
   is_extendable=False,
   extension_ranges=[],
-  serialized_start=515,
-  serialized_end=610,
+  serialized_start=533,
+  serialized_end=628,
 )
 
 
@@ -303,8 +310,8 @@ _BUILDBUCKETCFG = _descriptor.Descriptor(
   options=None,
   is_extendable=False,
   extension_ranges=[],
-  serialized_start=612,
-  serialized_end=666,
+  serialized_start=630,
+  serialized_end=684,
 )
 
 _ACL.fields_by_name['role'].enum_type = _ACL_ROLE
