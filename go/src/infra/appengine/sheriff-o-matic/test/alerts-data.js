122f8cc3b09e324e3dc94cf0d422b33e1ad34e56
seanmccullough <seanmccullough@google.com>
2016-05-24 14:43:59 -0700

    [som] Expand the examine page to select builder, step and test failure details.
    Review-Url: https://codereview.chromium.org/1978713002

---
+++ go/src/infra/appengine/sheriff-o-matic/test/alerts-data.js
@@ -0,0 +1,493 @@
+const TEST_ALERTS = {
+  "alerts": [
+    {
+      "body": "On step browser_tests on Mac-10.9",
+      "extension": {
+        "builders": [
+          {
+            "first_failure": 33505,
+            "latest_failure": 33505,
+            "name": "Mac GN (dbg)",
+            "start_time": 1463697008.813386,
+            "url": "https://build.chromium.org/p/chromium.mac/builders/Mac%20GN%20%28dbg%29"
+          }
+        ],
+        "reasons": [
+          {
+            "step": "browser_tests on Mac-10.9",
+            "test_names": null,
+            "url": "https://build.chromium.org/p/chromium.mac/builders/Mac%20GN%20%28dbg%29/builds/33505/steps/browser_tests%20on%20Mac-10.9"
+          }
+        ],
+        "regression_ranges": [],
+        "tree_closer": false
+      },
+      "key": "chromium.mac.Mac GN (dbg).browser_tests on Mac-10.9.[exception browser_tests on Mac-10.9]",
+      "links": null,
+      "severity": 3,
+      "start_time": 1463697008.813386,
+      "tags": null,
+      "time": 1463697008.813386,
+      "title": "Mac GN (dbg) infra failure",
+      "type": "infra-failure"
+    },
+    {
+      "body": "webkit_lint failing on chromium.webkit/WebKit Win7",
+      "extension": {
+        "builders": [
+          {
+            "first_failure": 42494,
+            "latest_failure": 42495,
+            "name": "WebKit Win7",
+            "start_time": 1463635584.498498,
+            "url": "https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Win7"
+          }
+        ],
+        "reasons": [
+          {
+            "step": "webkit_lint",
+            "test_names": null,
+            "url": "https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Win7/builds/42495/steps/webkit_lint"
+          }
+        ],
+        "regression_ranges": [
+          {
+            "positions": [
+              "refs/heads/master@{#394679}",
+              "refs/heads/master@{#394687}"
+            ],
+            "repo": "chromium",
+            "revisions": null,
+            "url": ""
+          },
+          {
+            "positions": [],
+            "repo": "https://chromium.googlesource.com/chromium/src",
+            "revisions": null,
+            "url": ""
+          },
+          {
+            "positions": [
+              "refs/heads/5.2.365@{#1}"
+            ],
+            "repo": "v8",
+            "revisions": null,
+            "url": ""
+          }
+        ],
+        "tree_closer": false
+      },
+      "key": "chromium.webkit.WebKit Win7.webkit_lint.",
+      "links": null,
+      "severity": 4,
+      "start_time": 1463635584.498498,
+      "tags": null,
+      "time": 1463637612.071807,
+      "title": "WebKit Win7 step failure",
+      "type": "build-failure"
+    },
+    {
+      "body": "webkit_tests failing on chromium.webkit/WebKit Win7",
+      "extension": {
+        "builders": [
+          {
+            "first_failure": 42477,
+            "latest_failure": 42524,
+            "name": "WebKit Win7",
+            "start_time": 1463599798.606302,
+            "url": "https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Win7"
+          }
+        ],
+        "reasons": [
+          {
+            "step": "webkit_tests",
+            "test_names": [
+              "paint/selection/invalidation-rect-includes-newline-for-rtl.html",
+              "paint/selection/invalidation-rect-includes-newline-for-vertical-lr.html",
+              "paint/selection/invalidation-rect-includes-newline-for-vertical-rl.html",
+              "paint/selection/invalidation-rect-includes-newline.html",
+              "paint/selection/invalidation-rect-with-br-includes-newline.html",
+              "transforms/3d/point-mapping/3d-point-mapping-preserve-3d.html"
+            ],
+            "url": "https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Win7/builds/42524/steps/webkit_tests"
+          }
+        ],
+        "regression_ranges": [
+          {
+            "positions": [
+              "refs/heads/master@{#394495}",
+              "refs/heads/master@{#394509}",
+              "refs/heads/master@{#394525}",
+              "refs/heads/master@{#394529}",
+              "refs/heads/master@{#394556}",
+              "refs/heads/master@{#394568}",
+              "refs/heads/master@{#394579}",
+              "refs/heads/master@{#394592}",
+              "refs/heads/master@{#394594}",
+              "refs/heads/master@{#394605}",
+              "refs/heads/master@{#394619}",
+              "refs/heads/master@{#394632}",
+              "refs/heads/master@{#394639}",
+              "refs/heads/master@{#394643}",
+              "refs/heads/master@{#394652}",
+              "refs/heads/master@{#394663}",
+              "refs/heads/master@{#394674}",
+              "refs/heads/master@{#394696}",
+              "refs/heads/master@{#394701}",
+              "refs/heads/master@{#394705}",
+              "refs/heads/master@{#394711}",
+              "refs/heads/master@{#394717}",
+              "refs/heads/master@{#394721}",
+              "refs/heads/master@{#394724}",
+              "refs/heads/master@{#394726}",
+              "refs/heads/master@{#394730}",
+              "refs/heads/master@{#394736}",
+              "refs/heads/master@{#394746}",
+              "refs/heads/master@{#394747}",
+              "refs/heads/master@{#394754}",
+              "refs/heads/master@{#394759}",
+              "refs/heads/master@{#394765}",
+              "refs/heads/master@{#394770}",
+              "refs/heads/master@{#394775}",
+              "refs/heads/master@{#394785}",
+              "refs/heads/master@{#394798}",
+              "refs/heads/master@{#394805}",
+              "refs/heads/master@{#394812}",
+              "refs/heads/master@{#394820}",
+              "refs/heads/master@{#394827}",
+              "refs/heads/master@{#394842}",
+              "refs/heads/master@{#394849}",
+              "refs/heads/master@{#394851}",
+              "refs/heads/master@{#394878}",
+              "refs/heads/master@{#394886}",
+              "refs/heads/master@{#394892}"
+            ],
+            "repo": "chromium",
+            "revisions": null,
+            "url": ""
+          },
+          {
+            "positions": [],
+            "repo": "https://chromium.googlesource.com/chromium/src",
+            "revisions": null,
+            "url": ""
+          },
+          {
+            "positions": [
+              "refs/heads/5.2.357@{#1}",
+              "refs/heads/5.2.361@{#1}",
+              "refs/heads/5.2.362@{#1}",
+              "refs/heads/5.2.365@{#1}",
+              "refs/heads/5.2.369@{#1}",
+              "refs/heads/5.2.371@{#1}"
+            ],
+            "repo": "v8",
+            "revisions": null,
+            "url": ""
+          }
+        ],
+        "tree_closer": false
+      },
+      "key": "chromium.webkit.WebKit Win7.webkit_tests.",
+      "links": null,
+      "severity": 4,
+      "start_time": 1463599798.606302,
+      "tags": null,
+      "time": 1463699464.195292,
+      "title": "WebKit Win7 step failure",
+      "type": "build-failure"
+    },
+    {
+      "body": "webkit_tests failing on chromium.webkit/WebKit Win7 (dbg)",
+      "extension": {
+        "builders": [
+          {
+            "first_failure": 5700,
+            "latest_failure": 5702,
+            "name": "WebKit Win7 (dbg)",
+            "start_time": 1463679782.477618,
+            "url": "https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Win7%20%28dbg%29"
+          }
+        ],
+        "reasons": [
+          {
+            "step": "webkit_tests",
+            "test_names": [
+              "paint/selection/invalidation-rect-includes-newline-for-rtl.html",
+              "paint/selection/invalidation-rect-includes-newline-for-vertical-lr.html",
+              "paint/selection/invalidation-rect-includes-newline-for-vertical-rl.html",
+              "paint/selection/invalidation-rect-includes-newline.html",
+              "paint/selection/invalidation-rect-with-br-includes-newline.html"
+            ],
+            "url": "https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Win7%20%28dbg%29/builds/5702/steps/webkit_tests"
+          }
+        ],
+        "regression_ranges": [
+          {
+            "positions": [
+              "refs/heads/master@{#394802}",
+              "refs/heads/master@{#394827}",
+              "refs/heads/master@{#394859}"
+            ],
+            "repo": "chromium",
+            "revisions": null,
+            "url": ""
+          },
+          {
+            "positions": [],
+            "repo": "https://chromium.googlesource.com/chromium/src",
+            "revisions": null,
+            "url": ""
+          },
+          {
+            "positions": [
+              "refs/heads/5.2.361@{#1}"
+            ],
+            "repo": "v8",
+            "revisions": null,
+            "url": ""
+          }
+        ],
+        "tree_closer": false
+      },
+      "key": "chromium.webkit.WebKit Win7 (dbg).webkit_tests.",
+      "links": null,
+      "severity": 4,
+      "start_time": 1463679782.477618,
+      "tags": null,
+      "time": 1463694885.008748,
+      "title": "WebKit Win7 (dbg) step failure",
+      "type": "build-failure"
+    },
+    {
+      "body": "WebKit Linux, WebKit Linux Trusty",
+      "extension": {
+        "builders": [
+          {
+            "first_failure": 64888,
+            "latest_failure": 64889,
+            "name": "WebKit Linux",
+            "start_time": 1463700341.603623,
+            "url": "https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux"
+          },
+          {
+            "first_failure": 12559,
+            "latest_failure": 12559,
+            "name": "WebKit Linux Trusty",
+            "start_time": 1463700596.653962,
+            "url": "https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%20Trusty"
+          }
+        ],
+        "reasons": [
+          {
+            "step": "webkit_tests",
+            "test_names": [
+              "transforms/3d/point-mapping/3d-point-mapping-preserve-3d.html"
+            ],
+            "url": "https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%20Trusty/builds/12559/steps/webkit_tests"
+          }
+        ],
+        "regression_ranges": [
+          {
+            "positions": [
+              "refs/heads/master@{#394901}",
+              "refs/heads/master@{#394908}"
+            ],
+            "repo": "chromium",
+            "revisions": null,
+            "url": ""
+          },
+          {
+            "positions": [],
+            "repo": "https://chromium.googlesource.com/chromium/src",
+            "revisions": null,
+            "url": ""
+          },
+          {
+            "positions": [
+              "refs/heads/5.2.361@{#1}"
+            ],
+            "repo": "v8",
+            "revisions": null,
+            "url": ""
+          }
+        ],
+        "tree_closer": false
+      },
+      "key": "chromium.webkit.WebKit Linux Trusty.webkit_tests.",
+      "links": null,
+      "severity": 5,
+      "start_time": 1463700341.603623,
+      "tags": null,
+      "time": 1463700596.653962,
+      "title": "webkit_tests,transforms/3d/point-mapping/3d-point-mapping-preserve-3d.html failing on 2 builders",
+      "type": "build-failure"
+    },
+    {
+      "body": "browser_tests on Windows-10-10586 failing on chromium.win/Win10 Tests x64",
+      "extension": {
+        "builders": [
+          {
+            "first_failure": 1214,
+            "latest_failure": 1214,
+            "name": "Win10 Tests x64",
+            "start_time": 1463692012.716963,
+            "url": "https://build.chromium.org/p/chromium.win/builders/Win10%20Tests%20x64"
+          }
+        ],
+        "reasons": [
+          {
+            "step": "browser_tests on Windows-10-10586",
+            "test_names": null,
+            "url": "https://build.chromium.org/p/chromium.win/builders/Win10%20Tests%20x64/builds/1214/steps/browser_tests%20on%20Windows-10-10586"
+          }
+        ],
+        "regression_ranges": [
+          {
+            "positions": [
+              "refs/heads/master@{#394845}"
+            ],
+            "repo": "chromium",
+            "revisions": null,
+            "url": ""
+          },
+          {
+            "positions": [],
+            "repo": "https://chromium.googlesource.com/chromium/src",
+            "revisions": null,
+            "url": ""
+          },
+          {
+            "positions": [
+              "refs/heads/5.2.361@{#1}"
+            ],
+            "repo": "v8",
+            "revisions": null,
+            "url": ""
+          }
+        ],
+        "tree_closer": false
+      },
+      "key": "chromium.win.Win10 Tests x64.browser_tests on Windows-10-10586.",
+      "links": null,
+      "severity": 5,
+      "start_time": 1463692012.716963,
+      "tags": null,
+      "time": 1463692012.716963,
+      "title": "Win10 Tests x64 step failure",
+      "type": "build-failure"
+    },
+    {
+      "body": "views_unittests on Windows-10-10586 failing on chromium.win/Win10 Tests x64",
+      "extension": {
+        "builders": [
+          {
+            "first_failure": 1215,
+            "latest_failure": 1215,
+            "name": "Win10 Tests x64",
+            "start_time": 1463698939.059013,
+            "url": "https://build.chromium.org/p/chromium.win/builders/Win10%20Tests%20x64"
+          }
+        ],
+        "reasons": [
+          {
+            "step": "views_unittests on Windows-10-10586",
+            "test_names": null,
+            "url": "https://build.chromium.org/p/chromium.win/builders/Win10%20Tests%20x64/builds/1215/steps/views_unittests%20on%20Windows-10-10586"
+          }
+        ],
+        "regression_ranges": [
+          {
+            "positions": [
+              "refs/heads/master@{#394861}"
+            ],
+            "repo": "chromium",
+            "revisions": null,
+            "url": ""
+          },
+          {
+            "positions": [],
+            "repo": "https://chromium.googlesource.com/chromium/src",
+            "revisions": null,
+            "url": ""
+          },
+          {
+            "positions": [
+              "refs/heads/5.2.361@{#1}"
+            ],
+            "repo": "v8",
+            "revisions": null,
+            "url": ""
+          }
+        ],
+        "tree_closer": false
+      },
+      "key": "chromium.win.Win10 Tests x64.views_unittests on Windows-10-10586.",
+      "links": null,
+      "severity": 5,
+      "start_time": 1463698939.059013,
+      "tags": null,
+      "time": 1463698939.059013,
+      "title": "Win10 Tests x64 step failure",
+      "type": "build-failure"
+    },
+    {
+      "body": "browser_tests on Ubuntu-12.04 failing on chromium.chromiumos/Linux ChromiumOS Tests (1)",
+      "extension": {
+        "builders": [
+          {
+            "first_failure": 20191,
+            "latest_failure": 20191,
+            "name": "Linux ChromiumOS Tests (1)",
+            "start_time": 1463701411.622166,
+            "url": "https://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%281%29"
+          }
+        ],
+        "reasons": [
+          {
+            "step": "browser_tests on Ubuntu-12.04",
+            "test_names": null,
+            "url": "https://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%281%29/builds/20191/steps/browser_tests%20on%20Ubuntu-12.04"
+          }
+        ],
+        "regression_ranges": [
+          {
+            "positions": [
+              "refs/heads/master@{#394897}"
+            ],
+            "repo": "chromium",
+            "revisions": null,
+            "url": ""
+          },
+          {
+            "positions": [],
+            "repo": "https://chromium.googlesource.com/chromium/src",
+            "revisions": null,
+            "url": ""
+          },
+          {
+            "positions": [
+              "refs/heads/5.2.361@{#1}"
+            ],
+            "repo": "v8",
+            "revisions": null,
+            "url": ""
+          }
+        ],
+        "tree_closer": false
+      },
+      "key": "chromium.chromiumos.Linux ChromiumOS Tests (1).browser_tests on Ubuntu-12.04.",
+      "links": null,
+      "severity": 5,
+      "start_time": 1463701411.622166,
+      "tags": null,
+      "time": 1463701411.622166,
+      "title": "Linux ChromiumOS Tests (1) step failure",
+      "type": "build-failure"
+    }
+  ],
+  "date": "2016-05-20 00:37:36.974185577 +0000 UTC",
+  "revision_summaries": {},
+  "timestamp": 1463704656.0
+};
+
