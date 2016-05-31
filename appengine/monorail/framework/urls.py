9f9f8444c871b8739b7bf587eb92f87d59b051e1
seanmccullough <seanmccullough@chromium.org>
2016-05-31 15:51:50 -0700

    Revert of [monorail] Add prober servlet for module default version match check. (patchset #2 id:20001 of https://codereview.chromium.org/1993903004/ )
    Reason for revert:
    This should be implemented with existing monitoring tools, which already track the deployed module versions.
    
    Original issue's description:
    > [monorail] Add prober servlet for module default version match check.
    >
    > BUG=monorail: 1348
    >
    > Committed: https://chromium.googlesource.com/infra/infra/+/45e0fbf867f8645414c17a225f0c3785a63d1fc8
    
    TBR=sheyang@chromium.org,seanmccullough@google.com
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=monorail: 1348
    
    Review-Url: https://codereview.chromium.org/2024113003

--- appengine/monorail/framework/urls.py
+++ appengine/monorail/framework/urls.py
@@ -124,5 +124,3 @@ CSP_REPORT = '/csp'
 TOKEN_REFRESH = '/hosting/tokenRefresh'
 
 SPAM_MODERATION_QUEUE = '/spamqueue'
-
-MODULE_VERSION_CHECK = '/moduleVersionCheck'
--- appengine/monorail/registerpages.py
+++ appengine/monorail/registerpages.py
@@ -56,7 +56,6 @@ from sitewide import userprofile
 from sitewide import userprojects
 from sitewide import usersettings
 from sitewide import userupdates
-from sitewide import module_version_match
 
 from tracker import componentcreate
 from tracker import componentdetail
@@ -284,7 +283,6 @@ class ServletRegistry(object):
         urls.GROUP_CREATE: groupcreate.GroupCreate,
         urls.GROUP_LIST: grouplist.GroupList,
         urls.GROUP_DELETE: grouplist.GroupList,
-        urls.MODULE_VERSION_CHECK: module_version_match.ModuleVersionMatch,
         })
 
     self._SetupUserServlets({
