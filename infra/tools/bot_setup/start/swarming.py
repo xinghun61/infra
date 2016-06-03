d6e1dddbc5c0c5fb7f3a59ccac3db1be38067412
hinoka <hinoka@google.com>
2016-06-03 16:43:24 -0700

    Revert of bot_start: Drop swarming signal file when running swarming (patchset #2 id:20001 of https://codereview.chromium.org/2038953002/ )
    Reason for revert:
    Going with https://codereview.chromium.org/1865943003/ instead
    
    Original issue's description:
    > bot_start: Drop swarming signal file when running swarming
    >
    > Swarming's bootstrap method assumes that it's solely responsible for startup, but
    > that's not true for chromebuild_startup.py based startup on CCompute.  This
    > drops a signal file so that swarming doesn't try to, otherwise they'll fight
    > for being the sole owner and weird things happen.
    >
    > BUG=
    > TBR=maruel
    >
    > Committed: https://chromium.googlesource.com/infra/infra/+/612d960f7cd36b128e91131411c98ee5c6667282
    
    TBR=maruel@chromium.org,vadimsh@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=
    
    Review-Url: https://codereview.chromium.org/2036013004

--- infra/tools/bot_setup/start/swarming.py
+++ infra/tools/bot_setup/start/swarming.py
@@ -7,21 +7,12 @@
 import os
 import re
 import requests
-import sys
 
 
 def is_staging(slave_name):
   return (
       slave_name.startswith('swarm-staging-') or
       re.match(r'^swarm[0-9]-c4$', slave_name))
-
-
-def drop_signal_file():
-  """Try to drop a signal file so that swarming doesn't."""
-  fn = ("C:\\Users\\chrome-bot\\AppData\\Roaming\\Microsoft\\Windows\\"
-        "Start Menu\\Programs\\Startup\\run_swarm_bot.bat")
-  with open(fn, 'w') as f:
-    f.write("exit 0\n")
 
 
 def start(slave_name, root_dir):
@@ -33,9 +24,6 @@ def start(slave_name, root_dir):
   url = 'https://chromium-swarm.appspot.com'
   if is_staging(slave_name):
     url = 'https://chromium-swarm-dev.appspot.com'
-
-  if sys.platform.startswith('win'):
-    drop_signal_file()
 
   exec requests.get('%s/bootstrap' % url).text
 
