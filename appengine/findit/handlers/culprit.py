5104e1ec834027da7db8c5eba2f8de2a97fb0422
stgao <stgao@chromium.org>
2016-06-28 09:51:51 -0700

    [Findit] Show the list of failed build cycles that a culprit is responsible for.
    BUG=621140
    
    Review-Url: https://codereview.chromium.org/2085513003

---
+++ appengine/findit/handlers/culprit.py
@@ -0,0 +1,43 @@
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from google.appengine.ext import ndb
+
+from common.base_handler import BaseHandler
+from common.base_handler import Permission
+
+
+def _FormatDatetime(dt):
+  if not dt:
+    return None  # pragma: no cover
+  else:
+    return dt.strftime('%Y-%m-%d %H:%M:%S UTC')
+
+
+class Culprit(BaseHandler):
+  PERMISSION_LEVEL = Permission.ANYONE
+
+  def HandleGet(self):
+    """Lists the build cycles in which the culprit caused failures."""
+    key = self.request.get('key', '')
+
+    culprit = ndb.Key(urlsafe=key).get()
+    if not culprit:  # pragma: no cover
+      return self.CreateError('Culprit not found', 404)
+
+    def ConvertBuildInfoToADict(build_info):
+      return {
+          'master_name': build_info[0],
+          'builder_name': build_info[1],
+          'build_number': build_info[2],
+      }
+
+    data = {
+        'project_name': culprit.project_name,
+        'revision': culprit.revision,
+        'cr_notified': culprit.cr_notified,
+        'cr_notification_time': _FormatDatetime(culprit.cr_notification_time),
+        'builds': map(ConvertBuildInfoToADict, culprit.builds),
+    }
+    return {'template': 'waterfall/culprit.html', 'data': data}
---
+++ appengine/findit/handlers/test/culprit_test.py
@@ -0,0 +1,50 @@
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from datetime import datetime
+
+import webapp2
+
+from testing_utils import testing
+
+from handlers import culprit
+from model import analysis_status as status
+from model.wf_culprit import WfCulprit
+
+
+class CulpritTest(testing.AppengineTestCase):
+  app_module = webapp2.WSGIApplication(
+      [('/culprit', culprit.Culprit), ], debug=True)
+
+  def testGetCulpritSuccess(self):
+    wf_culprit = WfCulprit.Create('chromium', 'r1')
+    wf_culprit.builds.append(['m', 'b1', 1])
+    wf_culprit.builds.append(['m', 'b2', 2])
+    wf_culprit.cr_notification_status = status.COMPLETED
+    wf_culprit.cr_notification_time = datetime(2016, 06, 24, 10, 03, 00)
+    wf_culprit.put()
+
+    expected_result = {
+        'project_name': 'chromium',
+        'revision': 'r1',
+        'cr_notified': True,
+        'cr_notification_time': '2016-06-24 10:03:00 UTC',
+        'builds': [
+            {
+                'master_name': 'm',
+                'builder_name': 'b1',
+                'build_number': 1,
+            },
+            {
+                'master_name': 'm',
+                'builder_name': 'b2',
+                'build_number': 2,
+            },
+        ],
+    }
+
+    response = self.test_app.get(
+        '/culprit?key=%s&format=json' % wf_culprit.key.urlsafe())
+    self.assertEqual(200, response.status_int)
+    self.assertEqual(expected_result, response.json_body)
