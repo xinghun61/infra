3e0bde9014a7dbcf4e5469af7f37680b6bb8560a
pgervais <pgervais@chromium.org>
2016-07-01 18:13:27 -0700

    pgervais's going away
    Deleting pgervais@ login from several files + remove antibody which is (sadly) abandoned.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2119953003

--- infra/tools/antibody/static/antibody_ui_all.js
+++
@@ -1,9 +0,0 @@
-var prev_width = $(window).width();
-var inflection_resolution = 991;
-$(window).resize(function() {
-  var curr_width = $(window).width();
-  if (prev_width < inflection_resolution && curr_width > inflection_resolution) {
-    $('table').bootstrapTable();
-  }
-  prev_width = curr_width;
-});
--- infra/tools/antibody/static/universal.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $(".nav li.disabled a").click(function() {
-    return false;
-  });
-
-});
