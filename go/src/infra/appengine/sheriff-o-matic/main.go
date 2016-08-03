5a4640ae7d5bfbd8a3822214624520b0ad054c3f
Stephen Martinis <martiniss@chromium.org>
2016-08-03 01:25:11 +0000

    Revert "Fix SOM with newest luci-go
    There was a change in the upstream auth library.
    
    BUG=Reviewed-on: https://chromium-review.googlesource.com/365491
    Reviewed-by: Sean McCullough <seanmccullough@chromium.org>
    "
    
    This reverts commit 0629dd555fd0345f3bf190f204b382cdecca7d60.
    
    Reason for revert: DEPS hadn't rolled yet
    
    Change-Id: I8da6ac25ff28b9ee91c1207fcc6550c728f7b9c0
    Reviewed-on: https://chromium-review.googlesource.com/365503
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>

--- go/src/infra/appengine/sheriff-o-matic/main.go
+++ go/src/infra/appengine/sheriff-o-matic/main.go
@@ -506,7 +506,7 @@ func getBugQueueHandler(ctx *router.Context) {
 }
 
 func getCrRevJSON(c context.Context, pos string) (map[string]string, error) {
-	c = client.UseServiceAccountTransport(c, nil)
+	c = client.UseServiceAccountTransport(c, nil, nil)
 
 	hc := &http.Client{Transport: urlfetch.Get(c)}
 
