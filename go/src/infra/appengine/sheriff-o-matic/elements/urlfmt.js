0492c479be537ecbea469ab410da5dfcecd84d9b
Sean McCullough <seanmccullough@chromium.org>
2016-08-24 18:16:15 -0700

    [som] Add new link to /results.html for webkit_test results, refactor url formatting.
    BUG=636252
    
    Change-Id: Idc4521cbbc6ca86a4f01443bd65bee2943125129
    Reviewed-on: https://chromium-review.googlesource.com/375518
    Reviewed-by: Tiffany Zhang <zhangtiff@chromium.org>
    Commit-Queue: Sean McCullough <seanmccullough@chromium.org>

---
+++ go/src/infra/appengine/sheriff-o-matic/elements/urlfmt.js
@@ -0,0 +1,29 @@
+(function(window) {
+ 'use strict';
+  var urlFmt = window.urlFmt || {};
+
+  const archivePrefix =
+      'https://storage.googleapis.com/chromium-layout-test-archives/';
+
+  urlFmt.layoutTestBase = function(builderName, buildNumber) {
+    let builderPath = builderName.replace(/[ .()]/g, '_');
+    return archivePrefix +
+        `${builderPath}/${buildNumber}/layout-test-results`;
+  };
+
+  urlFmt.layoutTest = function(builderName, buildNumber, testName) {
+    // Remove the file extension.
+    let testBase = testName.substr(0, testName.lastIndexOf('.')) || testName;
+    let basePath = urlFmt.layoutTestBase(builderName, buildNumber);
+
+    return `${basePath}/${testBase}`;
+  };
+
+  urlFmt.layoutTestAll = function(builderName, buildNumber) {
+    let basePath = urlFmt.layoutTestBase(builderName, buildNumber);
+    return `${basePath}/results.html`;
+  }
+
+  window.urlFmt = urlFmt;
+})(window);
+
