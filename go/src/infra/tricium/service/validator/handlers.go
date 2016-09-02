9e559395e4b0b93126207b175bbac328c8a107bb
Emma Soederberg <emso@chromium.org>
2016-09-02 13:05:50 +0200

    Adds Tricium validator module.
    This is currently just an empty module. An actual implementation will be added later.
    
    BUG=633851
    
    Change-Id: I8be18b2af7ae680c1f1b5707f7ed3b83b0b26bd2
    Reviewed-on: https://chromium-review.googlesource.com/380536
    Reviewed-by: Paweł Hajdan Jr. <phajdan@google.com>
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>

---
+++ go/src/infra/tricium/service/validator/handlers.go
@@ -0,0 +1,28 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package handlers implements HTTP handlers for the validator module.
+package handlers
+
+import (
+	"net/http"
+
+	"infra/tricium/service/common"
+)
+
+func init() {
+	http.HandleFunc("/validate", validateHandler)
+}
+
+func validateHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO(emso): Add validation code.
+	// This handler should validate the provided config (project and/or global),
+	// get the global config if needed from luci-config, merge configs, validate
+	// the merged config. Return merged config together with validation results
+	// in the response.
+	d := map[string]interface{}{
+		"Msg": "Status of the validator ...",
+	}
+	common.ShowBasePage(w, d)
+}
