bbbf66a4a818c0df51d41afb82085a5499d7a71f
Jason Robbins <jrobbins@chromium.org>
2016-09-15 10:18:30 -0700

    [Monorail] A more scalable procedure for copying data to CommentContent.
    The previous implementation was slow because MySQL took a longer and
    longer time to find Comment rows that had not been copied yet.
    
    BUG=monorail:1724
    
    Change-Id: I8f63a59ccd6a36f47355f1571474105771be6bba
    Reviewed-on: https://chromium-review.googlesource.com/385282
    Reviewed-by: Sean McCullough <seanmccullough@chromium.org>
    Commit-Queue: Jason Robbins <jrobbins@chromium.org>

--- appengine/monorail/tools/copy-comment-to-commentcontent.sql
+++ appengine/monorail/tools/copy-comment-to-commentcontent.sql
@@ -10,15 +10,28 @@ DROP PROCEDURE IF EXISTS CopyCommentToCommentContent;
 delimiter //
 
 CREATE PROCEDURE CopyCommentToCommentContent(
-    IN in_pid SMALLINT UNSIGNED, IN in_chunk_size SMALLINT UNSIGNED)
+    IN in_start INT, IN in_stop INT, IN in_step INT)
 BEGIN
+  comment_loop: LOOP
+    IF in_start >= in_stop THEN
+      LEAVE comment_loop;
+    END IF;
 
-  INSERT INTO CommentContent (comment_id, content, inbound_message)
-  SELECT id, content, inbound_message
-  FROM Comment
-  WHERE project_id = in_pid
-  AND Comment.id NOT IN (SELECT comment_id FROM CommentContent)
-  LIMIT in_chunk_size;
+    SELECT in_start AS StartingAt;
+    SELECT count(*)
+    FROM Comment
+    WHERE Comment.id >= in_start
+    AND Comment.id < in_start + in_step;
+
+    INSERT INTO CommentContent (comment_id, content, inbound_message)
+    SELECT id, content, inbound_message
+    FROM Comment
+    WHERE Comment.id >= in_start
+    AND Comment.id < in_start + in_step;
+
+    SET in_start = in_start + in_step;
+
+  END LOOP;
 
 END;
 
@@ -28,3 +41,23 @@ END;
 
 delimiter ;
 
+
+-- Copy and paste these individually and verify that the site is still responsive.
+-- the first one, takes about 30 sec, then 4-7 minutes for each of the rest.
+-- CALL CopyCommentToCommentContent(           0, 13 * 1000000, 10000);
+-- CALL CopyCommentToCommentContent(13 * 1000000, 16 * 1000000, 10000);
+-- CALL CopyCommentToCommentContent(16 * 1000000, 17 * 1000000, 10000);
+-- CALL CopyCommentToCommentContent(17 * 1000000, 18 * 1000000, 10000);
+-- CALL CopyCommentToCommentContent(18 * 1000000, 19 * 1000000, 10000);
+-- CALL CopyCommentToCommentContent(19 * 1000000, 20 * 1000000, 10000);
+-- CALL CopyCommentToCommentContent(20 * 1000000, 21 * 1000000, 10000);
+-- CALL CopyCommentToCommentContent(21 * 1000000, 22 * 1000000, 10000);
+-- CALL CopyCommentToCommentContent(22 * 1000000, 23 * 1000000, 10000);
+-- CALL CopyCommentToCommentContent(23 * 1000000, 24 * 1000000, 10000);
+-- CALL CopyCommentToCommentContent(24 * 1000000, 25 * 1000000, 10000);
+-- CALL CopyCommentToCommentContent(25 * 1000000, 26 * 1000000, 10000);
+-- CALL CopyCommentToCommentContent(26 * 1000000, 27 * 1000000, 10000);
+-- CALL CopyCommentToCommentContent(27 * 1000000, 28 * 1000000, 10000);
+-- CALL CopyCommentToCommentContent(28 * 1000000, 29 * 1000000, 10000);
+-- CALL CopyCommentToCommentContent(29 * 1000000, 30 * 1000000, 10000);
+-- CALL CopyCommentToCommentContent(30 * 1000000, 40 * 1000000, 10000);
