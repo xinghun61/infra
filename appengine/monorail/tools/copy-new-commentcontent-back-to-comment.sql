06f809e16cdfa63a1d607081b132c9559b98dcdd
Jason Robbins <jrobbins@chromium.org>
2016-09-20 17:06:13 -0700

    [Monorail] Use REPLACE to copy back Comment content.
    BUG=monorail:1724
    
    Change-Id: I7e81fdd78e70fa2f459015bb1230eec7d8ddcc86
    Reviewed-on: https://chromium-review.googlesource.com/387122
    Reviewed-by: Sean McCullough <seanmccullough@chromium.org>

--- appengine/monorail/tools/copy-new-commentcontent-back-to-comment.sql
+++ appengine/monorail/tools/copy-new-commentcontent-back-to-comment.sql
@@ -23,11 +23,39 @@ BEGIN
     WHERE comment_id >= in_start
     AND comment_id < in_start + in_step;
 
-    INSERT INTO Comment (content, inbound_message)
-    SELECT content, inbound_message
-    FROM CommentContent
+    DROP TEMPORARY TABLE IF EXISTS temp_comment;
+    CREATE TEMPORARY TABLE temp_comment (
+      id INT NOT NULL,
+      issue_id INT NOT NULL,
+      created INT NOT NULL,
+      project_id SMALLINT UNSIGNED NOT NULL,
+      commenter_id INT UNSIGNED NOT NULL,
+      content MEDIUMTEXT COLLATE utf8mb4_unicode_ci,
+      inbound_message MEDIUMTEXT COLLATE utf8mb4_unicode_ci,
+      deleted_by INT UNSIGNED,
+      is_spam BOOLEAN DEFAULT FALSE,
+      is_description BOOLEAN DEFAULT FALSE,
+
+      PRIMARY KEY(id)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+    INSERT INTO temp_comment
+    SELECT Comment.id, Comment.issue_id, Comment.created, Comment.project_id,
+           Comment.commenter_id, CommentContent.content,
+	   CommentContent.inbound_message, Comment.deleted_by, Comment.is_spam,
+	   Comment.is_description
+    FROM Comment
+    LEFT JOIN CommentContent on CommentContent.comment_id = Comment.id
     WHERE CommentContent.comment_id >= in_start
     AND CommentContent.comment_id < in_start + in_step;
+
+
+    REPLACE INTO Comment (id, issue_id, created, project_id, commenter_id,
+        content, inbound_message, deleted_by, is_spam, is_description)
+    SELECT id, issue_id, created, project_id, commenter_id,
+        content, inbound_message, deleted_by, is_spam, is_description
+    FROM temp_comment;
 
     SET in_start = in_start + in_step;
 
@@ -42,5 +70,14 @@ END;
 delimiter ;
 
 
+-- Temporarily disable these foreign key references so that we can do
+-- REPLACE commands.
+-- ALTER TABLE SpamReport DROP FOREIGN KEY spamreport_ibfk_2;
+-- ALTER TABLE SpamVerdict DROP FOREIGN KEY spamverdict_ibfk_2;
+
 -- This ID is the first comment entered that was stored in CommentContent only.
 -- CALL CopyNewCommentContentBackToComment(31459489, 40 * 1000000, 5000);
+
+-- Add back foreign key constraints.
+-- ALTER TABLE SpamReport ADD FOREIGN KEY (comment_id) REFERENCES Comment(id);
+-- ALTER TABLE SpamVerdict ADD FOREIGN KEY (comment_id) REFERENCES Comment(id);
