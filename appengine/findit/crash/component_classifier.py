bb670bc669df9eaa72a6b8699200adc5b06530da
wrengr <wrengr@chromium.org>
2016-11-03 16:43:15 -0700

    removed the redundant copy of the Component class
    It looks liek a rebase got messed up somewhere. This patch removes the
    copy of the Component class in component_classifier.py, and uses the
    version in component.py instead.
    
    BUG=
    R=katesonia@chromium.org, mbarbella@chromium.org
    TBR=stgao@chromium.org
    CC=aarya@google.com
    
    Review-Url: https://codereview.chromium.org/2467083002

--- appengine/findit/crash/component_classifier.py
+++ appengine/findit/crash/component_classifier.py
@@ -6,35 +6,8 @@ from collections import namedtuple
 import logging
 import re
 
+from crash.component import Component
 from crash.occurrence import RankByOccurrence
-
-# TODO(http://crbug.com/659346): write coverage tests.
-class Component(namedtuple('Component',
-    ['component_name', 'path_regex', 'function_regex'])): # pragma: no cover
-  """A representation of a "component" in Chromium.
-
-  For example: 'Blink>DOM' or 'Blink>HTML'. Notably, a component knows
-  how to identify itself. Hence, given a stack frame or change list
-  or whatever, we ask the Component whether it matches that frame,
-  CL, etc."""
-  __slots__ = ()
-
-  def __new__(cls, component_name, path_regex, function_regex=None):
-    return super(cls, Component).__new__(cls,
-      component_name,
-      re.compile(path_regex),
-      re.compile(function_regex) if function_regex else None)
-
-  def MatchesStackFrame(self, frame):
-    """Return true if this component matches the frame."""
-    if not self.path_regex.match(frame.dep_path + frame.file_path):
-      return False
-
-    # We interpret function_regex=None to mean the regex that matches
-    # everything.
-    if not self.function_regex:
-      return True
-    return self.function_regex.match(frame.function)
 
 
 class ComponentClassifier(object):
