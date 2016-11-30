6052f60f783ecb29e86f3520f2bbfb558606234d
Joanna Wang <jojwang@google.com>
2016-11-30 13:47:43 -0800

    Added some unit tests or TODOs for hotlist_helpers functions.
    BUG=monorail:50
    
    Change-Id: If08ee9bcee0079452ff121555e3f263aba785f3c
    Reviewed-on: https://chromium-review.googlesource.com/415387
    Reviewed-by: Sean McCullough <seanmccullough@chromium.org>
    Commit-Queue: Joanna Wang <jojwang@google.com>

--- appengine/monorail/features/test/hotlist_helpers_test.py
+++ appengine/monorail/features/test/hotlist_helpers_test.py
@@ -8,9 +8,129 @@
 import unittest
 
 from features import hotlist_helpers
+from framework import profiler
+from services import service_manager
+from testing import testing_helpers
+from testing import fake
 from proto import features_pb2
+from proto import tracker_pb2
+
+
+class CreateHotlistTableDataTest(unittest.TestCase):
+
+  def setUp(self):
+    self.services = service_manager.Services(
+        issue=fake.IssueService(),
+        features=fake.FeaturesService(),
+        issue_star=fake.AbstractStarService(),
+        config=fake.ConfigService(),
+        project=fake.ProjectService(),
+        user=fake.UserService())
+    self.services.project.TestAddProject('ProjectName', project_id = 001)
+
+    self.services.user.TestAddUser('annajowang@email.com', 111L)
+    self.services.user.TestAddUser('claremont@email.com', 222L)
+    issue1 = fake.MakeTestIssue(
+        001, 1, 'issue_summary', 'New', 111L, project_name='ProjectName')
+    self.services.issue.TestAddIssue(issue1)
+    issue2 = fake.MakeTestIssue(
+        001, 2, 'issue_summary2', 'New', 111L, project_name='ProjectName')
+    self.services.issue.TestAddIssue(issue2)
+    issue3 = fake.MakeTestIssue(
+        001, 3, 'issue_summary3', 'New', 222L, project_name='ProjectName')
+    self.services.issue.TestAddIssue(issue3)
+    issues = [issue1, issue2, issue3]
+    hotlist_issues = [
+        (issue.issue_id, rank) for rank, issue in enumerate(issues)]
+
+    self.iid_rank_pairs_list = [
+        features_pb2.MakeHotlistIssue(
+            issue_id, rank=rank) for (issue_id, rank) in hotlist_issues]
+    self.test_hotlist = self.services.features.TestAddHotlist(
+        'hotlist', hotlist_id=123,
+        iid_rank_pairs=hotlist_issues)
+    self.mr = testing_helpers.MakeMonorailRequest(
+        hotlist=self.test_hotlist)
+    self.services.user.TestAddUser('annajo@email.com', 148L)
+    self.mr.auth.effective_ids = {148L}
+    self.mr.col_spec = 'ID Summary Modified'
+
+  def testCreateHotlistTableData(self):
+    self.setUp()
+    table_data, table_related_dict = hotlist_helpers.CreateHotlistTableData(
+        self.mr, self.iid_rank_pairs_list, profiler.Profiler(), self.services)
+    self.assertEqual(len(table_data), 3)
+    start_index = 100001
+    for row in table_data:
+      self.assertEqual(row.project_name, 'ProjectName')
+      self.assertEqual(row.issue_id, start_index)
+      start_index += 1
+    self.assertEqual(len(table_related_dict['column_values']), 3)
+
+    # test none of the shown columns show up in unshown_columns
+    self.assertTrue(
+        set(self.mr.col_spec.split()).isdisjoint(
+            table_related_dict['unshown_columns']))
+    self.assertEqual(table_related_dict['is_cross_project'], False)
+    self.assertEqual(len(table_related_dict['pagination'].visible_results), 3)
+
+
+class GetAllProjectsOfIssuesTest(unittest.TestCase):
+
+  issue_x_1 = tracker_pb2.Issue()
+  issue_x_1.project_id = 789
+
+  issue_x_2 = tracker_pb2.Issue()
+  issue_x_2.project_id = 789
+
+  issue_y_1 = tracker_pb2.Issue()
+  issue_y_1.project_id = 678
+
+  def testGetAllProjectsOfIssues_Normal(self):
+    issues = [self.issue_x_1, self.issue_x_2]
+    self.assertEqual(
+        hotlist_helpers.GetAllProjectsOfIssues(issues),
+        set([789]))
+    issues = [self.issue_x_1, self.issue_x_2, self.issue_y_1]
+    self.assertEqual(
+        hotlist_helpers.GetAllProjectsOfIssues(issues),
+        set([678, 789]))
+
+  def testGetAllProjectsOfIssues_Empty(self):
+    self.assertEqual(
+        hotlist_helpers.GetAllProjectsOfIssues([]),
+        set())
+
 
 class HelpersUnitTest(unittest.TestCase):
+
+  # TODO(jojwang): Write Tests for GetAllProjectsOfIssues,
+  #  GetAllConfigsOfProjects, _MakeTableData
+  def setUp(self):
+    self.services = service_manager.Services(issue=fake.IssueService(),
+                                        config=fake.ConfigService(),
+                                        project=fake.ProjectService(),
+                                        user=fake.UserService())
+    self.project = self.services.project.TestAddProject(
+        'ProjectName', project_id = 001, owner_ids=[111L])
+
+    self.services.user.TestAddUser('annajowang@email.com', 111L)
+    self.services.user.TestAddUser('claremont@email.com', 222L)
+    self.issue1 = fake.MakeTestIssue(
+        001, 1, 'issue_summary', 'New', 111L,
+        project_name='ProjectName', labels='restrict-view-Googler')
+    self.services.issue.TestAddIssue(self.issue1)
+    self.issue3 = fake.MakeTestIssue(
+        001, 3, 'issue_summary3', 'New', 222L, project_name='ProjectName')
+    self.services.issue.TestAddIssue(self.issue3)
+    self.issues = [self.issue1, self.issue3]
+    self.mr = testing_helpers.MakeMonorailRequest()
+
+  def testFilterIssues(self):
+    test_allowed_issues = hotlist_helpers.FilterIssues(
+        self.mr, self.issues, self.services)
+    self.assertEqual(len(test_allowed_issues), 1)
+    self.assertEqual(test_allowed_issues[0].local_id, 3)
 
   def testMembersWithoutGivenIDs(self):
     h = features_pb2.Hotlist()
