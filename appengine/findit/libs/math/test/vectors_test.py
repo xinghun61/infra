b17938f2101a3ed9c608d058a0a65da1984dc1f3
wrengr <wrengr@chromium.org>
2016-12-02 15:37:23 -0800

    Added n-ary vector summation
    BUG=
    TBR=stgao@chromium.org
    
    Review-Url: https://codereview.chromium.org/2547133002

---
+++ appengine/findit/libs/math/test/vectors_test.py
@@ -0,0 +1,70 @@
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import math
+import numpy as np
+import unittest
+
+from libs.math.vectors import vsum
+
+
+BIG = 1e100
+LITTLE = 1
+
+
+class VectorsTest(unittest.TestCase):
+
+  def setUp(self):
+    self._xs = [BIG, LITTLE, -BIG]
+
+  def testFsumKeepsPrecision(self):
+    """Make sure ``testVsumKeepsPrecision`` should actually work.
+
+    That is, this is a meta-test to make sure that the particular values
+    of ``BIG`` and ``LITTLE`` we use will indeed behave as expected.
+    """
+    # N.B., with precision loss, we'll get 0 rather than LITTLE.
+    self.assertEqual(LITTLE, math.fsum(self._xs))
+
+  def testVsumKeepsPrecision(self):
+    """Ensure that ``vsum`` retains precision over ``sum``.
+
+    Because ``BIG`` is big and ``LITTLE`` is little, performing summation
+    naively will cause the ``LITTLE`` to be lost in rounding errors. This
+    is the same test case as ``testFsumKeepsPrecision``. We make the
+    arrays have more than one element to make sure ``vsum`` actually
+    does work on vectors, as intended. We use variations on ``x`` in the
+    different components just so we don't do the same thing over and over;
+    there's nothing special about negation or doubling.
+    """
+    vs = [np.array([x, -x, 2*x]) for x in self._xs]
+    total = vsum(vs)
+    self.assertIsNotNone(total)
+
+    self.assertListEqual([LITTLE, -LITTLE, 2*LITTLE], total.tolist())
+
+  def testVsumEmptyWithoutShape(self):
+    """Ensure ``vsum`` returns ``None`` when expected.
+
+    The empty summation should return the zero vector. However, since
+    we don't know the shape of the vectors in the list, we don't know
+    what shape of zero vector to return. Thus, we return ``None`` as
+    the only sensible result. This test ensures that actually does happen.
+    """
+    self.assertIsNone(vsum([]))
+
+  def testVSumEmptyWithShape(self):
+    """Ensure ``vsum`` returns the zero vector when expected.
+
+    The empty summation should return the zero vector. If we know the
+    shape of the vectors in the list then we can in fact return the
+    zero vector of the correct shape. This test ensures that actually
+    does happen.
+    """
+    expected_shape = (3,)
+    total = vsum([], shape=expected_shape)
+    self.assertIsNotNone(total)
+    self.assertTupleEqual(expected_shape, total.shape)
+    self.assertListEqual(np.zeros(expected_shape).tolist(), total.tolist())
+
---
+++ appengine/findit/libs/math/vectors.py
@@ -0,0 +1,42 @@
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import math
+import numpy as np
+
+
+def vsum(vs, shape=None):
+  """Accurate summation of a list of vectors.
+
+  This function is like ``math.fsum`` except operating over collections
+  of vectors rather than collections of scalars.
+
+  Args:
+    vs (list of np.ndarray of float): The vectors to add up.
+    shape (tuple of int): the shape of the vectors. Optional.
+
+  Returns:
+    Returns an ``np.ndarray`` if all is well, otherwise returns
+    ``None``. The situations where all is not well (and so we return
+    ``None``) are: (1) ``vs`` is empty and ``shape`` is not provided;
+    (2) a ``shape`` is provided but does not match the actual shape of
+    the vectors; (3) not all the vectors in the list have the same shape.
+  """
+  if shape is None:
+    if not vs:
+      return None
+
+    shape = vs[0].shape
+
+  # It'd be better to vectorize the implementation of Shewchuk's
+  # algorithm directly, so we can avoid needing to traverse ``vs``
+  # repeatedly. However, this is deemed to have too high a maintinence
+  # cost for the performance benefit.
+  total = np.zeros(shape)
+  it = np.nditer(total, flags=['multi_index'], op_flags=['writeonly'])
+  while not it.finished:
+    it[0] = math.fsum(v[it.multi_index] for v in vs)
+    it.iternext()
+
+  return total
