98ddcabed466021e7458196dba3ec89e8a604d6e
Andrii Shyshkalov <tandrii@chromium.org>
2017-01-04 15:36:37 +0100

    Improve patch_status of CQ status app.
    R=alancutter@chromium.org
    BUG=676921
    
    Change-Id: I802998a8c012f5f13e180e304e113e24b21dd0ce
    Reviewed-on: https://chromium-review.googlesource.com/422330
    Reviewed-by: Sergey Berezin <sergeyberezin@chromium.org>
    Commit-Queue: Andrii Shyshkalov <tandrii@chromium.org>

--- appengine/chromium_cq_status/handlers/patch_status.py
+++ appengine/chromium_cq_status/handlers/patch_status.py
@@ -2,10 +2,23 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import webapp2
+import json
 import cgi
+import webapp2
 
 from shared import utils
+
+
+def _render(codereview_hostname, full_patchset_url,
+            issue, patchset):  # pragma: no cover
+  return open('templates/patch_status.html').read() % {
+      'codereview_hostname_html': cgi.escape(codereview_hostname, quote=True),
+      'codereview_hostname_js': json.dumps(cgi.escape(codereview_hostname,
+                                                      quote=True)),
+      'full_patchset_url_html': cgi.escape(full_patchset_url, quote=True),
+      'issue': int(issue),
+      'patchset': int(patchset),
+    }
 
 
 class PatchStatus(webapp2.RequestHandler): # pragma: no cover
@@ -14,12 +27,8 @@ class PatchStatus(webapp2.RequestHandler): # pragma: no cover
     codereview_hostname = utils.guess_legacy_codereview_hostname(issue)
     full_patchset_url = utils.get_full_patchset_url(codereview_hostname, issue,
                                                     patchset)
-    self.response.write(open('templates/patch_status.html').read() % {
-      'codereview_hostname': cgi.escape(codereview_hostname, quote=True),
-      'full_patchset_url': cgi.escape(full_patchset_url, quote=True),
-      'issue': cgi.escape(issue, quote=True),
-      'patchset': cgi.escape(patchset, quote=True),
-    })
+    self.response.write(_render(
+        codereview_hostname, full_patchset_url, issue, patchset))
 
 
 class PatchStatusV2(webapp2.RequestHandler): # pragma: no cover
@@ -27,9 +36,5 @@ class PatchStatusV2(webapp2.RequestHandler): # pragma: no cover
   def get(self, codereview_hostname, issue, patchset): # pylint: disable=W0221
     full_patchset_url = utils.get_full_patchset_url(codereview_hostname, issue,
                                                     patchset)
-    self.response.write(open('templates/patch_status.html').read() % {
-      'codereview_hostname': cgi.escape(codereview_hostname, quote=True),
-      'full_patchset_url': cgi.escape(full_patchset_url, quote=True),
-      'issue': cgi.escape(issue, quote=True),
-      'patchset': cgi.escape(patchset, quote=True),
-    })
+    self.response.write(_render(
+        codereview_hostname, full_patchset_url, issue, patchset))
