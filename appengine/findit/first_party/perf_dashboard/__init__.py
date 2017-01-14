2302fb8010641bc96ea3920d85369129060fbb12
Shuotao Gao <stgao@chromium.org>
2017-01-13 23:34:44 -0800

    [Findit] Have a fork of the change point detection from perf dashboard.
    TBR=chanli, lijeffrey
    
    Change-Id: If2cc3d43a7d6f0d859c6e834e14f11ddb6b790b8
    Reviewed-on: https://chromium-review.googlesource.com/428236
    Reviewed-by: Shuotao Gao <stgao@chromium.org>
    Commit-Queue: Shuotao Gao <stgao@chromium.org>

---
+++ appengine/findit/first_party/perf_dashboard/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This is a fork of the change point detection from the Chromium perf dashboard.
