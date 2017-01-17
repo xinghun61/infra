9c97d8d4450ad178daeddb9022a5722e69fe9102
Tiff Zhang <zhangtiff@google.com>
2017-01-17 11:35:00 -0800

    SoM: Delete som-formatted-text and replace with marked-element's
    Marked-element documentation: https://elements.polymer-project.org/elements/marked-element
    
    BUG=
    
    Change-Id: Ic466cffbf230252a90dc5417ea3d0eb5b93f6fa3
    Reviewed-on: https://chromium-review.googlesource.com/428391
    Reviewed-by: Sean McCullough <seanmccullough@chromium.org>
    Commit-Queue: Tiffany Zhang <zhangtiff@chromium.org>

--- go/src/infra/appengine/sheriff-o-matic/elements/som-annotations/som-annotations.js
+++ go/src/infra/appengine/sheriff-o-matic/elements/som-annotations/som-annotations.js
@@ -12,17 +12,23 @@
       annotations: {
         notify: true,
         type: Object,
-        value: function() { return {}; },
+        value: function() {
+          return {};
+        },
         computed: '_computeAnnotations(_annotationsResp, localState)'
       },
       annotationError: {
-       type: Object,
-       value: function() { return {}; },
+        type: Object,
+        value: function() {
+          return {};
+        },
       },
       // The raw response from the server of annotations.
       _annotationsResp: {
         type: Array,
-        value: function() { return []; },
+        value: function() {
+          return [];
+        },
       },
       _bugErrorMessage: String,
       _bugInput: {
@@ -36,7 +42,8 @@
       _commentsModel: Object,
       _commentsModelAnnotation: {
         type: Object,
-        computed: '_computeCommentsModelAnnotation(annotations, _commentsModel)',
+        computed:
+            '_computeCommentsModelAnnotation(annotations, _commentsModel)',
       },
       _commentsHidden: {
         type: Boolean,
@@ -69,7 +76,7 @@
     },
 
     fetch: function() {
-      this.annotationError.action = "Fetching all annotations";
+      this.annotationError.action = 'Fetching all annotations';
       this.fetchAnnotations().catch((error) => {
         let old = this.annotationError;
         this.annotationError.message = error;
@@ -79,59 +86,66 @@
 
     // Fetches new annotations from the server.
     fetchAnnotations: function() {
-      return window.fetch('/api/v1/annotations', {
-        credentials: 'include'
-      }).then(jsonParsePromise).then((req) => {
-        this._annotationsResp = [];
-        this._annotationsResp = req;
-      });
+      return window.fetch('/api/v1/annotations', {credentials: 'include'})
+          .then(jsonParsePromise)
+          .then((req) => {
+            this._annotationsResp = [];
+            this._annotationsResp = req;
+          });
     },
 
-    // Send an annotation change. Also updates the in memory annotation database.
+    // Send an annotation change. Also updates the in memory annotation
+    // database.
     // Returns a promise of the POST request to the server to carry out the
     // annotation change.
     sendAnnotation: function(key, type, change) {
       let data = {
         xsrf_token: this.xsrfToken,
         data: change,
-      }
+      };
 
-      return this.postJSON('/api/v1/annotations/' + encodeURIComponent(key) + '/' + type, change)
-        .then(jsonParsePromise).then(this._postResponse.bind(this));
+      return this
+          .postJSON(
+              '/api/v1/annotations/' + encodeURIComponent(key) + '/' + type,
+              change)
+          .then(jsonParsePromise)
+          .then(this._postResponse.bind(this));
     },
 
     // FIXME: Move to common behavior if other code does POST requests.
     postJSON: function(url, data, options) {
-        options = options || {};
-        options.body = JSON.stringify({
-          xsrf_token: this.xsrfToken,
-          data: data,
-        });
-        options.method = 'POST';
-        options.credentials = 'include';
+      options = options || {};
+      options.body = JSON.stringify({
+        xsrf_token: this.xsrfToken,
+        data: data,
+      });
+      options.method = 'POST';
+      options.credentials = 'include';
       return new Promise((resolve, reject) => {
         window.fetch(url, options).then((value) => {
           if (!value.ok) {
             value.text().then((txt) => {
-              if (!(value.status == 403 && txt.includes("token expired"))) {
+              if (!(value.status == 403 && txt.includes('token expired'))) {
                 reject(txt);
                 return;
               }
 
               // We need to refresh our XSRF token!
-              window.fetch('/api/v1/xsrf_token', {credentials: 'include'}).then((respData) => {
-                return respData.json();
-              }).then((jsonData) => {
-                // Clone options because sinon.spy args from different calls
-                // to window.fetch clobber each other in this scenario.
-                let opts = JSON.parse(JSON.stringify(options));
-                this.xsrfToken = jsonData['token'];
-                opts.body = JSON.stringify({
-                  xsrf_token: this.xsrfToken,
-                  data: data,
-                });
-                window.fetch(url, opts).then(resolve, reject);
-              });
+              window.fetch('/api/v1/xsrf_token', {credentials: 'include'})
+                  .then((respData) => {
+                    return respData.json();
+                  })
+                  .then((jsonData) => {
+                    // Clone options because sinon.spy args from different calls
+                    // to window.fetch clobber each other in this scenario.
+                    let opts = JSON.parse(JSON.stringify(options));
+                    this.xsrfToken = jsonData['token'];
+                    opts.body = JSON.stringify({
+                      xsrf_token: this.xsrfToken,
+                      data: data,
+                    });
+                    window.fetch(url, opts).then(resolve, reject);
+                  });
             });
             return;
           }
@@ -189,18 +203,17 @@
     },
 
     handleAnnotation: function(evt) {
-      this.annotationError.action = "Fetching all annotations";
+      this.annotationError.action = 'Fetching all annotations';
       this.sendAnnotation(
-        evt.target.alert.key, evt.detail.type, evt.detail.change
-      ).then((response) => {
-        this.setLocalStateKey(response.key, {
-          opened: false
-        });
-      }).catch((error) => {
-        let old = this.annotationError;
-        this.annotationError.message = error;
-        this.notifyPath('annotationError.message');
-      });
+              evt.target.alert.key, evt.detail.type, evt.detail.change)
+          .then((response) => {
+            this.setLocalStateKey(response.key, {opened: false});
+          })
+          .catch((error) => {
+            let old = this.annotationError;
+            this.annotationError.message = error;
+            this.notifyPath('annotationError.message');
+          });
     },
 
     handleComment: function(evt) {
@@ -211,9 +224,10 @@
 
     handleLinkBug: function(evt) {
       this._bugModel = evt.target.alert;
-      this.$.fileBugLink.href = 'https://bugs.chromium.org/p/chromium/issues/entry?status=Available&labels='
-        + this.bugQueueLabel + '&summary=' + this._bugModel.title + '&comment='
-        + encodeURIComponent(this._commentForBug(this._bugModel));
+      this.$.fileBugLink.href =
+          'https://bugs.chromium.org/p/chromium/issues/entry?status=Available&labels=' +
+          this.bugQueueLabel + '&summary=' + this._bugModel.title +
+          '&comment=' + encodeURIComponent(this._commentForBug(this._bugModel));
       this._filedBug = false;
       this._bugErrorMessage = '';
       this.$.bugDialog.open();
@@ -231,22 +245,26 @@
     _commentForBug: function(bugModel) {
       let result = bugModel.title + '\n\nType: ' + bugModel.type + '\n\n';
       if (bugModel.extension) {
-        if (bugModel.extension.builders && bugModel.extension.builders.length > 0) {
+        if (bugModel.extension.builders &&
+            bugModel.extension.builders.length > 0) {
           result += 'Builders failed on: ';
           for (let i in bugModel.extension.builders) {
-            result += '\n- ' + bugModel.extension.builders[i].name + ': \n  '
-              + bugModel.extension.builders[i].url;
+            result += '\n- ' + bugModel.extension.builders[i].name + ': \n  ' +
+                bugModel.extension.builders[i].url;
           }
           result += '\n\n';
         }
-        if (bugModel.extension.reasons && bugModel.extension.reasons.length > 0) {
+        if (bugModel.extension.reasons &&
+            bugModel.extension.reasons.length > 0) {
           result += 'Reasons: ';
           for (let i in bugModel.extension.reasons) {
             result += '\n' + bugModel.extension.reasons[i].url;
             if (bugModel.extension.reasons[i].test_names) {
-              result += '\n' + 'Tests:';
+              result += '\n' +
+                  'Tests:';
               if (bugModel.extension.reasons[i].test_names) {
-                result += '\n* ' + bugModel.extension.reasons[i].test_names.join('\n* ');
+                result += '\n* ' +
+                    bugModel.extension.reasons[i].test_names.join('\n* ');
               }
             }
           }
@@ -262,21 +280,18 @@
 
     _saveBug: function() {
       // TODO(add proper error handling)
-      this.sendAnnotation(
-        this._bugModel.key, 'add', {
-          bugs: [this.$.bug.value]
-        }
-      ).then((response) => {
-        this._bugErrorMessage = '';
-        this.$.bug.value = '';
-        this.$.bugDialog.close();
+      this.sendAnnotation(this._bugModel.key, 'add', {bugs: [this.$.bug.value]})
+          .then(
+              (response) => {
+                this._bugErrorMessage = '';
+                this.$.bug.value = '';
+                this.$.bugDialog.close();
 
-        this.setLocalStateKey(response.key, {
-          opened: false
-        });
-      }, (error) => {
-        this._bugErrorMessage = error;
-      });
+                this.setLocalStateKey(response.key, {opened: false});
+              },
+              (error) => {
+                this._bugErrorMessage = error;
+              });
     },
 
     ////////////////////// Snooze ///////////////////////////
@@ -284,19 +299,18 @@
     _snooze: function() {
       // TODO(add proper error handling)
       this.sendAnnotation(
-        this._snoozeModel.key, 'add', {
-          snoozeTime: Date.now() + ONE_MIN_MS * this.$.snoozeTime.value
-        }
-      ).then((response) => {
-        this.$.snoozeTime.value = '';
-        this.$.snoozeDialog.close();
+              this._snoozeModel.key, 'add',
+              {snoozeTime: Date.now() + ONE_MIN_MS * this.$.snoozeTime.value})
+          .then(
+              (response) => {
+                this.$.snoozeTime.value = '';
+                this.$.snoozeDialog.close();
 
-        this.setLocalStateKey(response.key, {
-          opened: false
-        });
-      }, (error) => {
-        this._snoozeErrorMessage = error;
-      });
+                this.setLocalStateKey(response.key, {opened: false});
+              },
+              (error) => {
+                this._snoozeErrorMessage = error;
+              });
     },
 
     ////////////////////// Comments ///////////////////////////
@@ -307,28 +321,27 @@
         this._commentsErrorMessage = 'Comment text cannot be blank!';
         return;
       }
-      this.sendAnnotation(
-        this._commentsModel.key, 'add', {
-          comments: [text],
-        }
-      ).then((response) => {
-        this.$.commentText.value = '';
-        this._commentsErrorMessage = '';
-        this.setLocalStateKey(response.key, {
-          opened: false
-        });
-      }, (error) => {
-        this._commentsErrorMessage = error;
-      });
+      this.sendAnnotation(this._commentsModel.key, 'add', {
+            comments: [text],
+          })
+          .then(
+              (response) => {
+                this.$.commentText.value = '';
+                this._commentsErrorMessage = '';
+                this.setLocalStateKey(response.key, {opened: false});
+              },
+              (error) => {
+                this._commentsErrorMessage = error;
+              });
     },
 
-    _computeCommentsHidden: function (annotation) {
+    _computeCommentsHidden: function(annotation) {
       return !(annotation && annotation.comments);
     },
 
     // This is mostly to make sure the comments in the modal get updated
     // properly if changed.
-    _computeCommentsModelAnnotation: function (annotations, model) {
+    _computeCommentsModelAnnotation: function(annotations, model) {
       if (!annotations || !model) {
         return null;
       }
@@ -343,7 +356,7 @@
       if (!email) {
         return email;
       }
-      let cutoff = email.indexOf("@");
+      let cutoff = email.indexOf('@');
       if (cutoff < 0) {
         return email;
       }
@@ -357,20 +370,18 @@
       return new Date(time).toLocaleString(false, {timeZoneName: 'short'});
     },
 
-    _removeComment: function (evt) {
-      let request = this.sendAnnotation(
-        this._commentsModel.key, 'remove', {
-          comments: [evt.model.index],
-        }
-      );
+    _removeComment: function(evt) {
+      let request = this.sendAnnotation(this._commentsModel.key, 'remove', {
+        comments: [evt.model.index],
+      });
       if (request) {
-        request.then((response) => {
-          this.setLocalStateKey(response.key, {
-            opened: false
-          });
-        }, (error) => {
-          this._commentsErrorMessage = error;
-        });
+        request.then(
+            (response) => {
+              this.setLocalStateKey(response.key, {opened: false});
+            },
+            (error) => {
+              this._commentsErrorMessage = error;
+            });
       }
     },
   })
