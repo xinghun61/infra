4efaa95b9f166bfca836308238326a221069451d
Vadim Shtayura <vadimsh@chromium.org>
2017-01-19 17:25:23 -0800

    Add a dumb recipe to build and test infra.git code at HEAD.
    Will be used as a litmus test for continuous build capabilities of LUCI infra.
    
    R=iannucci@chromium.org, estaab@chromium.org, dnj@chromium.org
    BUG=
    
    Change-Id: I0b7ab784b1b20bd09c76b1b6ada8eea417e2dd3d
    Reviewed-on: https://chromium-review.googlesource.com/430265
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>

---
+++ recipes/recipes/infra_continuous_luci.py
@@ -0,0 +1,46 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Builds and tests infra.git code.
+
+Very dumb for now, with no side effects. Runs continuously on LUCI in both
+staging and prod environments.
+"""
+
+
+DEPS = [
+  'depot_tools/bot_update',
+  'depot_tools/gclient',
+  'recipe_engine/path',
+  'recipe_engine/properties',
+  'recipe_engine/python',
+  'recipe_engine/step',
+]
+
+
+def RunSteps(api):
+  api.gclient.set_config('infra')
+  api.bot_update.ensure_checkout()
+  api.gclient.runhooks()
+
+  with api.step.defer_results():
+    api.python(
+        'infra python tests',
+        'test.py',
+        ['test', '--jobs', 1],
+        cwd=api.path['checkout'])
+
+    api.python(
+        'go third parties',
+        api.path['checkout'].join('go', 'env.py'),
+        ['go', 'version'])
+
+    api.python(
+        'infra go tests',
+        api.path['checkout'].join('go', 'env.py'),
+        ['python', api.path['checkout'].join('go', 'test.py')])
+
+
+def GenTests(api):
+  yield api.test('default')
