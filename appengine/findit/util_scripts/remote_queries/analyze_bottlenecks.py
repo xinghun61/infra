98cb4b47cbb792e7cca89367a7095fcc361fa81b
Roberto Carrillo <robertocn@google.com>
2017-01-27 16:04:48 -0800

    Script to extract bottleneck stats.
    R=chanli@chromium.org, stgao@chromium.org
    BUG=
    
    Change-Id: I150d76e1d8b5f4d07ffdf0103168901d05e1d679
    Reviewed-on: https://chromium-review.googlesource.com/427373
    Reviewed-by: Chan Li <chanli@chromium.org>
    Commit-Queue: Roberto Carrillo <robertocn@chromium.org>

---
+++ appengine/findit/util_scripts/remote_queries/analyze_bottlenecks.py
@@ -0,0 +1,408 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+"""Analyze WfAnalysis entities and their pipelines/tryjobs/swarmjobs.
+
+This works on locally available pickled objects pulled via bottleneck_query.py
+
+The purpose of the analysis this script performs is to locate the parts of the
+process that have the biggest impact in the total turnaround time for successful
+analyses, so that the process can be streamlined.
+"""
+
+import csv
+import datetime
+import json
+import os
+import pickle
+import re
+import sys
+import time
+
+from collections import defaultdict
+import optparse
+import numpy
+
+_APPENGINE_SDK_DIR = os.path.join(os.path.dirname(__file__), os.path.pardir,
+                                  os.path.pardir, os.path.pardir,
+                                  os.path.pardir, os.path.pardir,
+                                  'google_appengine')
+sys.path.insert(1, _APPENGINE_SDK_DIR)
+from google.appengine.ext import ndb
+
+_REMOTE_API_DIR = os.path.join(os.path.dirname(__file__), os.path.pardir)
+sys.path.insert(1, _REMOTE_API_DIR)
+import remote_api
+from model.wf_analysis import WfAnalysis
+
+
+# Global stats
+all_labels = set()
+by_status = defaultdict(int)
+by_failure = defaultdict(int)
+
+
+class _Coalesce(object):
+  """Replace variable elements in event labels.
+
+  Replaces hashes with <hash>, test names with <test>, builder names with
+  <builder>, etc. Also keeps track of all the labels produced via an external
+  set().
+
+  Args:
+    label: A string representing the name of a timed event.
+  Returns:
+    a string with the appropriate replacements.
+  """
+  def __init__(self, options):
+    self.options = options
+
+  def __call__(self, label):
+    # TODO(coalesce buildernames)
+    options = self.options
+    hash_re = '[a-fA-F0-9]{40}'
+
+    result = label
+    # _Coalesce hashes
+    if options.coalesce_hashes:
+      result = re.sub(hash_re, '<hash>', result)
+
+    # _Coalesce test names
+    if options.coalesce_tests:
+      dot_sep = result.split('.')
+      if len(dot_sep) > 1:
+        test_name = dot_sep[1].split()[0]
+        if test_name.endswith('tests'):
+          result = result.replace(test_name, '<tests>')
+    all_labels.add(result)
+    return result
+
+
+def _LoadFilterAndSortRecords(options):
+  loaded_records = pickle.load(open('records.pickle'))
+  sorted_records = []
+  # Parse filter options
+  if options.result_status != 'all':
+    allowed_statuses = map(int, options.result_status.split(','))
+  if options.failure_type != 'all':
+    allowed_failure_types = map(int, options.failure_type.split(','))
+
+  # go over all records
+  for k, v in loaded_records.iteritems():
+    # Filter by Status and FailureType
+    if options.result_status != 'all' and (
+        v['wfa.result_status'] not in allowed_statuses):
+      continue
+    if options.failure_type != 'all' and (
+        v['wfa.build_failure_type'] not in allowed_failure_types):
+      continue
+
+    # Filter by date
+    if options.before_date:
+      cutoff = datetime.datetime(
+          *time.strptime(options.before_date, '%Y-%m-%d')[:6])
+      if v['request_time'] > cutoff:
+        continue
+    sorted_record = []
+    for label, t in v.iteritems():
+      if isinstance(t, datetime.datetime):
+        sorted_record.append((t, label))
+      else:
+        if label == 'wfa.result_status':
+          by_status[t] += 1
+        if label == 'wfa.build_failure_type':
+          by_failure[t] += 1
+    # Sort by ts
+    sorted_record.sort()
+
+    # Filter events that occur before a specific events
+    if options.after_label:
+      result = []
+      copy_all = False
+      for x in sorted_record:
+        if copy_all or x[1] == options.after_label:
+          copy_all = True
+          result.append(x)
+      sorted_record = result
+    sorted_records.append((sorted_record, k))
+  return sorted_records, loaded_records
+
+
+def _AggregateTransitions(options, sorted_records, loaded_records):
+  coalesce = _Coalesce(options)
+  transitions = defaultdict(list)
+  adjacent = defaultdict(int)
+  examples = {}
+  totals = defaultdict(float)
+  for record, k in sorted_records:
+    for i in range(len(record) -1):
+      label_pair = None
+      if record[i][1].endswith('.start'):
+        # Search for the matching .end label
+        label_pair = record[i][1], record[i][1].replace('.start', '.end')
+        transition = tuple(map(coalesce, label_pair))
+        if label_pair[1] in loaded_records[k].keys():
+          start = loaded_records[k][label_pair[0]]
+          end = loaded_records[k][label_pair[1]]
+          interval = (end - start).total_seconds()
+          if interval > 0:
+            # NB: Silently ignoring negative intervals.
+            transitions[transition].append(interval)
+            totals[transition] += interval
+            examples[transition] = k, record
+      neighbor_labels = (record[i][1], record[i + 1][1])
+      transition = tuple(map(coalesce, neighbor_labels))
+      # if x.start and x.end are adjacent)
+      if label_pair == neighbor_labels:
+        adjacent[transition] += 1
+        continue
+      start = record[i][0]
+      end = record[i + 1][0]
+      if (isinstance(start, datetime.datetime) and
+          isinstance(end, datetime.datetime)):
+        interval = (end - start).total_seconds()
+        adjacent[transition] += 1
+        transitions[transition].append(interval)
+        examples[transition] = k, record
+        totals[transition] += interval
+
+  result = []
+  for transition, t in transitions.iteritems():
+    current = {
+        'median': numpy.percentile(t, 50),
+        '90p': numpy.percentile(t, 90),
+        'max': max(t),
+        'count': len(t),
+        'transition': transition,
+        'total_time': totals[transition],
+        'adjacent_count': adjacent[transition],
+    }
+    if options.example:
+      current['example'] = examples[transition]
+    result.append(current)
+
+  if options.output_format == 'text':
+    print 'Processed %d analyses' % len(sorted_records)
+
+  return result
+
+
+def PrintCsv(dict_list):
+  columns = dict_list[0].keys()
+  # de-nest
+  for d in dict_list:
+    _from, _to = d.get('transition', ('', ''))
+    d['transition_from'] = _from
+    d['transition_to'] = _to
+    del(d['transition'])
+  columns.remove('transition')
+  columns = ['transition_from', 'transition_to'] + columns
+  writer = csv.DictWriter(sys.stdout, columns)
+  writer.writeheader()
+  for d in dict_list:
+    writer.writerow(d)
+
+
+def _MaybePrintRow(r, options):
+  """Prints a row to the console if not filtered.
+
+  This display function decides whether to print a row to stdout and returns
+  a bool indicating whether it displayed it."""
+
+  if not options.show_composites and not r['adjacent_count']:
+    return False
+  if options.label_filter and options.label_filter not in ''.join(
+      r['transition']):
+    return False
+  _from, _to = r['transition']
+  if _from.replace('.start', '.end') == _to:
+    r['transition_text'] = _from.replace('.start', '')
+  else:
+    if options.paired_only:
+      return False
+    r['transition_text'] = '%s -> %s' % (_from, _to)
+  r['adjacent_percentage'] = r['adjacent_count'] * 100 / r['count']
+  print ('%(transition_text)s:\n'
+         '\tn: %(count)d, median: %(median)0.2fs, 90p: %(90p)0.2fs, Max: '
+         '%(max)0.2fs, Total: %(total_time)02fs, '
+         'Adj.: %(adjacent_percentage)d%%' % r)
+
+  # Show example analysis time series.
+  if options.example:
+    print r['example'][0]
+    for ts, event in r['example'][1]:
+      print '\t', str(ts), event
+  return True
+
+
+def _ParseOptions():
+  usage = """usage: %prog [options]
+
+Analyze the times of the wf_Analysis objects pickled locally via
+bottleneck_query.py
+
+From each Wf_Analysis object all timestamps are extracted, including associated
+pipelines, swarming tasks and tryjobs.
+
+These records are filtered, coalesced and aggregated as per the options below
+and the summarized data is output as text, json or csv.
+"""
+  parser = optparse.OptionParser(usage=usage)
+
+  # Pre-aggregation filters
+  pre_aggregation = optparse.OptionGroup(parser, 'Pre-aggregation filters')
+
+  pre_aggregation.add_option('-r', '--result_status', default='30', help='Comma'
+                             '-separated list of statuses from findit/model/'
+                             'result_status.py(e.g. 30 for FOUND_UNTRIAGED) or'
+                             ' "all" for any status.')
+  pre_aggregation.add_option('-f', '--failure_type', default='8,16',
+                             help='Comma-separated list of failure types from '
+                             'findit/common/waterfall/failure_type.py (default:'
+                             '"8,16" i.e. Reliable compile and test failures)')
+  pre_aggregation.add_option('-a', '--after_label', help='Only aggregate events'
+                             ' that happen after this label. E.g. request_time.'
+                             ' Used to exclude events occurring before findit'
+                             ' is informed of the failure, for example.')
+
+  pre_aggregation.add_option('-b', '--before_date', help='A date in format '
+                             'YYYY-mm-dd used to filter jobs that are too '
+                             'recent and may be in progress. NB the data'
+                             ' is retrieved using a separate script with its '
+                             'own set of filters, this acts in addition to'
+                             ' those.')
+  parser.add_option_group(pre_aggregation)
+
+  # Coalescing options
+  coalesce_opts = optparse.OptionGroup(parser, 'Coalescing options', 'By '
+                                       'default hashes are converted to <hash> '
+                                       'and test steps are converted to <tests>'
+                                       ' e.g brower_tests and net_unittests are'
+                                       ' both replaced by <tests>.')
+
+  coalesce_opts.add_option('--no_coalesce_tests', action='store_false',
+                           dest='coalesce_tests', default=True, help='Use this'
+                           ' flag to group the times for different tests'
+                           ' separately.')
+  coalesce_opts.add_option('--no_coalesce_hashes', action='store_false',
+                           dest='coalesce_hashes', default=True, help='Use '
+                           'this flag to separate steps by revision.')
+  parser.add_option_group(coalesce_opts)
+
+
+  # Post-aggregation Filters
+  post_aggregation = optparse.OptionGroup(
+      parser, 'Text mode filters', 'Apply the following after the records have'
+      ' been aggregated.')
+  post_aggregation.add_option(
+      '-o', '--sort', type='str', default='-median', help='Sort the results '
+      'by one of the following fields (prepend with - to reverse order): '
+      'median(default), 90p, max, count, transition, total_time, or '
+      'adjacent_count.')
+  post_aggregation.add_option('-t', '--top', type='int', default=100,
+                              help='How many rows to display.')
+  post_aggregation.add_option('-l', '--label_filter', help='Only display '
+                              'transitions containing the given string.')
+  post_aggregation.add_option('-m', '--min_transition_count', type='int',
+                              default=10, help='Exclude transitions that occur '
+                              'fewer than this many times (default: 10)')
+  post_aggregation.add_option('-u', '--show-unpaired', action='store_false',
+                              dest='paired_only', default=True, help='Display '
+                              'transitions not matching the form "x.start -> '
+                              'x.end".')
+  post_aggregation.add_option('-c', '--show_composites', action='store_true',
+                              help='Show transitions of the form "x.start -> '
+                              'x.end" that always contain events in the middle.'
+                              )
+  post_aggregation.add_option(
+      '-x', '--example', action='store_true', help='Display one whole record '
+      'that contains each transition. Useful for debugging.')
+  parser.add_option_group(post_aggregation)
+
+
+  # Display options
+  display_opts = optparse.OptionGroup(parser, 'Output format options', 'Choose'
+                                      ' one of json/csv/text(default)/none.')
+
+  display_opts.add_option('--json', dest='output_format', action='store_const',
+                    const='json', help='Dump data as JSON to STDOUT.')
+  display_opts.add_option('--csv', dest='output_format', action='store_const',
+                          const='csv', help='Produce a comma-separated values '
+                          'file (with header row) and dump to STDOUT.')
+  display_opts.add_option('--text', dest='output_format', action='store_const',
+                          const='text', default='text', help='Apply text mode '
+                          'filters and display results in the console.')
+  display_opts.add_option('--none', dest='output_format', action='store_const',
+                          const='none', help='Do not display results (e.g. if '
+                          'only global stats are of interest)')
+  parser.add_option_group(display_opts)
+
+  # Global stats display
+  #TODO: not supported if json or csv
+  global_stats = optparse.OptionGroup(
+      parser, 'Global stats display', 'Display certain stats regarding all '
+      'locally pickled records if display mode is --text or --none.')
+
+  global_stats.add_option('--all_globals', action='store_true', help='Implies '
+                          'the following:')
+  global_stats.add_option('--status_counts', action='store_true')
+  global_stats.add_option('--failure_counts', action='store_true')
+  global_stats.add_option('--label_count', action='store_true')
+  parser.add_option_group(global_stats)
+
+  options, _ = parser.parse_args()
+
+  # Options validation
+  if options.output_format != ['csv', 'json'] and options.example:
+    options.example = False
+
+  return options
+
+
+def main():
+  options = _ParseOptions()
+
+  # Results
+  aggregated_transitions = _AggregateTransitions(options,
+      *_LoadFilterAndSortRecords(options))
+
+  # Filter out short transitions (< 0.1 seconds) and uncommon transitions.
+  aggregated_transitions = [
+      x for x in aggregated_transitions
+      if x['count'] >= options.min_transition_count and x['median'] > 0.1]
+
+  # Sort transition aggregates
+  key = options.sort
+  reverse = False
+  if key.startswith('-'):
+    key = key[1:]
+    reverse = True
+  aggregated_transitions.sort(key=lambda x: x[key], reverse=reverse)
+
+  if options.output_format == 'text':
+    # assemble <options.top> rows of results
+    result_count = options.top
+    for r in aggregated_transitions:
+      if _MaybePrintRow(r, options):
+        result_count -= 1
+      if not result_count:
+        break
+  elif options.output_format == 'json':
+    print json.dumps(aggregated_transitions, indent=4)
+  elif options.output_format == 'csv':
+    PrintCsv(aggregated_transitions)
+
+  # Show global stats
+  if options.output_format not in ['csv', 'json']:
+    if options.status_counts or options.all_globals:
+      print 'Counts by status:', dict(by_status)
+    if options.failure_counts or options.all_globals:
+      print 'Counts by failure type:', dict(by_failure)
+    if options.label_count or options.all_globals:
+      print len(all_labels), 'different labels found'
+
+
+if __name__ == '__main__':
+  main()
---
+++ appengine/findit/util_scripts/remote_queries/bottleneck_query.py
@@ -0,0 +1,302 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import datetime
+import json
+import os
+import pickle
+import Queue
+import re
+import sys
+import time
+from threading import Thread
+
+_APPENGINE_SDK_DIR = os.path.join(os.path.dirname(__file__), os.path.pardir,
+                                  os.path.pardir, os.path.pardir,
+                                  os.path.pardir, os.path.pardir,
+                                  'google_appengine')
+sys.path.insert(1, _APPENGINE_SDK_DIR)
+from google.appengine.ext import ndb
+
+_REMOTE_API_DIR = os.path.join(os.path.dirname(__file__), os.path.pardir)
+sys.path.insert(1, _REMOTE_API_DIR)
+import remote_api
+
+from gae_libs.http.http_client_appengine import HttpClientAppengine
+from libs import time_util
+from model.wf_analysis import WfAnalysis
+from pipeline_utils.appengine_third_party_pipeline_python_src_pipeline \
+    import pipeline
+
+_THIRD_PARTY_DIR = os.path.join(
+    os.path.dirname(__file__), os.path.pardir, os.path.pardir, 'third_party')
+sys.path.insert(0, _THIRD_PARTY_DIR)
+import google
+google.__path__.insert(0, os.path.join(_THIRD_PARTY_DIR, 'google'))
+
+from handlers import handlers_util
+from waterfall import swarming_util
+
+
+NOW = time_util.GetUTCNow()
+START_DATE, END_DATE = NOW - datetime.timedelta(days=30), NOW
+THREAD_COUNT = 64
+
+# If more than 1199 entities are requested at once, the ndb query.fetch_page()
+# operation will stall.
+MAX_ENTITIES_PER_REQUEST = 1199
+CLIENT_SINGLETON = HttpClientAppengine()
+
+
+def _BigFetch(query, page_size=MAX_ENTITIES_PER_REQUEST):
+  print 'Please wait. Fetching results at a rate of ~400 records per second...'
+  cursor = None
+  is_more = True
+  big_results = []
+
+  while is_more:
+    results, cursor, is_more = query.fetch_page(page_size, start_cursor=cursor)
+    big_results.extend(results)
+
+  print 'Finished fetching results.'
+  return big_results
+
+
+def _FetchAnalyses(start_date, end_date):
+  analyses_query = WfAnalysis.query(
+      WfAnalysis.build_start_time >= start_date,
+      WfAnalysis.build_start_time < end_date)
+
+  return _BigFetch(analyses_query)
+
+
+def _GetPickleFilePath():
+  findit_tmp_dir = os.environ.get('TMP_DIR', os.getcwd())
+  return (os.path.join(findit_tmp_dir, 'analyses.pickle'),
+          os.path.join(findit_tmp_dir, 'records.pickle'))
+
+
+def _SaveAnalyses(analyses, records):
+  pickled_analyses = pickle.dumps(analyses)
+  pickled_records = pickle.dumps(records)
+  analyses_path, records_path = _GetPickleFilePath()
+  with open(analyses_path , 'w') as f:
+    f.write(pickled_analyses)
+  with open(records_path , 'w') as f:
+    f.write(pickled_records)
+
+
+def _LoadAnalyses():
+  analyses_path, records_path = _GetPickleFilePath()
+  with open(analyses_path, 'r') as f:
+    pickled_analyses = f.read()
+  with open(records_path, 'r') as f:
+    pickled_records = f.read()
+  print 'Loading pickled analyses...'
+  analyses = pickle.loads(pickled_analyses)
+  times = [x.build_start_time for x in analyses
+           if hasattr(x, 'build_start_time')]
+  records = pickle.loads(pickled_records)
+  return analyses, min(times), max(times), records
+
+
+def _GetTimesFromPipeline(pid):
+  result = {}
+  suffixes = {}
+  status_tree = pipeline.get_status_tree(pid)
+  for _, child_status in status_tree.get('pipelines', {}).iteritems():
+    cls = child_status['classPath'].split('.')[-1]
+    start = child_status.get('startTimeMs')
+    end = child_status.get('endTimeMs')
+    if start and end:
+      if (cls + '.start' + suffixes.get(cls, '')) in result.keys():
+        old_suffix = suffixes.get(cls, '.0')
+        old_index = int(old_suffix[1:])
+        suffixes[cls] = '.' + str(old_index + 1)
+      result['pl.' + cls + '.start' + suffixes.get(cls, '')] = (
+          _UnknownToDatetime(start))
+      result['pl.' + cls + '.end' + suffixes.get(cls, '')] = (
+          _UnknownToDatetime(end))
+  return result
+
+
+def _GetTimes(q, r):
+  """Obtains times of interest from a WfAnalysis instance.
+
+  This is meant to be run by a worker thread."""
+  while True:
+    a = q.get()
+    result = {
+        'wfa.build_start_time': a.build_start_time,
+        'wfa.request_time': a.request_time,
+        'wfa.start_time': a.start_time,
+        'wfa.end_time': a.end_time,
+        'wfa.updated_time': a.updated_time,
+        'wfa.result_status': a.result_status,
+        'wfa.build_failure_type': a.build_failure_type,
+    }
+    try:
+      tryjobs_times = {}
+      step_to_tryjobs = handlers_util.GetAllTryJobResults(
+          a.master_name, a.builder_name, a.build_number, True)
+      for step in step_to_tryjobs.keys():
+        this_tryjobs = step_to_tryjobs[step]['try_jobs']
+        for job in this_tryjobs:
+          if job.get('try_job_url'):
+            tryjobs_times.setdefault('try.' + step, {})
+            times = _GetTimesFromBuildbot(job['try_job_url'])
+            tryjobs_times['try.' + step].update(times)
+          if job.get('task_url'):
+            tryjobs_times.setdefault('swarm.' + step, {})
+            times = _GetTimesFromSwarming(job['task_url'])
+            tryjobs_times['swarm.' + step].update(times)
+
+      result.update(tryjobs_times)
+      if a.pipeline_status_path:
+        pipeline_root = re.search(r'(?<=root\=)[^&]*', a.pipeline_status_path
+                                  ).group(0)
+        result.update(_GetTimesFromPipeline(pipeline_root))
+      r.put((a.key, result))
+    except Exception, e:
+      print 'Problem with ', a.key, e
+
+    q.task_done()
+
+
+def _GetTimesFromBuildbot(buildbot_url):
+  # TODO(robertocn): Use existing code that talks to milo.
+  # Convert the buildbot url to request to milo.
+  milo_url = ('https://luci-milo.appspot.com/prpc/'
+              'milo.Buildbot/GetBuildbotBuildJSON')
+  # http://build.chromium.org/p/<master>/builders/<builder>/builds/<number>
+  master, _, builder, _, buildnum = str(buildbot_url).split('/')[4:]
+  data = json.dumps({
+      'master': master, 'builder': builder, 'buildNum': buildnum})
+
+  _, response = CLIENT_SINGLETON.Post(
+      milo_url, data, headers={'Content-Type': 'application/json'})
+
+  response_json = response[response.index('{'):]
+  response_data = json.loads(response_json)
+  result = {}
+  for step in response_data['steps']:
+    if 'name' in step and 'times' in step:
+      result[step['name'] + '.start'] = (
+          _UnknownToDatetime(step['times'][0]))
+      result[step['name'] + '.end'] = (
+          _UnknownToDatetime(step['times'][1]))
+  return result
+
+
+def _GetTimesFromSwarming(url):
+  results = {}
+  task_id = url.split('/')[-1]
+  task_result, _ = swarming_util.GetSwarmingTaskResultById(
+      task_id, CLIENT_SINGLETON)
+  for k in task_result.keys():
+    if k.endswith('_ts'):
+      results[k] = _UnknownToDatetime(task_result[k])
+  return results
+
+
+# TODO: Instead of guessing, make the right conversions when retrieving data,
+# including timezone adjustment (i.e. make everything UTC)
+def _UnknownToDatetime(unknown):
+  if isinstance(unknown, basestring):
+    for fmt in ('%Y-%m-%dT%H:%M:%S.%f', '%Y-%m-%dT%H:%M:%S'):
+      try:
+        return datetime.datetime.strptime(unknown,fmt)
+      except ValueError:
+        pass
+  # This is only used to guess whether the time given is in seconds or ms
+  epoch = time.mktime(datetime.datetime(year=2008, month=9, day=2).timetuple())
+  try:
+    ts = float(unknown)
+    if ts > epoch:
+      if ts > epoch * 1000:
+          ts = float(ts)/1000
+    return datetime.datetime.fromtimestamp(ts)
+  except (TypeError, ValueError):
+    # Couldn't cast it, return the original value.
+    pass
+  return unknown
+
+
+def _PrependKeys(prefix, d):
+  new_d = {}
+  for k in d.keys():
+    new_d[prefix + k] = d[k]
+  return new_d
+
+
+def _Denest(d):
+  if isinstance(d, dict):
+    removals = []
+    insertions = {}
+    for k, v in d.iteritems():
+      if isinstance(v, dict):
+        insertions.update(_PrependKeys(k + '.', v))
+        removals.append(k)
+    for k in removals:
+      del(d[k])
+    d.update(insertions)
+  return d
+
+
+# This is hacky, we should make everything UTC as the times are read from the
+# source.
+def _IsUtcLabel(l):
+  parts = l.split('.')
+  return len(parts) == 1 or l.startswith('waterfall.') or (
+      len(parts) == 2 and l.endswith('_ts'))
+
+
+def main():
+  # TODO: add options to limit the date range to fetch
+  # TODO: add options to display summary of fetched info.
+
+  remote_api.EnableRemoteApi(app_id='findit-for-me')
+
+  try:
+    all_analyses, stored_start, stored_end, time_records = _LoadAnalyses()
+  except IOError:
+    all_analyses = _FetchAnalyses(START_DATE, END_DATE)
+    time_records = {}
+    _SaveAnalyses(all_analyses, time_records)
+  else:
+    if stored_start > START_DATE:
+      all_analyses = _FetchAnalyses(START_DATE, stored_start) + all_analyses
+    if END_DATE > stored_end:
+      all_analyses += _FetchAnalyses(stored_end, END_DATE)
+    _SaveAnalyses(all_analyses, time_records)
+  saved_count = len(time_records)
+
+  q = Queue.Queue()
+  r = Queue.Queue()
+
+  for _ in range(THREAD_COUNT):
+    t = Thread(target=_GetTimes, args=(q, r))
+    t.daemon = True
+    t.start()
+
+  for a in all_analyses:
+    if a.key not in time_records.keys():
+      q.put(a)
+
+  while not (q.empty() and r.empty()):
+    key, record = r.get()
+    time_records[key] = _Denest(record)
+    print len(time_records)
+    for k, v in time_records[key].iteritems():
+      if _IsUtcLabel(k) and isinstance(v, datetime.datetime):
+        time_records[key][k] = v - datetime.timedelta(hours=8)
+    if saved_count + THREAD_COUNT < len(time_records):
+      _SaveAnalyses(all_analyses, time_records)
+      saved_count = len(time_records)
+      print 'Saved %d results' % saved_count
+  q.join()
+
+
+if __name__ == '__main__':
+  main()
