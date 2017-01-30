xinghun61/infra

commit 8b596b736ae011c2a698f23aa0be6cffe2f0c436

swarmbucket: fail builds for non-exitent builders
Historically swarmbucket ignores builds for undefined builders. This was
done so we can migrate builders one by one from buildbot to swarmbucket
without changing bucket name.

The problem with this approach is that if there is no buildbot master
polling a bucket and someone misspels a builder name, the build will
hang ignored for 48h until expired.

Eventually we decided that we will always change bucket name from
master.x.y to luci.x.y (because it is not a buildbot master), which
means we can immediately mark a build as failed if it specifies a
non-existent builder. Do that in this CL.

R=vadimsh@chromium.org, estaab@chromium.org
BUG=

Change-Id: Iebd74265c51936e4e229a734fd7290bbf973beb2
Reviewed-on: https://chromium-review.googlesource.com/433520
Reviewed-by: Erik Staab <estaab@chromium.org>
Commit-Queue: Nodir Turakulov <nodir@chromium.org>

appengine/cr-buildbucket/swarming/swarming.py
@@ -107,28 +107,21 @@ def get_task_template_async(canary, canary_required=True):
   raise ndb.Return(json.loads(text) if text else None, canary)
 
 
-@utils.memcache_async(
-  'swarming/is_for_swarming', ['bucket_name', 'builder_name'], time=10 * 60)
+@utils.memcache_async('swarming/is_for_swarming', ['bucket_name'], time=10 * 60)
 @ndb.tasklet
-def _is_for_swarming_async(bucket_name, builder_name):
-  """Returns True if swarming is configured for |builder_name|."""
+def _is_for_swarming_async(bucket_name):
+  """Returns True if swarming is configured for the bucket."""
   _, cfg = yield config.get_bucket_async(bucket_name)
-  if cfg and cfg.swarming:  # pragma: no branch
-    for b in cfg.swarming.builders:
-      if b.name == builder_name:
-        raise ndb.Return(True)
-  raise ndb.Return(False)
+  raise ndb.Return(bool(cfg and cfg.swarming))
 
 
 @ndb.tasklet
 def is_for_swarming_async(build):
-  """Returns True if |build|'s bucket and builder are designed for swarming."""
+  """Returns True if |build|'s bucket are designed for swarming."""
   result = False
   task_template, _ = yield get_task_template_async(False)
-  if task_template and isinstance(build.parameters, dict):  # pragma: no branch
-    builder = build.parameters.get(BUILDER_PARAMETER)
-    if builder:  # pragma: no branch
-      result = yield _is_for_swarming_async(build.bucket, builder)
+  if task_template:  # pragma: no branch
+    result = yield _is_for_swarming_async(build.bucket)
   raise ndb.Return(result)
 
 
@@ -420,18 +413,29 @@ def create_task_async(build):
   """Creates a swarming task for the build and mutates the build.
 
   May be called only if is_for_swarming(build) == True.
+
+  Raises:
+    errors.InvalidInputError if build attribute values are inavlid.
   """
   if build.lease_key:
     raise errors.InvalidInputError(
-      'swarming builders do not support creation of leased builds')
-  builder_name = build.parameters[BUILDER_PARAMETER]
+      'Swarming buckets do not support creation of leased builds')
+  if not build.parameters:
+    raise errors.InvalidInputError(
+        'A build for bucket %r must have parameters' % build.bucket)
+  builder_name = build.parameters.get(BUILDER_PARAMETER)
+  if not isinstance(builder_name, basestring):
+    raise errors.InvalidInputError('Invalid builder name %r' % builder_name)
   project_id, bucket_cfg = yield config.get_bucket_async(build.bucket)
+
   builder_cfg = None
   for b in bucket_cfg.swarming.builders:  # pragma: no branch
     if b.name == builder_name:  # pragma: no branch
       builder_cfg = b
       break
-  assert builder_cfg, 'Builder %s not found' % builder_name
+  if not builder_cfg:
+    raise errors.InvalidInputError(
+        'Builder %r is not defined in bucket %r' % (builder_name, build.bucket))
 
   task = yield create_task_def_async(
       project_id, bucket_cfg.swarming, builder_cfg, build)

appengine/cr-buildbucket/swarming/test/swarming_test.py
@@ -168,7 +168,9 @@ class SwarmingTest(testing.AppengineTestCase):
     self.assertTrue(swarming.is_for_swarming_async(build).get_result())
 
     build.parameters['builder_name'] = 'other'
-    self.assertFalse(swarming.is_for_swarming_async(build).get_result())
+    # This build is still destined for Swarming, even though such builder
+    # does not exist.
+    self.assertTrue(swarming.is_for_swarming_async(build).get_result())
 
   def test_is_for_swarming_no_template(self):
     build = model.Build(
@@ -372,6 +374,22 @@ class SwarmingTest(testing.AppengineTestCase):
     })
     self.assertEqual(
       build.url, 'https://example.com/chromium-swarm.appspot.com/deadbeef')
+
+  def test_create_task_async_bad_request(self):
+    build = model.Build(bucket='bucket')
+
+    with self.assertRaises(errors.InvalidInputError):
+      swarming.create_task_async(build).get_result()
+
+    with self.assertRaises(errors.InvalidInputError):
+      build.parameters = {
+        'builder_name': 'non-existent builder',
+      }
+      swarming.create_task_async(build).get_result()
+
+    with self.assertRaises(errors.InvalidInputError):
+      build.parameters['builder_name'] = 2
+      swarming.create_task_async(build).get_result()
 
   def test_create_task_async_canary_template(self):
     build = model.Build(

appengine/cr-buildbucket/service.py
@@ -110,6 +110,9 @@ def add_async(
 
   Returns:
     A new Build.
+
+  Raises:
+    errors.InvalidInputError: if build creation parameters are invalid.
   """
   if client_operation_id is not None:
     if not isinstance(client_operation_id, basestring):  # pragma: no cover

commit 91dfcba6a7935117c52ea68225db089bad1b311c

swarmbucket: fix is_for_swarming
CL 8b596b736ae011c2a698f23aa0be6cffe2f0c436 introduced a bug:
is_for_swarming returns True for all buckets. Fix that.

R=vadimsh@chromium.org, estaab@chromium.org
BUG=685858

Change-Id: I6172d06ee922d35ece0bedec913ef53b9d04b2d5
Reviewed-on: https://chromium-review.googlesource.com/433469
Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
Commit-Queue: Nodir Turakulov <nodir@chromium.org>

appengine/cr-buildbucket/swarming/swarming.py
@@ -112,7 +112,7 @@ def get_task_template_async(canary, canary_required=True):
 def _is_for_swarming_async(bucket_name):
   """Returns True if swarming is configured for the bucket."""
   _, cfg = yield config.get_bucket_async(bucket_name)
-  raise ndb.Return(bool(cfg and cfg.swarming))
+  raise ndb.Return(bool(cfg and cfg.HasField('swarming')))
 
 
 @ndb.tasklet

appengine/cr-buildbucket/swarming/test/swarming_test.py
@@ -96,9 +96,12 @@ class SwarmingTest(testing.AppengineTestCase):
         ],
       ),
     )
-    self.mock(
-        config, 'get_bucket_async',
-        lambda name: futuristic(('chromium', self.bucket_cfg)))
+    def get_bucket_async(name):
+      if name == 'bucket':
+        return futuristic(('chromium', self.bucket_cfg))
+      else:
+        return futuristic(('chromium', project_config_pb2.Bucket(name=name)))
+    self.mock(config, 'get_bucket_async', get_bucket_async)
 
     self.task_template = {
       'name': 'buildbucket-$bucket-$builder',
@@ -171,6 +174,10 @@ class SwarmingTest(testing.AppengineTestCase):
     # This build is still destined for Swarming, even though such builder
     # does not exist.
     self.assertTrue(swarming.is_for_swarming_async(build).get_result())
+
+    self.assertFalse(
+        swarming.is_for_swarming_async(model.Build(bucket='other'))
+          .get_result())
 
   def test_is_for_swarming_no_template(self):
     build = model.Build(