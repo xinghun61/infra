7bc7d33360e55464c166a6a4fc70a87d26f2f978
Jeff Carpenter <jeffcarp@chromium.org>
2017-02-07 11:15:34 -0800

    Add WPT Export task to chromium.infra.cron
    BUG=689118
    
    Change-Id: I749bcead02e55a3b47bb6519d541186ad7af6872
    Reviewed-on: https://chromium-review.googlesource.com/437763
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Commit-Queue: Quinten Yearsley <qyearsley@chromium.org>

---
+++ recipes/recipes/wpt_export.py
@@ -0,0 +1,56 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Looks for exportable commits and exports them to GitHub.
+
+Runs the web-platform-tests exporter. The exporter expects to be run as a
+recurring job at a short interval. Each iteration, it will do one of two
+things:
+
+1. If an in-flight PR in the WPT repo exists, merge it.
+2. Pull Chromium and WPT, find the earliest exportable commit since the
+   last WPT export, and create a PR on GitHub for it.
+
+More details in the script at third_party/WebKit/Tools/Scripts/wpt-export.
+"""
+
+import contextlib
+
+DEPS = [
+  'depot_tools/bot_update',
+  'depot_tools/gclient',
+  'depot_tools/git',
+  'recipe_engine/json',
+  'recipe_engine/path',
+  'recipe_engine/properties',
+  'recipe_engine/python',
+]
+
+
+def RunSteps(api):
+  api.gclient.set_config('chromium')
+  api.bot_update.ensure_checkout()
+  api.git('config', 'user.name', 'Blink WPT Exporter',
+          name='set git config user.name')
+  api.git('config', 'user.email', 'blink-w3c-test-autoroller@chromium.org',
+          name='set git config user.email')
+
+  script = api.path['checkout'].join('third_party', 'WebKit', 'Tools',
+                                     'Scripts', 'wpt-export')
+  args = [
+    '--dry-run', # TODO(jeffcarp): remove before flight
+    '--github-credentials-json',
+    '/creds/github/wpt-export.json',
+  ]
+  api.python('create PR or merge in-flight PR', script, args)
+
+
+def GenTests(api):
+  yield (
+      api.test('wpt-export') +
+      api.properties(
+          mastername='chromium.infra.cron',
+          buildername='wpt-export',
+          slavename='fake-slave') +
+      api.step_data('create PR or merge in-flight PR'))
