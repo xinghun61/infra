e32be63ae5686dc9ed395ed2529cd07a792a9424
Andrii Shyshkalov <tandrii@chromium.org>
2017-02-21 10:03:51 +0100

    Gerrit demo: Make experimental builder failing.
    TBR=sergiyb@chromium.org
    
    Change-Id: I5e578458ca708a8f93c1eb0203e1826f75356f71
    Reviewed-on: https://chromium-review.googlesource.com/445678
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Reviewed-by: Sergiy Byelozyorov <sergiyb@chromium.org>
    Commit-Queue: Andrii Shyshkalov <tandrii@chromium.org>

--- recipes/recipes/gerrit_cq_test.py
+++ recipes/recipes/gerrit_cq_test.py
@@ -6,6 +6,7 @@ DEPS = [
   'depot_tools/bot_update',
   'depot_tools/gclient',
   'recipe_engine/properties',
+  'recipe_engine/step',
 ]
 
 REPO = 'https://chromium.googlesource.com/playground/gerrit-cq/normal'
@@ -14,10 +15,17 @@ REPO = 'https://chromium.googlesource.com/playground/gerrit-cq/normal'
 def RunSteps(api):
   api.gclient.set_config('gerrit_test_cq_normal')
   api.bot_update.ensure_checkout(patch=True)
+  if 'experimental' in api.properties.get('buildername'):
+    api.step('fail', ['unknown-command', '1'], infra_step=True)
 
 
 def GenTests(api):
   yield (
     api.test('try') +
     api.properties.tryserver(gerrit_project='playground/gerrit-cq/normal')
+  )
+  yield (
+    api.test('try-experimental') +
+    api.properties.tryserver(gerrit_project='playground/gerrit-cq/normal',
+                             buildername='experimental')
   )
