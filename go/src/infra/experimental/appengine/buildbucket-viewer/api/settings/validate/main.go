55d838d421b42986084367e733aa4a6ed13729a4
Dan Jacques <dnj@chromium.org>
2017-02-27 18:43:43 -0800

    Add "buildbucket-viewer" GAE application.
    Add the "buildbucket-viewer" GAE application. This provides a consistent
    framework for a versatile BuildBucket build status viewer. It also
    allows open queries.
    
    This app is fully tied to authentication and configuration services. It
    allows protobufs to be used to define per-project fixed views. It also
    allows open queries to experiment with views. All views/queries have a
    fairly versatile set of parameters that can be applied to define the
    views.
    
    When possible, click-through links to Milo and BuildBucket are
    integrated into the results for easy traversal.
    
    This app is currently experimental and unmaintained. If it is useful, we
    may integrate it as a BuildBucket UI or small standalone app sometime in
    the future.
    
    TBR=estaab@chromium.org
    BUG=None
    TEST=None
    
    Change-Id: I2fe518ca073b8dc262328461eb8b7fa53cc51bf2
    Reviewed-on: https://chromium-review.googlesource.com/409530
    Reviewed-by: Daniel Jacques <dnj@chromium.org>
    Commit-Queue: Daniel Jacques <dnj@chromium.org>

---
+++ go/src/infra/experimental/appengine/buildbucket-viewer/api/settings/validate/main.go
@@ -0,0 +1,44 @@
+// Copyright 2016 The LUCI Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"infra/experimental/appengine/buildbucket-viewer/api/settings"
+
+	"github.com/luci/luci-go/common/errors"
+
+	"github.com/golang/protobuf/proto"
+)
+
+func main() {
+	rv := 0
+	for _, arg := range os.Args[1:] {
+		if err := validateProjectConfig(arg); err != nil {
+			fmt.Fprintf(os.Stderr, "Error validating [%s]: %v", arg, err)
+			rv = 1
+		} else {
+			fmt.Println("Successfully validated:", arg)
+		}
+	}
+	os.Exit(rv)
+}
+
+func validateProjectConfig(path string) error {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return errors.Annotate(err).Reason("failed to read file").D("path", path).Err()
+	}
+
+	var pc settings.ProjectConfig
+	if err := proto.UnmarshalText(string(data), &pc); err != nil {
+		return errors.Annotate(err).Reason("failed to unmarshal protobuf").Err()
+	}
+
+	return nil
+}
---
+++ go/src/infra/experimental/appengine/buildbucket-viewer/frontend/templates.go
@@ -0,0 +1,82 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package frontend
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/luci/luci-go/common/clock"
+	"github.com/luci/luci-go/common/errors"
+	"github.com/luci/luci-go/server/auth"
+	"github.com/luci/luci-go/server/auth/identity"
+	"github.com/luci/luci-go/server/router"
+	"github.com/luci/luci-go/server/templates"
+
+	"github.com/luci/gae/service/info"
+
+	"golang.org/x/net/context"
+)
+
+func getTemplateBundle() *templates.Bundle {
+	return &templates.Bundle{
+		Loader:    templates.FileSystemLoader("templates"),
+		DebugMode: info.IsDevAppServer,
+		FuncMap: map[string]interface{}{
+			"timeToUTCString": func(v time.Time) string {
+				return v.UTC().String()
+			},
+			"timeToEpochSeconds": func(v time.Time) int64 {
+				return v.Unix()
+			},
+		},
+		DefaultTemplate: "bootstrap",
+		DefaultArgs: func(c context.Context) (templates.Args, error) {
+			return templates.Args{
+				"Title":       "BuildBucket Viewer",
+				"IsAnonymous": auth.CurrentIdentity(c) == identity.AnonymousIdentity,
+				"User":        auth.CurrentUser(c),
+				"CurrentTime": clock.Now(c),
+			}, nil
+		},
+	}
+}
+
+func withTemplates() router.Middleware {
+	return templates.WithTemplates(getTemplateBundle())
+}
+
+func getDefaultTemplateArgs(c context.Context, req *http.Request) (templates.Args, error) {
+	// Determine our login redirect. This is our request URL, bound to the current
+	// scheme/host.
+	loginRedirectURL := *req.URL
+	loginRedirectURL.Scheme = ""
+	loginRedirectURL.Host = ""
+	loginRedirectURL.User = nil
+
+	menuClass := map[string]string{}
+	switch req.URL.Path {
+	case "/builds/query":
+		menuClass["Query"] = "active"
+	default:
+		menuClass["Home"] = "active"
+	}
+
+	loginRedirect := loginRedirectURL.String()
+
+	loginURL, err := auth.LoginURL(c, loginRedirect)
+	if err != nil {
+		return nil, errors.Annotate(err).InternalReason("failed to generate login URL").Err()
+	}
+	logoutURL, err := auth.LogoutURL(c, loginRedirect)
+	if err != nil {
+		return nil, errors.Annotate(err).InternalReason("failed to generate logout URL").Err()
+	}
+	return templates.Args{
+		"LoginURL":  loginURL,
+		"LogoutURL": logoutURL,
+		"MenuClass": menuClass,
+	}, nil
+}
