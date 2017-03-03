f0baf7444a2b95f2b43c24ae7a12365aa6e20568
Benjamin Pastene <bpastene@google.com>
2017-03-03 15:08:30 -0800

    android-docker: Fetch depot_tools on container start-up.
    BUG=670879
    
    Change-Id: Ia198036d46aceff9d0ce432678838744aa5683fb
    Reviewed-on: https://chromium-review.googlesource.com/448100
    Commit-Queue: Benjamin Pastene <bpastene@chromium.org>
    Reviewed-by: Elliott Friedman <friedman@chromium.org>

--- docker/android_devices/start_swarm_bot.sh
+++ docker/android_devices/start_swarm_bot.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+#!/bin/bash -x
 
 SWARM_DIR=/b/swarming
 SWARM_URL="https://chromium-swarm.appspot.com/bot_code"
 SWARM_ZIP=swarming_bot.zip
+
+DEPOT_TOOLS_DIR=/b/depot_tools
+DEPOT_TOOLS_URL="https://chromium.googlesource.com/chromium/tools/depot_tools.git"
+DEPOT_TOOLS_REV="da3a29e13e816459234b0b08ed1059300bae46dd"
 
 # Wait until this container has access to a device before starting the bot.
 while [ ! -d /dev/bus/usb ]
@@ -10,6 +14,15 @@ do
   echo "Waiting for an available usb device..."
   sleep 10
 done
+
+# Some chromium tests need depot tools.
+mkdir -p $DEPOT_TOOLS_DIR
+/bin/chown chrome-bot:chrome-bot $DEPOT_TOOLS_DIR
+/bin/su -c "cd $DEPOT_TOOLS_DIR && \
+            /usr/bin/git init && \
+            /usr/bin/git remote add origin $DEPOT_TOOLS_URL ; \
+            /usr/bin/git fetch origin $DEPOT_TOOLS_REV && \
+            /usr/bin/git reset --hard FETCH_HEAD" chrome-bot
 
 mkdir -p $SWARM_DIR
 /bin/chown chrome-bot:chrome-bot $SWARM_DIR
