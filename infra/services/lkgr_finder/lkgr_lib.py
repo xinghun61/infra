b9debb06cfb3dbf13c660b5c006e5aa9cb6c5e2b
Ryan Tseng <hinoka@google.com>
2017-03-06 16:08:25 -0800

    lkgr_roller: Fix typo
    BUG=698374
    
    Change-Id: I83efe347e029dae97f076e2de1e605ce8870e156
    Reviewed-on: https://chromium-review.googlesource.com/450899
    Reviewed-by: Stephen Martinis <martiniss@chromium.org>
    Commit-Queue: Ryan Tseng <hinoka@chromium.org>

--- infra/services/lkgr_finder/lkgr_lib.py
+++ infra/services/lkgr_finder/lkgr_lib.py
@@ -151,7 +151,7 @@ def FetchBuilderJsonFromMilo(master, builder, limit=100,
   }
   if service_account_file:
       credentials = infra_libs.get_signed_jwt_assertion_credentials(
-                service_account_file, scopes=OAUTH_SCOPES)
+                service_account_file, scope=OAUTH_SCOPES)
       credentials.apply(headers)
 
   res = requests.post(
