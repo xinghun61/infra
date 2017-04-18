0d951fe654705c1e87a3f34bd18d6d902935ed5d
Sharu Jiang <katesonia@google.com>
2017-04-18 14:06:31 -0700

    [Culprit-Finder] Split non gae related part in TestCase in gae_libs/ to BaseTestCase in libs/
    Bug:705860
    
    Change-Id: I6869954a1d846d673ed035742dfa02ba051a3231
    Reviewed-on: https://chromium-review.googlesource.com/480965
    Reviewed-by: Chan Li <chanli@chromium.org>
    Commit-Queue: Sharu Jiang <katesonia@chromium.org>

--- appengine/findit/gae_libs/testcase.py
+++ appengine/findit/gae_libs/testcase.py
@@ -6,40 +6,10 @@ import os
 
 from testing_utils import testing
 
-from gae_libs.http.http_client_appengine import HttpClientAppengine
-from libs import time_util
-from libs.gitiles.gitiles_repository import GitilesRepository
-from libs.http import retry_http_client
+from libs.base_testcase import BaseTestCase
 
 
-class MockHttpClient(retry_http_client.RetryHttpClient):  # pragma: no cover.
-
-  def __init__(self, response_for_url=None):
-    super(MockHttpClient, self).__init__()
-    self.response_for_url = response_for_url or {}
-
-  def SetResponseForUrl(self, url, response):
-    self.response_for_url[url] = response
-
-  def GetBackoff(self, *_):
-    """Override to avoid sleep."""
-    return 0
-
-  def _Get(self, url, *_):
-    response = self.response_for_url.get(url)
-    if response is None:
-      return 404, 'Not Found'
-    else:
-      return 200, response
-
-  def _Post(self, *_):
-    pass
-
-  def _Put(self, *_):
-    pass
-
-
-class TestCase(testing.AppengineTestCase):  # pragma: no cover.
+class TestCase(BaseTestCase, testing.AppengineTestCase):  # pragma: no cover.
   # Setup the customized queues.
   taskqueue_stub_root_path = os.path.join(
     os.path.dirname(__file__), os.path.pardir)
@@ -64,20 +34,3 @@ class TestCase(testing.AppengineTestCase):  # pragma: no cover.
       return result
 
     self.mock(pipeline_class, 'run', Mocked_run)
-
-  def MockUTCNow(self, mocked_utcnow):
-    """Mocks utcnow with the given value for testing."""
-    self.mock(time_util, 'GetUTCNow', lambda: mocked_utcnow)
-
-  def MockUTCNowWithTimezone(self, mocked_utcnow):
-    """Mocks utcnow with the given value for testing."""
-    self.mock(time_util, 'GetUTCNowWithTimezone', lambda: mocked_utcnow)
-
-  def GetMockHttpClient(self, response_for_url=None):
-    """Returns mocked http client class."""
-    return MockHttpClient(response_for_url or {})
-
-  def GetMockRepoFactory(self, response_for_url=None):
-    """Returns mocked repository factory."""
-    return GitilesRepository.Factory(
-        self.GetMockHttpClient(response_for_url or {}))
---
+++ appengine/findit/libs/base_testcase.py
@@ -0,0 +1,56 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+from libs import time_util
+from libs.gitiles.gitiles_repository import GitilesRepository
+from libs.http import retry_http_client
+
+
+class MockHttpClient(retry_http_client.RetryHttpClient):  # pragma: no cover.
+
+  def __init__(self, response_for_url=None):
+    super(MockHttpClient, self).__init__()
+    self.response_for_url = response_for_url or {}
+
+  def SetResponseForUrl(self, url, response):
+    self.response_for_url[url] = response
+
+  def GetBackoff(self, *_):
+    """Override to avoid sleep."""
+    return 0
+
+  def _Get(self, url, *_):
+    response = self.response_for_url.get(url)
+    if response is None:
+      return 404, 'Not Found'
+    else:
+      return 200, response
+
+  def _Post(self, *_):
+    pass
+
+  def _Put(self, *_):
+    pass
+
+
+class BaseTestCase(unittest.TestCase):  # pragma: no cover.
+
+  def MockUTCNow(self, mocked_utcnow):
+    """Mocks utcnow with the given value for testing."""
+    self.mock(time_util, 'GetUTCNow', lambda: mocked_utcnow)
+
+  def MockUTCNowWithTimezone(self, mocked_utcnow):
+    """Mocks utcnow with the given value for testing."""
+    self.mock(time_util, 'GetUTCNowWithTimezone', lambda: mocked_utcnow)
+
+  def GetMockHttpClient(self, response_for_url=None):
+    """Returns mocked http client class."""
+    return MockHttpClient(response_for_url or {})
+
+  def GetMockRepoFactory(self, response_for_url=None):
+    """Returns mocked repository factory."""
+    return GitilesRepository.Factory(
+        self.GetMockHttpClient(response_for_url or {}))
