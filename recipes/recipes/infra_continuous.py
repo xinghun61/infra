5bfc9b110cdf9a14b861770190234f2f901b3ddf
Dan Jacques <dnj@chromium.org>
2017-05-12 10:24:17 -0700

    infra_continuous: Use "venv" instead of explicit
    Now that the Python recipe module takes a "venv" argument, use that to
    invoke "vpython" instead of explicitly.
    
    BUG=None
    TEST=simulation
    R=iannucci@chromium.org
    
    Change-Id: Ie217fd4d3691749bba149be114bd956091ac5424
    Reviewed-on: https://chromium-review.googlesource.com/504931
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Daniel Jacques <dnj@chromium.org>

--- recipes/recipes/infra_continuous.py
+++ recipes/recipes/infra_continuous.py
@@ -150,12 +150,10 @@ def RunSteps(api, mastername, buildername, buildnumber):
 
       # Run Glyco tests only on public Linux\Mac CI.
       if project_name == 'infra' and not api.platform.is_win:
-        api.step(
+        api.python(
             'Glyco tests',
-            [
-              'vpython',
-              api.path['checkout'].join('glyco', 'tests', 'run_all_tests.py'),
-            ])
+            api.path['checkout'].join('glyco', 'tests', 'run_all_tests.py'),
+            venv=True)
 
     # This downloads Go third parties, so that the next step doesn't have junk
     # output in it.
