c05d7447ff2b1afdd180eb38cfaaf3ceff364163
Tiff Zhang <zhangtiff@google.com>
2017-05-12 17:50:18 -0700

    SoM: Polish swarming bot headers, simplify CSS, remove animation from iron-collapse.
    Bug:718129
    
    Change-Id: I1dbe49586a571c58156b918a189e2d78a51f1e56
    Reviewed-on: https://chromium-review.googlesource.com/505218
    Reviewed-by: Sean McCullough <seanmccullough@chromium.org>
    Commit-Queue: Tiffany Zhang <zhangtiff@chromium.org>

--- go/src/infra/appengine/sheriff-o-matic/elements/som-swarming-bots/som-swarming-bots.js
+++ go/src/infra/appengine/sheriff-o-matic/elements/som-swarming-bots/som-swarming-bots.js
@@ -26,18 +26,53 @@
         computed: '_computeHideBots(bots.quarantined)',
         value: true,
       },
+      _toggleDeadIcon: {
+        type: String,
+        value: 'remove',
+      },
+      _toggleQuarantinedIcon: {
+        type: String,
+        value: 'remove',
+      },
     },
 
     toggleDead: function() {
       this.$.deadBotsList.toggle();
+
+      this._toggleDeadIcon = this._computeIcon(this.$.deadBotsList.opened);
     },
 
     toggleQuarantined: function() {
       this.$.quarantinedBotsList.toggle();
+
+      this._toggleQuarantinedIcon = this._computeIcon(
+        this.$.quarantinedBotsList.opened);
     },
 
     _computeHideBots: function(bots) {
       return !bots || bots.length == 0;
+    },
+
+    _collapseAll: function() {
+      this.$.deadBotsList.opened = false;
+      this.$.quarantinedBotsList.opened = false;
+
+      this._toggleDeadIcon = this._computeIcon(this.$.deadBotsList.opened);
+      this._toggleQuarantinedIcon = this._computeIcon(
+        this.$.quarantinedBotsList.opened);
+    },
+
+    _expandAll: function() {
+      this.$.deadBotsList.opened = true;
+      this.$.quarantinedBotsList.opened = true;
+
+      this._toggleDeadIcon = this._computeIcon(this.$.deadBotsList.opened);
+      this._toggleQuarantinedIcon = this._computeIcon(
+        this.$.quarantinedBotsList.opened);
+    },
+
+    _computeIcon(opened) {
+      return opened ? 'remove' : 'add';
     },
   });
 })();
