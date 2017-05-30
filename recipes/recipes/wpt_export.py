e422b2f718bed3136de093e3f124ad6b5fd93768
Jeff Carpenter <jeffcarp@chromium.org>
2017-05-30 19:05:54 +0000

    [WPT Export] Set --global for git config
    Since we're making commits in /tmp/wpt but git config is only called
    in the Chromium directory, this results in git being unable to find
    user.name and user.email when the exporter is creating a local commit.
    
    Bug: 725959
    Change-Id: Icf64375c6e1dbe39f744e5f8b87aa48aff6d6876
    Reviewed-on: https://chromium-review.googlesource.com/518026
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Commit-Queue: Jeff Carpenter <jeffcarp@chromium.org>

--- recipes/recipes/wpt_export.py
+++ recipes/recipes/wpt_export.py
@@ -32,9 +32,10 @@ DEPS = [
 def RunSteps(api):
   api.gclient.set_config('chromium')
   api.bot_update.ensure_checkout()
-  api.git('config', 'user.name', 'Chromium WPT Sync',
+  api.git('config', '--global', 'user.name', 'Chromium WPT Sync',
           name='set git config user.name')
-  api.git('config', 'user.email', 'blink-w3c-test-autoroller@chromium.org',
+  api.git('config', '--global', 'user.email',
+          'blink-w3c-test-autoroller@chromium.org',
           name='set git config user.email')
 
   script = api.path['checkout'].join('third_party', 'WebKit', 'Tools',
