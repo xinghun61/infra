xinghun61/infra

commit f3d37c5bee642fdea02905cb395765187707f698

Update protobuf
Change-Id: I92bf5b9f549afba7251a342d458a27df113470f4
Reviewed-on: https://chromium-review.googlesource.com/518464
Reviewed-by: Daniel Jacques <dnj@chromium.org>
Reviewed-by: Sergey Berezin <sergeyberezin@chromium.org>
Commit-Queue: Katie Thomas <katthomas@google.com>

appengine/cr-buildbucket/appengine_config.py
@@ -2,5 +2,18 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
+
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# AppEngine supplies its own "google" domain. Add our VirtualEnv's "google"
+# domain to the list of permissible "google" package paths.
+import google
+google.__path__.append(os.path.join(
+    APP_DIR, 'components', 'third_party', 'protobuf'))
+
+# Assert that "google.protobuf" imports.
+import google.protobuf
+
 components_ereporter2_RECIPIENTS_AUTH_GROUP = 'buildbucket-ereporter2-reports'
 components_ereporter2_VIEWERS_AUTH_GROUP = 'buildbucket-ereporter2-viewers'

appengine/chrome_infra_packages/appengine_config.py
@@ -5,3 +5,16 @@
 """Custom UI tweaks for components.auth module."""
 
 components_auth_UI_APP_NAME = 'CIPD'
+
+import os
+
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# AppEngine supplies its own "google" domain. Add our VirtualEnv's "google"
+# domain to the list of permissible "google" package paths.
+import google
+google.__path__.append(os.path.join(
+    APP_DIR, 'components', 'third_party', 'protobuf'))
+
+# Assert that "google.protobuf" imports.
+import google.protobuf

appengine/cr-buildbucket/main.py
@@ -2,14 +2,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os
-
-APP_DIR = os.path.dirname(os.path.abspath(__file__))
-import google
-
-google.__path__.insert(
-    0, os.path.join(APP_DIR, 'components', 'third_party', 'protobuf', 'google'))
-
 from components import config
 from components import ereporter2
 from components import utils

bootstrap/deps.pyl
@@ -158,10 +158,10 @@
     'rev': '496f40dca9a47c0f1dfe0cd841256485708c8442',
   },
   'protobuf': {
-    'version': '2.6.0',
+    'version': '3.3.0',
     'build': '0',
     'repo': 'external/github.com/google/protobuf',
-    'rev': '629a556879cc84e0f52546f0484b65b72ce44fe8',
+    'rev': 'a6189acd18b00611c1dc7042299ad75486f08a1a',
   },
   'pyasn1': {
     'version': '0.1.7',

commit ce86b096dc556b656ed8f6efdf999c8e378df9c0

buildbucket: fix protobuf path
Commit f3d37c5bee642fdea02905cb395765187707f698 broke protobuf path.
It replaced components/third_party/protobuf/google with
components/third_party/protobuf which does not have "protobuf" module.
Fix that.

Also move sys.path manipulation to apps.py, which is the entry point of
the application.

R=katthomas@google.com

Bug:
Change-Id: I6d0c2b47c42a03bbb4de33d2460830fe21a578d5
Reviewed-on: https://chromium-review.googlesource.com/528459
Commit-Queue: Nodir Turakulov <nodir@chromium.org>
Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>

appengine/cr-buildbucket/appengine_config.py
@@ -2,18 +2,5 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os
-
-APP_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# AppEngine supplies its own "google" domain. Add our VirtualEnv's "google"
-# domain to the list of permissible "google" package paths.
-import google
-google.__path__.append(os.path.join(
-    APP_DIR, 'components', 'third_party', 'protobuf'))
-
-# Assert that "google.protobuf" imports.
-import google.protobuf
-
 components_ereporter2_RECIPIENTS_AUTH_GROUP = 'buildbucket-ereporter2-reports'
 components_ereporter2_VIEWERS_AUTH_GROUP = 'buildbucket-ereporter2-viewers'

appengine/cr-buildbucket/apps.py
@@ -8,6 +8,12 @@ Function 'frontend.initialize' must be called from a separate module
 not imported in tests.
 """
 
+from components import utils
+utils.fix_protobuf_package()
+
+# Assert that "google.protobuf" imports.
+import google.protobuf
+
 import main
 
 html, endpoints, backend = main.initialize()