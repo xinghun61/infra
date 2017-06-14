bb62539e38b9ca0857e746a82efbd1a54680590f
Robert Iannucci <iannucci@chromium.org>
2017-06-13 17:55:17 -0700

    [led] add hack to set luci_project.
    This allows milo's embedded veiw inside swarming to render correctly.
    
    R=dnj@chromium.org, nodir@chromium.org
    
    Bug: 662654
    Change-Id: Icd8af07d9fb32580065e59931650b34a70effc8a
    Reviewed-on: https://chromium-review.googlesource.com/534659
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

--- go/src/infra/tools/led/cmd_get_builder.go
+++ go/src/infra/tools/led/cmd_get_builder.go
@@ -128,6 +128,18 @@ func (c *cmdGetBuilder) grabBuilderDefinition(ctx context.Context, bucket, build
 	if strings.Contains(c.bbHost, "-dev.") {
 		jd.SwarmingHostname = "chromium-swarm-dev.appspot.com"
 	}
+
+	// TODO(iannucci): this is a hack too; MILO should do real project
+	// authentication. This is grabbing X from *.X.*
+	bucketChunks := strings.Split(bucket, ".")
+	if len(bucketChunks) > 1 {
+		if bucketChunks[0] != "luci" {
+			logging.Warningf(ctx, "unsupported bucket %q: not setting luci_project", bucket)
+		} else {
+			jd.S.SwarmingTask.Tags = append(jd.S.SwarmingTask.Tags,
+				"luci_project:"+bucketChunks[1])
+		}
+	}
 	return jd, nil
 }
 
