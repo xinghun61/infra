f75d13ca84a548a0184b63294b9fc6640aa3698e
Roberto Carrillo <robertocn@google.com>
2017-07-05 15:34:46 -0700

    [Findit] Compare recent swarmbucket tryjobs vs buildbot.
    This change retrieves recent tryjobs done on swarmbucket and matching
    runs of the tryjob run on buildbot.
    
    A later CL will provide a user interface to re-run the jobs that do
    not have a match on buildbot for the purpose of comparing their
    performance.
    
    Bug:
    Change-Id: Ib1f802abb67e2eaa03385d8fdad72ead91886d5e
    Reviewed-on: https://chromium-review.googlesource.com/554061
    Reviewed-by: Chan Li <chanli@chromium.org>
    Commit-Queue: Roberto Carrillo <robertocn@chromium.org>

---
+++ appengine/findit/handlers/swarmbucket_performance.py
@@ -0,0 +1,167 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Handler for comparing the performance of tryjobs on swarming-backed bots."""
+
+import logging
+
+from datetime import datetime
+from datetime import timedelta
+
+from gae_libs.handlers.base_handler import BaseHandler
+from gae_libs.handlers.base_handler import Permission
+from libs import time_util
+from model.flake.flake_try_job_data import FlakeTryJobData
+from model.wf_config import FinditConfig
+from model.wf_try_job_data import WfTryJobData
+
+
+def _GetStartEndDates(start, end):
+  """Return the specified dates if both are correctly formatted.
+
+  Otherwise use the last 30 days as the interval.
+  """
+  if start and end:
+    try:
+      start = datetime.strptime(start, '%Y-%m-%d')
+      end = datetime.strptime(end, '%Y-%m-%d')
+      return min(start, end), max(start, end)
+    except (ValueError, TypeError):
+      pass
+  default_end = time_util.GetUTCNow()
+  default_start = default_end - timedelta(days=30)
+  return default_start, default_end
+
+
+def _GetSwarmbucketBuilders():
+  """Find bots that are available on swarming and buildbot.
+
+  Returns:
+    A dictionary that maps swarming builders like
+    'luci.chromium.try/linux_variable' to buildbot builders like
+    'tryserver.chromium.linux/linux_variable'
+  """
+  results = {}
+  all_bots = FinditConfig().Get().builders_to_trybots
+  for waterfall_master in all_bots:
+    for waterfall_builder in all_bots[waterfall_master]:
+      bot = all_bots[waterfall_master][waterfall_builder]
+      swarming_master = bot.get('swarmbucket_mastername')
+      swarming_bot = bot.get('swarmbucket_trybot')
+      buildbot_master = bot.get('mastername')
+      buildbot_bot = bot.get('waterfall_trybot')
+      if swarming_master and swarming_bot and buildbot_master and buildbot_bot:
+        results['%s/%s' % (swarming_master,
+                           swarming_bot)] = '%s/%s' % (buildbot_master,
+                                                       buildbot_bot)
+  return results
+
+
+def _GetBotFromBuildbucketResponse(response):
+  #TODO(move this to buildbucket_client.py or where appropriate.
+  master = response['bucket']
+  master = master[len('master.'):] if master.startswith('master.') else master
+  for tag in response['tags']:
+    if ':' in tag:
+      k, v = tag.split(':', 1)
+      if k == 'builder':
+        return '%s/%s' % (master, v)
+  logging.exception('Buildbucket response does not specify a builder tag')
+  return None
+
+
+def _FindRecentSwarmbucketJobs(start, end, builders):
+  start_date, end_date = _GetStartEndDates(start, end)
+
+  wf_try_job_query = WfTryJobData.query(WfTryJobData.created_time >= start_date,
+                                        WfTryJobData.created_time < end_date)
+  flake_try_job_query = FlakeTryJobData.query(
+      FlakeTryJobData.created_time >= start_date,
+      FlakeTryJobData.created_time < end_date)
+  try_job_data_list = wf_try_job_query.fetch() + flake_try_job_query.fetch()
+
+  # Find recent swarmbucket tryjobs.
+  relevant_try_job_data = []
+  for try_job_data in try_job_data_list:
+    this_builder = _GetBotFromBuildbucketResponse(
+        try_job_data.last_buildbucket_response)
+    if this_builder in builders:
+      relevant_try_job_data.append({
+          'try_job_data': try_job_data,
+          'builder': this_builder
+      })
+  return relevant_try_job_data
+
+
+def _FindJobPairs(recent_jobs, swarmbucket_builders):
+  """Search the try job entity for runs of the same job on buildbot.
+
+  Args:
+    recent_jobs (list): A list of try_job_data entities for swarmbucket runs.
+
+  Returns:
+    A list of pairs, where the first item of each pair will be the try_job_data
+    entity taken from the input, and the second will be the try_job_data entity
+    for the same job but run on buildbot, or None if no such job exists.
+  """
+  results = []
+  for item in recent_jobs:
+    try_job_data = item['try_job_data']
+    builder = item['builder']
+    matching_job_found = False
+    try_job = try_job_data.try_job_key.get()
+    for build_id in try_job.try_job_ids:
+      other_try_job_data = WfTryJobData.Get(build_id)
+      other_builder = _GetBotFromBuildbucketResponse(
+          other_try_job_data.last_buildbucket_response)
+      if builder and swarmbucket_builders[builder] == other_builder:
+        results.append((try_job_data, other_try_job_data))
+        matching_job_found = True
+        break
+
+    if not matching_job_found:
+      results.append((try_job_data, None))
+  return results
+
+
+def _FormatRows(pairs):
+  result = []
+  for pair in pairs:
+    row_data = {}
+    swarmbucket_job, buildbot_job = pair
+    row_data['swarmbucket_builder'] = _GetBotFromBuildbucketResponse(
+        swarmbucket_job.last_buildbucket_response)
+    row_data['swarmbucket_tryjob_id'] = swarmbucket_job.key.pairs()[0][1]
+    row_data['swarmbucket_try_job_url'] = swarmbucket_job.try_job_url
+    row_data['swarmbucket_completion_date'] = time_util.FormatDatetime(
+        swarmbucket_job.end_time)
+    if not swarmbucket_job.error_code and swarmbucket_job.end_time:
+      row_data['swarmbucket_swarm_run_time'] = (
+          swarmbucket_job.end_time -
+          swarmbucket_job.start_time).total_seconds()
+    if buildbot_job:
+      row_data['buildbot_builder'] = _GetBotFromBuildbucketResponse(
+          buildbot_job.last_buildbucket_response)
+      row_data['buildbot_tryjob_id'] = buildbot_job.key.pairs()[0][1]
+      row_data['buildbot_try_job_url'] = buildbot_job.try_job_url
+      row_data['buildbot_completion_date'] = time_util.FormatDatetime(
+          buildbot_job.end_time)
+      if not buildbot_job.error_code and buildbot_job.end_time:
+        row_data['buildbot_swarm_run_time'] = (
+            buildbot_job.end_time - buildbot_job.start_time).total_seconds()
+    result.append(row_data)
+  return result
+
+
+class SwarmbucketPerformance(BaseHandler):
+  PERMISSION_LEVEL = Permission.ADMIN
+
+  def HandleGet(self):
+    swarmbucket_builders = _GetSwarmbucketBuilders()
+    recent_jobs = _FindRecentSwarmbucketJobs(
+        self.request.get('start_date'),
+        self.request.get('end_date'), swarmbucket_builders)
+
+    rows = _FormatRows(_FindJobPairs(recent_jobs, swarmbucket_builders))
+
+    return {'data': rows}
---
+++ appengine/findit/handlers/test/swarmbucket_performance_test.py
@@ -0,0 +1,278 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import datetime
+import mock
+import webapp2
+
+from testing_utils import testing
+
+from libs import time_util
+from model.wf_config import FinditConfig
+from model.wf_try_job import WfTryJob
+from model.wf_try_job_data import WfTryJobData
+from handlers import swarmbucket_performance
+
+BUILDER_MAP = {
+    'master': {
+        'builder1': {
+            'mastername': 'chromium',
+            'waterfall_trybot': 'builder1',
+        },
+        'builder2': {
+            'swarmbucket_mastername': 'luci.chromium',
+            'swarmbucket_trybot': 'swarm_builder2',
+        },
+        'builder3': {
+            'mastername': 'chromium',
+            'waterfall_trybot': 'builder3',
+            'swarmbucket_mastername': 'luci.chromium',
+            'swarmbucket_trybot': 'swarm_builder3',
+        },
+    }
+}
+
+
+def _GenFakeBuildbucketResponse(master, builder):
+  """Make a response object to trick _GetBotFromBuildbucketResponse."""
+  result = {'bucket': master, 'tags': ['mock:Value', 'builder:' + builder]}
+  return result
+
+
+class SwarmbucketPerformanceTest(testing.AppengineTestCase):
+  masDiff = None
+  app_module = webapp2.WSGIApplication(
+      [
+          ('/swarmbucket_performance',
+           swarmbucket_performance.SwarmbucketPerformance),
+      ],
+      debug=True)
+
+  @mock.patch.object(
+      swarmbucket_performance,
+      "_GetStartEndDates",
+      return_value=(datetime.datetime(2017, 1, 1, 0, 0, 0), datetime.datetime(
+          2017, 1, 31, 0, 0, 0)))
+  def testFindRecentSwarmbucketJobs(self, *_):
+    builders = {'sm/sb': 'tm/tb'}
+    irrelevant = WfTryJob.Create('m', 'b2', 1)
+    irrelevant_run_1 = WfTryJobData.Create('ir1')
+    irrelevant_run_1.last_buildbucket_response = _GenFakeBuildbucketResponse(
+        'sm', 'sb2')
+    irrelevant_run_1.created_time = datetime.datetime(2017, 1, 2, 0, 0, 0)
+    irrelevant_run_1.try_job_key = irrelevant.key
+    irrelevant_run_1.put()
+    irrelevant.try_job_ids.append('ir1')
+    irrelevant.put()
+
+    relevant = WfTryJob.Create('m', 'b', 1)
+    swarmbucket_run_1 = WfTryJobData.Create('sbr1')
+    swarmbucket_run_1.last_buildbucket_response = _GenFakeBuildbucketResponse(
+        'sm', 'sb')
+    swarmbucket_run_1.created_time = datetime.datetime(2017, 1, 2, 0, 0, 0)
+    swarmbucket_run_1.try_job_key = relevant.key
+    swarmbucket_run_1.put()
+    relevant.try_job_ids.append('sbr1')
+    relevant.put()
+
+    self.assertEqual([{
+        'try_job_data': swarmbucket_run_1,
+        'builder': 'sm/sb'
+    }], swarmbucket_performance._FindRecentSwarmbucketJobs(
+        None, None, builders))
+
+  def testFindJobPairs(self):
+    builders = {'sm/sb': 'tm/tb'}
+    # We need one that has a match and one that doesnt
+    # TryJobA has [swarmbucket, swarmbucket, buildbot]
+    tryjob_a = WfTryJob.Create('m', 'b2', 1)
+    tryjob_a_run_1 = WfTryJobData.Create('tar1')
+    tryjob_a_run_1.last_buildbucket_response = _GenFakeBuildbucketResponse(
+        'sm', 'sb')
+    tryjob_a_run_1.created_time = datetime.datetime(2017, 1, 2, 0, 0, 0)
+    tryjob_a_run_1.try_job_key = tryjob_a.key
+    tryjob_a_run_1.put()
+    tryjob_a.try_job_ids.append('tar1')
+    tryjob_a_run_2 = WfTryJobData.Create('tar2')
+    tryjob_a_run_2.last_buildbucket_response = _GenFakeBuildbucketResponse(
+        'sm', 'sb')
+    tryjob_a_run_2.created_time = datetime.datetime(2017, 1, 2, 1, 0, 0)
+    tryjob_a_run_2.try_job_key = tryjob_a.key
+    tryjob_a_run_2.put()
+    tryjob_a.try_job_ids.append('tar2')
+    tryjob_a_run_3 = WfTryJobData.Create('tar3')
+    tryjob_a_run_3.last_buildbucket_response = _GenFakeBuildbucketResponse(
+        'tm', 'tb')
+    tryjob_a_run_3.created_time = datetime.datetime(2017, 1, 2, 3, 0, 0)
+    tryjob_a_run_3.try_job_key = tryjob_a.key
+    tryjob_a_run_3.put()
+    tryjob_a.try_job_ids.append('tar3')
+    tryjob_a.put()
+
+    # TryjobB has [swarmbucket only]
+    tryjob_b = WfTryJob.Create('m', 'b3', 1)
+    tryjob_b_run_1 = WfTryJobData.Create('tbr1')
+    tryjob_b_run_1.last_buildbucket_response = _GenFakeBuildbucketResponse(
+        'sm', 'sb')
+    tryjob_b_run_1.created_time = datetime.datetime(2017, 1, 2, 0, 0, 0)
+    tryjob_b_run_1.try_job_key = tryjob_b.key
+    tryjob_b_run_1.put()
+    tryjob_b.try_job_ids.append('tbr1')
+    tryjob_b.put()
+
+    self.assertEqual([(tryjob_a_run_1, tryjob_a_run_3), (tryjob_b_run_1, None)],
+                     swarmbucket_performance._FindJobPairs([{
+                         'try_job_data': tryjob_a_run_1,
+                         'builder': 'sm/sb'
+                     }, {
+                         'try_job_data': tryjob_b_run_1,
+                         'builder': 'sm/sb'
+                     }], builders))
+
+  def testGetBotFromBuildbucketReponse(self):
+    self.assertEqual(
+        'luci.chromium.mac/mac_variable',
+        swarmbucket_performance._GetBotFromBuildbucketResponse({
+            'bucket': 'luci.chromium.mac',
+            'tags': ['nocolontag', 'builder:mac_variable', 'other:tag']
+        }))
+    self.assertIsNone(
+        swarmbucket_performance._GetBotFromBuildbucketResponse({
+            'bucket': 'luci.chromium.mac',
+            'tags': ['other:tag']
+        }))
+    with self.assertRaises(Exception):
+      swarmbucket_performance._GetBotFromBuildbucketResponse({
+          'bucket': 'luci.chromium.mac'
+      })
+
+  @mock.patch.object(FinditConfig, 'builders_to_trybots', BUILDER_MAP)
+  def testGetSwarmbucketBuilders(self):
+    self.assertEqual({
+        'luci.chromium/swarm_builder3': 'chromium/builder3'
+    }, swarmbucket_performance._GetSwarmbucketBuilders())
+
+  def testGetStartEndDates(self):
+    # Normal
+    start, end = swarmbucket_performance._GetStartEndDates(
+        '2017-01-01', '2017-02-01')
+    self.assertEqual(datetime.datetime(2017, 1, 1, 0, 0, 0), start)
+    self.assertEqual(datetime.datetime(2017, 2, 1, 0, 0, 0), end)
+
+    # Reversed
+    start, end = swarmbucket_performance._GetStartEndDates(
+        '2017-01-01', '2017-02-01')
+    self.assertEqual(datetime.datetime(2017, 1, 1, 0, 0, 0), start)
+    self.assertEqual(datetime.datetime(2017, 2, 1, 0, 0, 0), end)
+
+    # The following sub cases should return the default (last 30 days)
+    with mock.patch.object(
+        time_util,
+        "GetUTCNow",
+        return_value=datetime.datetime(2017, 1, 31, 0, 0, 0)):
+      # Bad format
+      start, end = swarmbucket_performance._GetStartEndDates('2017-01-01', '1')
+      self.assertEqual(datetime.datetime(2017, 1, 1, 0, 0, 0), start)
+      self.assertEqual(datetime.datetime(2017, 1, 31, 0, 0, 0), end)
+
+      # One date missing
+      start, end = swarmbucket_performance._GetStartEndDates(None, '2017-01-01')
+      self.assertEqual(datetime.datetime(2017, 1, 1, 0, 0, 0), start)
+      self.assertEqual(datetime.datetime(2017, 1, 31, 0, 0, 0), end)
+
+      # Neither date provided
+      start, end = swarmbucket_performance._GetStartEndDates(None, None)
+      self.assertEqual(datetime.datetime(2017, 1, 1, 0, 0, 0), start)
+      self.assertEqual(datetime.datetime(2017, 1, 31, 0, 0, 0), end)
+
+  @mock.patch.object(FinditConfig, 'builders_to_trybots', BUILDER_MAP)
+  @mock.patch.object(
+      swarmbucket_performance,
+      "_GetStartEndDates",
+      return_value=(datetime.datetime(2017, 1, 1, 0, 0, 0), datetime.datetime(
+          2017, 1, 31, 0, 0, 0)))
+  def testHandler(self, *_):
+
+    def _addRun(try_job_entity,
+                t_master,
+                t_builder,
+                run_id,
+                created,
+                start,
+                end,
+                error=False):
+      base_time = datetime.datetime(2017, 1, 1, 0, 0, 0)
+      run = WfTryJobData.Create(run_id)
+      run.last_buildbucket_response = _GenFakeBuildbucketResponse(
+          t_master, t_builder)
+      run.created_time = base_time + datetime.timedelta(minutes=created)
+      run.start_time = base_time + datetime.timedelta(minutes=start)
+      run.end_time = base_time + datetime.timedelta(minutes=end)
+      if error:
+        run.error_code = 1
+        run.error = 'Unknown error'
+      run.try_job_key = try_job_entity.key
+      run.try_job_url = 'https://fake.url/' + run_id
+      run.put()
+      try_job_entity.try_job_ids.append(run_id)
+      try_job_entity.put()
+
+    swarmbucket_no_buildbot = WfTryJob.Create('luci.chromium', 'swarm_builder3',
+                                              1)
+    _addRun(swarmbucket_no_buildbot, 'luci.chromium', 'swarm_builder3', 'sbr1',
+            100, 110, 120)
+    _addRun(swarmbucket_no_buildbot, 'luci.chromium', 'swarm_builder3', 'sbr2',
+            100, 110, 120, True)
+
+    swarmbucket_with_buildbot = WfTryJob.Create('luci.chromium',
+                                                'swarm_builder3', 2)
+    _addRun(swarmbucket_with_buildbot, 'luci.chromium', 'swarm_builder3',
+            'bb_sbr1', 100, 110, 120)
+    _addRun(swarmbucket_with_buildbot, 'chromium', 'builder3', 'bb_bbr1', 100,
+            110, 120)
+    _addRun(swarmbucket_with_buildbot, 'chromium', 'builder3', 'bb_bbr2', 100,
+            110, 120, True)
+
+    swarmbucket_failed_buildbot = WfTryJob.Create('luci.chromium',
+                                                  'swarm_builder3', 3)
+    _addRun(swarmbucket_failed_buildbot, 'luci.chromium', 'swarm_builder3',
+            'bb2_sbr1', 100, 110, 120)
+    _addRun(swarmbucket_failed_buildbot, 'chromium', 'builder3', 'bb2_bbr2',
+            100, 110, 120, True)
+
+    self.mock_current_user(user_email='test@chromium.org', is_admin=True)
+    response = self.test_app.get('/swarmbucket_performance')
+    self.assertEqual([{
+        "buildbot_tryjob_id": "bb2_bbr2",
+        "swarmbucket_builder": "luci.chromium/swarm_builder3",
+        "buildbot_builder": "chromium/builder3",
+        "swarmbucket_swarm_run_time": 600.0,
+        "buildbot_completion_date": "2017-01-01 02:00:00 UTC",
+        "swarmbucket_completion_date": "2017-01-01 02:00:00 UTC",
+        "swarmbucket_try_job_url": "https://fake.url/bb2_sbr1",
+        "buildbot_try_job_url": "https://fake.url/bb2_bbr2",
+        "swarmbucket_tryjob_id": "bb2_sbr1"
+    }, {
+        "buildbot_tryjob_id": "bb_bbr1",
+        "swarmbucket_builder": "luci.chromium/swarm_builder3",
+        "buildbot_builder": "chromium/builder3",
+        "swarmbucket_swarm_run_time": 600.0,
+        "buildbot_completion_date": "2017-01-01 02:00:00 UTC",
+        "swarmbucket_completion_date": "2017-01-01 02:00:00 UTC",
+        "buildbot_swarm_run_time": 600.0,
+        "buildbot_try_job_url": "https://fake.url/bb_bbr1",
+        "swarmbucket_try_job_url": "https://fake.url/bb_sbr1",
+        "swarmbucket_tryjob_id": "bb_sbr1"
+    }, {
+        "swarmbucket_builder": "luci.chromium/swarm_builder3",
+        "swarmbucket_completion_date": "2017-01-01 02:00:00 UTC",
+        "swarmbucket_swarm_run_time": 600.0,
+        "swarmbucket_tryjob_id": "sbr1",
+        "swarmbucket_try_job_url": "https://fake.url/sbr1"
+    }, {
+        "swarmbucket_builder": "luci.chromium/swarm_builder3",
+        "swarmbucket_completion_date": "2017-01-01 02:00:00 UTC",
+        "swarmbucket_tryjob_id": "sbr2",
+        "swarmbucket_try_job_url": "https://fake.url/sbr2"
+    }], response.json_body)
