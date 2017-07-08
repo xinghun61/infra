c66dcc25362463d342aade0c51b7bdf10f6b7d99
Dan Jacques <dnj@chromium.org>
2017-07-07 17:32:50 -0700

    [depot_tools] Remove Git installer bundling.
    Remove Git installer bundling from the recipe. As of:
    https://chromium-review.googlesource.com/c/562531/
    
    The bleeding-edge Git bundle uses CIPD directly and does not need an
    installer package.
    
    BUG=chromium:740171
    TEST=expectations
    R=iannucci@chromium.org
    
    Change-Id: I9ea63690f604c1badddf982764899cd0b88ccd35
    Reviewed-on: https://chromium-review.googlesource.com/564339
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>

--- recipes/recipes/depot_tools_builder.py
+++ recipes/recipes/depot_tools_builder.py
@@ -62,77 +62,9 @@ def RunSteps(api, revision):
               api.path['checkout'].join('man', 'html'), DOC_UPLOAD_URL],
              name='upload docs')
 
-  # upload git cipd package
-
-
-  # TODO(crbug.com/638337): cipd module doesn't allow `create` to work in this
-  # way.
-  def create(pkg_dir, step_title, platform, refs=()):
-    """Pushes given package directory up to CIPD with provided refs."""
-    cmd = [
-      'cipd',
-      'create',
-      '-in', pkg_dir,
-      '-name', 'infra/depot_tools/git_installer/%s' % platform,
-      '-service-account-json',
-      api.cipd.default_bot_service_account_credentials,
-    ]
-    for r in refs:
-      cmd += ['-ref', r]
-    api.step('create installer package (%s)' % step_title, cmd)
-
-  def pull_and_upload(version, step_title, refs=()):
-    """Pulls given version from google storage, then pushes it up to CIPD.
-
-    Sets package refs to the provided ones, plus an additional ref of the
-    specific version number provided (prepended with 'v' and with '.'s replaced
-    with '_'). So version=='2.8.3' will create a ref 'v2_8_3'.
-
-    version (str) - A version string for one of the git installers in
-      gs://chrome-infra, like '2.8.3'.
-    """
-    refs = ['v' + version.replace('.', '_')] + refs
-    with api.tempfile.temp_dir('git_installer') as git_cipd_dir:
-      outfile = git_cipd_dir.join('git-installer.exe')
-
-      api.gsutil.download(
-        'chrome-infra', 'PortableGit-%s-64-bit.7z.exe' % version, outfile,
-        name='fetch 64-bit git installer (v%s)' % version)
-      create(git_cipd_dir, step_title, 'windows-amd64', refs)
-
-      api.gsutil.download(
-        'chrome-infra', 'PortableGit-%s-32-bit.7z.exe' % version, outfile,
-        name='fetch 32-bit git installer (v%s)' % version)
-      create(git_cipd_dir, step_title, 'windows-386', refs)
-
-
-  bs_win = api.path['checkout'].join('bootstrap', 'win')
-
-  version = api.file.read_text(
-    'read git version', bs_win.join('git_version.txt'),
-    test_data='1.2.3\n').strip()
-  api.step.active_result.presentation.step_text = 'got %r' % version
-
-  bleeding_edge = api.file.read_text('read git version (bleeding_edge)',
-    bs_win.join('git_version_bleeding_edge.txt'), test_data='2.2.3\n').strip()
-  api.step.active_result.presentation.step_text = 'got %r' % bleeding_edge
-
-  if version == bleeding_edge:
-    pull_and_upload(version, 'normal and bleeding_edge',
-                    ['latest', 'bleeding_edge'])
-  else:
-    pull_and_upload(version, 'normal', ['latest'])
-    pull_and_upload(bleeding_edge, 'bleeding_edge', ['bleeding_edge'])
-
 
 def GenTests(api):
   yield (
       api.test('basic') +
       api.properties(path_config='kitchen', revision='deadbeef')
-  )
-
-  yield (
-    api.test('identical_normal_bleeding_edge_git_versions') +
-    api.properties(path_config='kitchen', revision='deadbeef') +
-    api.step_data('read git version', api.raw_io.output_text('2.2.3'))
   )
