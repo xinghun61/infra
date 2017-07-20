ac4a87bceb90c4b949ed05a5fe62f049259ee7c8
Katie Dillon <kdillon@chromium.org>
2017-07-20 17:28:49 +0200

    Skeleton for flake-predictor app as well script for processing output snippets from json data
    Bug:
    Change-Id: I36b7a61f440db662dd489cc47506d724380553ef
    Reviewed-on: https://chromium-review.googlesource.com/561537
    Commit-Queue: Katie Dillon <kdillon@chromium.org>
    Reviewed-by: Sergiy Byelozyorov <sergiyb@chromium.org>

---
+++ appengine/flake-predictor/common/snippets.py
@@ -0,0 +1,92 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+import json
+import logging
+import os
+import re
+import sys
+
+from third_party import cloudstorage
+
+BUCKET = 'flake-predictor-data'
+
+def get_json_file(file_path):
+  """Retrieve the json file in GS.
+
+  Function to retrieve the test data, which is stored in
+  GS. If the file cannot be opened, returns None.
+  """
+  try:
+    gcs_file = cloudstorage.open(file_path)
+  except cloudstorage.NotFoundError:
+    logging.error('Error: file on path %s not found ' % file_path)
+    return None
+
+  json_string = gcs_file.read()
+  gcs_file.close()
+
+  try:
+    json_file = json.loads(json_string)
+  except ValueError:
+    logging.error('Error: could not parse json file %s' % file_path)
+    return None
+
+  return json_file
+
+def get_raw_output_snippet(json_file, test_name):
+  """Retrieve the output snippet from a json file.
+
+  Function to retrieve the output snippet from a json file.
+  If the test data is not in the flake-predictor bucket, the
+  returned file does not have the correct fields, or the test
+  data is not in the file, return None. Otherwise, return the snippet.
+  """
+  if json_file == None:
+    return None
+  if test_name not in json_file.get('all_tests', {}):
+    return None
+
+  test_data = json_file["per_iteration_data"][0][test_name]
+  snippet = test_data[0]['output_snippet']
+  return snippet
+
+def process_snippet(snippet):
+  """Process and clean output snippet.
+
+  Function for taking a snippet as a large string and processing it into
+  a list of separate words. Punctuation and other extaneous symbols are
+  also removed.
+  """
+  # Remove the pointers/noisy numbers
+  snippet = re.sub(r"\[\d+\:\d+\:\d+\/\d+\.\d+",'', snippet)
+  # Remove periods at the end of sentences
+  snippet = re.sub(r"\.(\s)", ' ', snippet)
+  # Remove some punctuation entirely
+  snippet = snippet.replace('[', '').replace(']', '').replace('"', '')
+  snippet = snippet.replace('(', '').replace(')', '')
+  # Replace some punctuation with spaces so that it will be split on
+  snippet = snippet.replace(':', ' ').replace('=', ' ')
+
+  return (snippet.split())
+
+def get_processed_output_snippet_batch(
+    master_name, builder_name, build_number, step_name, tests):
+  """Retrieve output snippet for many tests in a given step.
+
+  Function that takes a step and list of tests and returns the
+  output snippets, as a dictonary, for that each test properly formatted.
+  """
+  file_path = "/%s/%s/%s/%s/%s.json" % (BUCKET, master_name, builder_name,
+                                        build_number, step_name)
+  json_file = get_json_file(file_path)
+
+  cleaned_snippets = {}
+  for test_name in tests:
+    snippet = get_raw_output_snippet(json_file, test_name)
+    if snippet != None:
+      snippet = process_snippet(snippet)
+      cleaned_snippets[test_name] = snippet
+  return cleaned_snippets
---
+++ appengine/flake-predictor/common/test/snippets_test.py
@@ -0,0 +1,100 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+import json
+import unittest
+
+from google.appengine.api import memcache
+from google.appengine.ext import ndb
+from google.appengine.ext import testbed
+
+from common import snippets
+from mock import patch
+from third_party import cloudstorage
+
+
+SAMPLE_FILE = {
+  'all_tests':['test'],
+  'per_iteration_data': [{
+      'test':[{
+          'output_snippet' : '[RUN] test: pass [1:2:3/4.5:WARN:m] : () [] ='
+      }],
+    }],
+}
+
+
+class GetOutputSnippetTest(unittest.TestCase):
+  def setUp(self):
+    self.testbed = testbed.Testbed()
+    self.testbed.activate()
+    self.testbed.init_app_identity_stub()
+    self.testbed.init_blobstore_stub()
+    self.testbed.init_datastore_v3_stub()
+    self.testbed.init_memcache_stub()
+    self.testbed.init_urlfetch_stub()
+    cloudstorage.set_default_retry_params(None)
+    super(GetOutputSnippetTest, self).setUp()
+
+  def tearDown(self):
+    self.testbed.deactivate()
+    super(GetOutputSnippetTest, self).tearDown()
+
+  def test_get_json_file(self):
+    f = cloudstorage.open('/bucket/testfile', 'w')
+    f.write(json.dumps(SAMPLE_FILE))
+    f.close()
+    json_file = snippets.get_json_file('/bucket/testfile')
+    self.assertEqual(json_file, SAMPLE_FILE)
+
+  def test_get_json_file_with_improper_formatted_json_file(self):
+    f = cloudstorage.open('/bucket/testfile', 'w')
+    f.write(' { { ] { } bad json ] ] { }}} ')
+    f.close()
+    json_file = snippets.get_json_file('/bucket/testfile')
+    self.assertEqual(json_file, None)
+
+  def test_get_json_file_when_file_does_not_exist(self):
+    json_file = snippets.get_json_file('/bucket/testfile')
+    self.assertEqual(json_file, None)
+
+  def test_get_raw_output_sample_json(self):
+    snippet = snippets.get_raw_output_snippet(SAMPLE_FILE, 'test')
+    self.assertEqual(snippet, "[RUN] test: pass [1:2:3/4.5:WARN:m] : () [] =")
+
+  def test_fail_get_raw_output_given_empty_json(self):
+    snippet = snippets.get_raw_output_snippet({}, 'test')
+    self.assertEqual(snippet, None)
+
+  def test_fail_get_raw_output_test_not_in_json(self):
+    snippet = snippets.get_raw_output_snippet(
+        SAMPLE_FILE, 'test_not_there')
+    self.assertEqual(snippet, None)
+
+  def test_process_snippet(self):
+    snippet = snippets.process_snippet(
+        "[RUN] test: pass [1:2:3/4.5:WARN:m] : () [] =")
+    self.assertEqual(snippet, ["RUN", "test", "pass", "WARN", "m"])
+
+  @patch.object(snippets, 'get_json_file')
+  def test_get_processed_output_batch_from_sample_json(self, mock_json_file):
+    mock_json_file.return_value = SAMPLE_FILE
+    cleaned_snippets = snippets.get_processed_output_snippet_batch(
+        'test_master', 'test_builder', 1234,'test_step', ['test'])
+    self.assertDictEqual(
+        cleaned_snippets, {'test' : ["RUN", "test", "pass", "WARN", "m"]})
+
+  @patch.object(snippets, 'get_json_file')
+  def test_get_processed_output_batch_tests_not_in_file(self, mock_json_file):
+    mock_json_file.return_value = SAMPLE_FILE
+    cleaned_snippets = snippets.get_processed_output_snippet_batch(
+        'test_master', 'test_builder', 1234, 'test_step', ['test_not_there'])
+    self.assertEqual(cleaned_snippets, {})
+
+  @patch.object(snippets, 'get_json_file')
+  def test_get_processed_output_batch_from_empty_json(self, mock_json_file):
+    mock_json_file.return_value = None
+    cleaned_snippets = snippets.get_processed_output_snippet_batch(
+        'test_master', 'test_builder', 1234, 'test_step', ['test_not_there'])
+    self.assertEqual(cleaned_snippets, {})
