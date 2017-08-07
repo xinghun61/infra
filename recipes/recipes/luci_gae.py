d823ab39207cd9763d30df8dc65c3c98c7130c0d
Andrii Shyshkalov <tandrii@chromium.org>
2017-08-07 16:49:02 +0200

    Refactor luci_gae to use infra_checkout recipe module.
    Also enable the use of infra checkout cache.
    
    R=machenbach@chromium.org
    
    Change-Id: I55b62986147b509cee636e424bc2ab78a322b8ed
    Reviewed-on: https://chromium-review.googlesource.com/603712
    Commit-Queue: Andrii Shyshkalov <tandrii@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>

--- recipes/recipes/luci_gae.py
+++ recipes/recipes/luci_gae.py
@@ -8,6 +8,7 @@ DEPS = [
   'depot_tools/git',
   'depot_tools/presubmit',
   'depot_tools/tryserver',
+  'infra_checkout',
   'recipe_engine/context',
   'recipe_engine/json',
   'recipe_engine/path',
@@ -19,73 +20,21 @@ DEPS = [
 LUCI_GAE_PATH_IN_INFRA = 'infra/go/src/github.com/luci/gae'
 
 
-def _run_presubmit(api, patch_root, bot_update_step):
-  got_revision_properties = api.bot_update.get_project_revision_properties(
-      LUCI_GAE_PATH_IN_INFRA)
-  upstream = bot_update_step.json.output['properties'].get(
-      got_revision_properties[0])
-  # The presubmit must be run with proper Go environment.
-  # infra/go/env.py takes care of this.
-  presubmit_cmd = [
-    'python',  # env.py will replace with this its sys.executable.
-    api.presubmit.presubmit_support_path,
-    '--root', api.path['start_dir'].join(patch_root),
-    '--commit',
-    '--verbose', '--verbose',
-    '--issue', api.properties['issue'],
-    '--patchset', api.properties['patchset'],
-    '--skip_canned', 'CheckRietveldTryJobExecution',
-    '--skip_canned', 'CheckTreeIsOpen',
-    '--skip_canned', 'CheckBuildbotPendingBuilds',
-    '--rietveld_url', api.properties['rietveld'],
-    '--rietveld_fetch',
-    '--upstream', upstream,
-    '--rietveld_email', ''
-  ]
-  with api.context(env={'PRESUBMIT_BUILDER': '1'}):
-    api.python('presubmit', api.path['checkout'].join('go', 'env.py'),
-               presubmit_cmd)
-
-
-def _commit_change(api, patch_root):
-  with api.context(cwd=api.path['start_dir'].join(patch_root)):
-    api.git('-c', 'user.email=commit-bot@chromium.org',
-            '-c', 'user.name=The Commit Bot',
-            'commit', '-a', '-m', 'Committed patch',
-            name='commit git patch')
-
-
 def RunSteps(api):
-  api.gclient.set_config('luci_gae')
-  # patch_root must match the luci/gae repo, not infra checkout.
-  patch_root = LUCI_GAE_PATH_IN_INFRA
-  bot_update_step = api.bot_update.ensure_checkout(patch_root=patch_root)
-
+  co = api.infra_checkout.checkout('luci_gae',
+                                   patch_root=LUCI_GAE_PATH_IN_INFRA)
   is_presubmit = 'presubmit' in api.properties.get('buildername', '').lower()
   if is_presubmit:
-    _commit_change(api, patch_root)
-  api.gclient.runhooks()
+    co.commit_change()
+  co.gclient_runhooks()
 
-  # This downloads the third parties, so that the next step doesn't have junk
-  # output in it.
-  api.python(
-      'go third parties',
-      api.path['checkout'].join('go', 'env.py'),
-      ['go', 'version'])
-
+  co.ensure_go_env()
   if is_presubmit:
     with api.tryserver.set_failure_hash():
-      _run_presubmit(api, patch_root, bot_update_step)
+      co.run_presubmit_in_go_env()
   else:
-    api.python(
-        'go build',
-        api.path['checkout'].join('go', 'env.py'),
-        ['go', 'build', 'github.com/luci/gae/...'])
-
-    api.python(
-        'go test',
-        api.path['checkout'].join('go', 'env.py'),
-        ['go', 'test', 'github.com/luci/gae/...'])
+    co.go_env_step('go', 'build', 'github.com/luci/luci-gae/...')
+    co.go_env_step('go', 'test', 'github.com/luci/luci-gae/...')
 
 
 def GenTests(api):
