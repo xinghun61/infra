e85271e4ffd854fd044f7354239d3ed327769b43
Katie Dillon <kdillon@chromium.org>
2017-08-22 18:02:29 +0200

    Bump dataflow package version
    R=sergiyb@chromium.org
    
    Bug: 734595
    Change-Id: I8ea58fcb7608a8cfb659d7c3e0338d1b262d9e37
    Reviewed-on: https://chromium-review.googlesource.com/626066
    Reviewed-by: Sergiy Byelozyorov <sergiyb@chromium.org>
    Commit-Queue: Katie Dillon <kdillon@chromium.org>

--- packages/dataflow/setup.py
+++ packages/dataflow/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='dataflow',
-    version='0.0.1',
+    version='0.0.2',
     description='Chrome Infra Dataflow Workflows',
     long_description=('This package includes Chrome Infra workflows as well as '
                       'common modules.'),
