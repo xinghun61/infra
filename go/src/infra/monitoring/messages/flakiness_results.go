8bd7ec93ec67455eed4e7ae85c8eb3afe4158f3b
Joanna Wang <jojwang@google.com>
2017-08-30 17:18:26 -0700

    Add function to query bigquery.
    Planned structure pseudo-code: 
    https://docs.google.com/a/google.com/document/d/1ewZE6lR_lA10UdVmiK4cZdhNHs3I2uE3_vgd4sYXg_4/edit?usp=sharing
    
    Bug:757630
    
    Change-Id: I7fbe86e17aeda488636f23be55bfa5af8172edcc
    Reviewed-on: https://chromium-review.googlesource.com/622947
    Commit-Queue: Joanna Wang <jojwang@google.com>
    Reviewed-by: Sean McCullough <seanmccullough@chromium.org>

--- go/src/infra/monitoring/messages/flakiness_results.go
+++ go/src/infra/monitoring/messages/flakiness_results.go
@@ -25,7 +25,7 @@ type BuilderResults struct {
 
 // Results holds the test result from one build.
 type Results struct {
-	BuildNumber  string `json:"build_number"`
-	TestActual   string `json:"test_actual"`
-	TestExpected string `json:"test_expected"`
+	BuildNumber int   `json:"build_number" bigquery:"build_number"`
+	Actual      []int `json:"test_actual" bigquery:"actual"`
+	Expected    []int `json:"test_expected" bigquery:"expected"`
 }
