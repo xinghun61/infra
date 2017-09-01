a129aa5d08d0a063c58a479dcafaf38af988f5ab
Roberto Carrillo <robertocn@google.com>
2017-08-31 23:19:26 -0700

    [cr-audit-commits] Provide basic index file.
    All sites need an index pointing to the code and documentation.
    
    R=stgao,agable,iannucci,chanli
    BUG=753158
    
    Change-Id: I990471f794b28e05314577c6fd7c26ca4408fcda
    Reviewed-on: https://chromium-review.googlesource.com/634626
    Commit-Queue: Roberto Carrillo <robertocn@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>

--- go/src/infra/appengine/cr-audit-commits/app/app.go
+++ go/src/infra/appengine/cr-audit-commits/app/app.go
@@ -11,17 +11,33 @@ import (
 	"go.chromium.org/luci/appengine/gaemiddleware"
 	"go.chromium.org/luci/appengine/gaemiddleware/standard"
 	"go.chromium.org/luci/server/router"
+	"go.chromium.org/luci/server/templates"
 )
+
+// Lets the templates lib know where to load templates from.
+func getTemplatesMW() router.Middleware {
+	return templates.WithTemplates(&templates.Bundle{
+		Loader: templates.FileSystemLoader("templates"),
+	})
+}
 
 func init() {
 	r := router.New()
+
+	// This does not require auth. Needed for index page.
+	basemw := standard.Base()
 
 	// This ensures that the route is only accessible to cron jobs.
 	cronmw := standard.Base().Extend(gaemiddleware.RequireCron)
 
 	standard.InstallHandlers(r)
 
+	r.GET("/", basemw.Extend(getTemplatesMW()), index)
 	r.GET("/_cron/commitscanner", cronmw, CommitScanner)
 
 	http.DefaultServeMux.Handle("/", r)
+}
+
+func index(rc *router.Context) {
+	templates.MustRender(rc.Context, rc.Writer, "pages/index.html", templates.Args{})
 }
