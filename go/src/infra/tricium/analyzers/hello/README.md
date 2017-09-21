ac820f9939971f50ee6b8db41cedf8e5b1ccf441
Emma <emso@chromium.org>
2017-09-21 14:29:52 +0200

    Updates Hello analyzer to consume FILES data.
    Bug:764664
    Change-Id: I28fe29cf4830905aefd1b748e03a64aba4d1893d
    Reviewed-on: https://chromium-review.googlesource.com/671005
    Commit-Queue: Emma Söderberg <emso@chromium.org>
    Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>

--- go/src/infra/tricium/analyzers/hello/README.md
+++ go/src/infra/tricium/analyzers/hello/README.md
@@ -1,11 +1,24 @@
 # Hello Analyzer
 
-Consuming Tricium GIT_FILE_DETAILS and producing a Tricium RESULTS comment.
+Consuming Tricium FILES data and producing a Tricium RESULTS data with comments,
+one per file in the FILES data.
+
+## Development and Testing
+
+Local testing:
+
+```
+$ go build
+$ ./hello --input=test --output=output
+```
+
+## Deployment
 
 Deploy a new version of the analyzer using CIPD:
 
 ```
+$ go build
 $ cipd create -pkg-def=cipd.yaml
+<outputs the VERSION>
+$ cipd set-ref infra/tricium/analyzer/hello -ref live -version VERSION
 ```
-
-TODO(emso): Makes this analyzer depend on FILES and produce a comment per file.
--- go/src/infra/tricium/analyzers/hello/hello.go
+++ go/src/infra/tricium/analyzers/hello/hello.go
@@ -1,30 +1,51 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // Package main implements the Hello analyzer.
 package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 
 	"infra/tricium/api/v1"
 )
 
-func main() {
-	prefix := flag.String("prefix", "", "Output path prefix")
-	flag.Parse()
+const (
+	category = "Hello"
+	message  = "Hello"
+)
 
-	msg := &tricium.Data_Results{
-		Platforms: 0,
-		Comments: []*tricium.Data_Comment{
-			{
-				Category: "Hello",
-				Message:  "Hello",
-			},
-		},
+func main() {
+	inputDir := flag.String("input", "", "Path to root of Tricium input")
+	outputDir := flag.String("output", "", "Path to root of Tricium output")
+	flag.Parse()
+	if flag.NArg() != 0 {
+		log.Fatalf("Unexpected argument")
 	}
-	path, err := tricium.WriteDataType(*prefix, msg)
+
+	// Read Tricium input FILES data.
+	input := &tricium.Data_Files{}
+	if err := tricium.ReadDataType(*inputDir, input); err != nil {
+		log.Fatalf("Failed to read FILES data: %v", err)
+	}
+	log.Printf("Read FILES data: %+v", input)
+
+	// Create RESULTS data.
+	output := &tricium.Data_Results{}
+	for _, p := range input.Paths {
+		output.Comments = append(output.Comments, &tricium.Data_Comment{
+			Category: category,
+			Message:  message,
+			Path:     p,
+		})
+	}
+
+	// Write Tricium RESULTS data.
+	path, err := tricium.WriteDataType(*outputDir, output)
 	if err != nil {
-		log.Fatalf("failed to run hello analyzer: %v", err)
+		log.Fatalf("Failed to write RESULTS data: %v", err)
 	}
-	fmt.Printf("Wrote Hello comment, path: %q, prefix: %q\n", path, *prefix)
+	log.Printf("Wrote RESULTS data, path: %q, value: %v\n", path, output)
 }
