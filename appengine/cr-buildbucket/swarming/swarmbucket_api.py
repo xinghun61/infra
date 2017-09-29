eb62bec543b79391d547157179213d9b9121e168
Nodir Turakulov <nodir@google.com>
2017-09-29 11:23:05 -0700

    [buildbucket] remove api explorer link
    API Explorer link is not used. led is a better interface for this functionality
    anyway. Delete API Explorer link support.
    
    R=iannucci@chromium.org
    
    Bug:
    Change-Id: I676f08c7e7203e3a98efe5138393564af3958ad9
    Reviewed-on: https://chromium-review.googlesource.com/693136
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>

--- appengine/cr-buildbucket/swarming/swarmbucket_api.py
+++ appengine/cr-buildbucket/swarming/swarmbucket_api.py
@@ -65,18 +65,12 @@ class GetTaskDefinitionRequestMessage(messages.Message):
   # that would be created for the build. Build id will be 1 and build number (if
   # configured) will be 0.
   build_request = messages.MessageField(api.PutRequestMessage, 1, required=True)
-  # if True (default), an API Explorer link is returned.
-  # See GetTaskDefinitionResponseMessage for details.
-  api_explorer_link = messages.BooleanField(2)
 
 
 class GetTaskDefinitionResponseMessage(messages.Message):
   # A definition of the swarming task that would be created for the specified
   # build.
   task_definition = messages.StringField(1)
-  # An API Explorer link to Swarming's tasks.new API
-  # with the pre-filled request body.
-  api_explorer_link = messages.StringField(3)
 
 
 @auth.endpoints_api(
@@ -139,29 +133,7 @@ class SwarmbucketApi(remote.Service):
           fake_build=True).get_result()
       task_def_json = json.dumps(task_def)
 
-      res = GetTaskDefinitionResponseMessage(task_definition=task_def_json)
-      if request.api_explorer_link != False:  # pragma: no branch
-        res.api_explorer_link = shorten_url(
-            ('https://%s/_ah/api/explorer'
-             '#p/swarming/v1/swarming.tasks.new?resource=%s') %
-            (build.swarming_hostname, urllib.quote(task_def_json)))
-      return res
+      return GetTaskDefinitionResponseMessage(task_definition=task_def_json)
     except errors.InvalidInputError as ex:
       raise endpoints.BadRequestException(
           'invalid build request: %s' % ex.message)
-
-
-def shorten_url(long_url):
-  link_hash = hashlib.sha256(long_url).hexdigest()
-  cache_key = 'shortlink/' + link_hash
-  short_url = memcache.get(cache_key)
-  if not short_url:
-    res = net.json_request(
-        url='https://www.googleapis.com/urlshortener/v1/url',
-        method='POST',
-        payload={'longUrl': long_url},
-        scopes='https://www.googleapis.com/auth/urlshortener',
-    )
-    short_url = res['id']
-    memcache.set(cache_key, short_url)
-  return short_url
--- appengine/cr-buildbucket/swarming/test/swarmbucket_api_test.py
+++ appengine/cr-buildbucket/swarming/test/swarmbucket_api_test.py
@@ -141,12 +141,7 @@ class SwarmbucketApiTest(testing.EndpointsTestCase):
       ]
     })
 
-  @mock.patch('components.net.json_request', autospec=True)
-  def test_get_task_def(self, json_request):
-    json_request.return_value = {
-      'id': 'https://shorturl',
-    }
-
+  def test_get_task_def(self):
     req = {
       'build_request': {
         'bucket': 'luci.chromium.try',
@@ -154,10 +149,7 @@ class SwarmbucketApiTest(testing.EndpointsTestCase):
           'builder_name': 'linux_chromium_rel_ng',
         }),
       },
-      'api_explorer_link': True,
     }
-    self.call_api('get_task_def', req)
-    # Call second time to ensure we shorten the same long link only once.
     resp = self.call_api('get_task_def', req).json_body
     actual_task_def = json.loads(resp['task_definition'])
     expected_task_def = {
@@ -225,20 +217,6 @@ class SwarmbucketApiTest(testing.EndpointsTestCase):
     }
     self.assertEqual(actual_task_def, expected_task_def)
 
-    expected_long_api_explorer_link = (
-        'https://swarming.example.com/_ah/api/explorer'
-        '#p/swarming/v1/swarming.tasks.new?resource=' +
-        urllib.quote(resp['task_definition']))
-
-    self.assertEqual(resp['api_explorer_link'], 'https://shorturl')
-    self.assertEqual(json_request.call_count, 1)
-    json_request.assert_called_with(
-        url='https://www.googleapis.com/urlshortener/v1/url',
-        method='POST',
-        payload={'longUrl': expected_long_api_explorer_link},
-        scopes='https://www.googleapis.com/auth/urlshortener',
-    )
-
   def test_get_task_def_bad_request(self):
     req = {
       'build_request': {
@@ -247,7 +225,6 @@ class SwarmbucketApiTest(testing.EndpointsTestCase):
           'builder_name': 'linux_chromium_rel_ng',
         }),
       },
-      'api_explorer_link': False,
     }
     self.call_api('get_task_def', req, status=400)
 
