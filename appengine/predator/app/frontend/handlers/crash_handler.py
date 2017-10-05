7b7255369224f4c9768725e410fcf3415eaf6eb9
Shuotao Gao <stgao@chromium.org>
2017-10-05 12:24:58 -0700

    [Findit] Fix too big entity exception.
    TBR=katesonia@chromium.org
    Bug:
    Change-Id: I349fff2a3994c636bee38fdbe2f3699a20a9f214
    Reviewed-on: https://chromium-review.googlesource.com/703215
    Reviewed-by: Shuotao Gao <stgao@chromium.org>
    Commit-Queue: Shuotao Gao <stgao@chromium.org>

--- appengine/predator/app/frontend/handlers/crash_handler.py
+++ appengine/predator/app/frontend/handlers/crash_handler.py
@@ -6,6 +6,8 @@ import base64
 import json
 import logging
 
+from google.appengine.ext.db import BadRequestError
+
 from common import constants
 from common import crash_pipeline
 from common.model.crash_config import CrashConfig
@@ -78,7 +80,8 @@ class CrashHandler(BaseHandler):
       if need_analysis:
         StartNewAnalysis(json_crash_data['client_id'], crash_data.identifiers)
 
-    except (KeyError, ValueError):  # pragma: no cover.
+    except (KeyError, ValueError, BadRequestError):  # pragma: no cover.
+      # TODO(katesonia): fix the too big entity exception, and then revert.
       # TODO: save exception in datastore and create a page to show them.
       logging.exception('Failed to process crash message')
       logging.info(self.request.body)
