669192d0d09735a784e88ba9c45b22074abca208
Emma <emso@chromium.org>
2017-10-05 14:58:06 +0200

    Adds Spacey tests
    Bug:771579
    Change-Id: Icb204f58768ede1bf921b0516a84279bf59e6717
    Reviewed-on: https://chromium-review.googlesource.com/702476
    Reviewed-by: Paweł Hajdan Jr. <phajdan.jr@chromium.org>
    Commit-Queue: Emma Söderberg <emso@chromium.org>

--- go/src/infra/tricium/analyzers/spacey/spacey_test.go
+++ go/src/infra/tricium/analyzers/spacey/spacey_test.go
@@ -12,29 +12,55 @@ import (
 
 func TestCheckSpaceMix(t *testing.T) {
 	Convey("Test Environment", t, func() {
-		path := "test.file"
-		line := "\t some code"
-		pos := 1
-		c := checkSpaceMix(path, line, pos)
-		So(c, ShouldNotBeNil)
-		So(c.StartLine, ShouldEqual, pos)
-		So(c.EndLine, ShouldEqual, pos)
-		So(c.StartChar, ShouldEqual, 0)
-		So(c.EndChar, ShouldEqual, 1)
+		Convey("Tab and single space", func() {
+			path := "test.file"
+			line := "\t some code"
+			pos := 1
+			c := checkSpaceMix(path, line, pos)
+			So(c, ShouldNotBeNil)
+			So(c.StartLine, ShouldEqual, pos)
+			So(c.EndLine, ShouldEqual, pos)
+			So(c.StartChar, ShouldEqual, 0)
+			So(c.EndChar, ShouldEqual, 1)
+		})
+		Convey("Tab and multiple space", func() {
+			path := "test.file"
+			line := "\t  some code"
+			pos := 1
+			c := checkSpaceMix(path, line, pos)
+			So(c, ShouldNotBeNil)
+			So(c.StartLine, ShouldEqual, pos)
+			So(c.EndLine, ShouldEqual, pos)
+			So(c.StartChar, ShouldEqual, 0)
+			So(c.EndChar, ShouldEqual, 2)
+		})
 	})
 }
 
 func TestCheckTrailingSpace(t *testing.T) {
 	Convey("Test Environment", t, func() {
-		path := "test.file"
-		line := "some code  "
-		pos := 1
-		c := checkTrailingSpace(path, line, pos)
-		So(c, ShouldNotBeNil)
-		So(c.StartLine, ShouldEqual, pos)
-		So(c.EndLine, ShouldEqual, pos)
-		So(c.StartChar, ShouldEqual, 9)
-		So(c.EndChar, ShouldEqual, 10)
+		Convey("Single trailing spaces", func() {
+			path := "test.file"
+			line := "some code "
+			pos := 1
+			c := checkTrailingSpace(path, line, pos)
+			So(c, ShouldNotBeNil)
+			So(c.StartLine, ShouldEqual, pos)
+			So(c.EndLine, ShouldEqual, pos)
+			So(c.StartChar, ShouldEqual, len(line)-1)
+			So(c.EndChar, ShouldEqual, len(line)-1)
+		})
+		Convey("Multiple trailing spaces", func() {
+			path := "test.file"
+			line := "some code  "
+			pos := 1
+			c := checkTrailingSpace(path, line, pos)
+			So(c, ShouldNotBeNil)
+			So(c.StartLine, ShouldEqual, pos)
+			So(c.EndLine, ShouldEqual, pos)
+			So(c.StartChar, ShouldEqual, len(line)-2)
+			So(c.EndChar, ShouldEqual, len(line)-1)
+		})
 	})
 
 }
