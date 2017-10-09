8e7174244e08c1798640215a0b7c3266dfa9fb1c
Sergey Berezin <sergeyberezin@google.com>
2017-10-09 15:51:14 -0700

    Add boilerplate for mac_toolchain tool
    BUG=475693
    
    Change-Id: I01c91fe52f27b955bb730ebcbc20539b7ef6882b
    Reviewed-on: https://chromium-review.googlesource.com/701290
    Commit-Queue: Sergey Berezin <sergeyberezin@chromium.org>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>

---
+++ go/src/infra/cmd/mac_toolchain/README.md
@@ -0,0 +1,72 @@
+# Ephemeral Xcode Installer for Mac
+
+The purpose of this binary is to provide tools for temporary Xcode
+installations, e.g. to build and test Chrome / Chromium on Mac OS X and iOS.
+
+Specifically, it provides tools for creating appropriate Xcode CIPD packages and
+then installing them on demand on bots and developer machines.
+
+## Installation
+
+### From `depot_tools`
+
+The `mac_toolchain` tool is available in
+[`depot_tools`](https://chromium.googlesource.com/chromium/tools/depot_tools.git)
+as a shim which automatically installs the binary on Mac OS X.
+
+### Prebuilt CIPD Package
+
+For `depot_tool`-independent installation (e.g. on a bot, in a recipe), the
+preferred method of installing this tool is through CIPD, e.g.:
+
+    $ echo 'infra/tools/mac_toolchain/${platform}  latest' | cipd ensure -ensure-file - -root .
+
+will install the `mac_toolchain` binary in the current directory (as specified
+by the `-root` argument).
+
+Note, that the CIPD package currently exists only for Mac OS, and not for any
+other platform, since Xcode installation only makes sense on a Mac.
+
+You can also create shim scripts to install the actual binaries automatically,
+and optionally pin the `mac_toolchain` package to a specific revision. Pinning
+to `latest` will always install the latest available revision. For inspiration,
+see
+[`cipd_manifest.txt`](https://chromium.googlesource.com/chromium/tools/depot_tools.git/+/master/cipd_manifest.txt)
+in `depot_tools`, and the corresponding shims,
+e.g. [`vpython`](https://chromium.googlesource.com/chromium/tools/depot_tools.git/+/master/vpython).
+
+The prebuilt CIPD package is configured and built automatically for (almost)
+every committed revision of `infra.git`. It's configuration file is
+[`mac_toolchain.yaml`](https://chromium.googlesource.com/infra/infra/+/master/build/packages/mac_toolchain.yaml).
+
+### Compile from source
+
+Since this is a standard Go package, you can also install it by running `go
+install` in this folder. See the [`infra/go/README.md`](../../../../README.md) file
+for the Go setup.
+
+## Installing an Xcode package
+
+    mac_toolchain install -xcode-version XXXX -output-dir /path/to/root
+
+This will install the requested version of `Xcode.app` in the `/path/to/root`
+folder.  Run `mac_toolchain help install` for more information.
+
+_Note:_ to access the Xcode packages, you may need to run:
+
+    cipd auth-login
+
+or pass the appropriate `-service-account-json` argument to `cipd ensure`.
+
+## Creating a new Xcode package
+
+Download the Xcode zip file from your Apple's Developer account, unpack it, and
+point the script at the resulting `Xcode.app`.
+
+    mac_toolchain upload -xcode-path /path/to/Xcode.app
+
+This will split up the `Xcode.app` container into several CIPD packages and will
+upload and tag them properly. Run `mac_toolchain help upload` for more options.
+
+The upload command is meant to be run manually, and it will upload many GB of
+data. Be patient.
---
+++ go/src/infra/cmd/mac_toolchain/main.go
@@ -0,0 +1,153 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"os"
+
+	"golang.org/x/net/context"
+
+	"github.com/maruel/subcommands"
+	"go.chromium.org/luci/common/cli"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/flag/flagenum"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/logging/gologger"
+)
+
+// DefaultCipdPackage is a package prefix for all the xcode packages.
+const DefaultCipdPackage = "infra_internal/ios/xcode"
+
+// KindType is the type for enum values for the -kind argument.
+type KindType string
+
+var _ flag.Value = (*KindType)(nil)
+
+const (
+	macKind     = KindType("mac")
+	iosKind     = KindType("ios")
+	iosTestKind = KindType("iostest")
+	allKind     = KindType("all")
+	// DefaultKind is the default value for the -kind flag.
+	DefaultKind = allKind
+)
+
+// KindTypeEnum is the corresponding Enum type for the -kind argument.
+var KindTypeEnum = flagenum.Enum{
+	"mac":     macKind,
+	"ios":     iosKind,
+	"iostest": iosTestKind,
+	"all":     allKind,
+}
+
+// String implements flag.Value
+func (t *KindType) String() string {
+	return KindTypeEnum.FlagString(*t)
+}
+
+// Set implements flag.Value
+func (t *KindType) Set(v string) error {
+	return KindTypeEnum.FlagSet(t, v)
+}
+
+type commonFlags struct {
+	subcommands.CommandRunBase
+	cipdPackage string
+}
+
+type installRun struct {
+	commonFlags
+	xcodeVersion string
+	outputDir    string
+	kind         KindType
+}
+
+type uploadRun struct {
+	commonFlags
+	xcodePath string
+}
+
+func (c *installRun) Run(a subcommands.Application, args []string, env subcommands.Env) int {
+	ctx := cli.GetContext(a, c, env)
+	if c.xcodeVersion == "" {
+		errors.Log(ctx, errors.Reason("no Xcode version specified (-xcode-version)").Err())
+		return 1
+	}
+	if c.outputDir == "" {
+		errors.Log(ctx, errors.Reason("no output folder specified (-output-dir)").Err())
+		return 1
+	}
+	logging.Infof(ctx, "About to install Xcode %s in %s", c.xcodeVersion, c.outputDir)
+	// TODO(sergeyberezin): implement this.
+	return 0
+}
+
+func (c *uploadRun) Run(a subcommands.Application, args []string, env subcommands.Env) int {
+	// TODO(sergeyberezin): implement this.
+	return 0
+}
+
+func commonFlagVars(c *commonFlags) {
+	c.Flags.StringVar(&c.cipdPackage, "cipd-package", DefaultCipdPackage, "CIPD package prefix.")
+}
+
+func installFlagVars(c *installRun) {
+	commonFlagVars(&c.commonFlags)
+	c.Flags.StringVar(&c.xcodeVersion, "xcode-version", "", "Xcode version code. (required)")
+	c.Flags.StringVar(&c.outputDir, "output-dir", "", "Path where to install Xcode.app (required).")
+	c.Flags.Var(&c.kind, "kind", "Installation kind: "+KindTypeEnum.Choices()+". (default: \""+string(DefaultKind)+"\")")
+	c.kind = DefaultKind
+}
+
+func uploadFlagVars(c *uploadRun) {
+	commonFlagVars(&c.commonFlags)
+	c.Flags.StringVar(&c.xcodePath, "xcode-path", "", "Path to install Xcode.app. (required)")
+}
+
+var (
+	cmdInstall = &subcommands.Command{
+		UsageLine: "install <options>",
+		ShortDesc: "Installs Xcode.",
+		LongDesc:  "Installs the requested parts of Xcode toolchain.",
+		CommandRun: func() subcommands.CommandRun {
+			c := &installRun{}
+			installFlagVars(c)
+			return c
+		},
+	}
+
+	cmdUpload = &subcommands.Command{
+		UsageLine: "upload <options>",
+		ShortDesc: "Uploads Xcode CIPD packages.",
+		LongDesc:  "Creates and uploads Xcode toolchain CIPD packages.",
+		CommandRun: func() subcommands.CommandRun {
+			c := &uploadRun{}
+			uploadFlagVars(c)
+			return c
+		},
+	}
+)
+
+func main() {
+	application := &cli.Application{
+		Name:  "mac_toolchain",
+		Title: "Mac OS / iOS toolchain management",
+		Context: func(ctx context.Context) context.Context {
+			goLoggerCfg := gologger.LoggerConfig{Out: os.Stderr}
+			goLoggerCfg.Format = "[%{level:.1s} %{time:2006-01-02 15:04:05}] %{message}"
+			ctx = goLoggerCfg.Use(ctx)
+
+			ctx = (&logging.Config{Level: logging.Info}).Set(ctx)
+			return ctx
+		},
+		Commands: []*subcommands.Command{
+			subcommands.CmdHelp,
+			cmdInstall,
+			cmdUpload,
+		},
+	}
+	os.Exit(subcommands.Run(application, nil))
+}
