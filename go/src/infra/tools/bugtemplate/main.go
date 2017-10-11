f1f2c878525c49ce8b35e0533a2d01760939d9cd
Nodir Turakulov <nodir@google.com>
2017-10-11 15:27:50 -0700

    [bugtemplate] add the tool and example template
    Add a tool that accepts a template file and produces a deep link
    
    R=agable@chromium.org, jrobbins@chromium.org
    Bug: monorail:2652
    Change-Id: I19c2c02398456d5086f420701e5c51f4db321c12
    Reviewed-on: https://chromium-review.googlesource.com/713664
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>
    Reviewed-by: Jason Robbins <jrobbins@chromium.org>

---
+++ go/src/infra/tools/bugtemplate/main.go
@@ -0,0 +1,82 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/url"
+	"os"
+	"strings"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/proto"
+)
+
+var defPath = flag.String(
+	"path",
+	"",
+	"path to the file with text proto Template message, see template.proto")
+
+func (p Priority) Label() string {
+	if p == 0 {
+		return ""
+	}
+	return "Pri-" + strings.TrimPrefix(p.String(), "P")
+}
+
+func (t Type) Label() string {
+	if t == 0 {
+		return ""
+	}
+	return "Type-" + t.String()
+}
+
+func cleanStrings(slice []string) []string {
+	// keep it simple
+	clean := make([]string, 0, len(slice))
+	for _, s := range slice {
+		s = strings.TrimSpace(s)
+		if s != "" {
+			clean = append(clean, s)
+		}
+	}
+	return clean
+}
+
+func run() error {
+	flag.Parse()
+	if len(flag.Args()) > 0 {
+		return fmt.Errorf("unexpected args: %q", flag.Args())
+	}
+
+	contents, err := ioutil.ReadFile(*defPath)
+	if err != nil {
+		return errors.Annotate(err, "failed to read template file").Err()
+	}
+
+	var template Template
+	if err := proto.UnmarshalTextML(string(contents), &template); err != nil {
+		return errors.Annotate(err, "failed to parse template file").Err()
+	}
+	template.Labels = append(template.Labels, template.Pri.Label(), template.Type.Label())
+
+	params := url.Values{}
+	params.Set("summary", template.Summary)
+	params.Set("comment", template.Description)
+	params.Set("cc", strings.Join(cleanStrings(template.Cc), ","))
+	params.Set("components", strings.Join(cleanStrings(template.Components), ","))
+	params.Set("labels", strings.Join(cleanStrings(template.Labels), ","))
+	fmt.Printf("https://bugs.chromium.org/p/chromium/issues/entry?%s\n", params.Encode())
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
