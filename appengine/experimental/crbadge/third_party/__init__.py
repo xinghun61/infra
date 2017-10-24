4f9d4a8f9dd1c3cebd2b0c9817ed55ae4c6b9a33
Sharu Jiang <katesonia@google.com>
2017-10-24 16:17:43 -0700

    [crbadge] Add libs and local_libs from findit.
    Bug:
    Change-Id: I6b273dbbe004712e82376bb26e39f4dd1748482f
    Reviewed-on: https://chromium-review.googlesource.com/736780
    Reviewed-by: Roberto Carrillo <robertocn@chromium.org>

---
+++ appengine/experimental/crbadge/third_party/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import sys
+
+
+_THIS_DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, _THIS_DIR)
