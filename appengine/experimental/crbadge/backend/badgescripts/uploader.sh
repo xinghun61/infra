2bdea8866d04bfd436fb4dfaae9b2a7af027224f
Roberto Carrillo <robertocn@google.com>
2017-10-25 09:13:32 -0700

    crbadge: Make uploader.sh actually upload stuff.
    R=katesonia,chanli
    
    Bug:
    Change-Id: Ifab50c2a2962eef6dc9b241ac266a54032aa972b
    Reviewed-on: https://chromium-review.googlesource.com/737475
    Reviewed-by: Roberto Carrillo <robertocn@chromium.org>

--- appengine/experimental/crbadge/backend/badgescripts/uploader.sh
+++ appengine/experimental/crbadge/backend/badgescripts/uploader.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PYTHONPATH=$PYTHONPATH:$DIR/../../third_party
-python $1 $2
+python $DIR/../../testdata/upload.py -p `cat ~/.crbadgepw` -u https://crbadge.appspot.com/system/update >(python $@)
