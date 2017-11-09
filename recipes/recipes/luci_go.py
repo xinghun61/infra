7e6cf0833ad5d9fd18deb04898d0060bc73aefcb
Andrii Shyshkalov <tandrii@chromium.org>
2017-11-09 23:43:49 +0000

    Revert "luci-go recipe: make precise-32 builder fail if run on luci."
    This reverts commit 085df35a5af775e271d935a72f06fd0070758dc8.
    
    Reason for revert: test finished.
    
    Original change's description:
    > luci-go recipe: make precise-32 builder fail if run on luci.
    > 
    > This is a test of gatekeeper + MILO buildbot emulation API.
    > This will be reverted.
    > 
    > R=​iannucci@chromium.org
    > 
    > Bug: 767690
    > Change-Id: Ia432cbee69d945cf6741ba7973966adc10761d93
    > Reviewed-on: https://chromium-review.googlesource.com/761421
    > Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    > Commit-Queue: Andrii Shyshkalov <tandrii@chromium.org>
    
    TBR=iannucci@chromium.org,nodir@chromium.org,tandrii@chromium.org
    
    Change-Id: Iccf4c6a87cc8fd1f8da46539ce8c16d329b0f88a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 767690
    Reviewed-on: https://chromium-review.googlesource.com/762122
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Commit-Queue: Andrii Shyshkalov <tandrii@chromium.org>

--- recipes/recipes/luci_go.py
+++ recipes/recipes/luci_go.py
@@ -18,7 +18,6 @@ DEPS = [
   'recipe_engine/platform',
   'recipe_engine/properties',
   'recipe_engine/python',
-  'recipe_engine/runtime',
   'recipe_engine/step',
 ]
 
@@ -58,12 +57,6 @@ def RunSteps(api, presubmit, GOARCH):
       co.go_env_step('go', 'build', 'go.chromium.org/luci/...')
       co.go_env_step('go', 'test', 'go.chromium.org/luci/...')
 
-  # TODO(tandrii): remove this.
-  if (api.runtime.is_luci and
-      api.properties.get('buildername') == 'luci-go-precise32'):
-    # We need actual step to ensure gatekeeper picks this up.
-    api.python.inline('simulated failure', 'import sys; sys.exit(1);')
-
 
 def GenTests(api):
   yield (
@@ -99,15 +92,4 @@ def GenTests(api):
         buildername='Luci-go 32-on-64 Tests',
         GOARCH='386',
     )
-  )
-
-  yield (
-    api.test('temp_failure_on_precise32') +
-    api.platform('linux', 32)+
-    api.properties.tryserver(
-        path_config='generic',
-        buildername='luci-go-precise32',
-    ) +
-    api.runtime(is_luci=True, is_experimental=False) +
-    api.step_data('simulated failure', retcode=1)
   )
