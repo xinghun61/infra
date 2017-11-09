1f7c77e4d5a2f9c132fd04d8befd61bc108722d4
Shuotao Gao <stgao@chromium.org>
2017-11-09 12:29:17 -0800

    [Findit] Fix path conflict of protobuf for non-production environment.
    Bug: 775107
    Change-Id: I3f73336c1d928cfc9d79ab8536c13089e1dfce09
    Reviewed-on: https://chromium-review.googlesource.com/761405
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Chan Li <chanli@chromium.org>
    Commit-Queue: Shuotao Gao <stgao@chromium.org>

--- appengine/findit/appengine_config.py
+++ appengine/findit/appengine_config.py
@@ -4,6 +4,15 @@
 
 import os
 
+if not os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine'):
+  # In non-production environment, protobuf and GAE have a package name conflict
+  # on 'google'. This hack is to solve the conflict.
+  import google
+  google.__path__.insert(0,
+                         os.path.join(
+                             os.path.dirname(os.path.realpath(__file__)),
+                             'third_party', 'google'))
+
 from google.appengine.ext import vendor
 
 # Add all the first-party and third-party libraries.
