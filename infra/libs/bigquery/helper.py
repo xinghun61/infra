01aecd1b0d1f813d00e716a608a4e22ef68ce5b4
Katie Thomas <katthomas@google.com>
2017-11-17 09:44:35 -0800

    [bootstrap] update google-cloud-bigquery
    And update BigQueryHelper. The new version obviates the need for insert
    ids, so remove those too.
    
    Bug:784512
    Change-Id: Ia1e8c4b5226758d1aab9fc67b8fd27a702636432
    Reviewed-on: https://chromium-review.googlesource.com/776006
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Katie Thomas <katthomas@google.com>

--- infra/libs/bigquery/helper.py
+++ infra/libs/bigquery/helper.py
@@ -11,43 +11,28 @@ import time
 class BigQueryHelper(object):
   """BigQueryHelper is a thread safe helper for some common BigQuery tasks."""
 
-  def __init__(self, bq_client, now=time.time):
+  def __init__(self, bq_client):
     """bq_client: an instance of google.cloud.bigquery.client.Client"""
     self._bq_client = bq_client
-    self._now = now
-
     self._lock = threading.RLock()
-    self._id_counter = 0
 
-  def _generate_insert_id(self, prefix=None):
-    """Used by BigQuery for deduplicating upload attempts.
-
-      prefix: used to ensure uniqueness between different processes
-    """
-    with self._lock:
-      insert_id = '%s:%s:%d' % (prefix or os.urandom(4).encode('hex'),
-                                self._now(), self._id_counter)
-      self._id_counter += 1
-    return insert_id
-
-  def send_rows(self, dataset_id, table_id, rows, prefix=None):
+  def send_rows(self, dataset_id, table_id, rows):
     """
       rows: list of tuples, one tuple per row. Each tuple should contain data of
         the correct type for each schema field on the current table and in the
         same order as the schema fields.
     """
     with self._lock:
-      row_ids = [self._generate_insert_id(prefix) for _ in rows]
-      table = self._bq_client.dataset(dataset_id).table(table_id)
-      table.reload(self._bq_client)
-      insert_errors = table.insert_data(rows, row_ids)
+      table = self._bq_client.get_table(
+        self._bq_client.dataset(dataset_id).table(table_id))
+      insert_errors = self._bq_client.create_rows(table, rows)
     if insert_errors:
       logging.error('Failed to send event to bigquery: %s', insert_errors)
       raise BigQueryInsertError(insert_errors)
 
 
 class BigQueryInsertError(Exception):
-  """Error from insert_data() call on BigQuery tables.
+  """Error from create_rows() call on BigQuery client.
 
   insert_errors is in the form of a list of mappings, where each mapping
   contains an "index" key corresponding to a row and an "errors" key.
--- infra/libs/bigquery/test/helper_test.py
+++ infra/libs/bigquery/test/helper_test.py
@@ -12,39 +12,24 @@ from infra.libs.bigquery import helper
 class TestBigQueryHelper(unittest.TestCase):
   def setUp(self):
     super(TestBigQueryHelper, self).setUp()
-    now = lambda: 0
     bq_client = mock.Mock()
-    self.bq_helper = helper.BigQueryHelper(bq_client, now=now)
+    self.bq_helper = helper.BigQueryHelper(bq_client)
 
     self.dataset_id = 'test_dataset'
     self.table_id = 'test_table'
-    self.mock_insert_data = bq_client.dataset(self.dataset_id).table(
-        self.table_id).insert_data
-    self.mock_insert_data.return_value = None
-
-  @patch('os.urandom', new=lambda _: '123')
-  def test_generate_insert_id(self):
-    s = '123'.encode('hex')
-    self.assertEqual(self.bq_helper._generate_insert_id(), '%s:0:0' % s)
-    self.assertEqual(self.bq_helper._generate_insert_id('prefix'), 'prefix:0:1')
+    self.table = bq_client.get_table(
+        bq_client.dataset(self.dataset_id).table(self.table_id))
+    self.mock_create_rows = bq_client.create_rows
+    self.mock_create_rows.return_value = None
 
   def test_send_rows(self):
     rows = ['a', 'b', 'c']
-    self.bq_helper.send_rows(self.dataset_id, self.table_id, rows, '1')
-    self.mock_insert_data.assert_any_call(rows,
-                                          ['1:0:%s' % i for i in range(3)])
-
-  @patch('os.urandom', new=lambda _: '123')
-  def test_send_rows_without_ids(self):
-    s = '123'.encode('hex')
-    rows = ['a', 'b', 'c']
-    expected_row_ids = ['%s:0:%d' % (s, i) for i in range(3)]
     self.bq_helper.send_rows(self.dataset_id, self.table_id, rows)
-    self.mock_insert_data.assert_any_call(rows, expected_row_ids)
+    self.mock_create_rows.assert_any_call(self.table, rows)
 
   def test_send_rows_with_errors(self):
     rows = ['a', 'b', 'c']
-    self.mock_insert_data.return_value = [
+    self.mock_create_rows.return_value = [
         {
             'index': 0,
             'errors': ['some err'],
