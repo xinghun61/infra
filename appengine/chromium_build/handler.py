da194ebbcdb2c433441f0ca970d14009e9ce64a4
Erik Staab <estaab@chromium.org>
2017-11-30 19:37:57 +0000

    "Reland chromium-build redirection changes"
    This reverts commit 5ca67600afa285a54ff30e14aa289d9fd825b30f.
    
    Reason for revert: Reland redirect changes for LUCI UI launch.
    
    Original change's description:
    > Revert chromium-build redirection changes
    >
    > Revert "chromium-build: redirect to ci.chromium.org"
    > This reverts commit b78c8f693b613e334cb2220a1e880ee71a8bfcf7.
    > Revert "chromium-build: Reroute all main handler routes to ci.chromium.org"
    > This reverts commit 78468fe158543ce16febda0aba374806794ad882.
    >
    > Bug:
    > Change-Id: Iff16018f14fca626caaea67c72812543f7a6ea77
    > Reviewed-on: https://chromium-review.googlesource.com/784111
    > Reviewed-by: Erik Staab <estaab@chromium.org>
    > Commit-Queue: Ryan Tseng <hinoka@chromium.org>
    
    TBR=hinoka@chromium.org,estaab@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: I16aa68d121cefe7f7a2d5a390166d4bc041b3fc3
    Reviewed-on: https://chromium-review.googlesource.com/801343
    Commit-Queue: Erik Staab <estaab@chromium.org>
    Reviewed-by: Erik Staab <estaab@chromium.org>
    Reviewed-by: Ryan Tseng <hinoka@chromium.org>

--- appengine/chromium_build/handler.py
+++ appengine/chromium_build/handler.py
@@ -135,8 +135,7 @@ class FetchPagesAction(base_page.BasePage):
 class MainAction(base_page.BasePage):
 
   def get(self):
-    args = self.request.query_string
-    self.redirect('/p/chromium/console' + '?' + args)
+    self.redirect('https://ci.chromium.org/p/chromium/g/main/console')
 
 
 # Call initial bootstrap for the app module.
@@ -147,8 +146,6 @@ base_page.bootstrap()
 # http://code.google.com/appengine/docs/python/runtime.html#App_Caching for more
 # info.
 application = webapp2.WSGIApplication(
-  [('/', MainAction),
-   ('/p/(.*)/' + ONE_BOX_URL, OneBoxAction),
-   ('/p/(.*)', PageAction),
+  [('/.*', MainAction),
    ('/tasks/fetch_pages', FetchPagesAction)])
 gae_ts_mon.initialize(application)
