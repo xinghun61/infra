e831a43bf000f8eaba27031853ee089c92b6e9c6
Roberto Carrillo <robertocn@google.com>
2018-01-03 09:58:44 -0800

    [cr-audit-commits] Move package doc to doc.go
    R=stgao,tandrii
    
    Bug:
    Change-Id: I92ba4984bd46d3f02502eca13272ccfdada456b4
    Reviewed-on: https://chromium-review.googlesource.com/831345
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Reviewed-by: Shuotao Gao <stgao@chromium.org>
    Commit-Queue: Roberto Carrillo <robertocn@chromium.org>

--- go/src/infra/appengine/cr-audit-commits/app/commit_auditor_test.go
+++ go/src/infra/appengine/cr-audit-commits/app/commit_auditor_test.go
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Package crauditcommits implements cr-audit-commits.appspot.com services.
 package crauditcommits
 
 import (
