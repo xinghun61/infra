2db973d367f469ea9dc52d9b89f16dd303927aaa
Katie Thomas <katthomas@google.com>
2018-02-16 10:53:35 -0800

    [bigquery] move infra/libs/bigquery to infra_libs
    This way, we can make it available via vpython.
    
    Also:
    * modify __init__.py files to make bigquery available
    * modify setup.py to bump version and specify dependency version
      so infra_libs an import without error
    * update wheels.md for new version
    
    Bug:776430
    Change-Id: Idbf14c568c057a39cb8090c75bbc01c776dbe08d
    Reviewed-on: https://chromium-review.googlesource.com/923553
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Katie Thomas <katthomas@google.com>

--- infra/libs/bigquery/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2017 The Chromium Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-from .helper import BigQueryInsertError
-from .helper import UnsupportedTypeError
-from .helper import message_to_dict
-from .helper import send_rows
--- packages/infra_libs/infra_libs/__init__.py
+++ packages/infra_libs/infra_libs/__init__.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from . import bigquery
 from . import ts_mon  # Must be imported first so httplib2_utils can import it.
 
 from infra_libs.httplib2_utils import AuthError
