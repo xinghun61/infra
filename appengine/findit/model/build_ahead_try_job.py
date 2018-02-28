a5d9aa4575df359d2ea6d4dec2241b9f810e436a
Roberto Carrillo <robertocn@google.com>
2018-02-27 17:39:44 -0800

    [Findit] New model to track build ahead jobs.
    Note that these jobs are not associated to any failure in particular,
    hence no master/builder/buildnumer composite key.
    
    The use case is the following:
    The upcoming cron job to schedule build aheads will first check for
    ongoing build-aheads, and for that, it needs to query the datastore for
    those entries that are still in progress, check with buildbot if they
    are done, and if they are, update their status and metadata, and update
    the cache statistics.
    
    R=stgao,chanli,lijeffrey,wylieb
    BUG=810959
    
    Change-Id: I188d3a899883578c002d8b9a13a4a8bfe591c4b4
    Reviewed-on: https://chromium-review.googlesource.com/938972
    Reviewed-by: Chan Li <chanli@chromium.org>
    Commit-Queue: Roberto Carrillo <robertocn@chromium.org>

---
+++ appengine/findit/model/build_ahead_try_job.py
@@ -0,0 +1,54 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from google.appengine.ext import ndb
+
+from libs import analysis_status
+
+
+class BuildAheadTryJob(ndb.Model):
+  """Represents a single build ahead tryjob.
+
+  Note that this doesn't extend BaseTryJob because this doesn't have multiple
+  tryjob ids per entity, instead, the try job's buildbucket id is the key for
+  the object.
+  """
+
+  # Whether the build is running, index required to get which builds to monitor.
+  running = ndb.BooleanProperty(default=True, indexed=True)
+
+  # The build as returned by buildbucket.
+  last_buildbucket_response = ndb.JsonProperty(indexed=False, compressed=True)
+
+  # win|unix|mac|android|ios
+  platform = ndb.StringProperty(indexed=True)
+
+  cache_name = ndb.StringProperty()
+
+  @staticmethod
+  def Get(build_id):
+    return ndb.Key(BuildAheadTryJob, str(build_id)).get()
+
+  @staticmethod
+  def Create(build_id, platform, cache_name):
+    try_job = BuildAheadTryJob(
+        key=ndb.Key(BuildAheadTryJob, str(build_id)),
+        platform=platform,
+        cache_name=cache_name)
+    return try_job
+
+  @staticmethod
+  def RunningJobs(platform=None):
+    query = BuildAheadTryJob.query(BuildAheadTryJob.running == True)
+    if platform:
+      query = query.filter(BuildAheadTryJob.platform == platform)
+    return query.fetch()
+
+  def MarkComplete(self, metadata):
+    self.running = False
+    self.last_buildbucket_response = metadata
+
+  @property
+  def BuildId(self):
+    return self.key.pairs()[0][1]
---
+++ appengine/findit/model/test/build_ahead_try_job_test.py
@@ -0,0 +1,57 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from gae_libs.testcase import TestCase
+
+from model.build_ahead_try_job import BuildAheadTryJob
+
+
+class BuildAheadTryJobTest(TestCase):
+
+  def testCreate(self):
+    build_id = '8953484774825114240'
+    platform = 'mac'
+    cache_name = 'build_1231234'
+    try_job = BuildAheadTryJob.Create(build_id, platform, cache_name)
+    self.assertEqual(platform, try_job.platform)
+    self.assertEqual(True, try_job.running)
+    self.assertEqual(cache_name, try_job.cache_name)
+
+  def testGet(self):
+    build_id = '8953484774825114288'
+    platform = 'win'
+    cache_name = 'build_abcabcd'
+
+    dummy_metadata = {"dummy": "metadata"}
+
+    try_job_before = BuildAheadTryJob.Create(build_id, platform, cache_name)
+    try_job_before.MarkComplete(dummy_metadata)
+    try_job_before.put()
+
+    try_job_after = BuildAheadTryJob.Get(build_id)
+    self.assertEqual(dummy_metadata, try_job_after.last_buildbucket_response)
+    self.assertEqual(build_id, try_job_after.BuildId)
+    self.assertFalse(try_job_after.running)
+
+  def testRunningJobs(self):
+    build_id = '8953484774825114288'
+    platform = 'win'
+    cache_name = 'build_abcabcd'
+
+    BuildAheadTryJob.Create(build_id, platform, cache_name).put()
+
+    build_id = '8953484774825114289'
+    platform = 'mac'
+    cache_name = 'build_1231234'
+
+    BuildAheadTryJob.Create(build_id, platform, cache_name).put()
+
+    build_id = '8953484774825114290'
+    platform = 'unix'
+    cache_name = 'build_1010100'
+
+    BuildAheadTryJob.Create(build_id, platform, cache_name).put()
+
+    self.assertEqual(3, len(BuildAheadTryJob.RunningJobs()))
+    self.assertEqual(1, len(BuildAheadTryJob.RunningJobs(platform='win')))
