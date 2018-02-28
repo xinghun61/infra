7ad5fac6b8df642b05c80b094cf0d8d4f4a0759a
Joanna Wang <jojwang@chromium.org>
2018-02-28 11:11:31 -0800

    [Monorail]Extract template info.
    Plan on calling GetTemplateInfoFromParsed in both templatecreate
    and templatedetail servlets
    
    Bug: monorail:3537
    Change-Id: I687ab18d6f29353783c0d8bb1eeb571a068d494b
    Reviewed-on: https://chromium-review.googlesource.com/940773
    Commit-Queue: Joanna Wang <jojwang@chromium.org>
    Reviewed-by: Jason Robbins <jrobbins@chromium.org>

--- appengine/monorail/tracker/template_helpers.py
+++ appengine/monorail/tracker/template_helpers.py
@@ -11,12 +11,16 @@ import logging
 from framework import authdata
 from framework import framework_bizobj
 from framework import framework_helpers
+from tracker import field_helpers
+from tracker import tracker_bizobj
+from tracker import tracker_helpers
+from services import user_svc
 
 
 ParsedTemplate = collections.namedtuple(
     'ParsedTemplate', 'name, members_only, summary, summary_must_be_edited, '
     'content, status, owner_str, labels, field_val_strs, component_paths, '
-    'component_required, owner_defaults_to_member')
+    'component_required, owner_defaults_to_member, admin_str')
 
 
 def ParseTemplateRequest(post_data, config):
@@ -47,7 +51,37 @@ def ParseTemplateRequest(post_data, config):
 
   owner_defaults_to_member = post_data.get('owner_defaults_to_member') == 'on'
 
+  admin_str = post_data.get('admin_names', '')
+
   return ParsedTemplate(name, members_only, summary, summary_must_be_edited,
                         content, status, owner_str, labels, field_val_strs,
                         component_paths, component_required,
-                        owner_defaults_to_member)
+                        owner_defaults_to_member, admin_str)
+
+
+def GetTemplateInfoFromParsed(mr, services, parsed, config):
+  admin_ids, _ = tracker_helpers.ParseAdminUsers(
+      mr.cnxn, parsed.admin_str, services.user)
+
+  owner_id = 0
+  if parsed.owner_str:
+    try:
+      user_id = services.user.LookupUserID(mr.cnxn, parsed.owner_str)
+      auth = authdata.AuthData.FromUserID(mr.cnxn, user_id, services)
+      if framework_bizobj.UserIsInProject(mr.project, auth.effective_ids):
+        owner_id = user_id
+      else:
+        mr.errors.owner = 'User is not a member of this project.'
+    except user_svc.NoSuchUserException:
+      mr.errors.owner = 'Owner not found.'
+
+  component_ids = tracker_helpers.LookupComponentIDs(
+      parsed.component_paths, config, mr.errors)
+
+  field_values = field_helpers.ParseFieldValues(
+      mr.cnxn, services.user, parsed.field_val_strs, config)
+  for fv in field_values:
+    logging.info('field_value is %r: %r',
+                 fv.field_id, tracker_bizobj.GetFieldValue(fv, {}))
+
+  return admin_ids, owner_id, component_ids, field_values
--- appengine/monorail/tracker/test/template_helpers_test.py
+++ appengine/monorail/tracker/test/template_helpers_test.py
@@ -9,7 +9,9 @@ import unittest
 
 import settings
 
+from services import service_manager
 from testing import fake
+from testing import testing_helpers
 from tracker import template_helpers
 from tracker import tracker_bizobj
 from proto import tracker_pb2
@@ -18,37 +20,54 @@ from proto import tracker_pb2
 class TemplateHelpers(unittest.TestCase):
 
   def setUp(self):
-    self.config = tracker_bizobj.MakeDefaultProjectIssueConfig(789)
+    self.services = service_manager.Services(
+        user=fake.UserService(),
+        config=fake.ConfigService(),
+        project=fake.ProjectService(),
+        usergroup=fake.UserGroupService())
+    self.project = self.services.project.TestAddProject('proj')
+    self.mr = testing_helpers.MakeMonorailRequest(
+        project=self.project)
+    self.config = self.services.config.GetProjectConfig(
+        'fake cnxn', self.project.project_id)
+    self.fd_1 =  tracker_bizobj.MakeFieldDef(
+        1, 789, 'UXReview', tracker_pb2.FieldTypes.STR_TYPE, None,
+        '', False, False, False, None, None, '', False, '', '',
+        tracker_pb2.NotifyTriggers.NEVER, 'no_action',
+        'Approval for UX review', False)
+    self.fd_2 =  tracker_bizobj.MakeFieldDef(
+        2, 789, 'UXReview', tracker_pb2.FieldTypes.STR_TYPE, None,
+        '', False, False, False, None, None, '', False, '', '',
+        tracker_pb2.NotifyTriggers.NEVER, 'no_action',
+        'Approval for UX review', False)
+    self.cd_1 = tracker_bizobj.MakeComponentDef(
+        1, 789, 'BackEnd', 'doc', False, [111L], [], 100000, 222L)
+
+    self.services.user.TestAddUser('1@ex.com', 111L)
+    self.services.user.TestAddUser('2@ex.com', 222L)
+    self.services.user.TestAddUser('3@ex.com', 333L)
+    self.services.project.TestAddProjectMembers(
+        [111L], self.project, 'OWNER_ROLE')
 
   def testParseTemplateRequest_Empty(self):
     post_data = fake.PostData()
     parsed = template_helpers.ParseTemplateRequest(post_data, self.config)
     self.assertEqual(parsed.name, '')
-    self.assertEqual(parsed.members_only, False)
+    self.assertFalse(parsed.members_only)
     self.assertEqual(parsed.summary, '')
-    self.assertEqual(parsed.summary_must_be_edited, False)
+    self.assertFalse(parsed.summary_must_be_edited)
     self.assertEqual(parsed.content, '')
     self.assertEqual(parsed.status, '')
     self.assertEqual(parsed.owner_str, '')
     self.assertEqual(parsed.labels, [])
     self.assertEqual(parsed.field_val_strs, {})
     self.assertEqual(parsed.component_paths, [])
-    self.assertEqual(parsed.component_required, False)
-    self.assertEqual(parsed.owner_defaults_to_member, False)
+    self.assertFalse(parsed.component_required)
+    self.assertFalse(parsed.owner_defaults_to_member)
 
   def testParseTemplateRequest_Normal(self):
-    fd_1 = tracker_bizobj.MakeFieldDef(
-        1, 789, 'UXReview', tracker_pb2.FieldTypes.STR_TYPE, None,
-        '', False, False, False, None, None, '', False, '', '',
-        tracker_pb2.NotifyTriggers.NEVER, 'no_action',
-        'Approval for UX review', False)
-    fd_2 = tracker_bizobj.MakeFieldDef(
-        2, 789, 'InternalScream', tracker_pb2.FieldTypes.STR_TYPE, None,
-        '', False, False, False, None, None, '', False, '', '',
-        tracker_pb2.NotifyTriggers.NEVER, 'no_action',
-        'Approval for UX review', False)
-    self.config.field_defs.append(fd_1)
-    self.config.field_defs.append(fd_2)
+    self.config.field_defs.append(self.fd_1)
+    self.config.field_defs.append(self.fd_2)
     post_data = fake.PostData(
         name=['sometemplate'],
         members_only=['on'],
@@ -62,19 +81,54 @@ class TemplateHelpers(unittest.TestCase):
         custom_2=['MOOD'],
         components=['hey, hey2,he3'],
         component_required=['on'],
-        owner_defaults_to_memeber=['no']
+        owner_defaults_to_memeber=['no'],
+        admin_names=['jojwang@test.com, annajo@test.com']
     )
 
     parsed = template_helpers.ParseTemplateRequest(post_data, self.config)
     self.assertEqual(parsed.name, 'sometemplate')
-    self.assertEqual(parsed.members_only, True)
+    self.assertTrue(parsed.members_only)
     self.assertEqual(parsed.summary, 'TLDR')
-    self.assertEqual(parsed.summary_must_be_edited, True)
+    self.assertTrue(parsed.summary_must_be_edited)
     self.assertEqual(parsed.content, 'HEY WHY')
     self.assertEqual(parsed.status, 'Accepted')
     self.assertEqual(parsed.owner_str, 'someone@world.com')
     self.assertEqual(parsed.labels, ['label-One', 'label-Two'])
     self.assertEqual(parsed.field_val_strs, {1: ['NO'], 2: ['MOOD']})
     self.assertEqual(parsed.component_paths, ['hey', 'hey2', 'he3'])
-    self.assertEqual(parsed.component_required, True)
-    self.assertEqual(parsed.owner_defaults_to_member, False)
+    self.assertTrue(parsed.component_required)
+    self.assertFalse(parsed.owner_defaults_to_member)
+    self.assertEqual(parsed.admin_str, 'jojwang@test.com, annajo@test.com')
+
+  def testGetTemplateInfoFromParsed_Normal(self):
+    self.config.field_defs.append(self.fd_1)
+    self.config.field_defs.append(self.fd_2)
+    self.config.component_defs.append(self.cd_1)
+    parsed = template_helpers.ParsedTemplate(
+        'template', True, 'summary', True, 'content', 'Available',
+        '1@ex.com', ['label1', 'label1'], {1: ['NO'], 2: ['MOOD']},
+        ['BackEnd'], True, True, '2@ex.com')
+    (admin_ids, owner_id,
+     component_ids, field_values) = template_helpers.GetTemplateInfoFromParsed(
+        self.mr, self.services, parsed, self.config)
+    self.assertEqual(admin_ids, [222L])
+    self.assertEqual(owner_id, 111L)
+    self.assertEqual(component_ids, [1])
+    self.assertEqual(field_values[0].str_value, 'NO')
+    self.assertEqual(field_values[1].str_value, 'MOOD')
+
+  def testGetTemplateInfoFromParsed_Errors(self):
+    self.config.field_defs.append(self.fd_1)
+    self.config.field_defs.append(self.fd_2)
+    parsed = template_helpers.ParsedTemplate(
+        'template', True, 'summary', True, 'content', 'Available',
+        '4@ex.com', ['label1', 'label1'], {1: ['NO'], 2: ['MOOD']},
+        ['BackEnd'], True, True, '2@ex.com')
+    (admin_ids, _owner_id,
+     _component_ids, field_values) = template_helpers.GetTemplateInfoFromParsed(
+        self.mr, self.services, parsed, self.config)
+    self.assertEqual(admin_ids, [222L])
+    self.assertEqual(field_values[0].str_value, 'NO')
+    self.assertEqual(field_values[1].str_value, 'MOOD')
+    self.assertEqual(self.mr.errors.owner, 'Owner not found.')
+    self.assertEqual(self.mr.errors.components, 'Unknown component BackEnd')
