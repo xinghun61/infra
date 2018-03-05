xinghun61/infra

commit 7ffcd09433858526ebbe72576aa70ff4bf2ce84b

[Monorail] Add Charts service for querying
Bug: monorail:3518
Change-Id: I66c268e202bcdeeabc8e3fd1e2df4c4622265321
Reviewed-on: https://chromium-review.googlesource.com/923164
Commit-Queue: Jeff Carpenter <jeffcarp@chromium.org>
Reviewed-by: Jason Robbins <jrobbins@chromium.org>

appengine/monorail/search/search_helpers.py
@@ -1,0 +1,35 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is govered by a BSD-style
+# license that can be found in the LICENSE file or at
+# https://developers.google.com/open-source/licenses/bsd
+
+RESTRICT_VIEW_PATTERN = 'restrict-view-%'
+
+
+def GetPersonalAtRiskLabelIDs(
+  cnxn, _user, config_svc, effective_ids, project, perms):
+  """Return list of label_ids for restriction labels that user can't view.
+
+  Args:
+    cnxn: An instance of MonorailConnection.
+    _user: Unused.
+    config_svc: An instance of ConfigService.
+    effective_ids: The effective IDs of the current user.
+    project: A project object for the current project.
+    perms: A PermissionSet for the current user.
+  Returns:
+    A list of LabelDef IDs the current user is forbidden to access.
+  """
+  at_risk_label_ids = []
+  label_def_rows = config_svc.GetLabelDefRowsAnyProject(
+    cnxn, where=[('LOWER(label) LIKE %s', [RESTRICT_VIEW_PATTERN])])
+
+  for label_id, _pid, _rank, label, _docstring, _hidden in label_def_rows:
+    label_lower = label.lower()
+    needed_perm = label_lower.split('-', 1)[-1]
+    needed_perm = needed_perm.replace('-', '')
+
+    if not perms.CanUsePerm(needed_perm, effective_ids, project, []):
+      at_risk_label_ids.append(label_id)
+
+  return at_risk_label_ids

appengine/monorail/search/test/search_helpers_test.py
@@ -1,0 +1,109 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is govered by a BSD-style
+# license that can be found in the LICENSE file or at
+# https://developers.google.com/open-source/licenses/bsd
+
+"""Unit tests for monorail.search.search_helpers."""
+
+import mox
+import unittest
+
+from search import search_helpers
+
+from google.appengine.ext import testbed
+from framework import permissions
+from framework import sql
+from services import chart_svc
+from services import service_manager
+from testing import fake
+
+
+def MakeChartService(my_mox):
+  chart_service = chart_svc.ChartService()
+  for table_var in ['issuesnapshot_tbl', 'labeldef_tbl']:
+    setattr(chart_service, table_var, my_mox.CreateMock(sql.SQLTableManager))
+  return chart_service
+
+
+class SearchHelpersTest(unittest.TestCase):
+  """Tests for functions in search_helpers.
+
+  Also covered by search.backendnonviewable.GetAtRiskIIDs cases.
+  """
+
+  def setUp(self):
+    self.testbed = testbed.Testbed()
+    self.testbed.activate()
+    self.testbed.init_memcache_stub()
+
+    self.mox = mox.Mox()
+    self.cnxn = self.mox.CreateMock(sql.MonorailConnection)
+    self.services = service_manager.Services()
+    self.services.chart = MakeChartService(self.mox)
+    self.config_service = fake.ConfigService()
+
+  def testGetPersonalAtRiskLabelIDs_ReadOnly(self):
+    """Test returns risky IDs a read-only user cannot access."""
+    self.mox.StubOutWithMock(self.config_service, 'GetLabelDefRowsAnyProject')
+    self.config_service.GetLabelDefRowsAnyProject(
+      self.cnxn, where=[('LOWER(label) LIKE %s', ['restrict-view-%'])]
+    ).AndReturn([
+      (123, 789, 0, 'Restrict-Edit-Issue', 'docstring', 0),
+      (124, 789, 0, 'Restrict-View', 'docstring', 0),
+    ])
+
+    self.mox.ReplayAll()
+    ids = search_helpers.GetPersonalAtRiskLabelIDs(
+      self.cnxn,
+      None,
+      self.config_service,
+      effective_ids=[10L, 20L],
+      project=fake.Project(project_id=789),
+      perms=permissions.READ_ONLY_PERMISSIONSET)
+    self.mox.VerifyAll()
+
+    self.assertEqual(ids, [123])
+
+  def testGetPersonalAtRiskLabelIDs_LoggedInUser(self):
+    """Test returns restricted label IDs a logged in user cannot access."""
+    self.mox.StubOutWithMock(self.config_service, 'GetLabelDefRowsAnyProject')
+    self.config_service.GetLabelDefRowsAnyProject(
+      self.cnxn, where=[('LOWER(label) LIKE %s', ['restrict-view-%'])]
+    ).AndReturn([
+      (123, 789, 0, 'Restrict-Edit-Issue', 'docstring', 0),
+      (124, 789, 0, 'Restrict-View', 'docstring', 0),
+    ])
+
+    self.mox.ReplayAll()
+    ids = search_helpers.GetPersonalAtRiskLabelIDs(
+      self.cnxn,
+      None,
+      self.config_service,
+      effective_ids=[10L, 20L],
+      project=fake.Project(project_id=789),
+      perms=permissions.USER_PERMISSIONSET)
+    self.mox.VerifyAll()
+
+    self.assertEqual(ids, [123])
+
+  def testGetPersonalAtRiskLabelIDs_Admin(self):
+    """Test returns nothing for an admin (who can view everything)."""
+    self.mox.StubOutWithMock(self.config_service, 'GetLabelDefRowsAnyProject')
+    self.config_service.GetLabelDefRowsAnyProject(
+      self.cnxn, where=[('LOWER(label) LIKE %s', ['restrict-view-%'])]
+    ).AndReturn([
+      (123, 789, 0, 'Restrict-Edit-Issue', 'docstring', 0),
+      (124, 789, 0, 'Restrict-View', 'docstring', 0),
+    ])
+
+    self.mox.ReplayAll()
+    ids = search_helpers.GetPersonalAtRiskLabelIDs(
+      self.cnxn,
+      None,
+      self.config_service,
+      effective_ids=[10L, 20L],
+      project=fake.Project(project_id=789),
+      perms=permissions.ADMIN_PERMISSIONSET)
+    self.mox.VerifyAll()
+
+    self.assertEqual(ids, [])

appengine/monorail/search/backendnonviewable.py
@@ -21,9 +21,9 @@ from framework import framework_helpers
 from framework import jsonfeed
 from framework import permissions
 from framework import sql
+from search import search_helpers
 
 
-RESTRICT_VIEW_PATTERN = 'restrict-view-%'
 
 # We cache the set of IIDs that a given user cannot view, and we invalidate
 # that set when the issues are changed via Monorail.  Also, we limit the live
@@ -110,26 +110,13 @@ class BackendNonviewable(jsonfeed.InternalTask):
   def GetAtRiskIIDs(
     self, cnxn, user, effective_ids, project, perms, shard_id):
     """Return IIDs of restricted issues that user might not be able to view."""
-    at_risk_label_ids = self.GetPersonalAtRiskLabelIDs(
-      cnxn, user, effective_ids, project, perms)
+    at_risk_label_ids = search_helpers.GetPersonalAtRiskLabelIDs(
+      cnxn, user, self.services.config, effective_ids, project, perms)
     at_risk_iids = self.services.issue.GetIIDsByLabelIDs(
       cnxn, at_risk_label_ids, project.project_id, shard_id)
 
     return at_risk_iids
 
-  def GetPersonalAtRiskLabelIDs(
-    self, cnxn, _user, effective_ids, project, perms):
-    """Return list of label_ids for restriction labels that user can't view."""
-    at_risk_label_ids = []
-    label_def_rows = self.services.config.GetLabelDefRowsAnyProject(
-      cnxn, where=[('LOWER(label) LIKE %s', [RESTRICT_VIEW_PATTERN])])
-    for label_id, _pid, _rank, label, _docstring, _hidden in label_def_rows:
-      label_lower = label.lower()
-      needed_perm = label_lower.split('-', 2)[-1]
-      if not perms.CanUsePerm(needed_perm, effective_ids, project, []):
-        at_risk_label_ids.append(label_id)
-
-    return at_risk_label_ids
 
   def GetViewableIIDs(self, cnxn, effective_ids, project_id, shard_id):
     """Return IIDs of issues that user can view because they participate."""

appengine/monorail/search/test/backendnonviewable_test.py
@@ -67,7 +67,7 @@ class BackendNonviewableTest(unittest.TestCase):
     fake_restriction_label_rows = []
     fake_restriction_label_ids = []
     fake_at_risk_iids = []
-    self.mox.StubOutWithMock(self.services.config, 'GetLabelDefRowsAnyProject')    
+    self.mox.StubOutWithMock(self.services.config, 'GetLabelDefRowsAnyProject')
     self.services.config.GetLabelDefRowsAnyProject(
         self.mr.cnxn, where=[('LOWER(label) LIKE %s', ['restrict-view-%'])]
         ).AndReturn(fake_restriction_label_rows)
@@ -78,7 +78,7 @@ class BackendNonviewableTest(unittest.TestCase):
     self.mox.ReplayAll()
 
     at_risk_iids = self.servlet.GetAtRiskIIDs(
-        self.mr.cnxn, self.mr.auth.user_pb, self.mr.auth.effective_ids, 
+        self.mr.cnxn, self.mr.auth.user_pb, self.mr.auth.effective_ids,
         self.project, self.mr.perms, self.mr.shard_id)
     self.mox.VerifyAll()
     self.assertEqual([], at_risk_iids)
@@ -91,7 +91,7 @@ class BackendNonviewableTest(unittest.TestCase):
         ]
     fake_restriction_label_ids = [123, 234]
     fake_at_risk_iids = []
-    self.mox.StubOutWithMock(self.services.config, 'GetLabelDefRowsAnyProject')    
+    self.mox.StubOutWithMock(self.services.config, 'GetLabelDefRowsAnyProject')
     self.services.config.GetLabelDefRowsAnyProject(
         self.mr.cnxn, where=[('LOWER(label) LIKE %s', ['restrict-view-%'])]
         ).AndReturn(fake_restriction_label_rows)
@@ -102,7 +102,7 @@ class BackendNonviewableTest(unittest.TestCase):
     self.mox.ReplayAll()
 
     at_risk_iids = self.servlet.GetAtRiskIIDs(
-        self.mr.cnxn, self.mr.auth.user_pb, self.mr.auth.effective_ids, 
+        self.mr.cnxn, self.mr.auth.user_pb, self.mr.auth.effective_ids,
         self.project, self.mr.perms, self.mr.shard_id)
     self.mox.VerifyAll()
     self.assertEqual([], at_risk_iids)
@@ -115,7 +115,7 @@ class BackendNonviewableTest(unittest.TestCase):
         ]
     fake_restriction_label_ids = [123, 234]
     fake_at_risk_iids = [432, 543]
-    self.mox.StubOutWithMock(self.services.config, 'GetLabelDefRowsAnyProject')    
+    self.mox.StubOutWithMock(self.services.config, 'GetLabelDefRowsAnyProject')
     self.services.config.GetLabelDefRowsAnyProject(
       self.mr.cnxn, where=[('LOWER(label) LIKE %s', ['restrict-view-%'])]
       ).AndReturn(fake_restriction_label_rows)
@@ -126,13 +126,10 @@ class BackendNonviewableTest(unittest.TestCase):
     self.mox.ReplayAll()
 
     at_risk_iids = self.servlet.GetAtRiskIIDs(
-        self.mr.cnxn, self.mr.auth.user_pb, self.mr.auth.effective_ids, 
+        self.mr.cnxn, self.mr.auth.user_pb, self.mr.auth.effective_ids,
         self.project, self.mr.perms, self.mr.shard_id)
     self.mox.VerifyAll()
     self.assertEqual([432, 543], at_risk_iids)
-
-  def testGetPersonalAtRiskLabelIDs(self):
-    pass  # TODO(jrobbins): For now, this is covered by GetAtRiskIIDs cases.
 
   def testGetViewableIIDs_Anon(self):
     """Anon users are never participants in any issues."""
@@ -141,8 +138,8 @@ class BackendNonviewableTest(unittest.TestCase):
     self.assertEqual([], ok_iids)
 
   def testGetViewableIIDs_NoIssues(self):
-    """This visitor does not participate in any issues."""    
-    self.mox.StubOutWithMock(self.services.issue, 'GetIIDsByParticipant')    
+    """This visitor does not participate in any issues."""
+    self.mox.StubOutWithMock(self.services.issue, 'GetIIDsByParticipant')
     self.services.issue.GetIIDsByParticipant(
       self.mr.cnxn, {111L}, [789], 2).AndReturn([])
     self.mox.ReplayAll()
@@ -153,8 +150,8 @@ class BackendNonviewableTest(unittest.TestCase):
     self.assertEqual([], ok_iids)
 
   def testGetViewableIIDs_SomeIssues(self):
-    """This visitor  participates in some issues."""    
-    self.mox.StubOutWithMock(self.services.issue, 'GetIIDsByParticipant')    
+    """This visitor  participates in some issues."""
+    self.mox.StubOutWithMock(self.services.issue, 'GetIIDsByParticipant')
     self.services.issue.GetIIDsByParticipant(
       self.mr.cnxn, {111L}, [789], 2).AndReturn([543, 654])
     self.mox.ReplayAll()

appengine/monorail/services/chart_svc.py
@@ -1,0 +1,126 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is govered by a BSD-style
+# license that can be found in the LICENSE file or at
+# https://developers.google.com/open-source/licenses/bsd
+
+"""A service for querying data for charts.
+
+Functions for querying the IssueSnapshot table and associated join tables.
+"""
+
+from framework import sql
+from search import search_helpers
+
+
+ISSUESNAPSHOT_TABLE_NAME = 'IssueSnapshot'
+
+
+class ChartService(object):
+  """Class for querying chart data."""
+
+  def __init__(self):
+    self.issuesnapshot_tbl = sql.SQLTableManager(ISSUESNAPSHOT_TABLE_NAME)
+
+  def QueryIssueSnapshots(self, cnxn, config_svc, unixtime, bucketby,
+                          effective_ids, project, perms, label_prefix=None):
+    """Queries historical issue counts grouped by label or component.
+
+    Args:
+      cnxn: A MonorailConnection instance.
+      config_svc: A ConfigService instance.
+      unixtime: An integer representing the Unix time in seconds.
+      bucketby: Which dimension to group by. Either 'label' or 'component'.
+      effective_ids: The effective User IDs associated with the current user.
+      project: A project object representing the current project.
+      perms: A permissions object associated with the current user.
+      label_prefix: Required when bucketby is 'label.' Will limit the query to
+        only labels with the specified prefix (for example 'Pri').
+
+    Returns:
+      A list of pairs with values:
+        (label or component name,
+         number of occurrences for the given timestamp)
+    """
+    restricted_label_ids = search_helpers.GetPersonalAtRiskLabelIDs(
+      cnxn, None, config_svc, effective_ids, project, perms)
+
+    left_joins = [
+      ('Issue ON IssueSnapshot.issue_id = Issue.id', []),
+    ]
+
+    if restricted_label_ids:
+      left_joins.append(
+        (('Issue2Label AS Forbidden_label'
+          ' ON Issue.id = Forbidden_label.issue_id'
+          ' AND Forbidden_label.label_id IN (%s)' % (
+            sql.PlaceHolders(restricted_label_ids)
+        )), restricted_label_ids))
+
+    if effective_ids:
+      left_joins.append(
+        ('Issue2Cc AS I2cc'
+         ' ON Issue.id = I2cc.issue_id'
+         ' AND I2cc.cc_id IN (%s)' % sql.PlaceHolders(effective_ids),
+         effective_ids))
+
+    # TODO(jeffcarp, monorail:3534): Add support for user's query params.
+    # TODO(jeffcarp): Handle case where there are issues with no labels.
+    where = [
+      ('IssueSnapshot.period_start <= %s', [unixtime]),
+      ('IssueSnapshot.period_end > %s', [unixtime]),
+      ('IssueSnapshot.project_id = %s', [project.project_id]),
+      ('IssueSnapshot.is_open = %s', [True]),
+      ('Issue.is_spam = %s', [False]),
+      ('Issue.deleted = %s', [False]),
+    ]
+
+    forbidden_label_clause = 'Forbidden_label.label_id IS NULL'
+    if effective_ids:
+      if restricted_label_ids:
+        forbidden_label_clause = ' OR %s' % forbidden_label_clause
+      else:
+        forbidden_label_clause =  ''
+
+      where.append(
+        ((
+          '(Issue.reporter_id IN (%s)'
+          ' OR Issue.owner_id IN (%s)'
+          ' OR I2cc.cc_id IS NOT NULL'
+          '%s)'
+        ) % (
+          sql.PlaceHolders(effective_ids), sql.PlaceHolders(effective_ids),
+          forbidden_label_clause
+        ),
+          list(effective_ids) + list(effective_ids)
+        ))
+    else:
+      where.append((forbidden_label_clause, []))
+
+    if bucketby == 'component':
+      cols = ['Comp.path', 'COUNT(DISTINCT(IssueSnapshot.issue_id))']
+      left_joins.extend([
+        (('IssueSnapshot2Component AS Is2c ON'
+          ' Is2c.issuesnapshot_id = IssueSnapshot.id'), []),
+        ('ComponentDef AS Comp ON Comp.id = Is2c.component_id', []),
+      ])
+      group_by = ['Comp.path']
+    elif bucketby == 'label':
+      cols = ['Lab.label', 'COUNT(DISTINCT(IssueSnapshot.issue_id))']
+      left_joins.extend([
+        (('IssueSnapshot2Label AS Is2l'
+          ' ON Is2l.issuesnapshot_id = IssueSnapshot.id'), []),
+        ('LabelDef AS Lab ON Lab.id = Is2l.label_id', []),
+      ])
+
+      if not label_prefix:
+        raise ValueError('`label_prefix` required when grouping by label.')
+
+      # TODO(jeffcarp): If LookupIDsOfLabelsMatching() is called on output,
+      # ensure regex is case-insensitive.
+      where.append(('LOWER(Lab.label) LIKE %s', [label_prefix.lower() + '-%']))
+      group_by = ['Lab.label']
+    else:
+      raise ValueError('`bucketby` must be in (component, label)')
+
+    return self.issuesnapshot_tbl.Select(cnxn, cols=cols,
+      left_joins=left_joins, where=where, group_by=group_by)

commit 3cee5283f1b62013050e922d35aa7c24f5327f43

[Monorail] Fix permission checking in GetPersonalAtRiskLabelIDs() and add the case for admins.
Restriction labels have three parts:
+ The keyword "Restrict"
+ The action being restricted, e.g., "View" or "Edit"
+ The other permission that the user needs to have in order to use their
  action permission, e.g., "Google".

This fixes a problem introduced in 7ffcd09 that split after the first dash.

R=jeffcarp@chromium.org

Bug: monorail:3518
Change-Id: I2b4ad348e70770151829a4455f4d8acd6485a9c9
Reviewed-on: https://chromium-review.googlesource.com/949968
Reviewed-by: Jeff Carpenter <jeffcarp@chromium.org>
Commit-Queue: Jason Robbins <jrobbins@chromium.org>

appengine/monorail/search/search_helpers.py
@@ -7,12 +7,12 @@ RESTRICT_VIEW_PATTERN = 'restrict-view-%'
 
 
 def GetPersonalAtRiskLabelIDs(
-  cnxn, _user, config_svc, effective_ids, project, perms):
+  cnxn, user, config_svc, effective_ids, project, perms):
   """Return list of label_ids for restriction labels that user can't view.
 
   Args:
     cnxn: An instance of MonorailConnection.
-    _user: Unused.
+    user: User PB for the signed in user making the request, or None for anon.
     config_svc: An instance of ConfigService.
     effective_ids: The effective IDs of the current user.
     project: A project object for the current project.
@@ -20,14 +20,16 @@ def GetPersonalAtRiskLabelIDs(
   Returns:
     A list of LabelDef IDs the current user is forbidden to access.
   """
+  if user and user.is_site_admin:
+    return []
+
   at_risk_label_ids = []
   label_def_rows = config_svc.GetLabelDefRowsAnyProject(
     cnxn, where=[('LOWER(label) LIKE %s', [RESTRICT_VIEW_PATTERN])])
 
   for label_id, _pid, _rank, label, _docstring, _hidden in label_def_rows:
     label_lower = label.lower()
-    needed_perm = label_lower.split('-', 1)[-1]
-    needed_perm = needed_perm.replace('-', '')
+    needed_perm = label_lower.split('-', 2)[-1]
 
     if not perms.CanUsePerm(needed_perm, effective_ids, project, []):
       at_risk_label_ids.append(label_id)

appengine/monorail/search/test/search_helpers_test.py
@@ -13,6 +13,7 @@ from search import search_helpers
 from google.appengine.ext import testbed
 from framework import permissions
 from framework import sql
+from proto import user_pb2
 from services import chart_svc
 from services import service_manager
 from testing import fake
@@ -41,6 +42,7 @@ class SearchHelpersTest(unittest.TestCase):
     self.services = service_manager.Services()
     self.services.chart = MakeChartService(self.mox)
     self.config_service = fake.ConfigService()
+    self.user = user_pb2.User()
 
   def testGetPersonalAtRiskLabelIDs_ReadOnly(self):
     """Test returns risky IDs a read-only user cannot access."""
@@ -48,21 +50,21 @@ class SearchHelpersTest(unittest.TestCase):
     self.config_service.GetLabelDefRowsAnyProject(
       self.cnxn, where=[('LOWER(label) LIKE %s', ['restrict-view-%'])]
     ).AndReturn([
-      (123, 789, 0, 'Restrict-Edit-Issue', 'docstring', 0),
-      (124, 789, 0, 'Restrict-View', 'docstring', 0),
+      (123, 789, 0, 'Restrict-View-Google', 'docstring', 0),
+      (124, 789, 0, 'Restrict-View-SecurityTeam', 'docstring', 0),
     ])
 
     self.mox.ReplayAll()
     ids = search_helpers.GetPersonalAtRiskLabelIDs(
       self.cnxn,
-      None,
+      self.user,
       self.config_service,
       effective_ids=[10L, 20L],
       project=fake.Project(project_id=789),
       perms=permissions.READ_ONLY_PERMISSIONSET)
     self.mox.VerifyAll()
 
-    self.assertEqual(ids, [123])
+    self.assertEqual(ids, [123, 124])
 
   def testGetPersonalAtRiskLabelIDs_LoggedInUser(self):
     """Test returns restricted label IDs a logged in user cannot access."""
@@ -70,36 +72,52 @@ class SearchHelpersTest(unittest.TestCase):
     self.config_service.GetLabelDefRowsAnyProject(
       self.cnxn, where=[('LOWER(label) LIKE %s', ['restrict-view-%'])]
     ).AndReturn([
-      (123, 789, 0, 'Restrict-Edit-Issue', 'docstring', 0),
-      (124, 789, 0, 'Restrict-View', 'docstring', 0),
+      (123, 789, 0, 'Restrict-View-Google', 'docstring', 0),
+      (124, 789, 0, 'Restrict-View-SecurityTeam', 'docstring', 0),
     ])
 
     self.mox.ReplayAll()
     ids = search_helpers.GetPersonalAtRiskLabelIDs(
       self.cnxn,
-      None,
+      self.user,
       self.config_service,
       effective_ids=[10L, 20L],
       project=fake.Project(project_id=789),
       perms=permissions.USER_PERMISSIONSET)
     self.mox.VerifyAll()
 
-    self.assertEqual(ids, [123])
+    self.assertEqual(ids, [123, 124])
 
-  def testGetPersonalAtRiskLabelIDs_Admin(self):
-    """Test returns nothing for an admin (who can view everything)."""
+  def testGetPersonalAtRiskLabelIDs_UserWithRVG(self):
+    """Test returns restricted label IDs a logged in user cannot access."""
     self.mox.StubOutWithMock(self.config_service, 'GetLabelDefRowsAnyProject')
     self.config_service.GetLabelDefRowsAnyProject(
       self.cnxn, where=[('LOWER(label) LIKE %s', ['restrict-view-%'])]
     ).AndReturn([
-      (123, 789, 0, 'Restrict-Edit-Issue', 'docstring', 0),
-      (124, 789, 0, 'Restrict-View', 'docstring', 0),
+      (123, 789, 0, 'Restrict-View-Google', 'docstring', 0),
+      (124, 789, 0, 'Restrict-View-SecurityTeam', 'docstring', 0),
     ])
 
     self.mox.ReplayAll()
+    perms = permissions.PermissionSet(['Google'])
     ids = search_helpers.GetPersonalAtRiskLabelIDs(
       self.cnxn,
-      None,
+      self.user,
+      self.config_service,
+      effective_ids=[10L, 20L],
+      project=fake.Project(project_id=789),
+      perms=perms)
+    self.mox.VerifyAll()
+
+    self.assertEqual(ids, [124])
+
+  def testGetPersonalAtRiskLabelIDs_Admin(self):
+    """Test returns nothing for an admin (who can view everything)."""
+    self.user.is_site_admin = True
+    self.mox.ReplayAll()
+    ids = search_helpers.GetPersonalAtRiskLabelIDs(
+      self.cnxn,
+      self.user,
       self.config_service,
       effective_ids=[10L, 20L],
       project=fake.Project(project_id=789),