e7d4b847b22c67b1d0cc0d5a1eb3a02d28b6fb56
Brandon Wylie <wylieb@chromium.org>
2018-03-22 13:39:53 -0700

    [Findit] Flake Analyzer - Fix server error for browser_tests query
    Bug: 801206
    Change-Id: I3e88de2cedcab1b35d7f5a95cb03e54384dd371f
    Reviewed-on: https://chromium-review.googlesource.com/976549
    Reviewed-by: Jeffrey Li <lijeffrey@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>

--- appengine/findit/handlers/flake/list_flakes.py
+++ appengine/findit/handlers/flake/list_flakes.py
@@ -123,8 +123,9 @@ class ListFlakes(BaseHandler):
               master_flake_analysis.key.urlsafe(),
           'master_name':
               master_flake_analysis.master_name,
-          'request_utc_timestamp':
-              time_util.ConvertToTimestamp(master_flake_analysis.request_time),
+          'request_utc_timestamp': (
+              time_util.ConvertToTimestamp(master_flake_analysis.request_time)
+              if master_flake_analysis.request_time else 'None'),
           'result_status':
               result_status.RESULT_STATUS_TO_DESCRIPTION.get(
                   master_flake_analysis.result_status),
