0f7d92bf859e0ee7e19ecf38ea11d16a3555e410
Quinten Yearsley <qyearsley@chromium.org>
2018-04-30 11:59:22 -0700

    [tricium] Refactor analyzer unit tests
    In this CL:
     - Remove the "Test environment" level in tests where
       there is no test environment set up. Even if there
       is test environment set up, this level is not required
       unless we make assertions about the set-up.
     - Add blank lines between tests; this might help readability
     - Minor refactoring/reformatting. This includes using
       ShouldResemble on output comments in the spacey unit test,
       so a couple other fields in the comments are checked.
    
    Change-Id: I202927c648c7c834652c5a844fca35dd82855656
    Reviewed-on: https://chromium-review.googlesource.com/1034299
    Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>
    Commit-Queue: Marc-Antoine Ruel <maruel@chromium.org>

--- go/src/infra/tricium/functions/copyright/copyright_test.go
+++ go/src/infra/tricium/functions/copyright/copyright_test.go
@@ -11,56 +11,79 @@ import (
 	"infra/tricium/api/v1"
 )
 
-func TestCheckMissingCopyright(t *testing.T) {
-	Convey("Test Environment", t, func() {
-		Convey("Correct BSD copyright", func() {
-			path := "test/src/good.cpp"
-			So(checkCopyright(path), ShouldBeNil)
-		})
-		Convey("Correct MIT copyright", func() {
-			path := "test/src/good_mit.cpp"
-			So(checkCopyright(path), ShouldBeNil)
-		})
-		Convey("Incorrect copyright", func() {
-			path := "test/src/bad.cpp"
-			c := checkCopyright(path)
-			So(c, ShouldNotBeNil)
-			So(c, ShouldResemble, &tricium.Data_Comment{
-				Category:  "Copyright/Incorrect",
-				Message:   "Incorrect copyright statement.\nUse the following for BSD:\nCopyright <year> The <group> Authors. All rights reserved.\nUse of this source code is governed by a BSD-style license that can be\nfound in the LICENSE file.\n\nOr the following for MIT: Copyright <year> The <group> Authors\n\nUse of this source code is governed by a MIT-style\nlicense that can be found in the LICENSE file or at\nhttps://opensource.org/licenses/MIT",
-				Path:      path,
-				StartLine: 1,
-				EndLine:   1,
-				StartChar: 0,
-				EndChar:   1,
-				Url:       "https://chromium.googlesource.com/chromium/src/+/master/styleguide/c++/c++.md#file-headers",
-			})
-		})
-		Convey("Missing copyright", func() {
-			path := "test/src/missing.cpp"
-			c := checkCopyright(path)
-			So(c, ShouldNotBeNil)
-			So(c, ShouldResemble, &tricium.Data_Comment{
-				Category:  "Copyright/Missing",
-				Message:   "Missing copyright statement.\nUse the following for BSD:\nCopyright <year> The <group> Authors. All rights reserved.\nUse of this source code is governed by a BSD-style license that can be\nfound in the LICENSE file.\n\nOr the following for MIT: Copyright <year> The <group> Authors\n\nUse of this source code is governed by a MIT-style\nlicense that can be found in the LICENSE file or at\nhttps://opensource.org/licenses/MIT",
-				Path:      path,
-				StartLine: 1,
-				EndLine:   1,
-				StartChar: 0,
-				EndChar:   1,
-				Url:       "https://chromium.googlesource.com/chromium/src/+/master/styleguide/c++/c++.md#file-headers",
-			})
+// These tests read from files on the filesystem, so modifying the tests may
+// require modifying the example test files.
+const (
+	goodBsd string = "test/src/good.cpp"
+	goodMit string = "test/src/good_mit.cpp"
+	badBsd  string = "test/src/bad.cpp"
+	missing string = "test/src/missing.cpp"
+	old     string = "test/src/old.cpp"
+)
+
+func TestCopyrightChecker(t *testing.T) {
+
+	Convey("Produces no comment for file with correct BSD copyright", t, func() {
+		So(checkCopyright(goodBsd), ShouldBeNil)
+	})
+
+	Convey("Produces no comment for file with correct MIT copyright", t, func() {
+		So(checkCopyright(goodMit), ShouldBeNil)
+	})
+
+	Convey("Finds an issue when copyright doesn't match expected pattern", t, func() {
+		c := checkCopyright(badBsd)
+		So(c, ShouldNotBeNil)
+		So(c, ShouldResemble, &tricium.Data_Comment{
+			Category: "Copyright/Incorrect",
+			Message: ("Incorrect copyright statement.\n" +
+				"Use the following for BSD:\n" +
+				"Copyright <year> The <group> Authors. All rights reserved.\n" +
+				"Use of this source code is governed by a BSD-style license that can be\n" +
+				"found in the LICENSE file.\n\n" +
+				"Or the following for MIT: Copyright <year> The <group> Authors\n\n" +
+				"Use of this source code is governed by a MIT-style\n" +
+				"license that can be found in the LICENSE file or at\n" +
+				"https://opensource.org/licenses/MIT"),
+			Path:      badBsd,
+			StartLine: 1,
+			EndLine:   1,
+			StartChar: 0,
+			EndChar:   1,
+			Url:       "https://chromium.googlesource.com/chromium/src/+/master/styleguide/c++/c++.md#file-headers",
 		})
 	})
 
-	Convey("Out-of-date copyright", t, func() {
-		path := "test/src/old.cpp"
-		c := checkCopyright(path)
+	Convey("Makes a comment when there appears to be no copyright header", t, func() {
+		c := checkCopyright(missing)
+		So(c, ShouldNotBeNil)
+		So(c, ShouldResemble, &tricium.Data_Comment{
+			Category: "Copyright/Missing",
+			Message: ("Missing copyright statement.\n" +
+				"Use the following for BSD:\n" +
+				"Copyright <year> The <group> Authors. All rights reserved.\n" +
+				"Use of this source code is governed by a BSD-style license that can be\n" +
+				"found in the LICENSE file.\n\n" +
+				"Or the following for MIT: Copyright <year> The <group> Authors\n\n" +
+				"Use of this source code is governed by a MIT-style\n" +
+				"license that can be found in the LICENSE file or at\n" +
+				"https://opensource.org/licenses/MIT"),
+			Path:      missing,
+			StartLine: 1,
+			EndLine:   1,
+			StartChar: 0,
+			EndChar:   1,
+			Url:       "https://chromium.googlesource.com/chromium/src/+/master/styleguide/c++/c++.md#file-headers",
+		})
+	})
+
+	Convey("Makes a comment when there is a copyright statement but the old style is used", t, func() {
+		c := checkCopyright(old)
 		So(c, ShouldNotBeNil)
 		So(c, ShouldResemble, &tricium.Data_Comment{
 			Category:  "Copyright/OutOfDate",
 			Message:   "Out of date copyright statement (omit the (c) to update)",
-			Path:      path,
+			Path:      old,
 			StartLine: 1,
 			EndLine:   1,
 			StartChar: 0,
--- go/src/infra/tricium/functions/spacey/spacey_test.go
+++ go/src/infra/tricium/functions/spacey/spacey_test.go
@@ -8,65 +8,65 @@ import (
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
+	"infra/tricium/api/v1"
 )
 
 func TestCheckSpaceMix(t *testing.T) {
-	Convey("Test Environment", t, func() {
-		Convey("Tab and single space", func() {
-			path := "test.file"
-			line := "\t some code"
-			pos := 1
-			c := checkSpaceMix(path, line, pos)
-			So(c, ShouldNotBeNil)
-			So(c.StartLine, ShouldEqual, pos)
-			So(c.EndLine, ShouldEqual, pos)
-			So(c.StartChar, ShouldEqual, 0)
-			So(c.EndChar, ShouldEqual, 1)
+	Convey("Finds tab + single space mix", t, func() {
+		So(checkSpaceMix("test.file", "\t code", 1), ShouldResemble, &tricium.Data_Comment{
+			Path:      "test.file",
+			Category:  "Spacey/SpaceMix",
+			Message:   "Found mix of white space characters",
+			StartLine: 1,
+			EndLine:   1,
+			StartChar: 0,
+			EndChar:   1,
 		})
-		Convey("Tab and multiple space", func() {
-			path := "test.file"
-			line := "\t  some code"
-			pos := 1
-			c := checkSpaceMix(path, line, pos)
-			So(c, ShouldNotBeNil)
-			So(c.StartLine, ShouldEqual, pos)
-			So(c.EndLine, ShouldEqual, pos)
-			So(c.StartChar, ShouldEqual, 0)
-			So(c.EndChar, ShouldEqual, 2)
+	})
+
+	Convey("Finds tab + multiple space mix", t, func() {
+		So(checkSpaceMix("test.file", "\t  code", 1), ShouldResemble, &tricium.Data_Comment{
+			Path:      "test.file",
+			Category:  "Spacey/SpaceMix",
+			Message:   "Found mix of white space characters",
+			StartLine: 1,
+			EndLine:   1,
+			StartChar: 0,
+			EndChar:   2,
 		})
-		Convey("Tab and multiple space in ignored files (with extension)", func() {
-			So(checkSpaceMix("Makefile", "\t  some code", 1), ShouldBeNil)
-		})
-		Convey("Tab and multiple space in ignored files (no extension)", func() {
-			So(checkSpaceMix("Makefile", "\t  some code", 1), ShouldBeNil)
-		})
+	})
+
+	Convey("Produces no comment in Makefile", t, func() {
+		So(checkSpaceMix("Makefile", "\t  some code", 1), ShouldBeNil)
+	})
+
+	Convey("Produces no comment in makefile with extension", t, func() {
+		So(checkSpaceMix("my.mk", "\t  some code", 1), ShouldBeNil)
 	})
 }
 
 func TestCheckTrailingSpace(t *testing.T) {
-	Convey("Test Environment", t, func() {
-		Convey("Single trailing spaces", func() {
-			path := "test.file"
-			line := "some code "
-			pos := 1
-			c := checkTrailingSpace(path, line, pos)
-			So(c, ShouldNotBeNil)
-			So(c.StartLine, ShouldEqual, pos)
-			So(c.EndLine, ShouldEqual, pos)
-			So(c.StartChar, ShouldEqual, len(line)-1)
-			So(c.EndChar, ShouldEqual, len(line)-1)
-		})
-		Convey("Multiple trailing spaces", func() {
-			path := "test.file"
-			line := "some code  "
-			pos := 1
-			c := checkTrailingSpace(path, line, pos)
-			So(c, ShouldNotBeNil)
-			So(c.StartLine, ShouldEqual, pos)
-			So(c.EndLine, ShouldEqual, pos)
-			So(c.StartChar, ShouldEqual, len(line)-2)
-			So(c.EndChar, ShouldEqual, len(line)-1)
+	Convey("Finds single trailing space", t, func() {
+		So(checkTrailingSpace("test.file", "code ", 1), ShouldResemble, &tricium.Data_Comment{
+			Path:      "test.file",
+			Category:  "Spacey/TrailingSpace",
+			Message:   "Found trailing space",
+			StartLine: 1,
+			EndLine:   1,
+			StartChar: 4,
+			EndChar:   4,
 		})
 	})
 
+	Convey("Finds multiple trailing spaces", t, func() {
+		So(checkTrailingSpace("test.file", "code  ", 1), ShouldResemble, &tricium.Data_Comment{
+			Path:      "test.file",
+			Category:  "Spacey/TrailingSpace",
+			Message:   "Found trailing space",
+			StartLine: 1,
+			EndLine:   1,
+			StartChar: 4,
+			EndChar:   5,
+		})
+	})
 }
