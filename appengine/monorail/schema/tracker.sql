9f0be268da2499d8849ab9ba211dc6328857bbc6
Andrew McRae <amcrae@chromium.org>
2018-05-12 11:05:52 +1000

    Add 255 char limit to url_value.
    Bug: monorail: 3818
    Change-Id: If0a38856277bd11212363b0531b60d28e8f1c5e9
    Reviewed-on: https://chromium-review.googlesource.com/1055138
    Reviewed-by: Jason Robbins <jrobbins@chromium.org>
    Commit-Queue: Andrew McRae <amcrae@chromium.org>

--- appengine/monorail/schema/tracker.sql
+++ appengine/monorail/schema/tracker.sql
@@ -244,7 +244,7 @@ CREATE TABLE Issue2FieldValue (
   INDEX (field_id, issue_shard, str_value(255)),
   INDEX (field_id, issue_shard, user_id),
   INDEX (field_id, issue_shard, date_value),
-  INDEX (field_id, issue_shard, url_value),
+  INDEX (field_id, issue_shard, url_value(255)),
 
   FOREIGN KEY (issue_id) REFERENCES Issue(id),
   FOREIGN KEY (field_id) REFERENCES FieldDef(id),
