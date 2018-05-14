e8760a839a4788d4fdc26f7f27ace1b6ec28f8af
Nodir Turakulov <nodir@google.com>
2018-05-14 12:54:09 -0700

    [buildbucket] Fix Bulidbucket deployment
    Currently Buildbucket deployment is broken because Buildbucket tests do not cover
    handlers.get_frontend_routes. They did not catch a luci-py roll that introduced
    new required parameters to discovery_service_route.
    
    TBR=smut@chromium.org
    
    Bug: 832995
    Change-Id: I06b7631f22da0d589a4faf5bd0f98a4aecd9e718
    Reviewed-on: https://chromium-review.googlesource.com/1058120
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

--- appengine/cr-buildbucket/handlers.py
+++ appengine/cr-buildbucket/handlers.py
@@ -333,13 +333,17 @@ class UnregisterBuilders(webapp2.RequestHandler):  # pragma: no cover
 
 
 def get_frontend_routes():  # pragma: no cover
+  endpoints_services = [
+      api.BuildBucketApi,
+      swarmbucket_api.SwarmbucketApi,
+  ]
   routes = [
       webapp2.Route(r'/', MainHandler),
       webapp2.Route(r'/b/<build_id:\d+>', BuildHandler),
-      endpoints_webapp2.discovery_service_route(),
+      endpoints_webapp2.discovery_service_route(endpoints_services, '/api'),
   ]
-  routes += endpoints_webapp2.api_routes(api.BuildBucketApi)
-  routes += endpoints_webapp2.api_routes(swarmbucket_api.SwarmbucketApi)
+  for c in endpoints_services:
+    routes += endpoints_webapp2.api_routes(c)
 
   prpc_server = prpc.Server()
   prpc_server.add_interceptor(auth.prpc_interceptor)
