2ce03f98b5062ebc3c7d4bedfca7b6340e084bcb
Tiff Zhang <zhangtiff@google.com>
2018-05-18 11:20:00 -0700

    Monorail: Add mr-attachment-gallery component.
    Bug: monorail:3688
    Change-Id: Ib911b60964fcf7b35b66f5aca7bab41df6c25181
    Reviewed-on: https://chromium-review.googlesource.com/1065231
    Commit-Queue: Tiffany Zhang <zhangtiff@chromium.org>
    Reviewed-by: Jeff Carpenter <jeffcarp@chromium.org>

---
+++ appengine/monorail/elements/flt/mr-issue-details/mr-attachment-gallery.js
@@ -0,0 +1,108 @@
+'use strict';
+
+/**
+ * `<mr-attachment-gallery>`
+ *
+ * This creates a gallery of files for users to browse through.
+ *
+ */
+class MrAttachmentGallery extends Polymer.Element {
+  static get is() {
+    return 'mr-attachment-gallery';
+  }
+
+  static get properties() {
+    return {
+      files: {
+        type: Array,
+        value: [
+          {
+            url: 'screenshot1.png',
+            thumbUrl: 'screenshot1.png',
+            fileSize: 146998,
+          },
+          {
+            url: 'screenshot2.png',
+            thumbUrl: 'screenshot2.png',
+            fileSize: 58909,
+          },
+          {
+            url: 'screenshot3.png',
+            thumbUrl: 'screenshot3.png',
+            fileSize: 227927,
+          },
+          {
+            url: 'screenshot4.png',
+            thumbUrl: 'screenshot4.png',
+            fileSize: 45377,
+          },
+          {
+            url: 'screenshot5.png',
+            thumbUrl: 'screenshot5.png',
+            fileSize: 514,
+          },
+          {
+            url: 'gates-mock.json',
+            fileSize: 10000,
+          },
+        ],
+      },
+      _shownFile: {
+        type: Object,
+        computed: '_computeShownFile(files, _shownIndex)',
+      },
+      _shownText: String,
+      _shownIndex: {
+        type: Number,
+        value: 0,
+      },
+    };
+  }
+
+  _unzeroIndex(i) {
+    return i + 1;
+  }
+
+  _computeShownFile(files, i) {
+    return files[i];
+  }
+
+  _displaySize(bytes) {
+    if (bytes > 1000 * 1000) {
+      return `${this._truncateDecimal(bytes / (1000 * 1000))} MB`;
+    }
+    if (bytes > 1000) {
+      return `${this._truncateDecimal(bytes / 1000)} KB`;
+    }
+    return `${bytes} bytes`;
+  }
+
+  _truncateDecimal(n) {
+    return Math.floor(n * 100) / 100;
+  }
+
+  _showFile(evt) {
+    this._shownIndex = evt.currentTarget.dataset.index * 1;
+    this.$.showFile.open();
+  }
+
+  _showNext() {
+    this._shownIndex = (this._shownIndex + 1) % this.files.length;
+
+    // TODO(zhangtiff): This is because paper-dialog doesn't resize when its
+    // content changes. We should remove this eventually.
+    this.$.showFile.close();
+    this.$.showFile.open();
+  }
+
+  _showPrev() {
+    const l = this.files.length;
+    this._shownIndex = (this._shownIndex - 1 + l) % l;
+
+    // TODO(zhangtiff): This is because paper-dialog doesn't resize when its
+    // content changes. We should remove this eventually.
+    this.$.showFile.close();
+    this.$.showFile.open();
+  }
+}
+customElements.define(MrAttachmentGallery.is, MrAttachmentGallery);
