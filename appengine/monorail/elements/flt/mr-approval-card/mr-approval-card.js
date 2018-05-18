223960b46e0d86254dc31c7cab2bc7621df3871f
Tiff Zhang <zhangtiff@google.com>
2018-05-17 17:57:16 -0700

    Monorail: Add mr-approval-card.
    Bug: monorail:3596
    Change-Id: I36a51208d8e65fb413fdd7a6b083824e7f56f0be
    Reviewed-on: https://chromium-review.googlesource.com/1065252
    Reviewed-by: Joanna Wang <jojwang@chromium.org>
    Commit-Queue: Tiffany Zhang <zhangtiff@chromium.org>

--- appengine/monorail/elements/flt/mr-approval-card/mr-approval-card.js
+++ appengine/monorail/elements/flt/mr-approval-card/mr-approval-card.js
@@ -1,7 +1,25 @@
 'use strict';
 
+const STATUS_CLASS_MAP = {
+  'NotSet': 'status-notset',
+  'NeedsReview': 'status-pending',
+  'NA': 'status-notset',
+  'ReviewRequested': 'status-pending',
+  'ReviewStarted': 'status-pending',
+  'NeedInfo': 'status-pending',
+  'Approved': 'status-approved',
+  'NotApproved': 'status-rejected',
+};
+
+const CLASS_ICON_MAP = {
+  'status-notset': 'remove',
+  'status-pending': 'autorenew',
+  'status-approved': 'done',
+  'status-rejected': 'close',
+};
+
 /**
- * `<mr-approval-card>` ....
+ * `<mr-approval-card>`
  *
  * This element shows a card for a single approval.
  *
@@ -11,40 +29,115 @@ class MrApprovalCard extends Polymer.Element {
     return 'mr-approval-card';
   }
 
-  ready() {
-    super.ready();
-  }
-
   static get properties() {
     return {
-      teamTitle: String,
+      title: String,
       approvalComments: Array,
       survey: String,
+      surveyTemplate: String,
       urls: Array,
       labels: Array,
       users: Array,
-      _surveyLines: {
+      user: String,
+      class: {
+        type: String,
+        reflectToAttribute: true,
+        computed: '_computeClass(_status)',
+      },
+      editting: {
+        type: Boolean,
+        value: false,
+      },
+      opened: {
+        type: Boolean,
+        reflectToAttribute: true,
+        value: false,
+      },
+      statuses: {
         type: Array,
-        value: [],
-        computed: '_computeSurveyLines(survey)',
+        value: () => {
+          return Object.keys(STATUS_CLASS_MAP);
+        },
+      },
+      _isApprovalOwner: {
+        type: Boolean,
+        computed: '_computeIsApprovalOwner(users, user)',
+        observer: '_openUserCards',
+      },
+      _expandIcon: {
+        type: String,
+        computed: '_computeExpandIcon(opened)',
+      },
+      _status: {
+        type: String,
+        computed: '_computeStatus(labels)',
+      },
+      _statusIcon: {
+        type: String,
+        computed: '_computeStatusIcon(class)',
       },
     };
   }
 
-  editData() {
-    this.$.editDialog.open();
+  edit() {
+    this.editting = true;
+  }
+
+  cancel() {
+    this.editting = false;
+  }
+
+  save() {
+    this.editting = false;
+    let newLabels = Object.assign([], this.labels);
+    newLabels.forEach((l) => {
+      if (l.name === 'Status') {
+        l.values = [this.$.statusInput.value];
+      }
+    });
+    this.labels = newLabels;
   }
 
   toggleCard(evt) {
-    let path = evt.path;
-    if (path.some((el) => el.classList && el.classList.contains('no-toggle'))) {
-      return;
-    }
-    this.$.cardCollapse.toggle();
+    this.opened = !this.opened;
   }
 
-  _computeSurveyLines(survey) {
-    return survey.trim().split(/\r?\n/);
+  _computeClass(status) {
+    return STATUS_CLASS_MAP[status];
+  }
+
+  _computeExpandIcon(opened) {
+    if (opened) {
+      return 'expand-less';
+    }
+    return 'expand-more';
+  }
+
+  _computeIsSelected(a, b) {
+    return a === b;
+  }
+
+  _computeStatus(labels) {
+    let status = labels.find((l) => (l.name === 'Status'));
+    return status.values[0];
+  }
+
+  _computeStatusIcon(cl) {
+    return CLASS_ICON_MAP[cl];
+  }
+
+  _computeIsApprovalOwner(users, user) {
+    const approvers = users.find((u) => {
+      return u.name == 'Approvers';
+    });
+    return approvers.values.includes(user);
+  }
+
+  _openUserCards(isApprovalOwner) {
+    if (!this.opened && isApprovalOwner) {
+      this.opened = true;
+    }
   }
 }
+
 customElements.define(MrApprovalCard.is, MrApprovalCard);
