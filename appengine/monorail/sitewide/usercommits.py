647d1bed294647bf07bdb19406d31dffcfa97d52
Markie Wagner <markie@google.com>
2018-06-29 20:51:33 +0000

    Fixed date and url name in cron.yaml
    Bug: monorail:
    Change-Id: I9eadaaceb28914e5a762e615508315579d8baa58
    Reviewed-on: https://chromium-review.googlesource.com/1120939
    Reviewed-by: Tiffany Zhang <zhangtiff@chromium.org>
    Commit-Queue: Markie Wagner <markie@google.com>

--- appengine/monorail/sitewide/usercommits.py
+++ appengine/monorail/sitewide/usercommits.py
@@ -1,4 +1,4 @@
-# Copyright 2016 The Chromium Authors. All rights remake served.
+# Copyright 2018 The Chromium Authors. All rights reserved.
 # Use of this source code is govered by a BSD-style
 # license that can be found in the LICENSE file or at
 # https://developers.google.com/open-source/licenses/bsd
