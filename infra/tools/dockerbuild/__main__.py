ecdc034ac519e600001c536ebb1fb779dcbef08f
Robert Iannucci <iannucci@chromium.org>
2018-07-08 00:27:41 +0000

    Strip dockerbuild of all external python dependencies.
    Also include it in the recipe bundle.
    
    R=tandrii@chromium.org, vadimsh@chromium.org
    
    Bug: 823434
    Change-Id: I6e31531b37d1f62f6ca60a611d03386adc7e8363
    Reviewed-on: https://chromium-review.googlesource.com/1125519
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>

--- infra/tools/dockerbuild/__main__.py
+++ infra/tools/dockerbuild/__main__.py
@@ -18,9 +18,34 @@ import logging
 import os
 import sys
 
-import infra_libs.logs
-
 from . import dockerbuild
+
+
+def add_logging_options(parser):
+  """Adds logging related options to an argparse.ArgumentParser.
+
+  This is cribbed from infra_lib because depending on infra_lib.logs pulls in
+  an entire galaxy of ts_mon dependencies that we don't use. Doing it this
+  way allows dockerbuild to be used with bog-standard Python and no external
+  dependencies.
+  """
+
+  parser = parser.add_argument_group('Logging Options')
+  g = parser.add_mutually_exclusive_group()
+  g.set_defaults(log_level=logging.INFO)
+  g.add_argument('--logs-quiet', '--quiet',
+                 action='store_const', const=logging.ERROR,
+                 dest='log_level', help='Make the output quieter (ERROR).')
+  g.add_argument('--logs-warning', '--warning',
+                 action='store_const', const=logging.WARNING,
+                 dest='log_level',
+                 help='Set the output to an average verbosity (WARNING).')
+  g.add_argument('--logs-verbose', '--verbose',
+                 action='store_const', const=logging.INFO,
+                 dest='log_level', help='Make the output louder (INFO).')
+  g.add_argument('--logs-debug', '--debug',
+                 action='store_const', const=logging.DEBUG,
+                 dest='log_level', help='Make the output really loud (DEBUG).')
 
 
 def main(argv):
@@ -30,9 +55,11 @@ def main(argv):
     formatter_class=argparse.RawTextHelpFormatter)
 
   dockerbuild.add_argparse_options(parser)
-  infra_libs.logs.add_argparse_options(parser, default_level=logging.INFO)
+  add_logging_options(parser)
   args = parser.parse_args(argv)
-  infra_libs.logs.process_argparse_options(args)
+
+  logging.basicConfig(level=args.log_level)
+
   dockerbuild.run(args)
   return 0
 
