2a8c2c04508d0d98b24ea8bead43887b35fe30c8
Edward Lemur <ehmaldonado@chromium.org>
2018-08-15 23:39:37 +0000

    monorail: Use pRPC methods to star users, hotlist, issues and projects.
    Bug: monorail:4109
    Change-Id: I891b37b40ee541949a27c104dc8165014d9783b3
    Reviewed-on: https://chromium-review.googlesource.com/1176367
    Reviewed-by: Jason Robbins <jrobbins@chromium.org>
    Commit-Queue: Edward Lesmes <ehmaldonado@chromium.org>

--- appengine/monorail/static/js/framework/framework-stars.js
+++ appengine/monorail/static/js/framework/framework-stars.js
@@ -38,29 +38,36 @@ function TKR_toggleStar(el, projectName, localId, userId, hotlistId, token) {
   var starred = (el.textContent.trim() == TKR_STAR_OFF) ? 1 : 0;
   TKR_toggleStarLocal(el);
 
-  var type;
-  if (userId) type = 'users';
-  if (projectName) type = 'projects';
-  if (projectName && localId) type = 'issues';
-  if (hotlistId) type = 'hotlists';
+  const prpcClient = new window.chops.rpc.PrpcClient({
+    insecure: Boolean(location.hostname === 'localhost'),
+    fetchImpl: (url, options) => {
+      options.credentials = 'same-origin';
+      return fetch(url, options);
+    },
+  });
 
-  args = {'starred': starred};
-  if (type == 'issues') {
-    url = '/p/' + projectName + '/issues/setstar.do';
-    args['id'] = localId;
-  } else {
-    url = '/hosting/stars.do';
-    args['scope'] = type;
-    if (type == 'hotlists') {
-      args['item'] = hotlistId;
-    } else if (type == 'projects'){
-      args['item'] = projectName;
-    } else {
-      args['item'] =  userId;
-    };
+  const starRequestMessage = {
+    trace: {
+      token: window.CS_env.token,
+    },
+    starred: Boolean(starred),
   };
-
-  TKR_setStar(el, url, args, token, url);
+  if (userId) {
+    starRequestMessage.user_ref = {user_id: userId};
+    prpcClient.call('monorail.Users', 'StarUser', starRequestMessage);
+  } else if (projectName && localId) {
+    starRequestMessage.issue_ref = {
+      project_name: projectName,
+      local_id: localId
+    };
+    prpcClient.call('monorail.Issues', 'StarIssue', starRequestMessage);
+  } else if (projectName) {
+    starRequestMessage.project_name = projectName;
+    prpcClient.call('monorail.Projects', 'StarProject', starRequestMessage);
+  } else if (hotlistId) {
+    starRequestMessage.hotlist_ref = {hotlist_id: hotlistId};
+    prpcClient.call('monorail.Features', 'StarHotlist', starRequestMessage);
+  }
 }
 
 
@@ -81,41 +88,6 @@ function TKR_toggleStarLocal(el, opt_formElementId) {
 
   if (opt_formElementId) {
     $(opt_formElementId).value = '' + starred; // convert to string
-  }
-}
-
-
-/**
- * Send the new star state to the server and create a callback for its response.
- * @param {Element} el The star <a> element.
- * @param {String} url The server URL to post to.
- * @param {Dict} args The arguments to send in the POST request.
- * @param {String} opt_token The security token to send in the request.
- */
-function TKR_setStar(el, url, args, opt_token) {
-  if (opt_token) {
-    CS_doPost(url, function(event) { TKR_gotSetStar(el, event); }, args,
-              opt_token, url);
-  } else {
-    CS_doPost(url, function(event) { TKR_gotSetStar(el, event); }, args);
-  }
-}
-
-
-/**
- * Evaluates the server response after a starring operation completed.
- * @param {Element} el <a> element containing the star which was clicked.
- * @param {event} event with xhr JSON response from the server.
- */
-function TKR_gotSetStar(el, event) {
-  var xhr = event.target;
-  if (xhr.readyState != 4 || xhr.status != 200)
-    return;
-  var args = CS_parseJSON(xhr);
-  var localStarred = (el.textContent.trim() == TKR_STAR_ON) ? 1 : 0;
-  var serverStarred = args['starred'];
-  if (localStarred != serverStarred) {
-    TKR_toggleStarLocal(el);
   }
 }
 
