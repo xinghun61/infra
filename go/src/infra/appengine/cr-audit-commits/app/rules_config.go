500e832874c7e38d9c73d5560e4599527611924c
Eric Boren <borenet@google.com>
2018-08-20 17:53:03 +0000

    [cr-audit-commits] Add SPIRV into Chromium roller
    Bug: skia:8233,skia:8234
    Change-Id: I6e78e770fe63ca5bf7c2eb0f3929fad0ccf599b0
    Reviewed-on: https://chromium-review.googlesource.com/1181466
    Reviewed-by: Roberto Carrillo <robertocn@chromium.org>
    Commit-Queue: Eric Boren <borenet@chromium.org>

--- go/src/infra/appengine/cr-audit-commits/app/rules_config.go
+++ go/src/infra/appengine/cr-audit-commits/app/rules_config.go
@@ -107,6 +107,7 @@ var RuleMap = map[string]*RepoConfig{
 			"autoroll-rules-pdfium":       AutoRollRulesDEPS("pdfium-chromium-autoroll@skia-buildbots.google.com.iam.gserviceaccount.com"),
 			"autoroll-rules-perfetto":     AutoRollRulesDEPS("perfetto-chromium-autoroll@skia-buildbots.google.com.iam.gserviceaccount.com"),
 			"autoroll-rules-skia":         AutoRollRulesDEPS("skia-chromium-autoroll@skia-buildbots.google.com.iam.gserviceaccount.com"),
+			"autoroll-rules-spirv":        AutoRollRulesDEPS("spirv-chromium-autoroll@skia-buildbots.google.com.iam.gserviceaccount.com"),
 			"autoroll-rules-src-internal": AutoRollRulesDEPS("src-internal-chromium-autoroll@skia-buildbots.google.com.iam.gserviceaccount.com"),
 			"autoroll-rules-webrtc":       AutoRollRulesDEPS("webrtc-chromium-autoroll@skia-buildbots.google.com.iam.gserviceaccount.com"),
 			"findit-rules": AccountRules{
