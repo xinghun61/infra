f064062b969ec36f46ef9153a0ade7515c4c31ab
Ola Karlsson <olakar@chromium.org>
2018-08-29 18:42:06 +0000

    Adding in a LICENSE blub to all go files.
    Bug:845549
    Change-Id: I12260a9b4694c967854610f17ab844dc4b103b10
    Reviewed-on: https://chromium-review.googlesource.com/1194526
    Commit-Queue: Andrii Shyshkalov <tandrii@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>

--- go/src/infra/appengine/rotang/pkg/algo/algo.go
+++ go/src/infra/appengine/rotang/pkg/algo/algo.go
@@ -1,3 +1,7 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // Package algo contains shared functions to be used by rotation Generators.
 package algo
 
