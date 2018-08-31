2501b5108c95c6c9d4d5f35155e10a7d88295a00
Ravi Mistry <rmistry@google.com>
2018-08-31 15:32:51 +0000

    [cr-audit-commits] Fuchsia SDK roller now modifies more files
    Bug: skia:8338
    Bug: skia:8286
    Change-Id: Iea64ebc9c38d083725552254e504b9133dc7bb54
    Reviewed-on: https://chromium-review.googlesource.com/1199542
    Commit-Queue: Shuotao Gao <stgao@chromium.org>
    Reviewed-by: Shuotao Gao <stgao@chromium.org>
    Reviewed-by: Ravi Mistry <rmistry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#17349}

--- go/src/infra/appengine/cr-audit-commits/app/autoroll_rules.go
+++ go/src/infra/appengine/cr-audit-commits/app/autoroll_rules.go
@@ -49,7 +49,7 @@ func AutoRollRulesFuchsiaSDKVersion(account string) AccountRules {
 	return AccountRules{
 		Account: account,
 		Funcs: []RuleFunc{
-			OnlyModifiesFuchsiaSDKVersion,
+			OnlyModifiesFuchsiaSDKVersions,
 		},
 		notificationFunction: fileBugForAutoRollViolation,
 	}
@@ -89,10 +89,15 @@ func OnlyModifiesAFDOVersion(ctx context.Context, ap *AuditParams, rc *RelevantC
 	return OnlyModifiesFileRule(ctx, ap, rc, cs, "OnlyModifiesAFDOVersion", "chrome/android/profiles/newest.txt")
 }
 
-// OnlyModifiesFuchsiaSDKVersion is a RuleFunc which verifies that the only file
-// modified by the audited CL is ``build/fuchsia/sdk.sha1``.
-func OnlyModifiesFuchsiaSDKVersion(ctx context.Context, ap *AuditParams, rc *RelevantCommit, cs *Clients) *RuleResult {
-	return OnlyModifiesFileRule(ctx, ap, rc, cs, "OnlyModifiesFuchsiaSDKVersion", "build/fuchsia/sdk.sha1")
+// OnlyModifiesFuchsiaSDKVersions is a RuleFunc which verifies that the only
+// files modified by the audited CL is ``build/fuchsia/linux.sdk.sha1`` and
+// ``build/fuchsia/mac.sdk.sha1``.
+func OnlyModifiesFuchsiaSDKVersions(ctx context.Context, ap *AuditParams, rc *RelevantCommit, cs *Clients) *RuleResult {
+	files := []string{
+		"build/fuchsia/linux.sdk.sha1",
+		"build/fuchsia/mac.sdk.sha1",
+	}
+	return OnlyModifiesFilesRule(ctx, ap, rc, cs, "OnlyModifiesFuchsiaSDKVersions", files)
 }
 
 // OnlyModifiesSKCMS is a RuleFunc which verifies that the audited CL only
