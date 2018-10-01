2865e99f0862878438690a9de7a1c0f4b5401298
Sean McCullough <seanmccullough@chromium.org>
2018-10-01 21:13:46 +0000

    [mr] add copy and paste event counting to GA
    Bug: monorail:4330
    Change-Id: Ic01690dfd103b366a31e59478860ed2a68a96ef1
    Reviewed-on: https://chromium-review.googlesource.com/c/1252729
    Reviewed-by: Jason Robbins <jrobbins@chromium.org>
    Commit-Queue: Sean McCullough <seanmccullough@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#17977}

---
+++ appengine/monorail/static/jsm/track-copy.mjs
@@ -0,0 +1,26 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This counts copy and paste events.
+
+function labelForElement(el) {
+  let label = el.localName;
+  if (el.id) {
+    label = label + '#' + el.id;
+  }
+  return label;
+}
+
+window.addEventListener('copy', function(evt) {
+  let label = labelForElement(evt.srcElement);
+  const len = window.getSelection().toString().length;
+  ga('send', 'event', window.location.pathname, 'copy', label, len);
+});
+
+window.addEventListener('paste', function(evt) {
+  let label = labelForElement(evt.srcElement);
+  const text = evt.clipboardData.getData('text/plain');
+  const len = text ? text.length : 0;
+  ga('send', 'event', window.location.pathname, 'paste', label, len);
+});
