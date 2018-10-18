d644a653b195eacc50bdc19b39dc6f2938e20191
Aviv Keshet <akeshet@chromium.org>
2018-10-18 01:11:11 +0000

    qscheduler: rewrite scheduler tests with goconvey
    The existing scheduler logic tests are large and unweildy; rewrite them
    in goconvey, to make them easier to understand and maintain.
    
    BUG=None
    TEST=None
    
    Change-Id: I5d881f228c5f820c4db984adf043a315f24f971f
    Reviewed-on: https://chromium-review.googlesource.com/c/1281999
    Commit-Queue: Aviv Keshet <akeshet@chromium.org>
    Reviewed-by: Allen Li <ayatane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#18423}

--- go/src/infra/qscheduler/qslib/scheduler/scheduler_test.go
+++ go/src/infra/qscheduler/qslib/scheduler/scheduler_test.go
@@ -26,217 +26,134 @@ import (
 	"infra/qscheduler/qslib/types/vector"
 
 	"github.com/kylelemons/godebug/pretty"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 // TestMatchWithIdleWorkers tests that the scheduler correctly matches
 // requests with idle workers, if they are available.
 func TestMatchWithIdleWorkers(t *testing.T) {
-	t.Parallel()
-	s := &Scheduler{
-		&State{
-			Workers: map[string]*Worker{
-				"w0": NewWorker(),
-				"w1": &Worker{Labels: []string{"label1"}},
-			},
-			QueuedRequests: map[string]*TaskRequest{
-				"t1": &TaskRequest{AccountId: "a1", Labels: []string{"label1"}},
-				"t2": &TaskRequest{AccountId: "a1", Labels: []string{"label2"}},
-			},
-			Balances: map[string]*vector.Vector{
-				"a1": vector.New(2, 0, 0),
-			},
-		},
-		&Config{
-			AccountConfigs: map[string]*account.Config{
-				"a1": account.NewConfig(),
-			},
-		},
-	}
-
-	expects := []*Assignment{
-		&Assignment{Type: Assignment_IDLE_WORKER, Priority: 0, RequestId: "t1", WorkerId: "w1"},
-		&Assignment{Type: Assignment_IDLE_WORKER, Priority: 0, RequestId: "t2", WorkerId: "w0"},
-	}
-
-	muts := s.RunOnce()
-
-	if diff := pretty.Compare(muts, expects); diff != "" {
-		t.Errorf(fmt.Sprintf("Unexpected mutations diff (-got +want): %s", diff))
-	}
+	Convey("Given 2 tasks and 2 idle workers", t, func() {
+		s := New()
+		tm := time.Unix(0, 0)
+		s.MarkIdle("w0", []string{}, tm)
+		s.MarkIdle("w1", []string{"label1"}, tm)
+		s.AddRequest("t1", &TaskRequest{AccountId: "a1", Labels: []string{"label1"}}, tm)
+		s.AddRequest("t2", &TaskRequest{AccountId: "a1", Labels: []string{"label2"}}, tm)
+		s.state.Balances["a1"] = vector.New(2, 0, 0)
+		s.config.AccountConfigs["a1"] = account.NewConfig()
+		Convey("when scheduling jobs", func() {
+			muts := s.RunOnce()
+			Convey("then both jobs should be matched, with provisionable label used as tie-breaker", func() {
+				expects := []*Assignment{
+					&Assignment{Type: Assignment_IDLE_WORKER, Priority: 0, RequestId: "t1", WorkerId: "w1"},
+					&Assignment{Type: Assignment_IDLE_WORKER, Priority: 0, RequestId: "t2", WorkerId: "w0"},
+				}
+				So(muts, shouldResemblePretty, expects)
+			})
+		})
+	})
 }
 
 // TestReprioritize tests that the scheduler correctly changes the priority
 // of running jobs (promote or demote) if the account balance makes that
 // necessary.
 func TestSchedulerReprioritize(t *testing.T) {
-	t.Parallel()
 	// Prepare a situation in which one P0 job (out of 2 running) will be
 	// demoted, and a separate P2 job will be promoted to P1.
-	s := &Scheduler{
-		&State{
-			Balances: map[string]*vector.Vector{
-				"a1": vector.New(2*account.DemoteThreshold, 2*account.PromoteThreshold, 0),
-			},
-			Workers: map[string]*Worker{
-				"w1": &Worker{
-					RunningTask: &TaskRun{
-						Cost:      vector.New(1),
-						Priority:  0,
-						Request:   &TaskRequest{AccountId: "a1"},
-						RequestId: "r1",
-					},
-				},
-				"w2": &Worker{
-					RunningTask: &TaskRun{
-						Priority:  0,
-						Request:   &TaskRequest{AccountId: "a1"},
-						Cost:      vector.New(),
-						RequestId: "r2",
-					},
-				},
-				"w3": &Worker{
-					RunningTask: &TaskRun{
-						Cost:      vector.New(1),
-						Priority:  2,
-						Request:   &TaskRequest{AccountId: "a1"},
-						RequestId: "r3",
-					},
-				},
-				"w4": &Worker{
-					RunningTask: &TaskRun{
-						Priority:  2,
-						Request:   &TaskRequest{AccountId: "a1"},
-						Cost:      vector.New(),
-						RequestId: "r4",
-					},
-				},
-			},
-		},
-		&Config{
-			AccountConfigs: map[string]*account.Config{
-				"a1": &account.Config{ChargeRate: vector.New(1.5, 1.5)},
-			},
-		},
-	}
+	Convey("Given two running requests with different costs for an account that needs 1 demotion from P0, and supports 1 additional P1 job", t, func() {
+		s := New()
+		tm0 := time.Unix(0, 0)
+		s.config.AccountConfigs["a1"] = &account.Config{ChargeRate: vector.New(1.1, 0.9)}
+		s.state.Balances["a1"] = vector.New(2*account.DemoteThreshold, 2*account.PromoteThreshold, 0)
+		for _, i := range []int{1, 2} {
+			rid := fmt.Sprintf("r%d", i)
+			wid := fmt.Sprintf("w%d", i)
+			s.AddRequest(rid, &TaskRequest{AccountId: "a1"}, tm0)
+			s.MarkIdle(wid, []string{}, tm0)
+			s.state.applyAssignment(&Assignment{RequestId: rid, WorkerId: wid, Type: Assignment_IDLE_WORKER})
+		}
+		s.state.Workers["w2"].RunningTask.Cost = vector.New(1)
 
-	expects := s.state.Clone()
-	expects.Workers["w2"].RunningTask.Priority = 1
-	expects.Workers["w3"].RunningTask.Priority = 1
-	expects.regenCache()
+		Convey("given both requests running at P0", func() {
+			Convey("when scheduling", func() {
+				s.RunOnce()
+				Convey("then the cheaper request should be demoted.", func() {
+					So(s.state.Workers["w1"].RunningTask.Priority, ShouldEqual, 1)
+					So(s.state.Workers["w2"].RunningTask.Priority, ShouldEqual, 0)
+				})
+			})
+		})
 
-	muts := s.RunOnce()
+		Convey("given both requests running at P2", func() {
+			for _, wid := range []string{"w1", "w2"} {
+				s.state.Workers[wid].RunningTask.Priority = 2
+			}
+			Convey("when scheduling", func() {
 
-	if len(muts) != 0 {
-		t.Errorf("Unexpected muts, got %s want {}", muts)
-	}
-
-	assertStateEqual(t, "reprioritize", s.state, expects)
+				s.RunOnce()
+				Convey("then the more expensive should be promoted.", func() {
+					So(s.state.Workers["w1"].RunningTask.Priority, ShouldEqual, 2)
+					So(s.state.Workers["w2"].RunningTask.Priority, ShouldEqual, 1)
+				})
+			})
+		})
+	})
 }
 
 // TestPreempt tests that the scheduler correctly preempts lower priority jobs
 // running on a worker, when a higher priority job appears to take its place.
 func TestSchedulerPreempt(t *testing.T) {
-	t.Parallel()
-	cases := []struct {
-		S      *Scheduler
-		Expect []*Assignment
-	}{
-		// Case 0
-		//
-		// Basic preemption of a job by a higher priority job.
-		{
-			&Scheduler{
-				&State{
-					Balances: map[string]*vector.Vector{
-						"a1": vector.New(),
-						"a2": vector.New(1),
-					},
-					QueuedRequests: map[string]*TaskRequest{
-						"t1": &TaskRequest{AccountId: "a2"},
-					},
-					Workers: map[string]*Worker{
-						"w1": &Worker{
-							RunningTask: &TaskRun{
-								Cost:      vector.New(.5, .5, .5),
-								Priority:  1,
-								Request:   &TaskRequest{AccountId: "a1"},
-								RequestId: "t2",
-							},
-						},
-					},
-				},
-				&Config{
-					AccountConfigs: map[string]*account.Config{
-						"a1": account.NewConfig(),
-						"a2": account.NewConfig(),
-					},
-				},
-			},
-			[]*Assignment{
-				&Assignment{Type: Assignment_PREEMPT_WORKER, Priority: 0, WorkerId: "w1", RequestId: "t1", TaskToAbort: "t2"},
-			},
-		},
-		// Case 1
-		//
-		// Preemption will be skipped if:
-		// - the preempting account has insufficient funds.
-		// - the preempting account already has lower priority jobs.
-		{
-			&Scheduler{
-				&State{
-					// Both accounts a1 and a2 have P0 quota.
-					Balances: map[string]*vector.Vector{
-						// a1 has insufficient balance to preempt jobs.
-						"a1": vector.New(0.1 * account.PromoteThreshold),
-						// a2 would have sufficient balance to preempt jobs, but has
-						// insufficient balance to promote its already running job, and
-						// thus is banned from preempting jobs.
-						"a2": vector.New(0.9 * account.PromoteThreshold),
-					},
-					QueuedRequests: map[string]*TaskRequest{
-						"t1": &TaskRequest{AccountId: "a1"},
-						"t2": &TaskRequest{AccountId: "a2"},
-					},
-					Workers: map[string]*Worker{
-						// A job is running, but it is too costly for a1 to preempt.
-						"w1": &Worker{
-							RunningTask: &TaskRun{
-								Cost:      vector.New(0.5*account.PromoteThreshold, 0, 0),
-								Priority:  1,
-								Request:   &TaskRequest{},
-								RequestId: "other_req",
-							},
-						},
-						// A job is running for a2 at a lower priority, so a2 is banned
-						// from preempting jobs.
-						"w2": &Worker{
-							RunningTask: &TaskRun{
-								Cost:      vector.New(0.5 * account.PromoteThreshold),
-								Priority:  1,
-								Request:   &TaskRequest{AccountId: "a2"},
-								RequestId: "t3",
-							},
-						},
-					},
-				},
-				&Config{
-					AccountConfigs: map[string]*account.Config{
-						"a1": &account.Config{ChargeRate: vector.New(1)},
-						"a2": &account.Config{ChargeRate: vector.New(1)},
-					},
-				},
-			},
-			// No preemptions or other mutations should result.
-			[]*Assignment{},
-		},
-	}
-
-	for i, test := range cases {
-		actual := test.S.RunOnce()
-		if diff := pretty.Compare(actual, test.Expect); diff != "" {
-			t.Errorf(fmt.Sprintf("Case %d, unexpected mutations diff (-got +want): %s", i, diff))
+	Convey("Given a state with two running P1 tasks", t, func() {
+		s := New()
+		tm0 := time.Unix(0, 0)
+		s.config.AccountConfigs["a1"] = &account.Config{ChargeRate: vector.New(1, 1, 1)}
+		s.state.Balances["a1"] = vector.New(0.5*account.PromoteThreshold, 1)
+		for _, i := range []int{1, 2} {
+			rid := fmt.Sprintf("r%d", i)
+			wid := fmt.Sprintf("w%d", i)
+			s.AddRequest(rid, &TaskRequest{AccountId: "a1"}, tm0)
+			s.MarkIdle(wid, []string{}, tm0)
+			s.state.applyAssignment(&Assignment{RequestId: rid, WorkerId: wid, Type: Assignment_IDLE_WORKER, Priority: 1})
 		}
-	}
+		s.state.Workers["w1"].RunningTask.Cost = vector.New(0, 1)
+		Convey("given a new P0 request from a different account", func() {
+			s.config.AccountConfigs["a2"] = &account.Config{}
+			s.AddRequest("r3", &TaskRequest{AccountId: "a2"}, tm0)
+			Convey("given sufficient balance", func() {
+				s.state.Balances["a2"] = vector.New(1)
+				Convey("when scheduling", func() {
+					got := s.RunOnce()
+					Convey("then the cheaper running job is preempted.", func() {
+						want := &Assignment{Type: Assignment_PREEMPT_WORKER, Priority: 0, WorkerId: "w2", RequestId: "r3", TaskToAbort: "r2"}
+						So(got, shouldResemblePretty, []*Assignment{want})
+					})
+				})
+			})
+			Convey("given insufficient balance", func() {
+				stateBefore := s.state.Clone()
+				Convey("when scheduling", func() {
+					got := s.RunOnce()
+					Convey("then nothing happens.", func() {
+						So(got, ShouldBeEmpty)
+						So(s.state, shouldResemblePretty, stateBefore)
+					})
+				})
+			})
+		})
+
+		Convey("given a new P0 request from the same account", func() {
+			s.AddRequest("r3", &TaskRequest{AccountId: "a1"}, tm0)
+			stateBefore := s.state.Clone()
+			Convey("when scheduling", func() {
+				got := s.RunOnce()
+				Convey("then nothing happens.", func() {
+					So(got, ShouldBeEmpty)
+					So(s.state, shouldResemblePretty, stateBefore)
+				})
+			})
+		})
+	})
 }
 
 // TestUpdateErrors test that UpdateBalance returns the correct errors
