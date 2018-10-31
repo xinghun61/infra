7717baae7fa783ad67527fe326d4f24590df54cd
Joanna Wang <jojwang@chromium.org>
2018-10-31 23:20:48 +0000

    [monorail] Remove review_started from restricted approval statuses.
    Bug: monorail:
    Change-Id: Ia6395d5b516ce9a01d223f28a61d35ca6e761a9a
    Reviewed-on: https://chromium-review.googlesource.com/c/1310519
    Reviewed-by: Jason Robbins <jrobbins@chromium.org>
    Commit-Queue: Joanna Wang <jojwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#18712}

--- appengine/monorail/framework/permissions.py
+++ appengine/monorail/framework/permissions.py
@@ -98,7 +98,7 @@ VERDICT_SPAM = 'VerdictSpam'
 MODERATE_SPAM = 'ModerateSpam'
 
 RESTRICTED_APPROVAL_STATUSES = [
-    tracker_pb2.ApprovalStatus.NA, tracker_pb2.ApprovalStatus.REVIEW_STARTED,
+    tracker_pb2.ApprovalStatus.NA,
     tracker_pb2.ApprovalStatus.APPROVED,
     tracker_pb2.ApprovalStatus.NOT_APPROVED]
 
