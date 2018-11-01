a7f52535c8cf66fcd8d91e24c283968772eb6135
Takuto Ikuta <tikuta@chromium.org>
2018-11-01 06:17:20 +0000

    [chromium-build-stats] Add should-upload handler
    I will use this handler whether ninja log can be uploaded from autoninja.
    
    Bug: 900161
    Change-Id: Ib6eec8a95c0a447be2d2151a9efdf422408ad56d
    Reviewed-on: https://chromium-review.googlesource.com/c/1309275
    Reviewed-by: Shinya Kawanaka <shinyak@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#18720}

--- go/src/infra/appengine/chromium_build_stats/app/app.go
+++ go/src/infra/appengine/chromium_build_stats/app/app.go
@@ -38,7 +38,29 @@ See <a href="https://docs.google.com/a/chromium.org/document/d/16TdPTIIZbtAarXZI
 var ninjaLogRE = regexp.MustCompile(`gs://chrome-goma-log/.*/ninja_log.*\.gz`)
 
 func init() {
+	// This is handler to check whether ninja log can be uploaded or not from
+	// depot_tools user.
+	http.HandleFunc("/should-upload", shouldUploadHandler)
+
 	http.HandleFunc("/", handler)
+}
+
+// This code is copied from https://chromium.googlesource.com/infra/infra/+/b74c3b27b1fb8f60c969e9f158c86d49b56f3d4b/go/src/infra/appengine/depot_tools_metrics/metrics/app.go#52
+// shouldUploadHandler handles the '/should-upload' endpoint, which is used by
+// depot_tools to check whether it should collect and upload metrics.
+// It returns 200 if the request in coming from a corp machine, and thus a
+// Googler, and 403 otherwise.
+func shouldUploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/should-upload" {
+		http.NotFound(w, r)
+		return
+	}
+	// TRUSTED_IP_REQUEST=1 means the request is coming from a corp machine.
+	if r.Header.Get("X-AppEngine-Trusted-IP-Request") != "1" {
+		http.Error(w, "Access Denied: You're not on corp.", http.StatusForbidden)
+		return
+	}
+	fmt.Fprintf(w, "Success")
 }
 
 func handler(w http.ResponseWriter, req *http.Request) {
