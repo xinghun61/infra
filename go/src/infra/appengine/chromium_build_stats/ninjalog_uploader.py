8227d2de23caa7ae53e8d17a47bda9bf160e2f08
Takuto Ikuta <tikuta@chromium.org>
2018-11-21 05:31:53 +0000

    [chromium-build-stats] handle metadata in client
    Modify upload script to send metadata.
    This appends metadata json for request body.
    
    Server side implementation is
    https://chromium-review.googlesource.com/c/infra/infra/+/1341643
    
    Bug: 900161
    Change-Id: Ic970161b2ac289a3cb7f71749ab85a4e05dab815
    Reviewed-on: https://chromium-review.googlesource.com/c/1341647
    Reviewed-by: Shinya Kawanaka <shinyak@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Fumitoshi Ukai <ukai@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#19115}

--- go/src/infra/appengine/chromium_build_stats/ninjalog_uploader.py
+++ go/src/infra/appengine/chromium_build_stats/ninjalog_uploader.py
@@ -8,6 +8,12 @@
 import argparse
 import cStringIO
 import gzip
+import json
+import logging
+import multiprocessing
+import os
+import platform
+import socket
 import sys
 
 import httplib2
@@ -18,6 +24,22 @@ def IsGoogler(server):
     _, content = h.request('https://'+server+'/should-upload', 'GET')
     return content == 'Success'
 
+def GetMetadata(args):
+    """Get metadata for uploaded ninjalog."""
+
+    # TODO(tikuta): Support build_configs from args.gn.
+
+    build_dir = os.path.dirname(args.ninjalog)
+    metadata = {
+        'platform': platform.system(),
+        'cwd': build_dir,
+        'hostname': socket.gethostname(),
+        'cpu_core': multiprocessing.cpu_count(),
+        'cmdline': args.cmdline,
+    }
+
+    return metadata
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--server',
@@ -25,7 +47,18 @@ def main():
                         help='server to upload ninjalog file.')
     parser.add_argument('--ninjalog', required=True,
                         help='ninjalog file to upload.')
+    parser.add_argument('--verbose', action='store_true',
+                        help='Enable verbose logging.')
+    parser.add_argument('--cmdline', nargs=argparse.REMAINDER,
+                        help='command line args passed to ninja.')
+
     args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.INFO)
+    else:
+        # Disable logging.
+        logging.disable(logging.CRITICAL)
 
     if not IsGoogler(args.server):
         return 0
@@ -35,12 +68,24 @@ def main():
     with open(args.ninjalog) as f:
         with gzip.GzipFile(fileobj=output, mode='wb') as g:
             g.write(f.read())
+            g.write('# end of ninja log\n')
+
+            metadata = GetMetadata(args)
+            logging.info('send metadata: %s', metadata)
+            g.write(json.dumps(metadata))
 
     h = httplib2.Http()
-    h.request('https://'+args.server+'/upload_ninja_log/', 'POST',
-              body=output.getvalue(),
-              headers={'Content-Encoding': 'gzip'})
+    resp_headers, content = h.request(
+        'https://'+args.server+'/upload_ninja_log/', 'POST',
+        body=output.getvalue(), headers={'Content-Encoding': 'gzip'})
 
+    if resp_headers.status != 200:
+        logging.warn("unexpected status code for response: %s",
+                     resp_headers.status)
+        return 1
+
+    logging.info('response header: %s', resp_headers)
+    logging.info('response content: %s', content)
     return 0
 
 if __name__ == '__main__':
