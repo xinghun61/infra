6afc286203ea69ea19c0e8c7f361503151731cf0
Owen Rodley <orodley@chromium.org>
2019-01-17 04:01:42 +0000

    Work around gitiles returning 404 for +log of empty repos.
    Ultimately the part that's failing is optional, especially considering
    the builder is only triggered when new commits have been added. It's
    better to just give up on the check and continue under the assumption
    that we do need to do an update.
    
    Bug: 894279
    Change-Id: I51a36adbdac16e07a942fef9b9739edcbe1cfa8e
    Reviewed-on: https://chromium-review.googlesource.com/c/1416212
    Commit-Queue: Owen Rodley <orodley@chromium.org>
    Commit-Queue: Joey Scarr <jsca@chromium.org>
    Reviewed-by: Joey Scarr <jsca@chromium.org>
    Auto-Submit: Owen Rodley <orodley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#20043}

--- recipes/recipes/update_submodules_mirror.py
+++ recipes/recipes/update_submodules_mirror.py
@@ -71,27 +71,8 @@ def RunSteps(api, source_repo, target_repo):
   # Discard any commits from previous runs.
   api.git('reset', '--hard', 'origin/master')
 
-  with api.step.nest('Check for new commits') as step:
-    commits, _ = api.gitiles.log(target_repo, 'master', limit=2,
-                                 step_name='Find latest commit to target repo')
-    if commits and commits[0]['author']['name'] == COMMIT_USERNAME:
-      latest_real_commit_in_target = commits[1]['commit']
-      latest_commit_in_source = api.git(
-          'rev-parse', 'master',
-          stdout=api.raw_io.output(),
-          name='Get latest commit hash in source repo').stdout.strip()
-      if latest_real_commit_in_target == latest_commit_in_source:
-        step.presentation.step_text = 'no new commits, exiting'
-        return
-
-      # If we get here we'll need to generate a new commit. We don't care
-      # whether DEPS has changed, since we always want to include the latest
-      # commit in the target repo.
-    else:
-      # HEAD in the target repo isn't authored by the submodules bot. This means
-      # we definitely need to generate a new commit. Either we've never run on
-      # this repo or we somehow ended up in an invalid state.
-      pass
+  if not ShouldGenerateNewCommit(api, target_repo):
+    return
 
   gclient_spec = ("solutions=[{"
                   "'managed':False,"
@@ -151,6 +132,49 @@ def RunSteps(api, source_repo, target_repo):
   # We've effectively deleted the commit that was at HEAD before. This means
   # that we've diverged from the remote repo, and hence must do a force push.
   api.git('push', '--all', '--force', target_repo)
+
+def ShouldGenerateNewCommit(api, target_repo):
+  """
+   See if we can avoid running the rest of the recipe, if there's no new
+   commits to incorporate into the mirror. We should be conservative in the
+   direction of "True" - the worst case is we update the mirror without any new
+   commits, which will generate a new synthetic commit (with a different hash
+   due to a different timestamp) at the same underlying commit. Unnecessary,
+   but harmless.
+  """
+  with api.step.nest('Check for new commits') as step:
+    try:
+      commits, _ = api.gitiles.log(
+          target_repo,
+          'master',
+          limit=2,
+          step_name='Find latest commit to target repo')
+    except api.step.StepFailure:
+      # gitiles gives a 404 when making a log request for an empty repo. In
+      # this case, we do need to generate a new commit. Of course there are
+      # other reasons why this step may fail, but we would rather proceed in
+      # those cases than fail in the empty repo case.
+      return True
+
+    if commits and commits[0]['author']['name'] == COMMIT_USERNAME:
+      latest_real_commit_in_target = commits[1]['commit']
+      latest_commit_in_source = api.git(
+          'rev-parse', 'master',
+          stdout=api.raw_io.output(),
+          name='Get latest commit hash in source repo').stdout.strip()
+      if latest_real_commit_in_target == latest_commit_in_source:
+        step.presentation.step_text = 'no new commits, exiting'
+        return False
+
+      # If we get here we'll need to generate a new commit. We don't care
+      # whether DEPS has changed, since we always want to include the latest
+      # commit in the target repo.
+    else:
+      # HEAD in the target repo isn't authored by the submodules bot. This means
+      # we definitely need to generate a new commit. Either we've never run on
+      # this repo or we somehow ended up in an invalid state.
+      pass
+  return True
 
 
 fake_src_deps = """
@@ -256,6 +280,20 @@ def GenTests(api):
                             'author': {'name': 'Someone else'},
                         },
                     ]})) +
+      api.step_data('gclient evaluate DEPS',
+                    api.raw_io.stream_output(fake_src_deps, stream='stdout'))
+  )
+
+  yield (
+      api.test('existing_checkout_404_from_gitiles_log') +
+      api.properties(
+          source_repo='https://chromium.googlesource.com/chromium/src',
+          target_repo='https://chromium.googlesource.com/codesearch/src_mirror'
+      ) +
+      api.step_data('Check for existing source checkout dir',
+                    api.raw_io.stream_output('src', stream='stdout')) +
+      api.step_data('Check for new commits.Find latest commit to target repo',
+                    retcode=1) +
       api.step_data('gclient evaluate DEPS',
                     api.raw_io.stream_output(fake_src_deps, stream='stdout'))
   )
