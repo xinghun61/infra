0473c85428558afad969cc5bf69e14ac5b29e91c
Shuotao Gao <stgao@chromium.org>
2019-01-18 17:31:24 +0000

    [code coverage] Add timestamp to proto definition.
    Bug: 922252
    Change-Id: Ia2efda4fca4d6fc19e618852fcdb86789223ed11
    Reviewed-on: https://chromium-review.googlesource.com/c/1420617
    Reviewed-by: Yuke Liao <liaoyuke@chromium.org>
    Commit-Queue: Yuke Liao <liaoyuke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#20074}

--- appengine/findit/model/proto/gen/code_coverage_pb2.py
+++ appengine/findit/model/proto/gen/code_coverage_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='codecoverage',
   syntax='proto3',
   serialized_options=None,
-  serialized_pb=_b('\n\x13\x63ode_coverage.proto\x12\x0c\x63odecoverage\"7\n\tLineRange\x12\r\n\x05\x66irst\x18\x01 \x01(\x05\x12\x0c\n\x04last\x18\x02 \x01(\x05\x12\r\n\x05\x63ount\x18\x03 \x01(\x04\"*\n\x0b\x43olumnRange\x12\r\n\x05\x66irst\x18\x01 \x01(\x05\x12\x0c\n\x04last\x18\x02 \x01(\x05\"G\n\x0c\x43olumnRanges\x12\x0c\n\x04line\x18\x01 \x01(\x05\x12)\n\x06ranges\x18\x02 \x03(\x0b\x32\x19.codecoverage.ColumnRange\"6\n\x06\x42ranch\x12\x0c\n\x04line\x18\x01 \x01(\x05\x12\r\n\x05total\x18\x02 \x01(\x05\x12\x0f\n\x07\x63overed\x18\x03 \x01(\x05\"6\n\x06Metric\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\r\n\x05total\x18\x02 \x01(\x05\x12\x0f\n\x07\x63overed\x18\x03 \x01(\x05\"\xf8\x01\n\x04\x46ile\x12\x0c\n\x04path\x18\x01 \x01(\t\x12\x13\n\x0btotal_lines\x18\x02 \x01(\x05\x12,\n\x0bline_ranges\x18\x03 \x03(\x0b\x32\x17.codecoverage.LineRange\x12\x31\n\rcolumn_ranges\x18\x04 \x01(\x0b\x32\x1a.codecoverage.ColumnRanges\x12&\n\x08\x62ranches\x18\x05 \x03(\x0b\x32\x14.codecoverage.Branch\x12%\n\x07metrics\x18\x06 \x03(\x0b\x32\x14.codecoverage.Metric\x12\x1d\n\x15last_updated_revision\x18\x07 \x01(\t\"V\n\x0f\x43overageSummary\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0c\n\x04path\x18\x02 \x01(\t\x12\'\n\tsummaries\x18\x03 \x03(\x0b\x32\x14.codecoverage.Metric\"\xa8\x01\n\x14GroupCoverageSummary\x12\x0c\n\x04path\x18\x01 \x01(\t\x12+\n\x04\x64irs\x18\x02 \x03(\x0b\x32\x1d.codecoverage.CoverageSummary\x12,\n\x05\x66iles\x18\x03 \x03(\x0b\x32\x1d.codecoverage.CoverageSummary\x12\'\n\tsummaries\x18\x06 \x03(\x0b\x32\x14.codecoverage.Metric\"Y\n\rGitilesCommit\x12\x0c\n\x04host\x18\x01 \x01(\t\x12\x0f\n\x07project\x18\x02 \x01(\t\x12\x10\n\x08revision\x18\x03 \x01(\t\x12\x17\n\x0f\x63ommit_position\x18\x04 \x01(\x05\"G\n\x0bGerritPatch\x12\x0c\n\x04host\x18\x01 \x01(\t\x12\n\n\x02\x63l\x18\x02 \x01(\x05\x12\r\n\x05patch\x18\x03 \x01(\x05\x12\x0f\n\x07project\x18\x04 \x01(\t\"\\\n\rConfigSetting\x12.\n\x08\x63\x61tegory\x18\x01 \x01(\x0e\x32\x1c.codecoverage.ConfigCategory\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\r\n\x05value\x18\x03 \x01(\t\"\xe6\x02\n\x0e\x43overageReport\x12+\n\x06\x63ommit\x18\x01 \x01(\x0b\x32\x1b.codecoverage.GitilesCommit\x12(\n\x05patch\x18\x02 \x01(\x0b\x32\x19.codecoverage.GerritPatch\x12\x32\n\rbuild_configs\x18\x03 \x03(\x0b\x32\x1b.codecoverage.ConfigSetting\x12!\n\x05\x66iles\x18\x04 \x03(\x0b\x32\x12.codecoverage.File\x12\x13\n\x0b\x66ile_shards\x18\x05 \x03(\t\x12\x30\n\x04\x64irs\x18\x06 \x03(\x0b\x32\".codecoverage.GroupCoverageSummary\x12\x36\n\ncomponents\x18\x07 \x03(\x0b\x32\".codecoverage.GroupCoverageSummary\x12\'\n\tsummaries\x18\x08 \x03(\x0b\x32\x14.codecoverage.Metric*I\n\x0e\x43onfigCategory\x12\t\n\x05\x42UILD\x10\x00\x12\x08\n\x04TEST\x10\x01\x12\x0f\n\x0bTESTHARNESS\x10\x02\x12\x11\n\rCOVERAGE_TOOL\x10\x03\x62\x06proto3')
+  serialized_pb=_b('\n\x13\x63ode_coverage.proto\x12\x0c\x63odecoverage\"7\n\tLineRange\x12\r\n\x05\x66irst\x18\x01 \x01(\x05\x12\x0c\n\x04last\x18\x02 \x01(\x05\x12\r\n\x05\x63ount\x18\x03 \x01(\x04\"*\n\x0b\x43olumnRange\x12\r\n\x05\x66irst\x18\x01 \x01(\x05\x12\x0c\n\x04last\x18\x02 \x01(\x05\"G\n\x0c\x43olumnRanges\x12\x0c\n\x04line\x18\x01 \x01(\x05\x12)\n\x06ranges\x18\x02 \x03(\x0b\x32\x19.codecoverage.ColumnRange\"6\n\x06\x42ranch\x12\x0c\n\x04line\x18\x01 \x01(\x05\x12\r\n\x05total\x18\x02 \x01(\x05\x12\x0f\n\x07\x63overed\x18\x03 \x01(\x05\"6\n\x06Metric\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\r\n\x05total\x18\x02 \x01(\x05\x12\x0f\n\x07\x63overed\x18\x03 \x01(\x05\"\xfe\x01\n\x04\x46ile\x12\x0c\n\x04path\x18\x01 \x01(\t\x12\x13\n\x0btotal_lines\x18\x02 \x01(\x05\x12,\n\x0bline_ranges\x18\x03 \x03(\x0b\x32\x17.codecoverage.LineRange\x12\x31\n\rcolumn_ranges\x18\x04 \x01(\x0b\x32\x1a.codecoverage.ColumnRanges\x12&\n\x08\x62ranches\x18\x05 \x03(\x0b\x32\x14.codecoverage.Branch\x12%\n\x07metrics\x18\x06 \x03(\x0b\x32\x14.codecoverage.Metric\x12\x10\n\x08revision\x18\x07 \x01(\t\x12\x11\n\ttimestamp\x18\x08 \x01(\x03\"V\n\x0f\x43overageSummary\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0c\n\x04path\x18\x02 \x01(\t\x12\'\n\tsummaries\x18\x03 \x03(\x0b\x32\x14.codecoverage.Metric\"\xa8\x01\n\x14GroupCoverageSummary\x12\x0c\n\x04path\x18\x01 \x01(\t\x12+\n\x04\x64irs\x18\x02 \x03(\x0b\x32\x1d.codecoverage.CoverageSummary\x12,\n\x05\x66iles\x18\x03 \x03(\x0b\x32\x1d.codecoverage.CoverageSummary\x12\'\n\tsummaries\x18\x06 \x03(\x0b\x32\x14.codecoverage.Metric\"Y\n\rGitilesCommit\x12\x0c\n\x04host\x18\x01 \x01(\t\x12\x0f\n\x07project\x18\x02 \x01(\t\x12\x10\n\x08revision\x18\x03 \x01(\t\x12\x17\n\x0f\x63ommit_position\x18\x04 \x01(\x05\"G\n\x0bGerritPatch\x12\x0c\n\x04host\x18\x01 \x01(\t\x12\n\n\x02\x63l\x18\x02 \x01(\x05\x12\r\n\x05patch\x18\x03 \x01(\x05\x12\x0f\n\x07project\x18\x04 \x01(\t\"\\\n\rConfigSetting\x12.\n\x08\x63\x61tegory\x18\x01 \x01(\x0e\x32\x1c.codecoverage.ConfigCategory\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\r\n\x05value\x18\x03 \x01(\t\"\xe6\x02\n\x0e\x43overageReport\x12+\n\x06\x63ommit\x18\x01 \x01(\x0b\x32\x1b.codecoverage.GitilesCommit\x12(\n\x05patch\x18\x02 \x01(\x0b\x32\x19.codecoverage.GerritPatch\x12\x32\n\rbuild_configs\x18\x03 \x03(\x0b\x32\x1b.codecoverage.ConfigSetting\x12!\n\x05\x66iles\x18\x04 \x03(\x0b\x32\x12.codecoverage.File\x12\x13\n\x0b\x66ile_shards\x18\x05 \x03(\t\x12\x30\n\x04\x64irs\x18\x06 \x03(\x0b\x32\".codecoverage.GroupCoverageSummary\x12\x36\n\ncomponents\x18\x07 \x03(\x0b\x32\".codecoverage.GroupCoverageSummary\x12\'\n\tsummaries\x18\x08 \x03(\x0b\x32\x14.codecoverage.Metric*I\n\x0e\x43onfigCategory\x12\t\n\x05\x42UILD\x10\x00\x12\x08\n\x04TEST\x10\x01\x12\x0f\n\x0bTESTHARNESS\x10\x02\x12\x11\n\rCOVERAGE_TOOL\x10\x03\x62\x06proto3')
 )
 
 _CONFIGCATEGORY = _descriptor.EnumDescriptor(
@@ -48,8 +48,8 @@ _CONFIGCATEGORY = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=1452,
-  serialized_end=1525,
+  serialized_start=1458,
+  serialized_end=1531,
 )
 _sym_db.RegisterEnumDescriptor(_CONFIGCATEGORY)
 
@@ -322,9 +322,16 @@ _FILE = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='last_updated_revision', full_name='codecoverage.File.last_updated_revision', index=6,
+      name='revision', full_name='codecoverage.File.revision', index=6,
       number=7, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='timestamp', full_name='codecoverage.File.timestamp', index=7,
+      number=8, type=3, cpp_type=2, label=1,
+      has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
@@ -341,7 +348,7 @@ _FILE = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=324,
-  serialized_end=572,
+  serialized_end=578,
 )
 
 
@@ -385,8 +392,8 @@ _COVERAGESUMMARY = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=574,
-  serialized_end=660,
+  serialized_start=580,
+  serialized_end=666,
 )
 
 
@@ -437,8 +444,8 @@ _GROUPCOVERAGESUMMARY = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=663,
-  serialized_end=831,
+  serialized_start=669,
+  serialized_end=837,
 )
 
 
@@ -489,8 +496,8 @@ _GITILESCOMMIT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=833,
-  serialized_end=922,
+  serialized_start=839,
+  serialized_end=928,
 )
 
 
@@ -541,8 +548,8 @@ _GERRITPATCH = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=924,
-  serialized_end=995,
+  serialized_start=930,
+  serialized_end=1001,
 )
 
 
@@ -586,8 +593,8 @@ _CONFIGSETTING = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=997,
-  serialized_end=1089,
+  serialized_start=1003,
+  serialized_end=1095,
 )
 
 
@@ -666,8 +673,8 @@ _COVERAGEREPORT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1092,
-  serialized_end=1450,
+  serialized_start=1098,
+  serialized_end=1456,
 )
 
 _COLUMNRANGES.fields_by_name['ranges'].message_type = _COLUMNRANGE
