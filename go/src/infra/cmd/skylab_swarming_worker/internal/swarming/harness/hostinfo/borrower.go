0cd4aa84c62ef7d8ddb0c3b3379afbf75e7b174a
Allen Li <ayatane@chromium.org>
2019-01-30 19:23:28 +0000

    skylab_swarming_worker: Split harness/hostinfo into files
    R=akeshet@chromium.org
    
    Change-Id: Ic24a2e81ac13255824bb30dcfbf27e2454c95ac2
    Reviewed-on: https://chromium-review.googlesource.com/c/1441553
    Commit-Queue: Allen Li <ayatane@chromium.org>
    Reviewed-by: Aviv Keshet <akeshet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#20346}

---
+++ go/src/infra/cmd/skylab_swarming_worker/internal/swarming/harness/hostinfo/borrower.go
@@ -0,0 +1,72 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hostinfo
+
+import (
+	"fmt"
+	"strings"
+
+	"infra/cmd/skylab_swarming_worker/internal/autotest/hostinfo"
+	"infra/cmd/skylab_swarming_worker/internal/botinfo"
+)
+
+// Borrower represents borrowing BotInfo into a HostInfo.  It is used
+// for returning any relevant Hostinfo changes back to the BotInfo.
+type Borrower struct {
+	hostInfo *hostinfo.HostInfo
+	botInfo  *botinfo.BotInfo
+}
+
+// BorrowBotInfo takes some info stored in the BotInfo and adds it to
+// the HostInfo.  The returned Borrower should be closed to return any
+// relevant HostInfo changes back to the BotInfo.
+func BorrowBotInfo(hi *hostinfo.HostInfo, bi *botinfo.BotInfo) *Borrower {
+	for label, value := range bi.ProvisionableLabels {
+		hi.Labels = append(hi.Labels, fmt.Sprintf("%s:%s", label, value))
+	}
+	for attribute, value := range bi.ProvisionableAttributes {
+		hi.Attributes[attribute] = value
+	}
+	return &Borrower{
+		hostInfo: hi,
+		botInfo:  bi,
+	}
+}
+
+// Close returns any relevant Hostinfo changes back to the BotInfo.
+// Subsequent calls do nothing.  This is safe to call on a nil pointer.
+func (b *Borrower) Close() error {
+	if b == nil {
+		return nil
+	}
+	if b.botInfo == nil {
+		return nil
+	}
+	hi, bi := b.hostInfo, b.botInfo
+	for _, label := range hi.Labels {
+		parts := strings.SplitN(label, ":", 2)
+		if len(parts) != 2 {
+			continue
+		}
+		if _, ok := provisionableLabelKeys[parts[0]]; ok {
+			bi.ProvisionableLabels[parts[0]] = parts[1]
+		}
+	}
+	for attribute, value := range hi.Attributes {
+		if _, ok := provisionableAttributeKeys[attribute]; ok {
+			bi.ProvisionableAttributes[attribute] = value
+		}
+	}
+	b.botInfo = nil
+	return nil
+}
+
+var provisionableLabelKeys = map[string]struct{}{
+	"cros-version": {},
+}
+
+var provisionableAttributeKeys = map[string]struct{}{
+	"job_repo_url": {},
+}
---
+++ go/src/infra/cmd/skylab_swarming_worker/internal/swarming/harness/hostinfo/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package hostinfo implements the parts of harness management
+// pertaining to Autotest hostinfo.
+package hostinfo
