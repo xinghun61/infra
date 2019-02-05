f7035c98117aa08e2219b66a12e827209dc12f70
Aviv Keshet <akeshet@chromium.org>
2019-02-05 19:21:31 +0000

    swarming: regenerate protos to use new package name
    Bug: None
    Change-Id: I6c5f554747eb515ef77e032b5efea90216277a2f
    Reviewed-on: https://chromium-review.googlesource.com/c/1454180
    Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Aviv Keshet <akeshet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#20483}

--- go/src/infra/swarming/gen.go
+++ go/src/infra/swarming/gen.go
@@ -12,10 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Code generated but not really. DO NOT EDIT.
-// That's not really true but we need to silence golint.
-
-package swarming_v1
+package swarming
 
 //go:generate cproto -proto-path ../../../../luci/appengine/swarming/proto/api ../../../../luci/appengine/swarming/proto/api
 //go:generate svcdec -type ExternalSchedulerServer
