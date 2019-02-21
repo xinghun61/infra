583e94a4a0b0bb1b6a93cf1fc3aa4e926bfbe470
Allen Li <ayatane@chromium.org>
2019-02-21 22:23:35 +0000

    crosskylabadmin: Temporarily disable repair label update
    TBR=pprabhu@chromium.org
    
    Change-Id: Ib4374f39b34c5d8f07b4ad6ddf316dd655c4bcbf
    Reviewed-on: https://chromium-review.googlesource.com/c/1481970
    Reviewed-by: Allen Li <ayatane@chromium.org>
    Commit-Queue: Allen Li <ayatane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#20832}

--- go/src/infra/appengine/crosskylabadmin/app/frontend/tasker.go
+++ go/src/infra/appengine/crosskylabadmin/app/frontend/tasker.go
@@ -21,7 +21,6 @@ import (
 	"sync"
 
 	"github.com/google/uuid"
-	"go.chromium.org/gae/service/info"
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/sync/parallel"
 	"go.chromium.org/luci/grpc/grpcutil"
@@ -348,7 +347,10 @@ func luciferAdminTaskCmd(ctx context.Context, ttype fleet.TaskType, logdogURL st
 		skylabSwarmingWorkerPath,
 		"-task-name", fmt.Sprintf("admin_%s", strings.ToLower(ttype.String())),
 	}
-	s = append(s, "-admin-service", fmt.Sprintf("%s.appspot.com", info.AppID(ctx)))
+	// TODO(ayatane): Temporarily disable to rollout the config
+	// first.  The config has to be pushed first so it can be
+	// validated by LUCI config service.
+	// s = append(s, "-admin-service", fmt.Sprintf("%s.appspot.com", info.AppID(ctx)))
 	if logdogURL != "" {
 		s = append(s, "-logdog-annotation-url", logdogURL)
 	}
