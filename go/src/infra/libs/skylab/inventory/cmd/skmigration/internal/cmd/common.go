c29baa93790f6469e927851b589b03961a9d48d7
Prathmesh Prabhu <pprabhu@chromium.org>
2019-03-05 18:18:01 +0000

    skylab: Add a command to aid DUT migration to Skylab
    Subcommands added:
      list-duplicates: List duplicate DUTs between Autotest and Skylab
      migrate: Migrate DUTs to Skylab
      import-servo-attributes: Import just the servo attributes from
          Autotest for migrated DUTs
    
    BUG=chromium:927879
    
    Change-Id: I9bf1e7b1115f5686d9566a96ebac182d88119b85
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1497229
    Commit-Queue: Prathmesh Prabhu <pprabhu@chromium.org>
    Reviewed-by: Allen Li <ayatane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#21113}

---
+++ go/src/infra/libs/skylab/inventory/cmd/skmigration/internal/cmd/common.go
@@ -0,0 +1,37 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cmd
+
+import (
+	"infra/libs/skylab/inventory"
+	"path/filepath"
+
+	"go.chromium.org/luci/common/errors"
+)
+
+type allLabs struct {
+	AutotestProd *inventory.Lab
+	AutotestDev  *inventory.Lab
+	Skylab       *inventory.Lab
+}
+
+func loadAllLabsData(root string) (*allLabs, error) {
+	var labs allLabs
+	var err error
+	if labs.AutotestProd, err = inventory.LoadLab(filepath.Join(root, "prod")); err != nil {
+		return nil, errors.Annotate(err, "load autotest prod lab").Err()
+	}
+	if labs.AutotestDev, err = inventory.LoadLab(filepath.Join(root, "staging")); err != nil {
+		return nil, errors.Annotate(err, "load autotest dev lab").Err()
+	}
+	if labs.Skylab, err = inventory.LoadLab(filepath.Join(root, "data", "skylab")); err != nil {
+		return nil, errors.Annotate(err, "load skylab lab").Err()
+	}
+	return &labs, nil
+}
+
+func writeSkylabLabData(root string, lab *allLabs) error {
+	return inventory.WriteLab(lab.Skylab, filepath.Join(root, "data", "skylab"))
+}
