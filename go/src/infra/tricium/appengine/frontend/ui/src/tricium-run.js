9d12f32729fef88db94d265c269dc1165b294836
Quinten Yearsley <qyearsley@chromium.org>
2019-03-07 22:08:24 +0000

    [tricium] Show links to Milo on run page
    In this CL:
    
     - Change tricium-run.js to show different links if
       there's a buildbucket build ID.
     - Add a (very simple) unit test.
    
    Plus minor changes related to linting while making this change:
     - Change the Makefile to not depend on globally installed eslint
     - Change the eslint config and code so that the code now passes lint
    
    Change-Id: I25f9093e02d601ef7de4bc865861ef490e5a5868
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1503403
    Commit-Queue: Quinten Yearsley <qyearsley@chromium.org>
    Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#21189}

--- go/src/infra/tricium/appengine/frontend/ui/src/tricium-run.js
+++ go/src/infra/tricium/appengine/frontend/ui/src/tricium-run.js
@@ -18,6 +18,7 @@ class TriciumRun extends LitElement {
     };
   }
 
+  // Renders (returns a TemplateResult) for the run page.
   _render({run, data, error}) {
     if (error || !data || !data.runId) {
       return html`<p style="color:red">${error}</p>`;
@@ -26,25 +27,59 @@ class TriciumRun extends LitElement {
       <p>
         <b>Run ID: ${data.runId}</b> (State: ${data.state})
       </p>
-      ${repeat(data.functionProgress, f => f.name, this._renderFunction)}
+      ${repeat(data.functionProgress, (f) => f.name, this._renderFunction.bind(this))}
     `;
   }
 
+  // Renders a single function run with state and a link to more details.
   _renderFunction(f) {
+    const link = this._renderLink(f);
     return html`
       <p>
         <b>${f.name}</b>
         (State: ${f.state || 'PENDING'},
-        <a href$="${f.swarmingUrl}/task?id=${f.swarmingTaskId}">Swarming task</a>,
+        ${link},
         comments: ${f.numComments || 0})
       </p>
     `;
   }
 
-  _url(f) {
-    return `${f.swarmingUrl}/task?id=${f.swarmingTaskId}`;
+  // Renders a link to more details for the run, or an empty TemplateResult.
+  _renderLink(f) {
+    if (f.swarmingTaskId) {
+      return html`
+        <a href$="${f.swarmingUrl}/task?id=${f.swarmingTaskId}">
+          task ${f.swarmingTaskId}
+        </a>`;
+    } else if (f.buildbucketBuildId) {
+      const host = this._chromiumMiloHost(f.buildbucketHost);
+      if (!host) {
+        return html`build ${f.buildbucketBuildId}`;
+      }
+      return html`
+        <a href$="https://${host}/b/${f.buildbucketBuildId}">
+          build ${f.buildbucketBuildId}
+        </a>`;
+    }
+    return html`error: no swarming task ID or buildbucket build ID`;
   }
 
+  // Returns a host for viewing build information for buildbucket builds.
+  //
+  // This makes a "best-effort" attempt at guessing. For some specific
+  // instances of Buildbucket, we know which hosts to use; if the Buildbucket
+  // host doesn't match those, this function returns empty string.
+  _chromiumMiloHost(buildbucketHost) {
+    if (buildbucketHost == 'cr-buildbucket-dev.appspot.com') {
+      return 'luci-milo-dev.appspot.com';
+    } else if (buildbucketHost == 'cr-buildbucket.appspot.com') {
+      return 'ci.chromium.org';
+    }
+    return '';
+  }
+
+  // connectedCallback is called when the element is inserted (connected)
+  // into the document.
   connectedCallback() {
     super.connectedCallback();
     if (!this.run) {
@@ -53,6 +88,8 @@ class TriciumRun extends LitElement {
     this._refresh();
   }
 
+  // _refresh fetches data from Tricium and sets this.data or
+  // this.error; when those values change _render will be called.
   async _refresh() {
     try {
       this.data = await request('Progress', {runId: this.run});
