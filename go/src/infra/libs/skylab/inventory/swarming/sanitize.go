f856ceab83b479c10d67b95f041e79218c807538
Allen Li <ayatane@chromium.org>
2019-03-07 22:17:19 +0000

    skylab: Add Swarming sanitization
    isDupe benchmark:
    
    goos: linux
    goarch: amd64
    pkg: infra/libs/skylab/inventory/swarming
    BenchmarkIsDupe-24       	 5000000	       236 ns/op
    BenchmarkIsDupeMap-24    	 1000000	      1246 ns/op
    PASS
    ok  	infra/libs/skylab/inventory/swarming	2.798s
    
    R=pprabhu@chromium.org
    
    Bug: 929888
    Change-Id: I9d52077f79cd7a00130121a84735f118fa9a64a2
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1506369
    Commit-Queue: Allen Li <ayatane@chromium.org>
    Reviewed-by: Prathmesh Prabhu <pprabhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#21191}

---
+++ go/src/infra/libs/skylab/inventory/swarming/sanitize.go
@@ -0,0 +1,142 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package swarming
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+)
+
+// What is considered valid is taken from
+// infra/luci/appengine/swarming/server/config.py
+const (
+	maxKeyLength   = 64
+	maxValueLength = 256
+)
+
+var keyRegexp = regexp.MustCompile(`^[a-zA-Z_.-]+$`)
+
+// Reporter is used to report errors during Sanitize.
+type Reporter interface {
+	Report(error)
+}
+
+// Sanitize sanitizes the Swarming dimensions.  Invalid dimensions are
+// removed or fixed as appropriate.
+func Sanitize(dims Dimensions, r Reporter) {
+	for k := range dims {
+		if k == "" {
+			r.Report(ErrEmptyKey)
+			delete(dims, k)
+			continue
+		}
+		if len(k) > maxKeyLength {
+			r.Report(ErrLongKey{Key: k})
+			delete(dims, k)
+			continue
+		}
+		if !keyRegexp.MatchString(k) {
+			r.Report(ErrKeyChars{Key: k})
+			delete(dims, k)
+			continue
+		}
+		sanitizeDimensionValues(dims, r, k)
+	}
+}
+
+func sanitizeDimensionValues(dims Dimensions, r Reporter, k string) {
+	vs := dims[k]
+	for i := 0; i < len(vs); {
+		v := vs[i]
+		if v == "" {
+			r.Report(ErrEmptyValue{Key: k})
+			vs = deleteValue(vs, i)
+			continue
+		}
+		if len(v) > maxValueLength {
+			r.Report(ErrLongValue{Key: k, Value: v})
+			vs = deleteValue(vs, i)
+			continue
+		}
+		if isDupe(vs, i) {
+			r.Report(ErrRepeatedValue{Key: k, Value: v})
+			vs = deleteValue(vs, i)
+			continue
+		}
+		i++
+	}
+	dims[k] = vs
+}
+
+// deleteValue deletes a dimension value in place, preserving order.
+func deleteValue(s []string, i int) []string {
+	copy(s[i:], s[i+1:])
+	return s[:len(s)-1]
+}
+
+// isDupe checks if the item at i occurs in the subslice before i.
+// Dimensions should be small enough that allocating maps and hashing
+// keys is much more expensive than a quick loop over a small slice.
+func isDupe(s []string, i int) bool {
+	for _, v := range s[:i] {
+		if v == s[i] {
+			return true
+		}
+	}
+	return false
+}
+
+var (
+	// ErrEmptyKey is reported by Sanitize for empty keys.
+	ErrEmptyKey = errors.New("empty dimension key")
+)
+
+// ErrLongKey is reported by Sanitize for keys that are too long.
+type ErrLongKey struct {
+	Key string
+}
+
+func (e ErrLongKey) Error() string {
+	return fmt.Sprintf("key too long: %s", e.Key)
+}
+
+// ErrKeyChars is reported by Sanitize for keys with invalid characters.
+type ErrKeyChars struct {
+	Key string
+}
+
+func (e ErrKeyChars) Error() string {
+	return fmt.Sprintf("invalid key characters: %s", e.Key)
+}
+
+// ErrEmptyValue is reported by Sanitize for empty values.
+type ErrEmptyValue struct {
+	Key string
+}
+
+func (e ErrEmptyValue) Error() string {
+	return fmt.Sprintf("empty value for key %s", e.Key)
+}
+
+// ErrLongValue is reported by Sanitize for values that are too long.
+type ErrLongValue struct {
+	Key   string
+	Value string
+}
+
+func (e ErrLongValue) Error() string {
+	return fmt.Sprintf("value for key %s too long: %s", e.Key, e.Value)
+}
+
+// ErrRepeatedValue is reported by Sanitize for values that are repeated.
+type ErrRepeatedValue struct {
+	Key   string
+	Value string
+}
+
+func (e ErrRepeatedValue) Error() string {
+	return fmt.Sprintf("value for key %s is repeated: %s", e.Key, e.Value)
+}
---
+++ go/src/infra/libs/skylab/inventory/swarming/sanitize_test.go
@@ -0,0 +1,162 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package swarming
+
+import (
+	"reflect"
+	"testing"
+)
+
+type fakeReporter []error
+
+func (r *fakeReporter) Report(err error) {
+	*r = append(*r, err)
+}
+
+func TestSanitize(t *testing.T) {
+	t.Parallel()
+	const longKey = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+	const longVal = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+	cases := []struct {
+		name  string
+		input Dimensions
+		want  Dimensions
+		errs  []error
+	}{
+		{
+			name:  "empty key",
+			input: Dimensions{"": {}, "uss": {"essex"}},
+			want:  Dimensions{"uss": {"essex"}},
+			errs:  []error{ErrEmptyKey},
+		},
+		{
+			name:  "long key",
+			input: Dimensions{longKey: {}, "uss": {"essex"}},
+			want:  Dimensions{"uss": {"essex"}},
+			errs:  []error{ErrLongKey{Key: longKey}},
+		},
+		{
+			name:  "invalid key chars",
+			input: Dimensions{"!!": {}, "uss": {"essex"}},
+			want:  Dimensions{"uss": {"essex"}},
+			errs:  []error{ErrKeyChars{Key: "!!"}},
+		},
+		{
+			name:  "empty value",
+			input: Dimensions{"uss": {"essex", ""}},
+			want:  Dimensions{"uss": {"essex"}},
+			errs:  []error{ErrEmptyValue{Key: "uss"}},
+		},
+		{
+			name:  "long value",
+			input: Dimensions{"uss": {"essex", longVal}},
+			want:  Dimensions{"uss": {"essex"}},
+			errs:  []error{ErrLongValue{Key: "uss", Value: longVal}},
+		},
+		{
+			name:  "dupe value",
+			input: Dimensions{"uss": {"essex", "yorktown", "essex"}},
+			want:  Dimensions{"uss": {"essex", "yorktown"}},
+			errs:  []error{ErrRepeatedValue{Key: "uss", Value: "essex"}},
+		},
+	}
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+			dims := copyDims(c.input)
+			var r fakeReporter
+			Sanitize(dims, &r)
+			if !reflect.DeepEqual(dims, c.want) {
+				t.Errorf("Sanitize(%#v, r) = %#v; want %#v", c.input, dims, c.want)
+			}
+			if !reflect.DeepEqual([]error(r), c.errs) {
+				t.Errorf("Got errors %#v; want %#v", []error(r), c.errs)
+			}
+		})
+	}
+}
+
+func copyDims(dims Dimensions) Dimensions {
+	new := make(Dimensions, len(dims))
+	for k, v := range dims {
+		new[k] = make([]string, len(v))
+		copy(new[k], v)
+	}
+	return new
+}
+
+func TestDeleteValue(t *testing.T) {
+	t.Parallel()
+	cases := []struct {
+		name  string
+		input []string
+		i     int
+		want  []string
+	}{
+		{name: "middle", input: []string{"1", "2", "3"}, i: 1, want: []string{"1", "3"}},
+		{name: "start", input: []string{"1", "2", "3"}, i: 0, want: []string{"2", "3"}},
+		{name: "end", input: []string{"1", "2", "3"}, i: 2, want: []string{"1", "2"}},
+	}
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+			s := make([]string, len(c.input))
+			copy(s, c.input)
+			s = deleteValue(s, c.i)
+			if !reflect.DeepEqual(s, c.want) {
+				t.Errorf("deleteValue(%#v, %#v) = %#v; want %#v", c.input, c.i, s, c.want)
+			}
+		})
+	}
+}
+
+func TestIsDupe(t *testing.T) {
+	t.Parallel()
+	cases := []struct {
+		name  string
+		input []string
+		i     int
+		want  bool
+	}{
+		{name: "true", input: []string{"1", "2", "2"}, i: 2, want: true},
+		{name: "false", input: []string{"1", "2", "3"}, i: 2, want: false},
+		{name: "start", input: []string{"2", "2", "2"}, i: 0, want: false},
+	}
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+			got := isDupe(c.input, c.i)
+			if got != c.want {
+				t.Errorf("isDupe(%#v, %#v) = %#v; want %#v", c.input, c.i, got, c.want)
+			}
+		})
+	}
+}
+
+func BenchmarkIsDupe(b *testing.B) {
+	s := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < len(s); j++ {
+			isDupe(s, j)
+		}
+	}
+}
+
+func BenchmarkIsDupeMap(b *testing.B) {
+	s := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
+	for i := 0; i < b.N; i++ {
+		seen := make(map[string]bool, len(s))
+		for j := 0; j < len(s); j++ {
+			v := s[j]
+			if seen[v] {
+				continue
+			}
+			seen[v] = true
+		}
+	}
+}
