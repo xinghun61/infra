5141d6050632aa9f47e12335f0bc5e997878de21
Nodir Turakulov <nodir@chromium.org>
2019-03-20 20:23:13 +0000

    Revert "[buildbucket] Add bbutil.escape_markdown"
    This reverts commit 005361e8321f2f9b46fc67aefe420900eddd2d7b.
    
    Reason for revert: overly complex. Just use `
    
    Original change's description:
    > [buildbucket] Add bbutil.escape_markdown
    > 
    > R=​vadimsh@chromium.org
    > 
    > Bug: 943467
    > Change-Id: I0f78adec5611d7cfc1bd48cdc89cc9ce26455e90
    > Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1532783
    > Commit-Queue: Nodir Turakulov <nodir@chromium.org>
    > Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    > Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    > Auto-Submit: Nodir Turakulov <nodir@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#21649}
    
    TBR=vadimsh@chromium.org,nodir@chromium.org
    
    Change-Id: Ic553e327132fca4e27ac0616ab047336b4baccdc
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 943467
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1532923
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#21651}

--- appengine/cr-buildbucket/swarming.py
+++ appengine/cr-buildbucket/swarming.py
@@ -1124,7 +1124,7 @@ def _sync_build_in_memory(
   state = (task_result or {}).get('state')
   if build_run_result_error:
     bp.status = common_pb2.INFRA_FAILURE
-    bp.summary_markdown = bbutil.escape_markdown(
+    bp.summary_markdown = (
         '`%s` returned by the swarming task is bad: %s.' %
         (_BUILD_RUN_RESULT_FILENAME, build_run_result_error)
     )
