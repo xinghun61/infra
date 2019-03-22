11d3f16bb7664463ce5708c7f4e19cf0f970906f
Chan <chanli@chromium.org>
2019-03-22 18:23:50 +0000

    [Findit] Round the "Last Updated" and "last occurred" time to minutes, hours, days
    Bug: 938124
    Change-Id: I92d1c88753769345406538e3b22c5e12be33e551
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1504800
    Commit-Queue: Chan Li <chanli@chromium.org>
    Reviewed-by: Yuke Liao <liaoyuke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#21720}

--- appengine/findit/ui/js/common.js
+++ appengine/findit/ui/js/common.js
@@ -14,14 +14,51 @@
  * @param {boolean} preFormat If true, show the message in a <pre> tag.
  */
 function displayMessage(messageId, content, title, preFormat) {
-  var detail = {
+  const detail = {
     'messageId': messageId,
     'content': content,
     'title': title,
     'preFormat': preFormat,
   };
-  var event = new CustomEvent('message', {'detail': detail});
+  const event = new CustomEvent('message', {'detail': detail});
   console.log('Dispatching message event:');
   console.log(event);
   document.dispatchEvent(event);
+}
+
+
+/*
+ * Shorten the time delta.
+ * '2 days, 01:02:03' -> '2 days'
+ * '1 day, 01:02:03' -> '1 day'
+ * '01:02:03' -> '1 hour'
+ * '00:02:03' -> '2 minutes'
+ * '00:00:03' -> '3 seconds'
+ */
+// eslint-disable-line no-unused-vars
+function shortenTimeDelta(longTimeDelta) {
+  const pattern = /(?:(\d day[s]?),\s)?(\d*):(\d*):(\d*)/;
+  // [full match, n day(s), HH, MM, SS]
+  const res = longTimeDelta.match(pattern);
+
+  if (typeof(res[1]) != 'undefined') {
+    return res[1];
+  }
+
+  const timeDeltaParts = {
+    2: 'hour',
+    3: 'minute',
+    4: 'second',
+  };
+
+  for (let i=2; i<res.length; i++) {
+    const intTimeDelta = parseInt(res[i]);
+    if ( intTimeDelta == 1) {
+      return intTimeDelta + ' ' + timeDeltaParts[i];
+    }
+    if ( intTimeDelta > 1) {
+      return intTimeDelta + ' ' + timeDeltaParts[i] + 's';
+    }
+  }
+  return 'just now';
 }
