0e68fec1f702264feb28234fb9e345a58e54b8c8
Erik Chen <erikchen@chromium.org>
2019-03-25 21:50:12 +0000

    Add basic dataflow to join against buildbucket.
    This CL adds a new dataflow that extends the functionality of cq_attempts. In
    addition to sanitizing the CQ events, this workflow joins the results against
    buildbucket to set the failure type to PRESUBMIT_FAILURE if the only failures
    are caused by 'chromium_presubmit'.
    
    Bug: 937519
    Change-Id: I4d4b8c61667b2633a1f65037ee3acdd5a66e7daa
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1521042
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#21771}

---
+++ packages/dataflow/new_cq_attempts.py
@@ -0,0 +1,129 @@
+# Copyright 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import apache_beam as beam
+
+from dataflow import cq_attempts as sanitize_cq_attempts
+from dataflow.common import chops_beam
+
+
+class ExtractBuildBucketIdFn(beam.DoFn):
+  def process(self, cq_attempt_with_key):
+    # For a CQ attempt, we create one row for each contributing BuildBucket id.
+    key = cq_attempt_with_key[0]
+    cq_attempt_dict = cq_attempt_with_key[1]
+
+    bb_ids = cq_attempt_dict.get('contributing_bbucket_ids')
+    if bb_ids:
+      for bb_id in bb_ids:
+        yield str(bb_id), key
+
+class FilterJoinedBuildBucketCQAttempt(beam.DoFn):
+  def process(self, joined_result):
+    # The key is BuildBucket ID. We expect there to be exactly 1 cq_attempt, and
+    # up to 1 BuildBucket entry.
+    cq_attempt_key = joined_result[1]['cq_attempt_key']
+    bb_entry = joined_result[1]['bb_entries']
+    if len(bb_entry) != 1 or len(cq_attempt_key) != 1:
+      return
+    yield cq_attempt_key[0], bb_entry[0]
+
+def update_with_presubmit_failure(input_tuple):
+  value = input_tuple[1]
+  cq_attempts = value['cq_attempts']
+  assert len(cq_attempts) == 1, "There must be 1 cq_attempt."
+  cq_attempt = cq_attempts[0]
+
+  if cq_attempt['fail_type'] == 'FAILED_JOBS':
+    buildbucket_results = value['bb_entries']
+    presubmit_failures = 0
+    other_failures = 0
+    for bb_result in buildbucket_results:
+      if (bb_result['status'] == 'FAILURE' and
+          bb_result['builder'] == 'chromium_presubmit'):
+        presubmit_failures += 1
+      elif bb_result['status'] != 'SUCCESS':
+        other_failures += 1
+    if presubmit_failures >= 1 and other_failures == 0:
+      cq_attempt['fail_type'] = 'PRESUBMIT_FAILURE'
+
+  # Dictionaries are supposed to be returned in a single element list.
+  return [cq_attempt]
+
+def process_input(cq_events_pcol, bb_entries_pcol):
+  """Sets up the pipeline stages to return aggregated cq attempts pcol.
+
+  This function performs two tasks:
+    1) Computes CQ attempts from raw CQ events. This includes data sanitization.
+    2) If a CQ attempt fails only because of 'chromium_presubmit' builder, sets
+       the failure status to 'PRESUBMIT_FAILURE'.
+  """
+  # Pcol of cq_attempt_as_dict
+  sanitized_cq_attempts = (
+      cq_events_pcol | sanitize_cq_attempts.ComputeAttempts())
+
+  # Create Pcol of tuples: (cq_attempt_key, cq_attempt_as_dict)
+  def extract_key(cq_attempt_dict):
+    key_parts = [
+      cq_attempt_dict.get('attempt_start_msec'),
+      cq_attempt_dict.get('cq_name'),
+      cq_attempt_dict.get('issue'),
+      cq_attempt_dict.get('patchset')
+    ]
+    key = ':'.join([str(part) or '' for part in key_parts])
+    return key, cq_attempt_dict
+  cq_attempts_with_key = sanitized_cq_attempts | beam.Map(extract_key)
+
+  # Create Pcol of tuples: (build_bucket_id, cq_attempt_key)
+  cq_attempt_key_keyed_by_bb_id = cq_attempts_with_key | beam.ParDo(
+      ExtractBuildBucketIdFn())
+
+  # Create Pcol of tuples: (build_bucket_id, build_bucket_entry)
+  bb_entry_keyed_by_bb_id = bb_entries_pcol | beam.Map(
+      lambda e: (str(e.get('id')), e))
+
+  # Create Pcol of tuples: (cq_attempt_key, BuildBucket entry)
+  bb_entries_keyed_by_cq_attempt_key = ({
+    'bb_entries' : bb_entry_keyed_by_bb_id,
+    'cq_attempt_key': cq_attempt_key_keyed_by_bb_id
+  } | 'Join BuildBucket with cq attempts' >> beam.CoGroupByKey()
+    | beam.ParDo(FilterJoinedBuildBucketCQAttempt())
+  )
+
+  # Uses BuildBucket entries associated with a CQ attempt to potentially change
+  # the failure reason to PRESUBMIT_FAILURE. Creates a Pcol of
+  # cq_attempt_as_dict.
+  results = ({
+    'cq_attempts' : cq_attempts_with_key,
+    'bb_entries' : bb_entries_keyed_by_cq_attempt_key
+  } | beam.CoGroupByKey()
+    | beam.FlatMap(update_with_presubmit_failure)
+  )
+  return results
+
+def main():
+  p = chops_beam.EventsPipeline()
+  q = ('SELECT timestamp_millis, action, attempt_start_usec, cq_name, issue,'
+       '  patchset, dry_run, failure_reason, contributing_buildbucket_ids, '
+       '  earliest_equivalent_patchset '
+       'FROM `chrome-infra-events.raw_events.cq`')
+  cq_events_pcol = p | 'read raw CQ events' >> chops_beam.BQRead(q)
+
+  q = ('SELECT id, builder.builder, status from '
+       '`cr-buildbucket.chromium.completed_builds_BETA`')
+  bb_entries_pcol = p | 'read BuildBucket' >> chops_beam.BQRead(q)
+
+  results = process_input(cq_events_pcol, bb_entries_pcol)
+
+  # Temporary table for development.
+  # TODO(erikchen): Replace with a prod table. https://crbug.com/937519.
+  # pylint: disable=expression-not-assigned
+  results | chops_beam.BQWrite('erikchen-test', 'test_table',
+                               dataset='test_dataset')
+
+  p.run()
+
+
+if __name__ == '__main__':
+  main()
---
+++ packages/dataflow/test/new_cq_attempts_test.py
@@ -0,0 +1,151 @@
+# Copyright 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+import unittest
+
+import apache_beam as beam
+from apache_beam.testing import test_pipeline
+from apache_beam.testing import util
+
+from dataflow import cq_attempts as sanitize_cq_attempts
+from dataflow import new_cq_attempts
+
+
+def _beam_equals(expected, actual):
+  if expected != actual:
+    raise util.BeamAssertException(
+        'Expected {} to equal {}'.format(expected, actual))
+
+
+class IntegrationTest(unittest.TestCase):
+  def setUp(self):
+    self.pipeline = test_pipeline.TestPipeline()
+
+  def construct_cq_events_pcol(self, failure_reason=None):
+    attempt_start_usec = 1000000
+    cq_name = 'test_cq'
+    issue = '1'
+    patchset = '1'
+    combine_class = sanitize_cq_attempts.CombineEventsToAttempt
+    actions = [
+      combine_class.ACTION_PATCH_START,
+      combine_class.ACTION_VERIFIER_CUSTOM_TRYBOTS,
+      combine_class.ACTION_PATCH_FAILED,
+      combine_class.ACTION_PATCH_FAILED,
+      combine_class.ACTION_PATCH_STOP,
+    ]
+
+    event_basic = {
+      'attempt_start_usec': attempt_start_usec,
+      'cq_name': cq_name,
+      'issue': issue,
+      'patchset': patchset,
+    }
+
+    events = []
+    for i, action in enumerate(actions):
+      event = event_basic.copy()
+      event_failure_reason = (failure_reason if
+          action == combine_class.ACTION_PATCH_FAILED else None)
+      event.update({
+        'action': action,
+        'timestamp_millis': 1000 * (i + 1),
+        'failure_reason': event_failure_reason,
+      })
+      if action == combine_class.ACTION_VERIFIER_CUSTOM_TRYBOTS:
+        event['contributing_buildbucket_ids'] = [11, 12]
+      events.append(event)
+
+    return self.pipeline | beam.Create(events)
+
+  def construct_bb_entries_pcol(self, builder_name, status):
+    bb_entries = [{'id':11, 'builder': builder_name, 'status':status}]
+    return self.pipeline | 'Construct BuildBucket entries' >> beam.Create(
+        bb_entries)
+
+  # One CQ attempt, with matching BuildBucket entries that all pass.
+  def test_basic_pass(self):
+    cq_events_pcol = self.construct_cq_events_pcol()
+    bb_entries_pcol = self.construct_bb_entries_pcol('random_builder',
+                                                     'SUCCESS')
+    results = new_cq_attempts.process_input(cq_events_pcol, bb_entries_pcol)
+
+    # There should be exactly 1 CQ attempt, with no fail type.
+    def expectation_checker(cq_attempts):
+      _beam_equals(len(cq_attempts), 1)
+      _beam_equals(cq_attempts[0]['fail_type'], None)
+
+    util.assert_that(results, expectation_checker)
+    self.pipeline.run()
+
+  def test_basic_failure_random_builder(self):
+    failure_reason = {'fail_type': 'FAILED_JOBS'}
+    cq_events_pcol = self.construct_cq_events_pcol(failure_reason)
+    bb_entries_pcol = self.construct_bb_entries_pcol('random_builder',
+                                                     'FAILURE')
+    results = new_cq_attempts.process_input(cq_events_pcol, bb_entries_pcol)
+
+    # There should be exactly 1 CQ attempt, with no fail type.
+    def expectation_checker(cq_attempts):
+      _beam_equals(len(cq_attempts), 1)
+      _beam_equals(cq_attempts[0]['fail_type'], 'FAILED_JOBS')
+
+    util.assert_that(results, expectation_checker)
+    self.pipeline.run()
+
+  def test_basic_failure_chromium_presubmit(self):
+    failure_reason = {'fail_type': 'FAILED_JOBS'}
+    cq_events_pcol = self.construct_cq_events_pcol(failure_reason)
+    bb_entries_pcol = self.construct_bb_entries_pcol(
+        'chromium_presubmit', 'FAILURE')
+    results = new_cq_attempts.process_input(cq_events_pcol, bb_entries_pcol)
+
+    # There should be exactly 1 CQ attempt, with no fail type.
+    def expectation_checker(cq_attempts):
+      _beam_equals(len(cq_attempts), 1)
+      _beam_equals(cq_attempts[0]['fail_type'], 'PRESUBMIT_FAILURE')
+
+    util.assert_that(results, expectation_checker)
+    self.pipeline.run()
+
+  def test_two_failures(self):
+    failure_reason = {'fail_type': 'FAILED_JOBS'}
+    cq_events_pcol = self.construct_cq_events_pcol(failure_reason)
+    bb_entries = [
+        {'id':11, 'builder': 'chromium_presubmit', 'status':'FAILURE'},
+        {'id':12, 'builder': 'win7-rel', 'status':'INFRA_FAILURE'},
+    ]
+    bb_entries_pcol = (self.pipeline | 'Construct BuildBucket entries' >>
+        beam.Create(bb_entries))
+    results = new_cq_attempts.process_input(cq_events_pcol, bb_entries_pcol)
+
+    # There should be exactly 1 CQ attempt, with no fail type.
+    def expectation_checker(cq_attempts):
+      _beam_equals(len(cq_attempts), 1)
+      _beam_equals(cq_attempts[0]['fail_type'], 'FAILED_JOBS')
+
+    util.assert_that(results, expectation_checker)
+    self.pipeline.run()
+
+  def test_cq_attempt_no_bb_entries(self):
+    cq_events_pcol = self.construct_cq_events_pcol()
+    bb_entries_pcol = (
+        self.pipeline | 'Construct 0 BuildBucket entries' >> beam.Create([]))
+
+    results = new_cq_attempts.process_input(cq_events_pcol, bb_entries_pcol)
+
+    # There should be exactly 1 CQ attempt, with no fail type.
+    def expectation_checker(cq_attempts):
+      _beam_equals(len(cq_attempts), 1)
+      cq_attempt = cq_attempts[0]
+      _beam_equals(cq_attempt['fail_type'], None)
+      _beam_equals(cq_attempt['issue'], '1')
+
+    util.assert_that(results, expectation_checker)
+    self.pipeline.run()
+
+
+if __name__ == '__main__':
+  unittest.main()
