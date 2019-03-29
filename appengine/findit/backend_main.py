b1a2e3e4a1af705c0bc2e43b8a321f08d5a4b197
Chan <chanli@chromium.org>
2019-03-29 16:22:58 +0000

    [Findit] remove old detect-cq-false-rejection-flakes urls
    Bug: 941576
    Change-Id: Ic28c69480761779434eb908739a76e15196d7c9d
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1544942
    Reviewed-by: Yuke Liao <liaoyuke@chromium.org>
    Commit-Queue: Chan Li <chanli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#21924}

--- appengine/findit/backend_main.py
+++ appengine/findit/backend_main.py
@@ -46,16 +46,11 @@ if appengine_util.IsInProductionApp():
 
 # "flake-detection-backend" module.
 flake_detection_backend_web_pages_handler_mappings = [
-    # TODO(crbug.com/941576) remove obsolete urls.
-    ('/flake/detection/cron/detect-cq-false-rejection-flakes',
-     detect_flakes.DetectFlakesCronJob),
     ('/flake/detection/cron/detect-flakes', detect_flakes.DetectFlakesCronJob),
     ('/flake/detection/cron/generate-flakiness-report',
      generate_report.PrepareFlakinessReport),
     ('/flake/detection/cron/update-flake-counts',
      update_flake_counts.UpdateFlakeCountsCron),
-    ('/flake/detection/task/detect-cq-false-rejection-flakes',
-     detect_flakes.FlakeDetectionAndAutoAction),
     ('/flake/detection/task/detect-flakes',
      detect_flakes.FlakeDetectionAndAutoAction),
     ('/flake/detection/task/detect-flakes-from-build',
