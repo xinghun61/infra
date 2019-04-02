80315eb8d684cdf3721a4fab710d3f259739a720
Nodir Turakulov <nodir@google.com>
2019-04-02 21:19:53 +0000

    [buildbucket] Recompile protos
    Change-Id: I20743c523a36013a9bda42e3ecd4a59a383cdc3e
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1549840
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#22037}

--- infra/libs/buildbucket/proto/common_pb2.py
+++ infra/libs/buildbucket/proto/common_pb2.py
@@ -23,7 +23,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
         'Z4go.chromium.org/luci/buildbucket/proto;buildbucketpb'
     ),
     serialized_pb=_b(
-        '\n\x0c\x63ommon.proto\x12\x0e\x62uildbucket.v2\x1a\x1egoogle/protobuf/duration.proto\x1a\x1fgoogle/protobuf/timestamp.proto\"\xc3\x01\n\rStatusDetails\x12M\n\x13resource_exhaustion\x18\x03 \x01(\x0b\x32\x30.buildbucket.v2.StatusDetails.ResourceExhaustion\x12\x36\n\x07timeout\x18\x04 \x01(\x0b\x32%.buildbucket.v2.StatusDetails.Timeout\x1a\x14\n\x12ResourceExhaustion\x1a\t\n\x07TimeoutJ\x04\x08\x01\x10\x02J\x04\x08\x02\x10\x03\"O\n\x0cGerritChange\x12\x0c\n\x04host\x18\x01 \x01(\t\x12\x0f\n\x07project\x18\x02 \x01(\t\x12\x0e\n\x06\x63hange\x18\x03 \x01(\x03\x12\x10\n\x08patchset\x18\x04 \x01(\x03\"Y\n\rGitilesCommit\x12\x0c\n\x04host\x18\x01 \x01(\t\x12\x0f\n\x07project\x18\x02 \x01(\t\x12\n\n\x02id\x18\x03 \x01(\t\x12\x0b\n\x03ref\x18\x04 \x01(\t\x12\x10\n\x08position\x18\x05 \x01(\r\"(\n\nStringPair\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t\"i\n\tTimeRange\x12.\n\nstart_time\x18\x01 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12,\n\x08\x65nd_time\x18\x02 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\"_\n\x12RequestedDimension\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t\x12-\n\nexpiration\x18\x03 \x01(\x0b\x32\x19.google.protobuf.Duration*\x87\x01\n\x06Status\x12\x16\n\x12STATUS_UNSPECIFIED\x10\x00\x12\r\n\tSCHEDULED\x10\x01\x12\x0b\n\x07STARTED\x10\x02\x12\x0e\n\nENDED_MASK\x10\x04\x12\x0b\n\x07SUCCESS\x10\x0c\x12\x0b\n\x07\x46\x41ILURE\x10\x14\x12\x11\n\rINFRA_FAILURE\x10$\x12\x0c\n\x08\x43\x41NCELED\x10\x44*%\n\x07Trinary\x12\t\n\x05UNSET\x10\x00\x12\x07\n\x03YES\x10\x01\x12\x06\n\x02NO\x10\x02\x42\x36Z4go.chromium.org/luci/buildbucket/proto;buildbucketpbb\x06proto3'
+        '\n\x0c\x63ommon.proto\x12\x0e\x62uildbucket.v2\x1a\x1egoogle/protobuf/duration.proto\x1a\x1fgoogle/protobuf/timestamp.proto\"8\n\nExecutable\x12\x14\n\x0c\x63ipd_package\x18\x01 \x01(\t\x12\x14\n\x0c\x63ipd_version\x18\x02 \x01(\t\"\xc3\x01\n\rStatusDetails\x12M\n\x13resource_exhaustion\x18\x03 \x01(\x0b\x32\x30.buildbucket.v2.StatusDetails.ResourceExhaustion\x12\x36\n\x07timeout\x18\x04 \x01(\x0b\x32%.buildbucket.v2.StatusDetails.Timeout\x1a\x14\n\x12ResourceExhaustion\x1a\t\n\x07TimeoutJ\x04\x08\x01\x10\x02J\x04\x08\x02\x10\x03\"O\n\x0cGerritChange\x12\x0c\n\x04host\x18\x01 \x01(\t\x12\x0f\n\x07project\x18\x02 \x01(\t\x12\x0e\n\x06\x63hange\x18\x03 \x01(\x03\x12\x10\n\x08patchset\x18\x04 \x01(\x03\"Y\n\rGitilesCommit\x12\x0c\n\x04host\x18\x01 \x01(\t\x12\x0f\n\x07project\x18\x02 \x01(\t\x12\n\n\x02id\x18\x03 \x01(\t\x12\x0b\n\x03ref\x18\x04 \x01(\t\x12\x10\n\x08position\x18\x05 \x01(\r\"(\n\nStringPair\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t\"i\n\tTimeRange\x12.\n\nstart_time\x18\x01 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12,\n\x08\x65nd_time\x18\x02 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\"_\n\x12RequestedDimension\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t\x12-\n\nexpiration\x18\x03 \x01(\x0b\x32\x19.google.protobuf.Duration*\x87\x01\n\x06Status\x12\x16\n\x12STATUS_UNSPECIFIED\x10\x00\x12\r\n\tSCHEDULED\x10\x01\x12\x0b\n\x07STARTED\x10\x02\x12\x0e\n\nENDED_MASK\x10\x04\x12\x0b\n\x07SUCCESS\x10\x0c\x12\x0b\n\x07\x46\x41ILURE\x10\x14\x12\x11\n\rINFRA_FAILURE\x10$\x12\x0c\n\x08\x43\x41NCELED\x10\x44*%\n\x07Trinary\x12\t\n\x05UNSET\x10\x00\x12\x07\n\x03YES\x10\x01\x12\x06\n\x02NO\x10\x02\x42\x36Z4go.chromium.org/luci/buildbucket/proto;buildbucketpbb\x06proto3'
     ),
     dependencies=[
         google_dot_protobuf_dot_duration__pb2.DESCRIPTOR,
@@ -96,8 +96,8 @@ _STATUS = _descriptor.EnumDescriptor(
     ],
     containing_type=None,
     serialized_options=None,
-    serialized_start=714,
-    serialized_end=849,
+    serialized_start=772,
+    serialized_end=907,
 )
 _sym_db.RegisterEnumDescriptor(_STATUS)
 
@@ -120,8 +120,8 @@ _TRINARY = _descriptor.EnumDescriptor(
     ],
     containing_type=None,
     serialized_options=None,
-    serialized_start=851,
-    serialized_end=888,
+    serialized_start=909,
+    serialized_end=946,
 )
 _sym_db.RegisterEnumDescriptor(_TRINARY)
 
@@ -138,6 +138,62 @@ UNSET = 0
 YES = 1
 NO = 2
 
+_EXECUTABLE = _descriptor.Descriptor(
+    name='Executable',
+    full_name='buildbucket.v2.Executable',
+    filename=None,
+    file=DESCRIPTOR,
+    containing_type=None,
+    fields=[
+        _descriptor.FieldDescriptor(
+            name='cipd_package',
+            full_name='buildbucket.v2.Executable.cipd_package',
+            index=0,
+            number=1,
+            type=9,
+            cpp_type=9,
+            label=1,
+            has_default_value=False,
+            default_value=_b("").decode('utf-8'),
+            message_type=None,
+            enum_type=None,
+            containing_type=None,
+            is_extension=False,
+            extension_scope=None,
+            serialized_options=None,
+            file=DESCRIPTOR
+        ),
+        _descriptor.FieldDescriptor(
+            name='cipd_version',
+            full_name='buildbucket.v2.Executable.cipd_version',
+            index=1,
+            number=2,
+            type=9,
+            cpp_type=9,
+            label=1,
+            has_default_value=False,
+            default_value=_b("").decode('utf-8'),
+            message_type=None,
+            enum_type=None,
+            containing_type=None,
+            is_extension=False,
+            extension_scope=None,
+            serialized_options=None,
+            file=DESCRIPTOR
+        ),
+    ],
+    extensions=[],
+    nested_types=[],
+    enum_types=[],
+    serialized_options=None,
+    is_extendable=False,
+    syntax='proto3',
+    extension_ranges=[],
+    oneofs=[],
+    serialized_start=97,
+    serialized_end=153,
+)
+
 _STATUSDETAILS_RESOURCEEXHAUSTION = _descriptor.Descriptor(
     name='ResourceExhaustion',
     full_name='buildbucket.v2.StatusDetails.ResourceExhaustion',
@@ -153,8 +209,8 @@ _STATUSDETAILS_RESOURCEEXHAUSTION = _descriptor.Descriptor(
     syntax='proto3',
     extension_ranges=[],
     oneofs=[],
-    serialized_start=250,
-    serialized_end=270,
+    serialized_start=308,
+    serialized_end=328,
 )
 
 _STATUSDETAILS_TIMEOUT = _descriptor.Descriptor(
@@ -172,8 +228,8 @@ _STATUSDETAILS_TIMEOUT = _descriptor.Descriptor(
     syntax='proto3',
     extension_ranges=[],
     oneofs=[],
-    serialized_start=272,
-    serialized_end=281,
+    serialized_start=330,
+    serialized_end=339,
 )
 
 _STATUSDETAILS = _descriptor.Descriptor(
@@ -231,8 +287,8 @@ _STATUSDETAILS = _descriptor.Descriptor(
     syntax='proto3',
     extension_ranges=[],
     oneofs=[],
-    serialized_start=98,
-    serialized_end=293,
+    serialized_start=156,
+    serialized_end=351,
 )
 
 _GERRITCHANGE = _descriptor.Descriptor(
@@ -323,8 +379,8 @@ _GERRITCHANGE = _descriptor.Descriptor(
     syntax='proto3',
     extension_ranges=[],
     oneofs=[],
-    serialized_start=295,
-    serialized_end=374,
+    serialized_start=353,
+    serialized_end=432,
 )
 
 _GITILESCOMMIT = _descriptor.Descriptor(
@@ -433,8 +489,8 @@ _GITILESCOMMIT = _descriptor.Descriptor(
     syntax='proto3',
     extension_ranges=[],
     oneofs=[],
-    serialized_start=376,
-    serialized_end=465,
+    serialized_start=434,
+    serialized_end=523,
 )
 
 _STRINGPAIR = _descriptor.Descriptor(
@@ -489,8 +545,8 @@ _STRINGPAIR = _descriptor.Descriptor(
     syntax='proto3',
     extension_ranges=[],
     oneofs=[],
-    serialized_start=467,
-    serialized_end=507,
+    serialized_start=525,
+    serialized_end=565,
 )
 
 _TIMERANGE = _descriptor.Descriptor(
@@ -545,8 +601,8 @@ _TIMERANGE = _descriptor.Descriptor(
     syntax='proto3',
     extension_ranges=[],
     oneofs=[],
-    serialized_start=509,
-    serialized_end=614,
+    serialized_start=567,
+    serialized_end=672,
 )
 
 _REQUESTEDDIMENSION = _descriptor.Descriptor(
@@ -619,8 +675,8 @@ _REQUESTEDDIMENSION = _descriptor.Descriptor(
     syntax='proto3',
     extension_ranges=[],
     oneofs=[],
-    serialized_start=616,
-    serialized_end=711,
+    serialized_start=674,
+    serialized_end=769,
 )
 
 _STATUSDETAILS_RESOURCEEXHAUSTION.containing_type = _STATUSDETAILS
@@ -635,6 +691,7 @@ _TIMERANGE.fields_by_name[
     'end_time'].message_type = google_dot_protobuf_dot_timestamp__pb2._TIMESTAMP
 _REQUESTEDDIMENSION.fields_by_name[
     'expiration'].message_type = google_dot_protobuf_dot_duration__pb2._DURATION
+DESCRIPTOR.message_types_by_name['Executable'] = _EXECUTABLE
 DESCRIPTOR.message_types_by_name['StatusDetails'] = _STATUSDETAILS
 DESCRIPTOR.message_types_by_name['GerritChange'] = _GERRITCHANGE
 DESCRIPTOR.message_types_by_name['GitilesCommit'] = _GITILESCOMMIT
@@ -644,6 +701,17 @@ DESCRIPTOR.message_types_by_name['RequestedDimension'] = _REQUESTEDDIMENSION
 DESCRIPTOR.enum_types_by_name['Status'] = _STATUS
 DESCRIPTOR.enum_types_by_name['Trinary'] = _TRINARY
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
+
+Executable = _reflection.GeneratedProtocolMessageType(
+    'Executable',
+    (_message.Message,),
+    dict(
+        DESCRIPTOR=_EXECUTABLE,
+        __module__='common_pb2'
+        # @@protoc_insertion_point(class_scope:buildbucket.v2.Executable)
+    )
+)
+_sym_db.RegisterMessage(Executable)
 
 StatusDetails = _reflection.GeneratedProtocolMessageType(
     'StatusDetails',
