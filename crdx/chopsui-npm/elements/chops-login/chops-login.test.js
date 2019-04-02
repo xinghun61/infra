31de99dbc3786d9feca0fd9f63175c56dfa37399
Sean McCullough <seanmccullough@chromium.org>
2019-04-02 16:58:40 +0000

    [chopsui] add npm package for chops-login element
    Change-Id: Ic2f39923ba053e71155cc8cae097ca3301e662f2
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1548347
    Commit-Queue: Sean McCullough <seanmccullough@chromium.org>
    Reviewed-by: Dave Tu <dtu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#22026}

---
+++ crdx/chopsui-npm/elements/chops-login/chops-login.test.js
@@ -0,0 +1,30 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chai';
+import {ChopsLogin} from './index.js';
+
+let element;
+
+suite('chops-login');
+
+beforeEach(() => {
+  element = document.createElement('chops-login');
+  document.body.appendChild(element);
+});
+
+afterEach(() => {
+  document.body.removeChild(element);
+});
+
+test('initializes', () => {
+  assert.instanceOf(element, ChopsLogin);
+});
+
+test('changing user sets user', async () => {
+  element.user = 'test user';
+  await element.updateComplete;
+  const link = element.shadowRoot.querySelector('a');
+  assert.equal(link.textContent.trim(), 'LOGOUT');
+});
---
+++ crdx/chopsui-npm/elements/chops-login/index.js
@@ -0,0 +1,61 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {LitElement, html} from 'lit-element/lit-element.js';
+
+/**
+ * `chops-login` shows the login/logout links and current user, if provided.
+ *
+ * When the user is logged in provide, at least, the user and logoutURL
+ * properties. When there is no user logged in provide, at least the loginURL
+ * property.  * In either case, bother URLs may be provided but only one will
+ * be shown.
+ *
+ * @customElement
+ * @polymer
+ * @demo /demo/chops-login_demo.html
+ */
+
+export class ChopsLogin extends LitElement {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+    <style>
+      :host {
+        --chops-login-link-color: inherit;
+      }
+      a, a:link {
+        color: var(--chops-login-link-color);
+        font-size: 0.75em;
+        text-decoration: none;
+      }
+    </style>
+    ${this.user
+        ? html`${this.user} (<a href="${this.logoutUrl}">LOGOUT</a>)`
+        : html`<a href="${this.loginUrl}">LOGIN</a>`
+    }`;
+  }
+  static get properties() {
+    return {
+      /** The login URL must be provided if no user is given. */
+      loginUrl: {
+        type: String,
+      },
+      /** The logout URL must be provided if a user is given. */
+      logoutUrl: {
+        type: String,
+      },
+      /** The logged in user. If this isn't given the login URL will be shown.*/
+      user: {
+        type: String,
+        value: '',
+      },
+    };
+  }
+}
+
+customElements.define('chops-login', ChopsLogin);
