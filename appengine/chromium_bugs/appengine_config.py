f30a518e22cd1bcc3b0f0ea66c63ebda5a4be5fc
Jason Robbins <jrobbins@chromium.org>
2019-04-03 19:30:40 +0000

    [Monorail wizard] Add third_party libs needed by gae_ts_mon.
    This should resolve the problem that blocked me from deploying the
    wizard in 2018.
    
    R=jojwang@chromium.org
    
    Change-Id: If54ef311b36c06a0c034b640f96c51f3f77b53b7
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1551819
    Commit-Queue: Jason Robbins <jrobbins@chromium.org>
    Reviewed-by: Joanna Wang <jojwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#22087}

---
+++ appengine/chromium_bugs/appengine_config.py
@@ -0,0 +1,12 @@
+# Copyright 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is govered by a BSD-style
+# license that can be found in the LICENSE file or at
+# https://developers.google.com/open-source/licenses/bsd
+
+"""Configuration."""
+
+import os
+import sys
+
+# Enable third-party imports
+sys.path.append(os.path.join(os.path.dirname(__file__), 'third_party'))
