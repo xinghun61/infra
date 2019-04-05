e9a7f0c58bdec1904578c3cfbc3a32d862d78fdc
Nodir Turakulov <nodir@google.com>
2019-04-05 18:34:25 +0000

    [buildbucket] Log revision without buildset
    Log a warning  if parameteres.changes.revision is passed without commit
    buildset. Compatibiltiy layer currently ignores this case.
    
    When no warnings, reject such requests.
    
    Bug: 949980
    Change-Id: I5f4fd780295370e538285e6979d28a03c8b18f5b
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1554229
    Auto-Submit: Nodir Turakulov <nodir@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#22185}

--- appengine/cr-buildbucket/legacy/api.py
+++ appengine/cr-buildbucket/legacy/api.py
@@ -170,6 +170,10 @@ def put_request_message_to_build_request(put_request):
     sbr.gitiles_commit.CopyFrom(gitiles_commit)
   sbr.gerrit_changes.extend(gerrit_changes)
 
+  legacy_revision = parameters.get('changes', {}).get('revision')
+  if legacy_revision and not gitiles_commit:  # pragma: no cover
+    logging.warning('legacy revision without buildset')
+
   # Populate Gerrit project from patch_project property.
   # V2 API users will have to provide this.
   patch_project = props.get('patch_project')
