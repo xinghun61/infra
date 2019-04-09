ad229cf52a9995e7481a72e0e075bde82b65a49f
Gary Miguel <garymm@google.com>
2019-04-09 19:40:14 +0000

    New package for YAPF tool.
    Will be used in the fuchsia format recipe:
    https://fuchsia.git.corp.google.com/infra/recipes/+/51c4ab968d89b39e8b4f7a4b6f6982dfe441b1d9/recipes/tricium/format.py#
    
    Fuchsia-Bug: IN-287
    Change-Id: I173ece58083f342745212d2ae2b6535a48d85c01
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1560081
    Auto-Submit: Gary Miguel <garymm@google.com>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#22270}

---
+++ infra/tools/yapf/__main__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env vpython
+
+# Copyright 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# [VPYTHON:BEGIN]
+# wheel: <
+#   name: "infra/python/wheels/yapf-py2_py3"
+#   version: "version:0.27.0"
+# >
+# [VPYTHON:END]
+
+# -*- coding: utf-8 -*-
+import sys
+
+from yapf import run_main
+
+if __name__ == '__main__':
+    sys.exit(run_main())
