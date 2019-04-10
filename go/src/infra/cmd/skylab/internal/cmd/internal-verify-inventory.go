c93e6a909f130dbf3c56b806e14730027ea8f7a3
Congbin Guo <guocb@google.com>
2019-04-10 02:12:22 +0000

    skylab inventory verify: verify speicified inventory types
    This change allows us to specifiy which type of inventory to check, e.g.
    LAB, or INFRA.
    
    BUG=chromium:925139
    TEST=Ran the command locally.
    
    Change-Id: Ic666f9e91da192e73422569d921b4a07895345dd
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1554234
    Reviewed-by: Allen Li <ayatane@chromium.org>
    Commit-Queue: Congbin Guo <guocb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#22288}

--- go/src/infra/cmd/skylab/internal/cmd/internal-verify-inventory.go
+++ go/src/infra/cmd/skylab/internal/cmd/internal-verify-inventory.go
@@ -5,7 +5,9 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/maruel/subcommands"
 	"go.chromium.org/luci/common/errors"
@@ -15,18 +17,58 @@ import (
 
 // InternalVerifyInventory subcommand: Verify inventory.
 var InternalVerifyInventory = &subcommands.Command{
-	UsageLine: "internal-verify-inventory DATA_DIR",
+	UsageLine: "internal-verify-inventory [-inv-type TYPE]... DATA_DIR",
 	ShortDesc: "Verify skylab inventory.",
 	LongDesc: `Verify skylab inventory.
 
 For internal use only.`,
 	CommandRun: func() subcommands.CommandRun {
-		return &internalVerifyInventory{}
+		c := &internalVerifyInventory{}
+		c.Flags.Var(&c.typeToVerify, "inv-type", inventoryTypeUsage())
+		return c
 	},
+}
+
+// Inventory types defined in package `inventory`.
+type inventoryTypes uint
+
+//go:generate stringer -type inventoryTypes
+
+const (
+	inventoryTypeLab inventoryTypes = 1 << iota
+	inventoryTypeInfra
+)
+
+const allInventoryTypes = inventoryTypeLab | inventoryTypeInfra
+
+var inventoryTypeNameDict = map[string]inventoryTypes{
+	"lab":   inventoryTypeLab,
+	"infra": inventoryTypeInfra,
+}
+
+func inventoryTypeUsage() string {
+	keys := make([]string, 0, len(inventoryTypeNameDict))
+	for k := range inventoryTypeNameDict {
+		keys = append(keys, k)
+	}
+	return fmt.Sprintf("Inventory type to verify. This flag can be used "+
+		"multiple times. The type can be %s (case insensitive).", strings.Join(keys, ", "))
+
+}
+
+// Set is for flag.value of '-inv-type'.
+func (i *inventoryTypes) Set(value string) error {
+	v, ok := inventoryTypeNameDict[strings.ToLower(value)]
+	if !ok {
+		return fmt.Errorf("unsupported inventory type: %s", value)
+	}
+	*i |= v
+	return nil
 }
 
 type internalVerifyInventory struct {
 	subcommands.CommandRunBase
+	typeToVerify inventoryTypes
 }
 
 func (c *internalVerifyInventory) Run(a subcommands.Application, args []string, env subcommands.Env) int {
@@ -38,18 +80,36 @@ func (c *internalVerifyInventory) Run(a subcommands.Application, args []string, 
 }
 
 func (c *internalVerifyInventory) innerRun(a subcommands.Application, args []string, env subcommands.Env) error {
-	if len(args) != 1 {
-		return NewUsageError(c.Flags, "want 1 positional argument, have %d", len(args))
+	if narg := c.Flags.NArg(); narg != 1 {
+		return NewUsageError(c.Flags, "want 1 positional argument, have %d", narg)
 	}
-	ddir := args[0]
+	ddir := c.Flags.Arg(0)
 	if _, err := os.Stat(ddir); os.IsNotExist(err) {
 		return err
 	}
-	if err := inventory.VerifyLabInventory(ddir); err != nil {
-		return err
+
+	if c.typeToVerify == 0 {
+		c.typeToVerify = allInventoryTypes
 	}
-	if err := inventory.VerifyInfraInventory(ddir); err != nil {
-		return err
+	var resultErrors []error
+	if c.typeToVerify&inventoryTypeLab != 0 {
+		if err := inventory.VerifyLabInventory(ddir); err != nil {
+			resultErrors = append(resultErrors, err)
+		}
 	}
-	return nil
+	if c.typeToVerify&inventoryTypeInfra != 0 {
+		if err := inventory.VerifyInfraInventory(ddir); err != nil {
+			resultErrors = append(resultErrors, err)
+		}
+	}
+	switch len(resultErrors) {
+	case 0:
+		return nil
+	case 1:
+		return resultErrors[0]
+	case 2:
+		return errors.New(resultErrors[0].Error() + resultErrors[1].Error())
+	default:
+		panic("This shouldn't happen!")
+	}
 }
