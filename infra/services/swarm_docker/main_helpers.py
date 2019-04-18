b99e3e5d7de4ea522a1a2e4d661f9d33117023a1
Anushruth <anushruth@chromium.org>
2019-04-18 23:11:32 +0000

    Added '/b/reboot.stamp' trigger to container harness
    Added new trigger to swarm_docker. If '/b/reboot.stamp' file is found
    then the following operations are performed
            1. Attempt to kill all containers
            2. If all containers are drained
                    a. Delete '/b/reboot.stamp'
                    b. Reboot host
            3.Else, do nothing
    If not all containers can be drained then, on the next run reboot is
    attempted again as the file is not deleted
    
    R=bpastene@chromium.org
    
    Bug: 937366
    Change-Id: Ie0007ea925bf0f07a17007fcf4090f04e357d11e
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1561900
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Anushruth <anushruth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#22488}

--- infra/services/swarm_docker/main_helpers.py
+++ infra/services/swarm_docker/main_helpers.py
@@ -28,6 +28,10 @@ _REGISTRY_URL = 'gcr.io'
 # container's shutdown file as returned by its descriptor class.
 BOT_SHUTDOWN_FILE = '/b/shutdown.stamp'
 
+# Similar to BOT_SHUTDOWN_FILE, but triggers a host reboot after all containers
+# have been drained.
+BOT_REBOOT_FILE = '/b/reboot.stamp'
+
 # Minimum time a host must be up before it can be restarted again.
 _MIN_HOST_UPTIME = 60
 
@@ -180,6 +184,7 @@ def reboot_gracefully(args, running_containers):
 
 def launch_containers(
     docker_client, container_descriptors, args):  # pragma: no cover
+  rebooting_host = os.path.exists(BOT_REBOOT_FILE)
   draining_host = os.path.exists(BOT_SHUTDOWN_FILE)
   draining_container_descriptors = [
       cd for cd in container_descriptors if os.path.exists(cd.shutdown_file)
@@ -188,6 +193,10 @@ def launch_containers(
     logging.info(
         'In draining state due to existence of %s. No new containers will be '
         'created.', BOT_SHUTDOWN_FILE)
+  elif rebooting_host:
+    logging.info(
+        'In draining state due to existence of %s. No new containers will be '
+        'created and host will be restarted.', BOT_REBOOT_FILE)
   elif draining_container_descriptors:
     logging.info(
         'Draining containers %s due to existence of files: %s. They will not '
@@ -196,7 +205,8 @@ def launch_containers(
         [cd.shutdown_file for cd in draining_container_descriptors])
 
   running_containers = docker_client.get_running_containers()
-  if not draining_host and reboot_gracefully(args, running_containers):
+  if (not draining_host and not rebooting_host
+      and reboot_gracefully(args, running_containers)):
     return
 
   # Fetch the image from the registry if it's not present locally.
@@ -218,9 +228,12 @@ def launch_containers(
   # to associated devices missing, so we need to examine *all* containers here
   # instead of doing that inside the per-container flock below.
   current_cipd_version = get_cipd_version()
-  if draining_host:
+  if draining_host or rebooting_host:
     for c in running_containers:
       c.kill_swarming_bot()
+    if rebooting_host and not running_containers:
+      os.remove(BOT_REBOOT_FILE)
+      reboot_host(args.canary)
   else:
     for cd in draining_container_descriptors:
       c = docker_client.get_container(cd)
