956178e385ba86c30c5a6c5bf4bd8c2775a20641
Tiff Zhang <zhangtiff@google.com>
2019-04-26 21:41:48 +0000

    Monorail: Extract a css const that can be used alongside lit-element from mr-shared styles
    Lit-element allows elements to define an array of style strings (See: https://lit-element.polymer-project.org/guide/styles#style-element ), so we can share styles simply by exposing them as importable consts.
    
    Using this method to also allow backwards compatibility with mr-shared-styles. Lit-elements that need these styles can use the exportable SHARED_STYLES const. Once all elements that depend on mr-shared-styles are upgraded, mr-shared-styles can be deleted.
    
    Bug: monorail:5358
    Change-Id: I99aad6527dfb0ea26eb2a011d23f74ac78ae3972
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1586206
    Reviewed-by: Dave Tu <dtu@chromium.org>
    Commit-Queue: Tiffany Zhang <zhangtiff@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#22620}

--- appengine/monorail/static_src/elements/shared/mr-shared-styles.js
+++ appengine/monorail/static_src/elements/shared/mr-shared-styles.js
@@ -2,107 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {SHARED_STYLES} from './shared-styles';
+
 const $_documentContainer = document.createElement('template');
 
 $_documentContainer.innerHTML = `<dom-module id="mr-shared-styles">
   <template>
     <style>
-      :root {
-        --mr-edit-field-styles: {
-          box-sizing: border-box;
-          width: 95%;
-          padding: 0.125em 4px;
-          font-size: var(--chops-main-font-size);
-        };
-        --mr-input-grid-gap: 6px;
-        --mr-content-heading-color: hsl(227, 60%, 39%);
-      }
-      .linkify {
-        text-decoration: underline;
-        color: var(--chops-link-color);
-        display: inline;
-        padding: 0;
-        margin: 0;
-        border: 0;
-        background: 0;
-        cursor: pointer;
-      }
-      h1, h2, h3, h4 {
-        background: none;
-      }
-      a.button, .button {
-        /* Links that look like buttons. */
-        display: inline-flex;
-        align-items: center;
-        justify-content: center;
-        text-decoration: none;
-        transition: filter 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
-      }
-      a.button:hover, .button:hover {
-        filter: brightness(95%);
-      }
-      chops-button, a.button, .button {
-        box-sizing: border-box;
-        font-size: var(--chops-main-font-size);
-        background: white;
-        border-radius: 6px;
-        padding: 0.25em 8px;
-        margin: 0;
-        margin-left: auto;
-        color: var(--chops-link-color);
-      }
-      chops-button i.material-icons, a.button i.material-icons, .button i.material-icons {
-        display: block;
-        margin-right: 4px;
-      }
-      chops-button.emphasized, a.button.emphasized, .button.emphasized {
-        background: var(--chops-primary-button-bg);
-        color: var(--chops-primary-button-color);
-        text-shadow: 1px 1px 3px hsla(0, 0%, 0%, 0.25);
-      }
-      textarea {
-        font-size: var(--chops-main-font-size);
-      }
-      /* Note: decoupling heading levels from styles is useful for
-       * accessibility because styles will not always line up with semantically
-       * appropriate heading levels.
-       */
-      .medium-heading {
-        font-size: var(--chops-large-font-size);
-        font-weight: normal;
-        line-height: 1;
-        padding: 0.25em 0;
-        color: var(--mr-content-heading-color);
-        margin: 0;
-        margin-top: 0.25em;
-        border-bottom: var(--chops-normal-border);
-      }
-      .medium-heading chops-button {
-        line-height: 1.6;
-      }
-      .input-grid {
-        padding: 0.5em 0;
-        display: grid;
-        max-width: 100%;
-        grid-gap: var(--mr-input-grid-gap);
-        grid-template-columns: 120px auto;
-        align-items: flex-start;
-      }
-      .input-grid label {
-        font-weight: bold;
-        text-align: right;
-        word-wrap: break-word;
-      }
-      @media (max-width: 600px) {
-        .input-grid label {
-          margin-top: var(--mr-input-grid-gap);
-          text-align: left;
-        }
-        .input-grid {
-          grid-gap: var(--mr-input-grid-gap);
-          grid-template-columns: 100%;
-        }
-      }
+      ${SHARED_STYLES}
     </style>
   </template>
 </dom-module>`;
