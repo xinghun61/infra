f54932c6531d04ef649e396d21da0f3b6fc48c0c
Edward Lemur <ehmaldonado@chromium.org>
2019-04-26 21:00:08 +0000

    monorail: Convert mr-error and mr-warning to lit-element.
    Bug: monorail:5419
    Change-Id: Ie34e7cd71d398e3926de0dff9673483277fbe9d5
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1586241
    Reviewed-by: Tiffany Zhang <zhangtiff@chromium.org>
    Commit-Queue: Edward Lesmes <ehmaldonado@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#22619}

--- appengine/monorail/static_src/elements/framework/mr-warning/mr-warning.js
+++ appengine/monorail/static_src/elements/framework/mr-warning/mr-warning.js
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '@polymer/polymer/polymer-legacy.js';
-import {PolymerElement, html} from '@polymer/polymer';
+import {LitElement, html, css} from 'lit-element';
+
 
 /**
  * `<mr-warnings>`
@@ -11,40 +11,39 @@ import {PolymerElement, html} from '@polymer/polymer';
  * A container for showing warnings.
  *
  */
-export class MrWarning extends PolymerElement {
-  static get template() {
+export class MrWarning extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+        display: flex;
+        align-items: center;
+        flex-direction: row;
+        justify-content: flex-start;
+        box-sizing: border-box;
+        width: 100%;
+        margin: 0.5em 0;
+        padding: 0.25em 8px;
+        border: 1px solid #FF6F00;
+        border-radius: 4px;
+        background: #FFF8E1;
+      }
+      :host([hidden]) {
+        display: none;
+      }
+      i.material-icons {
+        color: #FF6F00;
+        margin-right: 4px;
+      }
+    `;
+  }
+
+  render() {
     return html`
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <style>
-        :host {
-          display: flex;
-          align-items: center;
-          flex-direction: row;
-          justify-content: flex-start;
-          box-sizing: border-box;
-          width: 100%;
-          margin: 0.5em 0;
-          padding: 0.25em 8px;
-          border: 1px solid #FF6F00;
-          border-radius: 4px;
-          background: #FFF8E1;
-        }
-        :host([hidden]) {
-          display: none;
-        }
-        i.material-icons {
-          color: #FF6F00;
-          margin-right: 4px;
-        }
-      </style>
       <i class="material-icons">warning</i>
       <slot></slot>
     `;
   }
-
-  static get is() {
-    return 'mr-warning';
-  }
 }
 
-customElements.define(MrWarning.is, MrWarning);
+customElements.define('mr-warning', MrWarning);
