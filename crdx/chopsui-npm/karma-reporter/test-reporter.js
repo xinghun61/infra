97cc5c2eaa980f80e6fa3eeecc8770ba598bbbdd
Sean McCullough <seanmccullough@chromium.org>
2019-05-07 00:06:19 +0000

    [chopsui-npm] karma reporter for test-results.appspot.com's JSON format.
    This produces json files that we should be able to upload to the
    test results server, so we can analyze results in aggregate using BQ.
    
    Note that the JSON format should still work after test-results
    gets replaced by something better from LUCI team. The PRD assumes the
    new solution will also read this same JSON format:
    https://docs.google.com/document/d/1FeYxmxd1UMtk-i94mZIW3SG_L2Ymt05tr5F68XG67O8/edit#
    
    Change-Id: I1b580dfb6aed85ee4a9e4994ff7b25e362df395a
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1593426
    Commit-Queue: Sean McCullough <seanmccullough@chromium.org>
    Reviewed-by: Tiffany Zhang <zhangtiff@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#22782}

---
+++ crdx/chopsui-npm/karma-reporter/test-reporter.js
@@ -0,0 +1,85 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This is a karma reporter that produces test results JSON suitable for
+// uploading to test-results.appspot.com. For more details, see
+// https://chromium.googlesource.com/chromium/src/+/master/docs/testing/json_test_results_format.md
+
+const path = require('path');
+const fs = require('fs');
+
+// Creates a nested path of objects starting at root,
+// each step named by elements of s.  Returns the last
+// node of the path.
+function nest(s, root) {
+  let curr = root;
+  s.forEach((part) => {
+    if (!curr.hasOwnProperty(part)) {
+      curr[part] = {};
+    }
+    curr = curr[part];
+  });
+  return curr;
+}
+
+const ChOpsJSONReporter = function(
+    baseReporterDecorator, config, helper, logger) {
+  const log = logger.create('chopsui-json-reporter');
+  baseReporterDecorator(this);
+  const history = {
+    interrupted: false,
+    num_failures_by_type: {},
+    path_delimiter: '/',
+    seconds_since_epoch: new Date().getTime()/1000,
+    tests: {},
+    version: 3,
+  };
+  const reporterConfig = config.chopsUiReporter || {};
+  const stdout = typeof reporterConfig.stdout !== 'undefined' ?
+    reporterConfig.stdout : true;
+  const outputFile = reporterConfig.outputFile
+    ? helper.normalizeWinPath(path.resolve(
+      config.basePath, reporterConfig.outputFile))
+    : null;
+
+  this.onSpecComplete = function(browser, result) {
+    const res = nest([...result.suite, result.description], history.tests);
+    res.expected = 'PASS';
+    res.actual = result.success ? 'PASS' : 'FAIL';
+    res.start = result.startTime;
+    res.end = result.endTime;
+    res.time = result.endTime - result.startTime;
+  };
+
+  this.onRunComplete = function(browser, result) {
+    history.num_failures_by_type['FAIL'] = result.failed;
+    history.num_failures_by_type['PASS'] = result.success;
+
+    const json = JSON.stringify(history, undefined, '\t');
+    if (stdout) {
+      process.stdout.write(json);
+    }
+    if (outputFile) {
+      helper.mkdirIfNotExists(path.dirname(outputFile), function() {
+        fs.writeFile(outputFile, json, function(err) {
+          if (err) {
+            log.warn('Cannot write JSON\n\t' + err.message);
+          } else {
+            log.debug('JSON written to "%s".', outputFile);
+          }
+        });
+      });
+    } else {
+      history.result = {};
+    }
+  };
+};
+
+ChOpsJSONReporter.$inject = ['baseReporterDecorator', 'config', 'helper',
+  'logger'];
+
+// PUBLISH DI MODULE
+module.exports = {
+  'reporter:chopsui-json': ['type', ChOpsJSONReporter],
+};
