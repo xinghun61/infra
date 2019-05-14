1708ecdc6c6dbb23457391a127647eca3afb441b
Jeff Carpenter <jeffcarp@chromium.org>
2019-05-14 21:38:55 +0000

    [Monorail] Fix typo in license headers: s/govered/governed/
    Bug: monorail:5547
    Change-Id: I88dc9ba6ad57323c67efe944a0339fe3c4c31b48
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1610949
    Reviewed-by: Jason Robbins <jrobbins@chromium.org>
    Commit-Queue: Jeff Carpenter <jeffcarp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#22950}

--- appengine/monorail/tools/ml/spam.py
+++ appengine/monorail/tools/ml/spam.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Copyright 2016 The Chromium Authors. All rights reserved.
-# Use of this source code is govered by a BSD-style
+# Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file or at
 # https://developers.google.com/open-source/licenses/bsd
 
