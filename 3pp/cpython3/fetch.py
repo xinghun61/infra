c537eee802f5511d5edbd134cdedd77a65df4829
smut <smut@google.com>
2019-05-29 01:21:48 +0000

    [3pp] Update cpython3/fetch.py to support prerelease versions
    The 3.8.0 directory doesn't contain an installer for 3.8.0.
    
    Bug: 898348
    Change-Id: Ibcfe1af0f24f3d0c5ba942853659b8af4a8a19e7
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1633611
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: smut <smut@google.com>
    Cr-Commit-Position: refs/heads/master@{#23287}

--- 3pp/cpython3/fetch.py
+++ 3pp/cpython3/fetch.py
@@ -14,10 +14,20 @@ from pkg_resources import parse_version
 import requests
 
 
-def do_latest():
+def get_webinstaller_suffix(platform):
+  if platform == 'windows-386':
+    return '-webinstall.exe'
+  if platform == 'windows-amd64':
+    return '-amd64-webinstall.exe'
+  raise ValueError('fetch.py is only supported for windows-386, windows-amd64')
+
+
+def do_latest(platform):
   """This is pretty janky, but the apache generic Index page hasn't changed
   since forever. It contains links (a tags with href's) to the different
   version folders."""
+  suf = get_webinstaller_suffix(platform)
+  # Find the highest version e.g. 3.8.0.
   r = requests.get('https://www.python.org/ftp/python/')
   r.raise_for_status()
   highest = None
@@ -26,14 +36,28 @@ def do_latest():
     v = parse_version(m.group(1))
     if not highest or v > highest:
       highest = v
+  r = requests.get('https://www.python.org/ftp/python/%s/' % highest)
+  r.raise_for_status()
+  # Find the highest release e.g. 3.8.0a4.
+  highest = None
+  href_re = re.compile(r'href="python-(\d+\.\d+\.\d+((a|b|rc)\d+)?)%s"' % suf)
+  for m in href_re.finditer(r.text):
+    v = parse_version(m.group(1))
+    if not highest or v > highest:
+      highest = v
   print highest
 
 
 def do_checkout(version, platform, checkout_path):
-  assert '386' not in platform, 'only the 32bit python2 is supported'
+  # e.g. 3.8.0a4 -> 3.8.0
+  short = version
+  short_re = re.compile(r'(\d+\.\d+\.\d+)')
+  m = short_re.match(version)
+  if m:
+    short = m.group(0)
   download_url = (
-    'https://www.python.org/ftp/python/%(v)s/python-%(v)s-amd64-webinstall.exe'
-    % {'v': version}
+    'https://www.python.org/ftp/python/%(short)s/python-%(ver)s%(suf)s'
+    % {'short': short, 'ver': version, 'suf': get_webinstaller_suffix(platform)}
   )
   print >>sys.stderr, 'fetching %r' % (download_url,)
   outfile = 'install.exe'
@@ -53,7 +77,7 @@ def main():
   sub = ap.add_subparsers()
 
   latest = sub.add_parser("latest")
-  latest.set_defaults(func=lambda _opts: do_latest())
+  latest.set_defaults(func=lambda _opts: do_latest(os.environ['_3PP_PLATFORM']))
 
   checkout = sub.add_parser("checkout")
   checkout.add_argument("checkout_path")
