779337f1dd1e2db46d1dac75f2f8328c4449559a
smut <smut@google.com>
2019-05-30 00:27:29 +0000

    [3pp] Support cpython on windows-amd64
    Bug: 965937
    Change-Id: I6eabeb2dbe5c419a6b3892ea6a6c0fae6c92a0b3
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1636772
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: smut <smut@google.com>
    Cr-Commit-Position: refs/heads/master@{#23313}

--- 3pp/cpython/fetch.py
+++ 3pp/cpython/fetch.py
@@ -15,21 +15,28 @@ import requests
 VERSION = '2.7.15'
 
 
+def get_installer_suffix(platform):
+  if platform == 'windows-386':
+    return '.msi'
+  if platform == 'windows-amd64':
+    return '.amd64.msi'
+  raise ValueError('fetch.py is only supported for windows-386, windows-amd64')
+
+
 def do_latest():
   print VERSION
 
 
 def do_checkout(version, platform, checkout_path):
-  assert version == VERSION, 'only version %r is supported, got %r' % (
-    VERSION, version)
-  assert 'amd64' not in platform, 'only the 32bit python2 is supported'
-  download_url = 'https://www.python.org/ftp/python/%(v)s/python-%(v)s.msi' % {
-    'v': version,
+  if version != VERSION:
+    raise ValueError('fetch.py is only supported for cpython %s' % VERSION)
+  url = 'https://www.python.org/ftp/python/%(v)s/python-%(v)s%(suf)s' % {
+    'v': version, 'suf': get_installer_suffix(platform),
   }
-  print >>sys.stderr, 'fetching %r' % (download_url,)
+  print >>sys.stderr, 'fetching %r' % (url,)
   outfile = 'install.msi'
   with open(os.path.join(checkout_path, outfile), 'wb') as f:
-    r = requests.get(download_url, stream=True)
+    r = requests.get(url, stream=True)
     r.raise_for_status()
     for chunk in r.iter_content(1024**2):
       f.write(chunk)
