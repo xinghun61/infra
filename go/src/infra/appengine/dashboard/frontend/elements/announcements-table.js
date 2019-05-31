b919d29be8e279d545406b38e9af2c69867db60a
Joanna Wang <jojwang@chromium.org>
2019-05-31 19:58:39 +0000

    [chops-announcements]Add <announcements-table> for displaying live and retired announcements.
    Bug: 968896
    Change-Id: I7fe7edf6b6f44600d0484c09793f66e84308ffc2
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1638318
    Commit-Queue: Joanna Wang <jojwang@chromium.org>
    Reviewed-by: Tiffany Zhang <zhangtiff@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#23368}

---
+++ go/src/infra/appengine/dashboard/frontend/elements/announcements-table.js
@@ -0,0 +1,141 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {LitElement, html, css} from 'lit-element';
+import {prpcClient} from 'prpc.js';
+
+/**
+ * `<announcements-table>`
+ *
+ * Table to display all retired or all live announcements.
+ *
+ */
+export class AnnouncementsTable extends LitElement {
+  static get properties() {
+    return {
+      isTrooper: {type: Boolean},
+      announcements: {type: Array},
+      retired: {type: Boolean},
+    };
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        font-family: Roboto, Noto, sans-serif;
+      }
+      button {
+        background-color: transparent;
+        border: none;
+        color: #649af4;
+        font-weight: bolder;
+        cursor: pointer;
+      }
+      table {
+        border-collapse: collapse;
+        width: 100%;
+      }
+      td.announcement-content {
+        width: 60%;
+      }
+      td > div.date {
+        color: grey;
+      }
+      .small {
+        font-size: 12px;
+      }
+      td.platforms {
+        color: grey;
+      }
+      td, th {
+        padding: 7px;
+      }
+      th {
+        color: grey;
+        font-size: .9em;
+      }
+      tr {
+        background-color: white;
+        text-align: center;
+        border: solid #f4f4f4;
+        border-width: .5px 0;
+      }
+      tr:first-child {
+        border-top: none;
+      }
+      tr:last-child {
+        border-bottom: none;
+      }
+    `;
+  }
+
+  render() {
+    return html`
+      <table cellspacing="0" cellpadding="0">
+        <tbody>
+          <tr class="header-row">
+            ${this.isTrooper && !this.retired ? html`<th></th>` : ''}
+            <th>Platforms</th>
+            <th>Announcement</th>
+            <th>Timestamp</th>
+          </tr>
+
+          ${(this.announcements && this.announcements.length
+  ) ? this.announcements.map((ann) => html`
+            <tr>
+              ${this.isTrooper && !this.retired ? html`
+                <td>
+                  <button
+                    data-annId="${ann.id}" @click=${this.retireAnnouncement}
+                  >RETIRE</button>
+                </td>` : ''}
+              <td class="platforms">
+                ${ann.platforms && ann.platforms.length ? ann.platforms.map(
+    (plat) => html`
+                   <div>${plat.name}</div>`) : html` <i>No platforms found</i>
+                `}
+              </td>
+              <td class="announcement-content">${ann.messageContent}</td>
+              <td class="small">
+                <div class="date">${this._formatDate(ann.startTime)}</div>
+                <div class="creator">${ann.creator}</div>
+              </td>
+            </tr>
+          `) : html`
+            <tr>
+              <td
+                colspan=${this.isTrooper && !this.retired ? '4' : '3'}
+              ><i>No announcements.</i></td>
+            </tr>
+          `}
+        </tbody>
+      </table>
+    `;
+  }
+
+  // TODO(jojwang): use chops-timestamp when it's shared.
+  _formatDate(timeStr) {
+    const date = new Date(timeStr);
+    return new Intl.DateTimeFormat('en-US', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: '2-digit',
+      timeZoneName: 'short',
+    }).format(date);
+  }
+
+  retireAnnouncement(event) {
+    const retireMessage = {
+      announcementId: event.target.getAttribute('data-annId'),
+    };
+    const respPromise = prpcClient.call(
+      'dashboard.ChopsAnnouncements', 'RetireAnnouncement', retireMessage);
+    respPromise.then((resp) => {
+      this.dispatchEvent(new CustomEvent('announcements-changed'));
+    });
+  }
+}
+customElements.define('announcements-table', AnnouncementsTable);
---
+++ go/src/infra/appengine/dashboard/frontend/elements/announcements-table.test.js
@@ -0,0 +1,125 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chai';
+import {AnnouncementsTable} from './announcements-table.js';
+import {prpcClient} from 'prpc.js';
+
+let element;
+let prpcStub;
+
+const ANN = [
+  {
+    platforms: [{name: 'gerrit'}, {name: 'monorail'}],
+    messageContent: 'everything is on fire.',
+    creator: 'chicken',
+    id: '1234',
+    startTime: '2019-05-28T23:41:18.637847Z',
+  },
+  {
+    platforms: [{name: 'gerrit'}],
+    messageContent: 'chickens need feeding.',
+    creator: 'notchicken',
+    id: '12345',
+    startTime: '2019-05-28T23:41:18.637847Z',
+  },
+  {
+    messageContent: 'abandoned announcement.',
+    creator: 'fox',
+    id: '223',
+    startTime: '2019-05-28T23:41:18.637847Z',
+  }];
+
+suite('announcements-table', () => {
+  setup(() => {
+    element = document.createElement('announcements-table');
+    document.body.appendChild(element);
+    prpcStub = sinon.stub(prpcClient, 'call');
+  });
+
+  teardown(function() {
+    prpcStub.restore();
+  });
+
+  test('initializes', () => {
+    assert.instanceOf(element, AnnouncementsTable);
+  });
+
+  test('no announcements', async () => {
+    element.announcements = [];
+    await element.updateComplete;
+    const tableRows = element.shadowRoot.querySelector('table').rows;
+    assert.equal(tableRows.length, 2);
+    assert.equal(tableRows[1].innerText, 'No announcements.');
+  });
+
+  test('live announcements trooper', async () => {
+    const retirePromise = Promise.resolve(
+      {'retired': true, 'id': 123});
+    prpcStub.returns(retirePromise);
+    element.isTrooper = true;
+    element.announcements = ANN;
+    await element.updateComplete;
+    const tableRows = element.shadowRoot.querySelector('table').rows;
+    assert.equal(tableRows.length, 4);
+    assert.equal(tableRows[0].cells.length, 4);
+    assert.equal(tableRows[1].cells.length, 4);
+
+    // Assert announcements-changed fired and RetireAnnouncement called
+    const listener = sinon.spy();
+    element.addEventListener('announcements-changed', listener);
+
+    element.shadowRoot.querySelector('button').click();
+
+    sinon.assert.calledWith(
+      prpcStub, 'dashboard.ChopsAnnouncements', 'RetireAnnouncement',
+      {announcementId: element.announcements[0].id});
+    await retirePromise;
+    sinon.assert.calledOnce(listener);
+  });
+
+  test('live announcements non-trooper', async () => {
+    element.announcements = ANN;
+    await element.updateComplete;
+
+    const tableRows = element.shadowRoot.querySelector('table').rows;
+    assert.equal(tableRows.length, 4);
+    assert.equal(tableRows[0].cells.length, 3);
+    assert.equal(tableRows[1].cells.length, 3);
+
+    // Assert no buttons offered.
+    assert.equal(element.shadowRoot.querySelector('button'), null);
+  });
+
+  test('retired announcements trooper', async () => {
+    element.announcements = ANN;
+    element.isTrooper = true;
+    element.retired = true;
+
+    await element.updateComplete;
+
+    const tableRows = element.shadowRoot.querySelector('table').rows;
+    assert.equal(tableRows.length, 4);
+    assert.equal(tableRows[0].cells.length, 3);
+    assert.equal(tableRows[1].cells.length, 3);
+
+    // Assert no buttons offered.
+    assert.equal(element.shadowRoot.querySelector('button'), null);
+  });
+
+  test('retired announcements non-trooper', async () => {
+    element.announcements = ANN;
+    element.retired = true;
+
+    await element.updateComplete;
+
+    const tableRows = element.shadowRoot.querySelector('table').rows;
+    assert.equal(tableRows.length, 4);
+    assert.equal(tableRows[0].cells.length, 3);
+    assert.equal(tableRows[1].cells.length, 3);
+
+    // Assert no buttons offered.
+    assert.equal(element.shadowRoot.querySelector('button'), null);
+  });
+});
