b3ad10d55513e3c8102d574eedfd85e44adc7cb9
Quinten Yearsley <qyearsley@chromium.org>
2019-06-28 22:04:07 +0000

    [tricium] Convert tricium to go111 runtime
    Partly using https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1680967
    as an example.
    
    Change-Id: I17b1912364b1f90d4c4f9b55f5611b3db7313be1
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1679998
    Commit-Queue: Quinten Yearsley <qyearsley@chromium.org>
    Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#24033}

--- go/src/infra/tricium/appengine/frontend/handlers.go
+++ go/src/infra/tricium/appengine/frontend/handlers.go
@@ -2,8 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Package frontend implements HTTP handlers for the frontend (default) module.
-package frontend
+package main
 
 import (
 	"context"
