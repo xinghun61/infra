9c68058524685936de1bf79fa4a8560af97904dd
Takuto Ikuta <tikuta@chromium.org>
2019-07-03 05:18:52 +0000

    [chromium_build_stats] use appengine.Main
    Need to use appengine.Main when we use appengine package.
    https://cloud.google.com/appengine/docs/standard/go111/go-differences#writing_a_main_package
    
    Bug: 980820
    Change-Id: Ie1bcdd4ddab5b9fef88dcb1c655faef12349b95e
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1687151
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Fumitoshi Ukai <ukai@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#24132}

--- go/src/infra/appengine/chromium_build_stats/app/main.go
+++ go/src/infra/appengine/chromium_build_stats/app/main.go
@@ -5,19 +5,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
+	"google.golang.org/appengine"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
-	}
-
-	log.Printf("Listening on port %s", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	appengine.Main()
 }
