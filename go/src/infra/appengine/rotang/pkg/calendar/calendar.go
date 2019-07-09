721c346eab9431f7859a7c18207a17bcdd9a4509
Ola Karlsson <olakar@chromium.org>
2019-07-09 05:28:08 +0000

    [rotang] Fix duplicate calendar entries for rotations starting at 00:00 UTC
    Expanding the time range searched when fetching Google calendar events.
    
    Bug: 978260
    Change-Id: Icd29df9e6f5136b2ba921edbb12156d764b813d6
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1683707
    Commit-Queue: Ola Karlsson <olakar@chromium.org>
    Reviewed-by: Andrew Bennetts <abennetts@google.com>
    Cr-Commit-Position: refs/heads/master@{#24222}

--- go/src/infra/appengine/rotang/pkg/calendar/calendar.go
+++ go/src/infra/appengine/rotang/pkg/calendar/calendar.go
@@ -72,13 +72,18 @@ func (c *Calendar) CreateEvent(ctx *router.Context, cfg *rotang.Configuration, s
 	}, cfg.Config.Name, &cfg.Config.Shifts)
 }
 
+// eventsRange specifies the time-range used when finding and matching Google calendar events to RotaNG shifts.
+// The reason for searching instead of just making a query for the ID directly is that when querying for an ID'
+// sometimes the resulting Calendar Event is returned with blank Start/End times.
+const eventsRange = 2 * fullDay
+
 // Event returns the information about the provided shift from the associated calendar event.
 func (c *Calendar) Event(ctx *router.Context, cfg *rotang.Configuration, shift *rotang.ShiftEntry) (*rotang.ShiftEntry, error) {
 	if err := ctx.Context.Err(); err != nil {
 		return nil, err
 	}
 	// For some reason using the Google calendar Event.Get does not give Start and End times.
-	shifts, err := c.Events(ctx, cfg, shift.StartTime.Add(-24*time.Hour), shift.EndTime.Add(24*time.Hour))
+	shifts, err := c.Events(ctx, cfg, shift.StartTime.Add(-eventsRange), shift.EndTime.Add(eventsRange))
 	if err != nil {
 		return nil, err
 	}
