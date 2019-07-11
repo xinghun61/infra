701852868d382229817ea60676e493cbc41c7d03
Vadim Shtayura <vadimsh@chromium.org>
2019-07-11 22:37:43 +0000

    Delete experimental buildbucket-viewer.
    It is not used and becomes maintenance burden.
    
    R=nodir@chromium.org
    
    Change-Id: I472e7440894bad59ddb5984be4cb35c304b810d2
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1699187
    Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#24322}

--- go/src/infra/experimental/appengine/buildbucket-viewer/api/settings/generate.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2016 The LUCI Authors. All rights reserved.
-// Use of this source code is governed under the Apache License, Version 2.0
-// that can be found in the LICENSE file.
-
-package settings
-
-//go:generate go install go.chromium.org/luci/grpc/cmd/cproto
-//go:generate go install go.chromium.org/luci/grpc/cmd/svcdec
-//go:generate cproto
