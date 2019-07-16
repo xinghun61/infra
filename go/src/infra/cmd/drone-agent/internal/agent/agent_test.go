bbe9be50dda767c205574c0b810f27983c9cde1c
Allen Li <ayatane@chromium.org>
2019-07-16 19:50:59 +0000

    drone-agent: Move test doubles
    Bug: 976968
    Change-Id: Ie9156babd1ddad7f6971c4956b4816e4ea7fa505
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1699456
    Commit-Queue: Allen Li <ayatane@chromium.org>
    Reviewed-by: Xixuan Wu <xixuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#24426}

--- go/src/infra/cmd/drone-agent/internal/agent/agent_test.go
+++ go/src/infra/cmd/drone-agent/internal/agent/agent_test.go
@@ -9,18 +9,14 @@ import (
 	"io/ioutil"
 	"os"
 	"sort"
-	"sync"
 	"testing"
 	"time"
 
 	"github.com/golang/protobuf/ptypes"
 	"github.com/golang/protobuf/ptypes/timestamp"
 	"github.com/google/go-cmp/cmp"
-	"google.golang.org/grpc"
 
 	"infra/appengine/drone-queen/api"
-	"infra/cmd/drone-agent/internal/agent/state"
-	"infra/cmd/drone-agent/internal/bot"
 	"infra/cmd/drone-agent/internal/draining"
 )
 
@@ -366,154 +362,6 @@ type testLogger struct {
 
 func (t testLogger) Printf(format string, args ...interface{}) {
 	t.t.Logf(format, args...)
-}
-
-// startFakeBot is a fake implementation of Agent.startBotFunc.
-func startFakeBot(bot.Config) (bot.Bot, error) {
-	return bot.NewFakeBot(), nil
-}
-
-// stateSpyFactory implements a method that can be used as
-// wrapStateFunc to inspect created states.
-type stateSpyFactory struct {
-	states chan *stateSpy
-}
-
-func newStateSpyFactory() stateSpyFactory {
-	return stateSpyFactory{
-		// These channels need to have big enough buffers to
-		// capture events needed by tests.  Events that
-		// overfill the channel buffers are discarded.
-		states: make(chan *stateSpy, 1),
-	}
-}
-
-// wrapState wraps a state in a stateSpy and sends it to waitForState.
-// This method is used as wrapStateFunc.
-func (f stateSpyFactory) wrapState(s *state.State) stateInterface {
-	s2 := &stateSpy{
-		State: s,
-		// These channels need to have big enough buffers to
-		// capture events needed by tests.  Events that
-		// overfill the channel buffers are discarded.
-		addedDUTs:     make(chan string, 8),
-		drainedDUTs:   make(chan string, 8),
-		terminatedAll: make(chan struct{}, 1),
-		drainedAll:    make(chan struct{}, 1),
-	}
-	select {
-	case f.states <- s2:
-	default:
-	}
-	return s2
-}
-
-func (f stateSpyFactory) waitForState() *stateSpy {
-	return <-f.states
-}
-
-// stateSpy wraps a state and allows inspecting state manipulations.
-type stateSpy struct {
-	*state.State
-	addedDUTs     chan string
-	drainedDUTs   chan string
-	terminatedAll chan struct{}
-	drainedAll    chan struct{}
-}
-
-func (s *stateSpy) AddDUT(dutID string) {
-	s.State.AddDUT(dutID)
-	select {
-	case s.addedDUTs <- dutID:
-	default:
-	}
-}
-
-func (s *stateSpy) DrainDUT(dutID string) {
-	s.State.DrainDUT(dutID)
-	select {
-	case s.drainedDUTs <- dutID:
-	default:
-	}
-}
-
-func (s *stateSpy) TerminateAll() {
-	s.State.TerminateAll()
-	select {
-	case s.terminatedAll <- struct{}{}:
-	default:
-	}
-}
-
-func (s *stateSpy) DrainAll() {
-	s.State.DrainAll()
-	select {
-	case s.drainedAll <- struct{}{}:
-	default:
-	}
-}
-
-// stubClient is a stub implementation of api.DroneClient.  The
-// response can be modified, but writers should use withLock if
-// modifying the response concurrent with other users.
-type stubClient struct {
-	m   sync.Mutex
-	res *api.ReportDroneResponse
-	err error
-}
-
-// newStubClient makes a new stubClient with sane default values.
-// Tests MUST NOT rely on the exact drone UUID; the test should
-// explicitly set a UUID.
-func newStubClient() *stubClient {
-	return &stubClient{
-		res: &api.ReportDroneResponse{
-			Status:         api.ReportDroneResponse_OK,
-			DroneUuid:      "3679687c-b341-4422-ad6d-a935887ed6a7",
-			ExpirationTime: protoTime(time.Date(9999, 1, 2, 3, 4, 5, 6, time.UTC)),
-		},
-	}
-}
-
-// withLock calls the function with a lock.  This is used in tests to
-// modify the stubbed response.
-func (c *stubClient) withLock(f func()) {
-	c.m.Lock()
-	f()
-	c.m.Unlock()
-}
-
-func (c *stubClient) ReportDrone(ctx context.Context, req *api.ReportDroneRequest, _ ...grpc.CallOption) (*api.ReportDroneResponse, error) {
-	c.m.Lock()
-	defer c.m.Unlock()
-	return c.res, c.err
-}
-
-func (c *stubClient) ReleaseDuts(ctx context.Context, req *api.ReleaseDutsRequest, _ ...grpc.CallOption) (*api.ReleaseDutsResponse, error) {
-	return &api.ReleaseDutsResponse{}, nil
-}
-
-type spyClient struct {
-	*stubClient
-	reports chan *api.ReportDroneRequest
-}
-
-func newSpyClient() *spyClient {
-	return &spyClient{
-		stubClient: newStubClient(),
-		// These channels need to have big enough buffers to
-		// capture events needed by tests.  Events that
-		// overfill the channel buffers are discarded.
-		reports: make(chan *api.ReportDroneRequest, 2),
-	}
-}
-
-func (c *spyClient) ReportDrone(ctx context.Context, req *api.ReportDroneRequest, o ...grpc.CallOption) (*api.ReportDroneResponse, error) {
-	select {
-	case c.reports <- req:
-	default:
-	}
-	return c.stubClient.ReportDrone(ctx, req, o...)
 }
 
 // protoTime returns a protobuf time type.
