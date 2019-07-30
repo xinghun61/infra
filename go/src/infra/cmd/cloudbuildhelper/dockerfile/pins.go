cdf838cdc1df839c14ea7f3d4e9c4492d131e261
Vadim Shtayura <vadimsh@chromium.org>
2019-07-30 19:35:13 +0000

    [cloudbuildhelper] Add utility methods for working with pins.yaml.
    R=iannucci@chromium.org
    BUG=985561
    
    Change-Id: Idf522591f047b150cdd3b062ceed00864cbb9ddb
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1726931
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#24748}

--- go/src/infra/cmd/cloudbuildhelper/dockerfile/pins.go
+++ go/src/infra/cmd/cloudbuildhelper/dockerfile/pins.go
@@ -8,11 +8,14 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"sort"
 	"strings"
 
 	"gopkg.in/yaml.v2"
 
+	"go.chromium.org/luci/common/data/stringset"
 	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/sync/parallel"
 )
 
 // Pins is a mapping (image name, tag) => digest.
@@ -22,9 +25,15 @@ type Pins struct {
 
 // Pin is a single "(image name, tag) => digest" pin.
 type Pin struct {
-	Image  string `yaml:"image"`         // required
-	Tag    string `yaml:"tag,omitempty"` // default is "latest"
-	Digest string `yaml:"digest"`        // required
+	Comment string `yaml:"comment,omitempty"` // arbitrary string, for humans
+	Image   string `yaml:"image"`             // required
+	Tag     string `yaml:"tag,omitempty"`     // default is "latest"
+	Digest  string `yaml:"digest"`            // required
+	Freeze  string `yaml:"freeze,omitempty"`  // if set, don't update in pins-update
+}
+
+func (p *Pin) key() string {
+	return p.Image + ":" + p.Tag
 }
 
 // ReadPins loads and validates YAML file with pins.
@@ -37,48 +46,139 @@ func ReadPins(r io.Reader) (*Pins, error) {
 	if err = yaml.Unmarshal(body, &out); err != nil {
 		return nil, errors.Annotate(err, "failed to parse the pins file").Err()
 	}
+	seen := stringset.New(len(out.Pins))
 	for idx, p := range out.Pins {
-		if err := validatePin(p); err != nil {
+		if p, err = normalizePin(p); err != nil {
 			return nil, errors.Annotate(err, "pin #%d", idx+1).Err()
 		}
+		if !seen.Add(p.key()) {
+			return nil, errors.Reason("pin #%d: duplicate entry for %q", idx+1, p.key()).Err()
+		}
+		out.Pins[idx] = p
 	}
 	return &out, nil
 }
 
-func validatePin(p Pin) error {
+// WritePins generates YAML with pins.
+//
+// Entries are normalized and sorted.
+func WritePins(w io.Writer, p *Pins) error {
+	pins := Pins{Pins: append([]Pin(nil), p.Pins...)}
+	sort.Slice(pins.Pins, func(i, j int) bool {
+		l, r := pins.Pins[i], pins.Pins[j]
+		if l.Image == r.Image {
+			return l.Tag < r.Tag
+		}
+		return l.Image < r.Image
+	})
+
+	blob, err := yaml.Marshal(pins)
+	if err != nil {
+		return errors.Annotate(err, "failed to serialize pins").Err()
+	}
+
+	_, err = fmt.Fprintf(w, `# Managed by cloudbuildhelper.
+#
+# All comments or unrecognized fields will be overwritten. To comment an entry
+# use "comment" field.
+#
+# To update digests of all entries:
+#   $ cloudbuildhelper pins-update <path-to-this-file>
+#
+# To add an entry (or update an existing one):
+#   $ cloudbuildhelper pins-add <path-to-this-file> <image>[:<tag>]
+#
+# To remove an entry just delete it from the file.
+#
+# To prevent an entry from being updated by pins-update, add "freeze" field with
+# an explanation why it is frozen.
+
+%s`, blob)
+
+	return errors.Annotate(err, "failed to write").Err()
+}
+
+// Add adds or updates a pin (which should already be resolved).
+func (p *Pins) Add(pin Pin) error {
+	pin, err := normalizePin(pin)
+	if err != nil {
+		return err
+	}
+
+	key := pin.key()
+	for i := range p.Pins {
+		if p.Pins[i].key() == key {
+			p.Pins[i] = pin
+			return nil
+		}
+	}
+
+	p.Pins = append(p.Pins, pin)
+	return nil
+}
+
+// Visit calls 'cb' concurrently for all pins.
+//
+// The callback can mutate any pin fields except Image and Tag (doing so will
+// panic).
+//
+// Calls the callback even for pins that are marked as frozen. The callback
+// should handle this itself (e.g. by logging and skipping them).
+//
+// Returns a multi-error with all errors that happened.
+func (p *Pins) Visit(cb func(p *Pin) error) error {
+	return parallel.WorkPool(16, func(tasks chan<- func() error) {
+		for i := range p.Pins {
+			i := i
+			tasks <- func() error {
+				pin := p.Pins[i]
+				key := pin.key()
+				if err := cb(&pin); err != nil {
+					return errors.Annotate(err, "when visiting %q", key).Err()
+				}
+				pin, err := normalizePin(pin)
+				if err != nil {
+					return errors.Annotate(err, "when visiting %q", key).Err()
+				}
+				if pin.key() != key {
+					panic(fmt.Sprintf("the callback changed the pin key from %q to %q", key, pin.key()))
+				}
+				p.Pins[i] = pin
+				return nil
+			}
+		}
+	})
+}
+
+func normalizePin(p Pin) (Pin, error) {
 	switch {
 	case p.Image == "":
-		return errors.Reason("'image' field is required").Err()
+		return p, errors.Reason("'image' field is required").Err()
 	case p.Digest == "":
-		return errors.Reason("'digest' field is required").Err()
-	default:
-		return nil
+		return p, errors.Reason("'digest' field is required").Err()
 	}
+	// See https://github.com/docker/distribution/blob/master/reference/normalize.go
+	// for defaults.
+	switch strings.Count(p.Image, "/") {
+	case 0: // e.g. "ubuntu"
+		p.Image = "docker.io/library/" + p.Image
+	case 1: // e.g. "library/ubuntu"
+		p.Image = "docker.io/" + p.Image
+	default: // e.g. "gcr.io/something/something", do nothing, good enough
+	}
+	if p.Tag == "" {
+		p.Tag = "latest"
+	}
+	return p, nil
 }
 
 // Resolver returns a Resolver that uses a snapshot of Pins as a source.
 func (p *Pins) Resolver() Resolver {
 	m := make(pinsResolver, len(p.Pins))
 	for _, pin := range p.Pins {
-		m[pinKey(pin.Image, pin.Tag)] = pin.Digest
+		m[pin.key()] = pin.Digest
 	}
 	return m
-}
-
-func pinKey(img, tag string) string {
-	// See https://github.com/docker/distribution/blob/master/reference/normalize.go
-	// for defaults.
-	switch strings.Count(img, "/") {
-	case 0: // e.g. "ubuntu"
-		img = "docker.io/library/" + img
-	case 1: // e.g. "library/ubuntu"
-		img = "docker.io/" + img
-	default: // e.g. "gcr.io/something/something", do nothing, good enough
-	}
-	if tag == "" {
-		tag = "latest"
-	}
-	return fmt.Sprintf("%s:%s", img, tag)
 }
 
 type pinsResolver map[string]string
@@ -87,7 +187,17 @@ func (p pinsResolver) ResolveTag(image, tag string) (digest string, err error) {
 	if len(p) == 0 {
 		return "", errors.Reason("not using pins YAML, the Dockerfile must use @<digest> refs").Err()
 	}
-	d, ok := p[pinKey(image, tag)]
+
+	pin, err := normalizePin(Pin{
+		Image:  image,
+		Tag:    tag,
+		Digest: "to-be-resolved", // to pass validation, we don't use this field
+	})
+	if err != nil {
+		return "", errors.Annotate(err, "bad image:tag combination").Err()
+	}
+
+	d, ok := p[pin.key()]
 	if !ok {
 		// Note: the outer error wrapper usually has enough context already, adding
 		// 'image' and 'tag' values here causes duplication.
--- go/src/infra/cmd/cloudbuildhelper/dockerfile/pins_test.go
+++ go/src/infra/cmd/cloudbuildhelper/dockerfile/pins_test.go
@@ -5,6 +5,8 @@
 package dockerfile
 
 import (
+	"bytes"
+	"errors"
 	"strings"
 	"testing"
 
@@ -15,12 +17,14 @@ import (
 func TestPins(t *testing.T) {
 	t.Parallel()
 
+	pinsYAML := `{"pins": [
+		{"image": "library/yyy", "tag": "old", "digest": "sha256:456"},
+		{"image": "xxx", "digest": "sha256:123", "comment": "zzz", "freeze": "yyy"},
+		{"image": "gcr.io/example/zzz", "tag": "1.2.3", "digest": "sha256:789"}
+	]}`
+
 	Convey("Works", t, func() {
-		p, err := ReadPins(strings.NewReader(`{"pins": [
-      {"image": "xxx", "digest": "sha256:123"},
-      {"image": "library/yyy", "tag": "old", "digest": "sha256:456"},
-      {"image": "gcr.io/example/zzz", "tag": "1.2.3", "digest": "sha256:789"}
-    ]}`))
+		p, err := ReadPins(strings.NewReader(pinsYAML))
 		So(err, ShouldBeNil)
 
 		r := p.Resolver()
@@ -56,6 +60,15 @@ func TestPins(t *testing.T) {
 		So(err, ShouldErrLike, "no such pinned <image>:<tag> combination in pins YAML")
 	})
 
+	Convey("Duplicate pins YAML", t, func() {
+		_, err := ReadPins(strings.NewReader(`{"pins": [
+			{"image": "library/xxx", "tag": "tag", "digest": "sha256:456"},
+			{"image": "library/xxx", "tag": "another", "digest": "sha256:456"},  # OK
+			{"image": "xxx", "tag": "tag", "digest": "sha256:456"}               # dup
+		]}`))
+		So(err, ShouldErrLike, `pin #3: duplicate entry for "docker.io/library/xxx:tag"`)
+	})
+
 	Convey("Incomplete pins", t, func() {
 		_, err := ReadPins(strings.NewReader(`{"pins": [
       {"digest": "sha256:123"}
@@ -72,5 +85,109 @@ func TestPins(t *testing.T) {
 		r := (&Pins{}).Resolver()
 		_, err := r.ResolveTag("img", "tag")
 		So(err, ShouldErrLike, "not using pins YAML, the Dockerfile must use @<digest> refs")
+	})
+
+	Convey("WritePins", t, func() {
+		p, err := ReadPins(strings.NewReader(pinsYAML))
+		So(err, ShouldBeNil)
+
+		out := bytes.Buffer{}
+		So(WritePins(&out, p), ShouldBeNil)
+		So(out.String(), ShouldEqual, `# Managed by cloudbuildhelper.
+#
+# All comments or unrecognized fields will be overwritten. To comment an entry
+# use "comment" field.
+#
+# To update digests of all entries:
+#   $ cloudbuildhelper pins-update <path-to-this-file>
+#
+# To add an entry (or update an existing one):
+#   $ cloudbuildhelper pins-add <path-to-this-file> <image>[:<tag>]
+#
+# To remove an entry just delete it from the file.
+#
+# To prevent an entry from being updated by pins-update, add "freeze" field with
+# an explanation why it is frozen.
+
+pins:
+- comment: zzz
+  image: docker.io/library/xxx
+  tag: latest
+  digest: sha256:123
+  freeze: yyy
+- image: docker.io/library/yyy
+  tag: old
+  digest: sha256:456
+- image: gcr.io/example/zzz
+  tag: 1.2.3
+  digest: sha256:789
+`)
+	})
+
+	Convey("Add", t, func() {
+		p := Pins{}
+
+		// Adds new, normalizing it.
+		So(p.Add(Pin{Image: "xxx", Digest: "yyy"}), ShouldBeNil)
+		So(p.Pins, ShouldResemble, []Pin{
+			{Image: "docker.io/library/xxx", Tag: "latest", Digest: "yyy"},
+		})
+
+		// Overwrites existing.
+		So(p.Add(Pin{Image: "library/xxx", Digest: "zzz"}), ShouldBeNil)
+		So(p.Pins, ShouldResemble, []Pin{
+			{Image: "docker.io/library/xxx", Tag: "latest", Digest: "zzz"},
+		})
+
+		// Handle bad pins.
+		So(p.Add(Pin{}), ShouldErrLike, `'image' field is required`)
+	})
+
+	Convey("Visit success", t, func() {
+		p := Pins{Pins: []Pin{
+			{Image: "example.com/repo/img1", Tag: "t1", Digest: "d1"},
+			{Image: "example.com/repo/img1", Tag: "t2", Digest: "d2"},
+			{Image: "example.com/repo/img2", Tag: "t3", Digest: "d3"},
+			{Image: "example.com/repo/img2", Tag: "t4", Digest: "d4"},
+		}}
+
+		err := p.Visit(func(p *Pin) error {
+			p.Digest += "_new"
+			return nil
+		})
+		So(err, ShouldBeNil)
+
+		So(p.Pins, ShouldResemble, []Pin{
+			{Image: "example.com/repo/img1", Tag: "t1", Digest: "d1_new"},
+			{Image: "example.com/repo/img1", Tag: "t2", Digest: "d2_new"},
+			{Image: "example.com/repo/img2", Tag: "t3", Digest: "d3_new"},
+			{Image: "example.com/repo/img2", Tag: "t4", Digest: "d4_new"},
+		})
+	})
+
+	Convey("Visit failure", t, func() {
+		p := Pins{Pins: []Pin{
+			{Image: "example.com/repo/img1", Tag: "t1", Digest: "d1"},
+			{Image: "example.com/repo/img1", Tag: "t2", Digest: "d2"},
+			{Image: "example.com/repo/img2", Tag: "t3", Digest: "d3"},
+			{Image: "example.com/repo/img2", Tag: "t4", Digest: "d4"},
+		}}
+
+		err := p.Visit(func(p *Pin) error {
+			p.Digest += "_new"
+			if p.Image == "example.com/repo/img2" {
+				return errors.New("blarg")
+			}
+			return nil
+		})
+		So(err, ShouldErrLike, `blarg (and 1 other error)`)
+
+		// Updated only img1 ones.
+		So(p.Pins, ShouldResemble, []Pin{
+			{Image: "example.com/repo/img1", Tag: "t1", Digest: "d1_new"},
+			{Image: "example.com/repo/img1", Tag: "t2", Digest: "d2_new"},
+			{Image: "example.com/repo/img2", Tag: "t3", Digest: "d3"},
+			{Image: "example.com/repo/img2", Tag: "t4", Digest: "d4"},
+		})
 	})
 }
