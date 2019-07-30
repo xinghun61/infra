20c6616602cf07655d17684a98e4930286f617f6
navil <navil@google.com>
2019-07-30 17:12:13 +0000

    Disabled Tests Query V2
    Redesigned disabled tests query that reduces the amount of data
    retrieved from ~277 GB to ~101 MB by elimination repetition and omit
    unnecessary columns.
    
    [Query design](https://docs.google.com/document/d/1rwJ-N3zCpecrF43ms4lfT-ACVdx7H23qPU-QqLtTC4U/edit?usp=sharing)
    Usage design info at go/cr-disabled-test-dashboard-doc under datasets.
    
    Bug: 967758, 973623
    Change-Id: Ia37a3a47ce25478bd12e91c5e324b9c515d820c3
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1710906
    Commit-Queue: Navil Perez <navil@google.com>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    Reviewed-by: Yuke Liao <liaoyuke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#24737}

--- appengine/findit/services/disabled_tests/disabled_tests.sql
+++ appengine/findit/services/disabled_tests/disabled_tests.sql
@@ -3,18 +3,18 @@
 # Assumptions for the disabled tests in this query are:
 # 1. A disabled test should have 'SKIP' in run.expected
 SELECT
-  path as test_name,
+  path AS test_name,
   step_name,
-  buildbot_info.master_name,
   buildbot_info.builder_name,
-  buildbot_info.build_number,
-  run.bugs
-FROM
-  `test-results-hrd.events.test_results`
+  ARRAY_AGG(STRUCT(build_id, run.bugs, run.expected)
+ORDER BY start_time DESC
+LIMIT 1) [OFFSET(0)] latest_run
+FROM `test-results-hrd.events.test_results`
 WHERE
   # According to https://cloud.google.com/bigquery/docs/partitioned-tables,
   # _PARTITIONTIME is always the start of each day, so to make sure all data
   # within the past 1 day is included, _PARTITIONTIME needs to be greater
   # than or equal to the timestamp of 2 days ago.
   _PARTITIONTIME >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 2 day)
-  AND 'SKIP' IN UNNEST(run.expected)
+GROUP BY 1, 2, 3
+HAVING 'SKIP' IN UNNEST(latest_run.expected)
