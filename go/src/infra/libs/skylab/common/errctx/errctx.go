2552b39dd66d182cb34694000a2dad5915437171
Aviv Keshet <akeshet@chromium.org>
2019-08-07 17:05:13 +0000

    [errctx] add errctx package, for contexts with custom error messages
    BUG=None
    TEST=unit tests. go test; go test -race
    
    Change-Id: I8cd4705c8bee867357ee79e4c08b9307226b55a7
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1726715
    Commit-Queue: Aviv Keshet <akeshet@chromium.org>
    Reviewed-by: Allen Li <ayatane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#24950}

---
+++ go/src/infra/libs/skylab/common/errctx/errctx.go
@@ -0,0 +1,95 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package errctx provides implementations of context.Context that allow for
+// cancellation or deadline expiration with custom error messages.
+package errctx
+
+import (
+	"context"
+	"sync"
+	"time"
+)
+
+// cancelContext is an implementation of context.Context, in which the context
+// can be cancelled with a custom error.
+type cancelContext struct {
+	parent context.Context
+
+	cancelOnce sync.Once
+	errMutex   sync.RWMutex
+	done       chan struct{}
+	err        error
+}
+
+// Deadline implements context.Context.
+func (c *cancelContext) Deadline() (time.Time, bool) {
+	return c.parent.Deadline()
+}
+
+// Done implements context.Context.
+func (c *cancelContext) Done() <-chan struct{} {
+	return c.done
+}
+
+// Err implements context.Context.
+func (c *cancelContext) Err() error {
+	c.errMutex.RLock()
+	e := c.err
+	c.errMutex.RUnlock()
+	return e
+}
+
+// Value implements context.Context.
+func (c *cancelContext) Value(key interface{}) interface{} {
+	return c.parent.Value(key)
+}
+
+// cancel cancels the context with the given error.
+func (c *cancelContext) cancel(err error) {
+	c.cancelOnce.Do(func() {
+		c.errMutex.Lock()
+		c.err = err
+		close(c.done)
+		c.errMutex.Unlock()
+	})
+}
+
+// propagate launches a goroutine that propagates context cancellation from
+// parent context.
+func (c *cancelContext) propagate() {
+	go func() {
+		select {
+		case <-c.parent.Done():
+			c.cancel(c.parent.Err())
+		case <-c.Done():
+		}
+	}()
+}
+
+func newCancelContext(parent context.Context) *cancelContext {
+	c := &cancelContext{
+		parent: parent,
+		done:   make(chan struct{}),
+	}
+	c.propagate()
+	return c
+}
+
+// WithCancel returns a child context with a cancellation function that accepts
+// custom errors.
+func WithCancel(parent context.Context) (context.Context, func(error)) {
+	c := newCancelContext(parent)
+	return c, c.cancel
+}
