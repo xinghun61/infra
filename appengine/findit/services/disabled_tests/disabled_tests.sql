6a3d66b85ea2cbf0677f2b16c27f7bd45f6a0e81
navil <navil@google.com>
2019-08-09 16:34:08 +0000

    Disabled Test Query V3
    disabled_tests.sql currently returns a STRUCT object which contains
    build_id, bugs, and expected. However, bigquery_helper does not
    currently support interpreting a schema which contains a STRUCT.
    
    Solution:
    Unpack the arguments in the STRUCT before returning.
    
    Bug: 992013
    Change-Id: Ifbb75950d745d15b99c3488f0d651b97f886c73b
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1745553
    Reviewed-by: Chan Li <chanli@chromium.org>
    Reviewed-by: Yuke Liao <liaoyuke@chromium.org>
    Commit-Queue: Navil Perez <navil@google.com>
    Cr-Commit-Position: refs/heads/master@{#25009}

--- appengine/findit/services/disabled_tests/disabled_tests.sql
+++ appengine/findit/services/disabled_tests/disabled_tests.sql
@@ -2,19 +2,31 @@
 #
 # Assumptions for the disabled tests in this query are:
 # 1. A disabled test should have 'SKIP' in run.expected
+WITH
+  last_runs AS (
+    SELECT
+      path AS test_name,
+      step_name,
+      buildbot_info.builder_name,
+      ARRAY_AGG(STRUCT(build_id, run.bugs, run.expected)
+        ORDER BY start_time DESC
+        LIMIT 1)[OFFSET(0)] AS latest_run
+    FROM
+      `test-results-hrd.events.test_results`
+    WHERE
+      # According to https://cloud.google.com/bigquery/docs/partitioned-tables,
+      # _PARTITIONTIME is always the start of each day, so to make sure all data
+      # within the past 1 day is included, _PARTITIONTIME needs to be greater
+      # than or equal to the timestamp of 2 days ago.
+      _PARTITIONTIME >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 2 day)
+    GROUP BY 1, 2, 3
+    HAVING 'SKIP' IN UNNEST(latest_run.expected)
+  )
 SELECT
-  path AS test_name,
+  test_name,
   step_name,
-  buildbot_info.builder_name,
-  ARRAY_AGG(STRUCT(build_id, run.bugs, run.expected)
-ORDER BY start_time DESC
-LIMIT 1) [OFFSET(0)] latest_run
-FROM `test-results-hrd.events.test_results`
-WHERE
-  # According to https://cloud.google.com/bigquery/docs/partitioned-tables,
-  # _PARTITIONTIME is always the start of each day, so to make sure all data
-  # within the past 1 day is included, _PARTITIONTIME needs to be greater
-  # than or equal to the timestamp of 2 days ago.
-  _PARTITIONTIME >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 2 day)
-GROUP BY 1, 2, 3
-HAVING 'SKIP' IN UNNEST(latest_run.expected)
+  builder_name,
+  latest_run.build_id,
+  latest_run.bugs
+FROM
+  last_runs
