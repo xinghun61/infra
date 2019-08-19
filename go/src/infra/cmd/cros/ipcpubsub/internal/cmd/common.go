1482db56221e43d734ee6a7d3355198c878b256d
Jacob Kopczynski <jkop@google.com>
2019-08-19 23:20:22 +0000

    Add authcli auth using TokenSource to the command
    BUG=chromium:928535
    TEST=None
    
    Change-Id: I370297bba21d2d74027f1511be836b271e98dc9c
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1704802
    Commit-Queue: Jacob Kopczynski <jkop@chromium.org>
    Auto-Submit: Jacob Kopczynski <jkop@chromium.org>
    Reviewed-by: Aviv Keshet <akeshet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#25199}

--- go/src/infra/cmd/cros/ipcpubsub/internal/cmd/common.go
+++ go/src/infra/cmd/cros/ipcpubsub/internal/cmd/common.go
@@ -5,21 +5,47 @@
 package cmd
 
 import (
+	"context"
 	"flag"
 
+	"infra/cmd/cros/ipcpubsub/internal/site"
+
+	"cloud.google.com/go/pubsub"
 	"github.com/maruel/subcommands"
+	"go.chromium.org/luci/auth"
+	"go.chromium.org/luci/auth/client/authcli"
+	"google.golang.org/api/option"
 )
 
 type baseRun struct {
 	subcommands.CommandRunBase
 	topic     string
 	project   string
-	credsFile string
+	authFlags authcli.Flags
 }
 
-func (c *baseRun) registerCommonFlags(fs *flag.FlagSet) {
-	fs.StringVar(&c.topic, "topic", "", "Pubsub topic to use")
-	fs.StringVar(&c.project, "project", "", "Pubsub project to use")
+func (r *baseRun) registerCommonFlags(fs *flag.FlagSet) {
+	r.authFlags.Register(fs, site.DefaultAuthOptions)
+	fs.StringVar(&r.topic, "topic", "", "Pubsub topic to use")
+	fs.StringVar(&r.project, "project", "", "Pubsub project to use")
+}
+
+func (r *baseRun) createClient(ctx context.Context) (*pubsub.Client, error) {
+	opts, err := r.authFlags.Options()
+	if err != nil {
+		return nil, err
+	}
+	authenticator := auth.NewAuthenticator(ctx, auth.SilentLogin, opts)
+	ts, err := authenticator.TokenSource()
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := pubsub.NewClient(ctx, r.project, option.WithTokenSource(ts))
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
 }
 
 func bytesMapToList(m map[string][]byte) [][]byte {
