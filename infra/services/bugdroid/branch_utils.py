a832d6949c19abcd826a65a8d52188ac55e075f6
Michael Moss <mmoss@google.com>
2019-09-12 17:38:56 +0000

    Temporarily hard-code milestone branch information.
    This replaces the obsolete method of getting branch information from the
    chromepmo service, and provides a bridge until we can get it from
    chromiumdash instead.
    
    BUG=1002078
    R=benmason@chromium.org, ehmaldonado@chromium.org
    
    Change-Id: Iabfa5a55e028f2be10d06efe801f315aee21be88
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1801554
    Auto-Submit: Michael Moss <mmoss@chromium.org>
    Reviewed-by: Ben Mason <benmason@chromium.org>
    Commit-Queue: Ben Mason <benmason@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#25714}

--- infra/services/bugdroid/branch_utils.py
+++ infra/services/bugdroid/branch_utils.py
@@ -7,18 +7,26 @@ import urllib2
 
 
 def get_mstone(branch, raise_exception=True):
-  try:
-    u = urllib2.urlopen('https://chromepmo.appspot.com/history/'
-                        'mstone?branch=%s' % str(branch))
-  except Exception:
-    return None
-  data = json.load(u)
-  u.close()
-
-  if data and "error" in data and raise_exception:
-    raise Exception(data["error"])
-
-  if data and "mstone" in data:
-    return int(data["mstone"])
-
-  return None
+  # TODO(crbug.com/1002078): Remove this hard-coded hack once chromiumdash adds
+  # an endpoint to provide the same branch/milestone information. That interface
+  # is TBD, but should be something like:
+  #
+  #   milestones_json = urllib2.urlopen(
+  #     'https://chromiumdash.appspot.com/branches/mstone?limit=5')
+  #   recent_milestones = json.load(milestones_json)
+  #
+  # Note, the result should probably also be memoized, since it should almost
+  # always be constant throughout a given run of bugdroid (and in the rare case
+  # where bugdroid runs at exactly the time when a new milestone branch is being
+  # created, there's not likely to be any legitimate bugs for that brand new
+  # branch), so no need to repeatedly fetch the data.
+  recent_milestones = {
+      '3729': 74,
+      '3770': 75,
+      '3809': 76,
+      '3865': 77,
+      '3904': 78,
+  }
+  if not recent_milestones and raise_exception:
+    raise Exception('Failed to fetch milestone data')
+  return recent_milestones.get(str(branch), None)
