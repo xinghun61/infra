6cf356058ed9210801bb5e9dd02846172cf3f762
Alex Zamorzaev <zamorzaev@chromium.org>
2019-09-16 20:54:11 +0000

    skylab_swarming_worker: Add \n before annotations
    LogDog only recognizes annotations that are on a separate line.
    
    Bug: 1003398
    Change-Id: Ib1e4c87710d7cfd9a1b302c7c826bd8cf11cba59
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1807780
    Commit-Queue: Alex Zamorzaev <zamorzaev@chromium.org>
    Commit-Queue: Aviv Keshet <akeshet@chromium.org>
    Auto-Submit: Alex Zamorzaev <zamorzaev@chromium.org>
    Reviewed-by: Aviv Keshet <akeshet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#25771}

--- go/src/infra/cmd/skylab_swarming_worker/internal/annotations/annotations.go
+++ go/src/infra/cmd/skylab_swarming_worker/internal/annotations/annotations.go
@@ -16,40 +16,40 @@ import (
 
 // BuildStep prints a BUILD_STEP annotation.
 func BuildStep(w io.Writer, name string) (int, error) {
-	return fmt.Fprintf(w, "@@@BUILD_STEP %s@@@\n", name)
+	return fmt.Fprintf(w, "\n@@@BUILD_STEP %s@@@\n", name)
 }
 
 // SeedStep prints a SEED_STEP annotation.
 func SeedStep(w io.Writer, name string) (int, error) {
-	return fmt.Fprintf(w, "@@@SEED_STEP %s@@@\n", name)
+	return fmt.Fprintf(w, "\n@@@SEED_STEP %s@@@\n", name)
 }
 
 // StepCursor prints a STEP_CURSOR annotation.
 func StepCursor(w io.Writer, name string) (int, error) {
-	return fmt.Fprintf(w, "@@@STEP_CURSOR %s@@@\n", name)
+	return fmt.Fprintf(w, "\n@@@STEP_CURSOR %s@@@\n", name)
 }
 
 // StepLink prints a STEP_LINK annotation.
 func StepLink(w io.Writer, label, url string) (int, error) {
-	return fmt.Fprintf(w, "@@@STEP_LINK@%s@%s@@@\n", label, url)
+	return fmt.Fprintf(w, "\n@@@STEP_LINK@%s@%s@@@\n", label, url)
 }
 
 // StepStarted prints a STEP_STARTED annotation.
 func StepStarted(w io.Writer) (int, error) {
-	return fmt.Fprintf(w, "@@@STEP_STARTED@@@\n")
+	return fmt.Fprintf(w, "\n@@@STEP_STARTED@@@\n")
 }
 
 // StepFailure prints a STEP_FAILURE annotation.
 func StepFailure(w io.Writer) (int, error) {
-	return fmt.Fprintf(w, "@@@STEP_FAILURE@@@\n")
+	return fmt.Fprintf(w, "\n@@@STEP_FAILURE@@@\n")
 }
 
 // StepException prints a STEP_EXCEPTION annotation.
 func StepException(w io.Writer) (int, error) {
-	return fmt.Fprintf(w, "@@@STEP_EXCEPTION@@@\n")
+	return fmt.Fprintf(w, "\n@@@STEP_EXCEPTION@@@\n")
 }
 
 // StepClosed prints a STEP_CLOSED annotation.
 func StepClosed(w io.Writer) (int, error) {
-	return fmt.Fprintf(w, "@@@STEP_CLOSED@@@\n")
+	return fmt.Fprintf(w, "\n@@@STEP_CLOSED@@@\n")
 }
