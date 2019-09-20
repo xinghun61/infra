67284271b87bcd8d4a160f7214468e32cf17d7dd
Tina Lu <ltina@google.com>
2019-09-20 17:19:45 +0000

    Update Tricium analyzer for metrics to ensure expiry conditions in histograms.xml are present and correct
    Among other features, this update allows the analyzer to:
    1. Check there is an expiry condition
    2. Inform submitter and reviewer how many days there are until the expiry condition
    3. If the expiry condition is "never", check that there is a comment explaining why
    4. If the expiry condition is not "never", check that the expiry condition is either formatted as YYYY-MM-DD or M* (for Milestone notation)
    5. For correctly formatted expiry conditions, warn if the date provided is in the past or is over 1 year in the future.
    
    Bug: 1004564
    Change-Id: Ia5794e6153bfa21429c16f763383d096fde743ca
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1811897
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Commit-Queue: Tina Lu <ltina@google.com>
    Cr-Commit-Position: refs/heads/master@{#25914}

--- go/src/infra/tricium/functions/metrics/histograms_check.go
+++ go/src/infra/tricium/functions/metrics/histograms_check.go
@@ -12,7 +12,9 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
+	"time"
 
 	tricium "infra/tricium/api/v1"
 )
@@ -20,34 +22,58 @@ import (
 const (
 	category        = "Metrics"
 	histogramEndTag = "</histogram>"
-	/* We want both of these histogram tags because there are other tags that
-	share the "histogram" prefix like "histogram-suffixes" */
-	histogramStartTagAtt = "<histogram "
-	histogramStartTagEmp = "<histogram>"
-	ownerStartTag        = "<owner"
+	ownerStartTag   = "<owner"
+	dateFormat      = "2006-01-02"
 
-	oneOwnerError = `It's a best practice to list multiple owners, 
-	so that there's no single point of failure for communication:
-	https://chromium.googlesource.com/chromium/src/+/HEAD/tools/metrics/histograms/README.md#Owners.`
-	firstOwnerTeamError = `Please list an individual as the primary owner for this metric: 
-	https://chromium.googlesource.com/chromium/src/+/HEAD/tools/metrics/histograms/README.md#Owners.`
+	oneOwnerError = `[WARNING] It's a best practice to list multiple owners,
+so that there's no single point of failure for communication:
+https://chromium.googlesource.com/chromium/src/+/HEAD/tools/metrics/histograms/README.md#Owners.`
+	firstOwnerTeamError = `[WARNING] Please list an individual as the primary owner for this metric:
+https://chromium.googlesource.com/chromium/src/+/HEAD/tools/metrics/histograms/README.md#Owners.`
+	noExpiryError = `[ERROR] Please specify an expiry condition for this histogram:
+https://chromium.googlesource.com/chromium/src/+/HEAD/tools/metrics/histograms/README.md#Histogram-Expiry`
+	badExpiryError = `[ERROR] Could not parse histogram expiry. Please format as YYYY-MM-DD or MXX:
+https://chromium.googlesource.com/chromium/src/+/HEAD/tools/metrics/histograms/README.md#Histogram-Expiry`
+	pastExpiryWarning = `[WARNING] This expiry date is in the past. Did you mean to set an expiry date in the future?`
+	farExpiryWarning  = `[WARNING] It's a best practice to choose an expiry that is at most one year out:
+https://chromium.googlesource.com/chromium/src/+/HEAD/tools/metrics/histograms/README.md#Histogram-Expiry`
+	neverExpiryInfo = `[INFO] The expiry should only be set to \"never\" in rare cases.
+Please double-check that this use of \"never\" is appropriate:
+https://chromium.googlesource.com/chromium/src/+/HEAD/tools/metrics/histograms/README.md#Histogram-Expiry`
+	neverExpiryError = `[ERROR] Histograms that never expire must have an XML comment describing why,
+such as <!-- expires-never: \"heartbeat\" metric (internal: go/uma-heartbeats) -->:
+https://chromium.googlesource.com/chromium/src/+/HEAD/tools/metrics/histograms/README.md#Histogram-Expiry`
+)
+
+var (
+	// We need a pattern for matching the histogram start tag because
+	// there are other tags that share the "histogram" prefix like "histogram-suffixes"
+	histogramStartPattern     = regexp.MustCompile(`^<histogram(\s|>)`)
+	neverExpiryCommentPattern = regexp.MustCompile(`^<!--\s?expires-never`)
+	// Match date patterns of format YYYY-MM-DD
+	expiryDatePattern      = regexp.MustCompile(`^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$`)
+	expiryMilestonePattern = regexp.MustCompile(`^M([0-9]{2,3})$`)
+
+	// Now is an alias for time.Now, can be overwritten by tests
+	now = time.Now
 )
 
 // Histogram contains all info about a UMA histogram
 type Histogram struct {
-	Name       string   `xml:"name,attr"`
-	Enum       string   `xml:"enum,attr"`
-	Units      string   `xml:"units,attr"`
-	Expiration string   `xml:"expiry,attr"`
-	Obsolete   string   `xml:"obsolete"`
-	Owners     []string `xml:"owner"`
-	Summary    string   `xml:"summary"`
+	Name     string   `xml:"name,attr"`
+	Enum     string   `xml:"enum,attr"`
+	Units    string   `xml:"units,attr"`
+	Expiry   string   `xml:"expires_after,attr"`
+	Obsolete string   `xml:"obsolete"`
+	Owners   []string `xml:"owner"`
+	Summary  string   `xml:"summary"`
 }
 
-// LineNum will eventually contain all line nums for tags inside a histogram
-type LineNum struct {
-	HistogramLineNum int
-	OwnerLineNum     int
+// Metadata contains metadata about histogram tags and required comments
+type Metadata struct {
+	HistogramLineNum      int
+	OwnerLineNum          int
+	HasNeverExpiryComment bool
 }
 
 func main() {
@@ -89,7 +115,7 @@ func main() {
 func analyzeFile(scanner *bufio.Scanner, path string) []*tricium.Data_Comment {
 	var comments []*tricium.Data_Comment
 	// Struct that holds line numbers of different tags in histogram
-	var tagLineNums *LineNum
+	var metadata *Metadata
 	// Buffer that holds current histogram
 	var currHistogram []byte
 	// Start line number for current histogram
@@ -101,69 +127,134 @@ func analyzeFile(scanner *bufio.Scanner, path string) []*tricium.Data_Comment {
 			// Add line to currHistogram if currently between some histogram tags
 			currHistogram = append(currHistogram, scanner.Bytes()...)
 		}
-		if strings.HasPrefix(line, histogramStartTagEmp) || strings.HasPrefix(line, histogramStartTagAtt) {
-			// Initialize currHistogram and tagLineNums when a new histogram is encountered
+		if histogramStartPattern.MatchString(line) {
+			// Initialize currHistogram and metadata when a new histogram is encountered
 			histogramStart = lineNum
-			tagLineNums = newLineNum(histogramStart)
+			metadata = newMetadata(histogramStart)
 			currHistogram = scanner.Bytes()
 		} else if strings.HasPrefix(line, histogramEndTag) {
 			// Analyze entire histogram after histogram end tag is encountered
-			if comment := checkHistogram(path, currHistogram, tagLineNums); comment != nil {
+			if comment := checkHistogram(path, currHistogram, metadata); comment != nil {
 				comments = append(comments, comment...)
 			}
 			currHistogram = nil
 		} else if strings.HasPrefix(line, ownerStartTag) {
-			if tagLineNums.OwnerLineNum == histogramStart {
-				tagLineNums.OwnerLineNum = lineNum
+			if metadata.OwnerLineNum == histogramStart {
+				metadata.OwnerLineNum = lineNum
 			}
+		} else if neverExpiryCommentPattern.MatchString(line) {
+			metadata.HasNeverExpiryComment = true
 		}
 		lineNum++
 	}
 	return comments
 }
 
-func checkHistogram(path string, histBytes []byte, tagLineNums *LineNum) []*tricium.Data_Comment {
+func checkHistogram(path string, histBytes []byte, metadata *Metadata) []*tricium.Data_Comment {
 	var histogram Histogram
 	if err := xml.Unmarshal(histBytes, &histogram); err != nil {
-		log.Printf("WARNING: Failed to unmarshal histogram at line %d", tagLineNums.HistogramLineNum)
+		log.Printf("WARNING: Failed to unmarshal histogram at line %d", metadata.HistogramLineNum)
 		return nil
 	}
 	var comments []*tricium.Data_Comment
-	if comment := checkNumOwners(path, histogram, tagLineNums); comment != nil {
+	if comment := checkNumOwners(path, histogram, metadata); comment != nil {
 		comments = append(comments, comment)
 	}
-	if comment := checkNonTeamOwner(path, histogram, tagLineNums); comment != nil {
+	if comment := checkNonTeamOwner(path, histogram, metadata); comment != nil {
 		comments = append(comments, comment)
+	}
+	if expiryComments := checkExpiry(path, histogram, metadata); expiryComments != nil {
+		comments = append(comments, expiryComments...)
 	}
 	return comments
 }
 
-func checkNumOwners(path string, histogram Histogram, tagLineNums *LineNum) *tricium.Data_Comment {
+func checkNumOwners(path string, histogram Histogram, metadata *Metadata) *tricium.Data_Comment {
 	if len(histogram.Owners) <= 1 {
-		log.Printf("ADDING Comment for %s at line %d: One Owner", histogram.Name, tagLineNums.OwnerLineNum)
-		comment := &tricium.Data_Comment{
-			Category:  fmt.Sprintf("%s/%s", category, "Owners"),
-			Message:   oneOwnerError,
-			Path:      path,
-			StartLine: int32(tagLineNums.OwnerLineNum),
-		}
+		comment := createOwnerComment(oneOwnerError, path, metadata)
+		log.Printf("ADDING Comment for %s at line %d: %s", histogram.Name, comment.StartLine, "[ERROR]: One Owner")
 		return comment
 	}
 	return nil
 }
 
-func checkNonTeamOwner(path string, histogram Histogram, tagLineNums *LineNum) *tricium.Data_Comment {
+func checkNonTeamOwner(path string, histogram Histogram, metadata *Metadata) *tricium.Data_Comment {
 	if len(histogram.Owners) > 0 && strings.Contains(histogram.Owners[0], "-") {
-		log.Printf("ADDING Comment for %s at line %d: First Owner Team", histogram.Name, tagLineNums.OwnerLineNum)
-		comment := &tricium.Data_Comment{
-			Category:  fmt.Sprintf("%s/%s", category, "Owners"),
-			Message:   firstOwnerTeamError,
-			Path:      path,
-			StartLine: int32(tagLineNums.OwnerLineNum),
-		}
+		comment := createOwnerComment(firstOwnerTeamError, path, metadata)
+		log.Printf("ADDING Comment for %s at line %d: %s", histogram.Name, comment.StartLine, "[ERROR]: First Owner Team")
 		return comment
 	}
 	return nil
+}
+
+func createOwnerComment(message string, path string, metadata *Metadata) *tricium.Data_Comment {
+	return &tricium.Data_Comment{
+		Category:  fmt.Sprintf("%s/%s", category, "Owners"),
+		Message:   message,
+		Path:      path,
+		StartLine: int32(metadata.OwnerLineNum),
+	}
+}
+
+func checkExpiry(path string, histogram Histogram, metadata *Metadata) []*tricium.Data_Comment {
+	var commentMessage string
+	var logMessage string
+	var extraComment *tricium.Data_Comment
+	if expiry := histogram.Expiry; expiry == "" {
+		commentMessage = noExpiryError
+		logMessage = "[ERROR]: No Expiry"
+	} else if expiry == "never" {
+		commentMessage = neverExpiryInfo
+		logMessage = "[INFO]: Never Expiry"
+		// Add second Tricium comment if an expiry of never has no comment
+		if !metadata.HasNeverExpiryComment {
+			extraComment = createExpiryComment(neverExpiryError, path, metadata)
+			logMessage += " & [ERROR]: No Comment"
+		}
+	} else {
+		dateMatch := expiryDatePattern.MatchString(expiry)
+		milestoneMatch := expiryMilestonePattern.MatchString(expiry)
+		if dateMatch {
+			inputDate, err := time.Parse(dateFormat, expiry)
+			if err != nil {
+				log.Fatalf("Failed to parse expiry date")
+			}
+			dateDiff := int(inputDate.Sub(now()).Hours()/24) + 1
+			if dateDiff <= 0 {
+				commentMessage = pastExpiryWarning
+				logMessage = "[WARNING]: Expiry in past"
+			} else if dateDiff >= 365 {
+				commentMessage = farExpiryWarning
+				logMessage = "[WARNING]: Expiry past one year"
+			} else {
+				commentMessage = fmt.Sprintf("[INFO]: Expiry date is in %d days", dateDiff)
+				logMessage = commentMessage
+			}
+		} else if milestoneMatch {
+			// TODO: check that milestone is within time limit
+		} else {
+			commentMessage = badExpiryError
+			logMessage = "[ERROR]: Expiry condition badly formatted"
+		}
+	}
+	if commentMessage == "" {
+		log.Fatalf("Primary expiry comment should not be empty")
+	}
+	expiryComments := []*tricium.Data_Comment{createExpiryComment(commentMessage, path, metadata)}
+	if extraComment != nil {
+		expiryComments = append(expiryComments, extraComment)
+	}
+	log.Printf("ADDING Comment for %s at line %d: %s", histogram.Name, metadata.HistogramLineNum, logMessage)
+	return expiryComments
+}
+
+func createExpiryComment(message string, path string, metadata *Metadata) *tricium.Data_Comment {
+	return &tricium.Data_Comment{
+		Category:  fmt.Sprintf("%s/%s", category, "Expiry"),
+		Message:   message,
+		Path:      path,
+		StartLine: int32(metadata.HistogramLineNum),
+	}
 }
 
 func openFileOrDie(path string) *os.File {
@@ -180,9 +271,9 @@ func closeFileOrDie(f *os.File) {
 	}
 }
 
-// constructor for creating a LineNum struct with defaultLineNum
-func newLineNum(defaultLineNum int) *LineNum {
-	return &LineNum{
+// Constructor for creating a Metadata struct with defaultLineNum
+func newMetadata(defaultLineNum int) *Metadata {
+	return &Metadata{
 		HistogramLineNum: defaultLineNum,
 		OwnerLineNum:     defaultLineNum,
 	}
--- go/src/infra/tricium/functions/metrics/histograms_check_test.go
+++ go/src/infra/tricium/functions/metrics/histograms_check_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"time"
 
 	. "github.com/smartystreets/goconvey/convey"
 
@@ -12,6 +13,8 @@ import (
 )
 
 func analyzeTestFile(t *testing.T, name string) []*tricium.Data_Comment {
+	// Mock current time for testing
+	now = func() time.Time { return time.Date(2019, time.January, 1, 0, 0, 0, 0, time.UTC) }
 	f, err := os.Open("test/src/" + name)
 	if err != nil {
 		t.Errorf("Failed to open %s: %v", name, err)
@@ -24,42 +27,56 @@ func analyzeTestFile(t *testing.T, name string) []*tricium.Data_Comment {
 func TestHistogramsCheck(t *testing.T) {
 	Convey("Analyze XML file with no errors", t, func() {
 		results := analyzeTestFile(t, "good.xml")
-		So(results, ShouldBeNil)
+		So(results, ShouldResemble, []*tricium.Data_Comment{
+			defaultExpiryInfo("good.xml"),
+		})
 	})
 
 	Convey("Analyze XML file with no errors: both owners individuals", t, func() {
 		results := analyzeTestFile(t, "good_individuals.xml")
-		So(results, ShouldBeNil)
+		So(results, ShouldResemble, []*tricium.Data_Comment{
+			defaultExpiryInfo("good_individuals.xml"),
+		})
 	})
 
+	// OWNER tests
 	Convey("Analyze XML file with error: only one owner", t, func() {
 		results := analyzeTestFile(t, "one_owner.xml")
-		So(results, ShouldResemble, []*tricium.Data_Comment{{
-			Category:  fmt.Sprintf("%s/%s", category, "Owners"),
-			Message:   oneOwnerError,
-			StartLine: 4,
-			Path:      "one_owner.xml",
-		}})
+		So(results, ShouldResemble, []*tricium.Data_Comment{
+			{
+				Category:  fmt.Sprintf("%s/%s", category, "Owners"),
+				Message:   oneOwnerError,
+				StartLine: 4,
+				Path:      "one_owner.xml",
+			},
+			defaultExpiryInfo("one_owner.xml"),
+		})
 	})
 
 	Convey("Analyze XML file with error: no owners", t, func() {
 		results := analyzeTestFile(t, "no_owners.xml")
-		So(results, ShouldResemble, []*tricium.Data_Comment{{
-			Category:  fmt.Sprintf("%s/%s", category, "Owners"),
-			Message:   oneOwnerError,
-			StartLine: 3,
-			Path:      "no_owners.xml",
-		}})
+		So(results, ShouldResemble, []*tricium.Data_Comment{
+			{
+				Category:  fmt.Sprintf("%s/%s", category, "Owners"),
+				Message:   oneOwnerError,
+				StartLine: 3,
+				Path:      "no_owners.xml",
+			},
+			defaultExpiryInfo("no_owners.xml"),
+		})
 	})
 
 	Convey("Analyze XML file with error: first owner is team", t, func() {
 		results := analyzeTestFile(t, "first_team_owner.xml")
-		So(results, ShouldResemble, []*tricium.Data_Comment{{
-			Category:  fmt.Sprintf("%s/%s", category, "Owners"),
-			Message:   firstOwnerTeamError,
-			StartLine: 4,
-			Path:      "first_team_owner.xml",
-		}})
+		So(results, ShouldResemble, []*tricium.Data_Comment{
+			{
+				Category:  fmt.Sprintf("%s/%s", category, "Owners"),
+				Message:   firstOwnerTeamError,
+				StartLine: 4,
+				Path:      "first_team_owner.xml",
+			},
+			defaultExpiryInfo("first_team_owner.xml"),
+		})
 	})
 
 	Convey("Analyze XML file with multiple owner errors", t, func() {
@@ -77,6 +94,95 @@ func TestHistogramsCheck(t *testing.T) {
 				StartLine: 4,
 				Path:      "first_team_one_owner.xml",
 			},
+			defaultExpiryInfo("first_team_one_owner.xml"),
 		})
 	})
+
+	// EXPIRY tests
+	Convey("Analyze XML file with no expiry", t, func() {
+		results := analyzeTestFile(t, "no_expiry.xml")
+		So(results, ShouldResemble, []*tricium.Data_Comment{
+			{
+				Category:  fmt.Sprintf("%s/%s", category, "Expiry"),
+				Message:   noExpiryError,
+				StartLine: 3,
+				Path:      "no_expiry.xml",
+			},
+		})
+	})
+
+	Convey("Analyze XML file with expiry of never", t, func() {
+		results := analyzeTestFile(t, "never_expiry_with_comment.xml")
+		So(results, ShouldResemble, []*tricium.Data_Comment{
+			{
+				Category:  fmt.Sprintf("%s/%s", category, "Expiry"),
+				Message:   neverExpiryInfo,
+				StartLine: 3,
+				Path:      "never_expiry_with_comment.xml",
+			},
+		})
+	})
+
+	Convey("Analyze XML file with expiry of never and no comment", t, func() {
+		results := analyzeTestFile(t, "never_expiry_no_comment.xml")
+		So(results, ShouldResemble, []*tricium.Data_Comment{
+			{
+				Category:  fmt.Sprintf("%s/%s", category, "Expiry"),
+				Message:   neverExpiryInfo,
+				StartLine: 3,
+				Path:      "never_expiry_no_comment.xml",
+			},
+			{
+				Category:  fmt.Sprintf("%s/%s", category, "Expiry"),
+				Message:   neverExpiryError,
+				StartLine: 3,
+				Path:      "never_expiry_no_comment.xml",
+			},
+		})
+	})
+
+	Convey("Analyze XML file with badly formatted expiry", t, func() {
+		results := analyzeTestFile(t, "unformatted_expiry.xml")
+		So(results, ShouldResemble, []*tricium.Data_Comment{
+			{
+				Category:  fmt.Sprintf("%s/%s", category, "Expiry"),
+				Message:   badExpiryError,
+				StartLine: 3,
+				Path:      "unformatted_expiry.xml",
+			},
+		})
+	})
+
+	Convey("Analyze XML file with expiry in past", t, func() {
+		results := analyzeTestFile(t, "past_expiry.xml")
+		So(results, ShouldResemble, []*tricium.Data_Comment{
+			{
+				Category:  fmt.Sprintf("%s/%s", category, "Expiry"),
+				Message:   pastExpiryWarning,
+				StartLine: 3,
+				Path:      "past_expiry.xml",
+			},
+		})
+	})
+
+	Convey("Analyze XML file with expiry in over one year", t, func() {
+		results := analyzeTestFile(t, "over_year_expiry.xml")
+		So(results, ShouldResemble, []*tricium.Data_Comment{
+			{
+				Category:  fmt.Sprintf("%s/%s", category, "Expiry"),
+				Message:   farExpiryWarning,
+				StartLine: 3,
+				Path:      "over_year_expiry.xml",
+			},
+		})
+	})
+}
+
+func defaultExpiryInfo(path string) *tricium.Data_Comment {
+	return &tricium.Data_Comment{
+		Category:  fmt.Sprintf("%s/%s", category, "Expiry"),
+		Message:   fmt.Sprintf("[INFO]: Expiry date is in 364 days"),
+		StartLine: 3,
+		Path:      path,
+	}
 }
