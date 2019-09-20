be75fc76977f873305fa20027c124df0f9123ec7
Andrii Shyshkalov <tandrii@google.com>
2019-09-20 13:14:35 +0000

    git_cache_updater: accept input via proto.
    * Still maintains legacy buildername-based config.
    * Adds support for aggressive gc.
    
    R=iannucci
    
    Change-Id: I48245e4619d901891259f194d09620a896bf01c1
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1815640
    Commit-Queue: Andrii Shyshkalov <tandrii@google.com>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#25911}

--- recipes/README.recipes.md
+++ recipes/README.recipes.md
@@ -859,13 +859,11 @@ Pushes a trivial CL to Gerrit to verify git authentication works on LUCI.
 &mdash; **def [RunSteps](/recipes/recipes/gerrit_hello_world.py#22)(api):**
 ### *recipes* / [git\_cache\_updater](/recipes/recipes/git_cache_updater.py)
 
-[DEPS](/recipes/recipes/git_cache_updater.py#9): [depot\_tools/depot\_tools][depot_tools/recipe_modules/depot_tools], [recipe\_engine/buildbucket][recipe_engine/recipe_modules/buildbucket], [recipe\_engine/context][recipe_engine/recipe_modules/context], [recipe\_engine/file][recipe_engine/recipe_modules/file], [recipe\_engine/path][recipe_engine/recipe_modules/path], [recipe\_engine/properties][recipe_engine/recipe_modules/properties], [recipe\_engine/runtime][recipe_engine/recipe_modules/runtime], [recipe\_engine/step][recipe_engine/recipe_modules/step], [recipe\_engine/url][recipe_engine/recipe_modules/url]
+[DEPS](/recipes/recipes/git_cache_updater.py#16): [depot\_tools/depot\_tools][depot_tools/recipe_modules/depot_tools], [recipe\_engine/buildbucket][recipe_engine/recipe_modules/buildbucket], [recipe\_engine/context][recipe_engine/recipe_modules/context], [recipe\_engine/file][recipe_engine/recipe_modules/file], [recipe\_engine/path][recipe_engine/recipe_modules/path], [recipe\_engine/properties][recipe_engine/recipe_modules/properties], [recipe\_engine/runtime][recipe_engine/recipe_modules/runtime], [recipe\_engine/step][recipe_engine/recipe_modules/step], [recipe\_engine/url][recipe_engine/recipe_modules/url]
 
 Updates the Git Cache zip files.
 
-&mdash; **def [RunSteps](/recipes/recipes/git_cache_updater.py#55)(api, bucket, repo_urls):**
-
-&mdash; **def [list\_host\_repos](/recipes/recipes/git_cache_updater.py#46)(api, host_url):**
+&mdash; **def [RunSteps](/recipes/recipes/git_cache_updater.py#80)(api, inputs):**
 ### *recipes* / [goma\_hello\_world](/recipes/recipes/goma_hello_world.py)
 
 [DEPS](/recipes/recipes/goma_hello_world.py#12): [build/goma][build/recipe_modules/goma], [recipe\_engine/context][recipe_engine/recipe_modules/context], [recipe\_engine/file][recipe_engine/recipe_modules/file], [recipe\_engine/path][recipe_engine/recipe_modules/path], [recipe\_engine/platform][recipe_engine/recipe_modules/platform], [recipe\_engine/properties][recipe_engine/recipe_modules/properties], [recipe\_engine/step][recipe_engine/recipe_modules/step], [recipe\_engine/time][recipe_engine/recipe_modules/time]
--- recipes/recipes/git_cache_updater.py
+++ recipes/recipes/git_cache_updater.py
@@ -4,7 +4,14 @@
 
 """Updates the Git Cache zip files."""
 
+import re
+
 from recipe_engine import recipe_api
+from PB.recipe_engine import result as result_pb
+from PB.go.chromium.org.luci.buildbucket.proto import common as bb_common_pb
+
+from PB.recipes.infra import git_cache_updater as git_cache_updater_pb
+
 
 DEPS = [
   'depot_tools/depot_tools',
@@ -18,44 +25,65 @@ DEPS = [
   'recipe_engine/url',
 ]
 
-
-PROPERTIES = {
-  'bucket': recipe_api.Property(
-      default=None, help='override GS bucket to upload cached git repos to'),
-  'repo_urls': recipe_api.Property(
-      default=None,
-      help='List of repo urls to limit work to just these repos. Each must:\n'
-           ' * not have /a/ as path prefix\n'
-           ' * no trailing slash\n'
-           ' * no .git suffix\n'
-           'For example, "https://chromium.googlesource.com/infra/infra".')
-}
+PROPERTIES = git_cache_updater_pb.Inputs
 
 
+# TODO(tandrii): delete this.
 BUILDER_MAPPING = {
   'git-cache-chromium': 'https://chromium.googlesource.com/',
 }
 
-TEST_REPOS = """All-Projects
-All-Users
-apps
-chromium/src
-foo/bar"""
 
-
-def list_host_repos(api, host_url):
+def _list_host_repos(api, host_url):
+  host_url = host_url.rstrip('/')
   with api.depot_tools.on_path():
-    output = api.url.get_text('%s?format=TEXT' % host_url,
+    output = api.url.get_text('%s/?format=TEXT' % host_url,
                               default_test_data=TEST_REPOS).output
-    return ['%s%s' % (host_url, repo)
-            for repo in output.splitlines()
-            if repo.lower() not in ['all-projects', 'all-users']]
+    return output.strip().splitlines()
 
 
-def RunSteps(api, bucket, repo_urls):
-  if not repo_urls:
-    repo_urls = list_host_repos(
-        api, BUILDER_MAPPING[api.buildbucket.builder_name])
+def _repos_to_urls(host_url, repos):
+  host_url = host_url.rstrip('/')
+  return ['%s/%s' % (host_url, repo) for repo in repos]
+
+
+class _InvalidRegexp(Exception):
+  pass
+
+
+def _get_repo_urls(api, inputs):
+  if inputs.git_host.host:
+    assert not inputs.repo_urls, 'only 1 of (git_host, repo_urls) allowed'
+    repos = _list_host_repos(api, 'https://' + inputs.git_host.host)
+    if inputs.git_host.exclude_repos:
+      exclude_regexps = []
+      for i, r in enumerate(inputs.git_host.exclude_repos):
+        try:
+          exclude_regexps.append(re.compile('^' + r + '$', re.IGNORECASE))
+        except Exception as e:
+          raise _InvalidRegexp(
+              'invalid regular expression[%d] %r: %s' % (i, r, e))
+      repos = [repo for repo in repos
+               if all(not r.match(repo) for r in exclude_regexps)]
+    return _repos_to_urls('https://' + inputs.git_host.host, repos)
+
+  if inputs.repo_urls:
+    return list(inputs.repo_urls)
+
+  # Legacy. TODO(tandrii): remove this.
+  host_url = BUILDER_MAPPING[api.buildbucket.builder_name]
+  repos = [repo for repo in _list_host_repos(api, host_url)
+           if repo.lower() not in ('all-projects', 'all-users')]
+  return _repos_to_urls(host_url, repos)
+
+
+def RunSteps(api, inputs):
+  try:
+    repo_urls = _get_repo_urls(api, inputs)
+  except _InvalidRegexp as e:
+    return result_pb.RawResult(
+        status=bb_common_pb.FAILURE,
+        summary_markdown=e.message)
 
   work_dir = api.path['cache'].join('builder', 'w')
   api.file.ensure_directory('ensure work_dir', work_dir)
@@ -68,13 +96,14 @@ def RunSteps(api, bucket, repo_urls):
     'CHROME_HEADLESS': '1',
   }
   if api.runtime.is_experimental:
-    assert bucket, 'bucket property is required in experimental mode'
-    env['OVERRIDE_BOOTSTRAP_BUCKET'] = bucket
+    assert inputs.override_bucket, 'override_bucket required for experiments'
+  if inputs.override_bucket:
+    env['OVERRIDE_BOOTSTRAP_BUCKET'] = inputs.override_bucket
 
   with api.context(env=env):
     # Run the updater script.
     with api.depot_tools.on_path():
-      for url in repo_urls:
+      for url in sorted(repo_urls):
         api.step(
             name='Updating %s' % url,
             cmd=[
@@ -91,17 +120,56 @@ def RunSteps(api, bucket, repo_urls):
             ],
             # It's fine for this to fail, just move on to the next one.
             ok_ret='any')
+  return result_pb.RawResult(
+      status=bb_common_pb.SUCCESS,
+      summary_markdown='Updated cache for %d repos' % len(repo_urls),
+  )
+
+
+TEST_REPOS = """
+All-Projects
+All-Users
+apps
+chromium/src
+foo/bar
+"""
 
 
 def GenTests(api):
   yield (
-      api.test('git-cache-chromium') +
-      api.buildbucket.try_build(builder='git-cache-chromium')
+      # TODO(tandrii): remove this legacy buildername-based config.
+      api.test('git-cache-chromium')
+      + api.buildbucket.try_build(builder='git-cache-chromium')
   )
   yield (
-      api.test('git-cache-chromium-led-triggered') +
-      api.runtime(is_luci=True, is_experimental=True) +
-      api.properties(bucket='experimental-gs-bucket',
-                     repo_urls=['https://chromium.googlesource.com/v8/v8']) +
-      api.buildbucket.try_build(builder='git-cache-chromium')
+      api.test('one-repo-experiment')
+      + api.runtime(is_experimental=True, is_luci=True)
+      + api.properties(git_cache_updater_pb.Inputs(
+          override_bucket='experimental-gs-bucket',
+          repo_urls=['https://chromium.googlesource.com/v8/v8'],
+      ))
+  )
+  yield (
+      api.test('host-with-exclusions')
+      + api.properties(git_cache_updater_pb.Inputs(
+          git_host=git_cache_updater_pb.Inputs.GitHost(
+              host='chromium.googlesource.com',
+              exclude_repos=[
+                'foo/.+',
+                'all-projects',
+                'all-users',
+              ],
+          ),
+      ))
+  )
+  yield (
+      api.test('host-with-incorrect-regexp-exclude')
+      + api.properties(git_cache_updater_pb.Inputs(
+          git_host=git_cache_updater_pb.Inputs.GitHost(
+              host='chromium.googlesource.com',
+              exclude_repos=[
+                '?.\\',
+              ],
+          ),
+      ))
   )
