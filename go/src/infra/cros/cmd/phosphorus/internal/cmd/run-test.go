f2c031f5f850acae71a3da558f4e918eeb990af2
Alex Zamorzaev <zamorzaev@chromium.org>
2019-09-26 01:13:21 +0000

    phosphorus: Implement run-test
    A refactoring of `lucifer runtest`.
    
    The change is almost exactly the same as in crrev.com/c/1790609.
    
    Tested via requests asking for client, server and multi-bot tests and
    sanity checked the resulting autoserv command.
    
    Bug: 1006007
    Change-Id: I568d6c8fc59bb940427e9004835feea344eaa8c0
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1823931
    Reviewed-by: Aviv Keshet <akeshet@chromium.org>
    Commit-Queue: Alex Zamorzaev <zamorzaev@chromium.org>
    Auto-Submit: Alex Zamorzaev <zamorzaev@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#26037}

--- go/src/infra/cros/cmd/phosphorus/internal/cmd/run-test.go
+++ go/src/infra/cros/cmd/phosphorus/internal/cmd/run-test.go
@@ -5,9 +5,18 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/maruel/subcommands"
+	"github.com/pkg/errors"
+	"go.chromium.org/chromiumos/infra/proto/go/test_platform/phosphorus"
+	"go.chromium.org/luci/common/cli"
+
+	"infra/cros/cmd/phosphorus/internal/autotest/atutil"
 )
 
 // RunTest subcommand: Run a test against one or multiple DUTs.
@@ -16,25 +25,90 @@ var RunTest = &subcommands.Command{
 	ShortDesc: "Run a test against one or multiple DUTs.",
 	LongDesc: `Run a test against one or multiple DUTs.
 
-Placeholder only, not yet implemented.`,
+A wrapper around 'autoserv'.`,
 	CommandRun: func() subcommands.CommandRun {
 		c := &runTestRun{}
+		c.Flags.StringVar(&c.inputPath, "input_json", "", "Path that contains JSON encoded test_platform.phosphorus.RunTestRequest")
 		return c
 	},
 }
 
 type runTestRun struct {
-	subcommands.CommandRunBase
+	commonRun
 }
 
 func (c *runTestRun) Run(a subcommands.Application, args []string, env subcommands.Env) int {
+	if err := c.validateArgs(); err != nil {
+		fmt.Fprintln(a.GetErr(), err.Error())
+		c.Flags.Usage()
+		return 1
+	}
+
 	if err := c.innerRun(a, args, env); err != nil {
-		fmt.Fprintf(a.GetErr(), "%s\n", err)
+		fmt.Fprintf(a.GetErr(), err.Error())
 		return 1
 	}
 	return 0
 }
 
 func (c *runTestRun) innerRun(a subcommands.Application, args []string, env subcommands.Env) error {
+	var r phosphorus.RunTestRequest
+	if err := readJSONPb(c.inputPath, &r); err != nil {
+		return err
+	}
+
+	if err := validateRunTestRequest(r); err != nil {
+		return err
+	}
+
+	ctx := cli.GetContext(a, c, env)
+
+	return runTestStep(ctx, r)
+}
+
+func validateRunTestRequest(r phosphorus.RunTestRequest) error {
+	missingArgs := validateRequestConfig(r.Config)
+
+	if len(r.DutHostnames) == 0 {
+		missingArgs = append(missingArgs, "DUT hostname(s)")
+	}
+
+	if r.GetAutotest().GetName() == "" {
+		missingArgs = append(missingArgs, "test name")
+	}
+
+	if len(missingArgs) > 0 {
+		return fmt.Errorf("no %s provided", strings.Join(missingArgs, ", "))
+	}
+
+	return nil
+}
+
+// runTestStep runs an individual test. It is a wrapper around autoserv.
+func runTestStep(ctx context.Context, r phosphorus.RunTestRequest) error {
+	j := getMainJob(r.Config)
+
+	dir := filepath.Join(r.Config.Task.ResultsDir, "autoserv_test")
+
+	t := &atutil.HostTest{
+		HostlessTest: atutil.HostlessTest{
+			Args:        r.GetAutotest().GetTestArgs(),
+			ClientTest:  r.GetAutotest().GetIsClientTest(),
+			ControlName: r.GetAutotest().GetName(),
+			Keyvals:     r.GetAutotest().GetKeyvals(),
+			Name:        r.GetAutotest().GetDisplayName(),
+			ResultsDir:  dir,
+		},
+		Hosts:             r.DutHostnames,
+		LocalOnlyHostInfo: true,
+		RequireSSP:        !r.GetAutotest().GetIsClientTest(),
+	}
+
+	_, err := atutil.RunAutoserv(ctx, j, t, os.Stdout)
+
+	if err != nil {
+		return errors.Wrap(err, "run test")
+	}
+
 	return nil
 }
