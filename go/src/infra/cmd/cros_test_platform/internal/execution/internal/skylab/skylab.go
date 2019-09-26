4fefddc20b99f396a682c3b50f3e4f0a5f0920d9
Prathmesh Prabhu <pprabhu@chromium.org>
2019-09-26 14:55:35 +0000

    cros_test_platform: Introduce argsGenearator
    Bug: 1003874
    Change-Id: I5bbfa387422dbfdc6933f1b30c0839edbe069011
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/1823923
    Reviewed-by: Alex Zamorzaev <zamorzaev@chromium.org>
    Commit-Queue: Prathmesh Prabhu <pprabhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#26059}

--- go/src/infra/cmd/cros_test_platform/internal/execution/internal/skylab/skylab.go
+++ go/src/infra/cmd/cros_test_platform/internal/execution/internal/skylab/skylab.go
@@ -58,7 +58,8 @@ type testRun struct {
 
 func newTestRun(ctx context.Context, invocation *steps.EnumerationResponse_AutotestInvocation, params *test_platform.Request_Params, workerConfig *config.Config_SkylabWorker, parentTaskID string) (*testRun, error) {
 	t := testRun{runnable: true}
-	a, err := requestArgs(ctx, invocation, params, workerConfig, parentTaskID)
+	ag := argsGenerator{invocation: invocation, params: params, workerConfig: workerConfig, parentTaskID: parentTaskID}
+	a, err := ag.GenerateArgs(ctx)
 	if err != nil {
 		return nil, errors.Annotate(err, "new test run").Err()
 	}
@@ -81,73 +82,153 @@ func maxInt32IfZero(v int32) int32 {
 	return v
 }
 
-func requestArgs(ctx context.Context, invocation *steps.EnumerationResponse_AutotestInvocation, params *test_platform.Request_Params, workerConfig *config.Config_SkylabWorker, parentTaskID string) (request.Args, error) {
-	isClient, err := isClientTest(invocation)
-	if err != nil {
-		return request.Args{}, errors.Annotate(err, "create request args").Err()
-	}
-
-	provisionableDimensions, err := toProvisionableDimensions(params.SoftwareDependencies)
-	if err != nil {
-		return request.Args{}, errors.Annotate(err, "create request args").Err()
-	}
-
-	timeout, err := toTimeout(params)
-	if err != nil {
-		return request.Args{}, errors.Annotate(err, "create request args").Err()
-	}
-
-	kv := getKeyvals(params, parentTaskID)
-	updateWithInvocationKeyvals(kv, invocation)
-	addKeyvalsForDisplayName(ctx, kv, params, invocation)
-
-	cmd := &worker.Command{
-		TaskName:        invocation.Test.Name,
-		ClientTest:      isClient,
-		OutputToIsolate: true,
-		TestArgs:        invocation.TestArgs,
-		Keyvals:         kv,
-	}
-	cmd.Config(wrap(workerConfig))
-
-	labels, err := toInventoryLabels(params, invocation.Test.Dependencies)
-	if err != nil {
-		return request.Args{}, errors.Annotate(err, "create request args").Err()
-	}
-
-	args := request.Args{
-		Cmd:                     *cmd,
-		SchedulableLabels:       *labels,
-		Dimensions:              params.GetFreeformAttributes().GetSwarmingDimensions(),
-		ParentTaskID:            parentTaskID,
-		Priority:                params.GetScheduling().GetPriority(),
-		ProvisionableDimensions: provisionableDimensions,
-		SwarmingTags:            swarmingTags(cmd, workerConfig, params),
-		Timeout:                 timeout,
-	}
-
-	return args, nil
+type argsGenerator struct {
+	invocation   *steps.EnumerationResponse_AutotestInvocation
+	params       *test_platform.Request_Params
+	workerConfig *config.Config_SkylabWorker
+	parentTaskID string
 }
 
-func isClientTest(invocation *steps.EnumerationResponse_AutotestInvocation) (bool, error) {
-	switch invocation.Test.ExecutionEnvironment {
+func (a *argsGenerator) GenerateArgs(ctx context.Context) (request.Args, error) {
+	isClient, err := a.isClientTest()
+	if err != nil {
+		return request.Args{}, errors.Annotate(err, "create request args").Err()
+	}
+
+	provisionableDimensions, err := a.provisionableDimensions()
+	if err != nil {
+		return request.Args{}, errors.Annotate(err, "create request args").Err()
+	}
+
+	timeout, err := a.timeout()
+	if err != nil {
+		return request.Args{}, errors.Annotate(err, "create request args").Err()
+	}
+
+	kv := a.baseKeyvals()
+	a.updateWithInvocationKeyvals(kv)
+	a.addKeyvalsForDisplayName(ctx, kv)
+
+	cmd := &worker.Command{
+		TaskName:        a.invocation.Test.Name,
+		ClientTest:      isClient,
+		OutputToIsolate: true,
+		TestArgs:        a.invocation.TestArgs,
+		Keyvals:         kv,
+	}
+	cmd.Config(wrap(a.workerConfig))
+
+	labels, err := a.inventoryLabels()
+	if err != nil {
+		return request.Args{}, errors.Annotate(err, "create request args").Err()
+	}
+
+	return request.Args{
+		Cmd:                     *cmd,
+		SchedulableLabels:       *labels,
+		Dimensions:              a.params.GetFreeformAttributes().GetSwarmingDimensions(),
+		ParentTaskID:            a.parentTaskID,
+		Priority:                a.params.GetScheduling().GetPriority(),
+		ProvisionableDimensions: provisionableDimensions,
+		SwarmingTags:            a.swarmingTags(cmd),
+		Timeout:                 timeout,
+	}, nil
+
+}
+
+func (a *argsGenerator) isClientTest() (bool, error) {
+	switch a.invocation.Test.ExecutionEnvironment {
 	case build_api.AutotestTest_EXECUTION_ENVIRONMENT_CLIENT:
 		return true, nil
 	case build_api.AutotestTest_EXECUTION_ENVIRONMENT_SERVER:
 		return false, nil
 	default:
-		return false, errors.Reason("unknown exec environment %s", invocation.Test.ExecutionEnvironment).Err()
+		return false, errors.Reason("unknown exec environment %s", a.invocation.Test.ExecutionEnvironment).Err()
 	}
 }
 
-func addKeyvalsForDisplayName(ctx context.Context, kv map[string]string, params *test_platform.Request_Params, invocation *steps.EnumerationResponse_AutotestInvocation) {
+func (a *argsGenerator) inventoryLabels() (*inventory.SchedulableLabels, error) {
+	deps := a.invocation.Test.Dependencies
+	flatDims := make([]string, len(deps))
+	for i, dep := range deps {
+		flatDims[i] = dep.Label
+	}
+
+	inv := labels.Revert(flatDims)
+
+	if a.params.GetSoftwareAttributes().GetBuildTarget() != nil {
+		*inv.Board = a.params.SoftwareAttributes.BuildTarget.Name
+	}
+	if a.params.GetHardwareAttributes().GetModel() != "" {
+		*inv.Model = a.params.HardwareAttributes.Model
+	}
+
+	if p := a.params.GetScheduling().GetPool(); p != nil {
+		switch v := p.(type) {
+		case *test_platform.Request_Params_Scheduling_ManagedPool_:
+			pool, ok := poolMap[v.ManagedPool]
+			if !ok {
+				return nil, errors.Reason("unknown managed pool %s", v.ManagedPool.String()).Err()
+			}
+			inv.CriticalPools = append(inv.CriticalPools, pool)
+		case *test_platform.Request_Params_Scheduling_UnmanagedPool:
+			inv.SelfServePools = append(inv.SelfServePools, v.UnmanagedPool)
+		case *test_platform.Request_Params_Scheduling_QuotaAccount:
+			inv.CriticalPools = append(inv.CriticalPools, inventory.SchedulableLabels_DUT_POOL_QUOTA)
+		default:
+			panic(fmt.Sprintf("unhandled scheduling type %#v", p))
+		}
+	}
+	return inv, nil
+}
+
+const (
+	// These prefixes are interpreted by autotest's provisioning behavior;
+	// they are defined in the autotest repo, at utils/labellib.py
+	prefixChromeOS   = "cros-version"
+	prefixFirmwareRO = "fwro-version"
+	prefixFirmwareRW = "fwrw-version"
+)
+
+func (a *argsGenerator) provisionableDimensions() ([]string, error) {
+	deps := a.params.SoftwareDependencies
+	builds, err := common.ExtractBuilds(deps)
+	if err != nil {
+		return nil, errors.Annotate(err, "get provisionable dimensions").Err()
+	}
+
+	var dims []string
+	if b := builds.ChromeOS; b != "" {
+		dims = append(dims, "provisionable-"+prefixChromeOS+":"+b)
+	}
+	if b := builds.FirmwareRO; b != "" {
+		dims = append(dims, "provisionable-"+prefixFirmwareRO+":"+b)
+	}
+	if b := builds.FirmwareRW; b != "" {
+		dims = append(dims, "provisionable-"+prefixFirmwareRW+":"+b)
+	}
+	return dims, nil
+}
+
+func (a *argsGenerator) timeout() (time.Duration, error) {
+	if a.params.Time == nil {
+		return 0, errors.Reason("get timeout: nil params.time").Err()
+	}
+	duration, err := ptypes.Duration(a.params.Time.MaximumDuration)
+	if err != nil {
+		return 0, errors.Annotate(err, "get timeout").Err()
+	}
+	return duration, nil
+}
+
+func (a *argsGenerator) addKeyvalsForDisplayName(ctx context.Context, kv map[string]string) {
 	const displayNameKey = "label"
 
-	if invocation.DisplayName != "" {
-		kv[displayNameKey] = invocation.DisplayName
+	if a.invocation.DisplayName != "" {
+		kv[displayNameKey] = a.invocation.DisplayName
 		return
 	}
-	kv[displayNameKey] = constructDisplayNameFromRequestParams(ctx, kv, params, invocation.GetTest().GetName())
+	kv[displayNameKey] = a.constructDisplayNameFromRequestParams(ctx, kv)
 }
 
 const (
@@ -159,8 +240,9 @@ const (
 // (aka "label") keyval to obtain semantic information about the request.
 // TODO(crbug.com/1003490): Drop this once result reporting is updated to stop
 // parsing the "label" keyval.
-func constructDisplayNameFromRequestParams(ctx context.Context, kv map[string]string, params *test_platform.Request_Params, testName string) string {
-	builds, err := common.ExtractBuilds(params.SoftwareDependencies)
+func (a *argsGenerator) constructDisplayNameFromRequestParams(ctx context.Context, kv map[string]string) string {
+	testName := a.invocation.GetTest().GetName()
+	builds, err := common.ExtractBuilds(a.params.SoftwareDependencies)
 	if err != nil {
 		logging.Warningf(ctx,
 			"Failed to get build due to error %s\n Defaulting to test name as display name: %s",
@@ -182,40 +264,40 @@ func constructDisplayNameFromRequestParams(ctx context.Context, kv map[string]st
 	return build + "/" + suite + "/" + testName
 }
 
-func updateWithInvocationKeyvals(kv map[string]string, invocation *steps.EnumerationResponse_AutotestInvocation) {
-	for k, v := range invocation.GetResultKeyvals() {
+func (a *argsGenerator) updateWithInvocationKeyvals(kv map[string]string) {
+	for k, v := range a.invocation.GetResultKeyvals() {
 		if _, ok := kv[k]; !ok {
 			kv[k] = v
 		}
 	}
 }
 
-func getKeyvals(params *test_platform.Request_Params, parentTaskID string) map[string]string {
-	keyvals := params.GetDecorations().GetAutotestKeyvals()
+func (a *argsGenerator) baseKeyvals() map[string]string {
+	keyvals := a.params.GetDecorations().GetAutotestKeyvals()
 	if keyvals == nil {
 		keyvals = make(map[string]string)
 	}
-	if parentTaskID != "" {
+	if a.parentTaskID != "" {
 		// This keyval is inspected by some downstream results consumers such as
 		// goldeneye and stainless.
 		// TODO(akeshet): Consider whether parameter-specified parent_job_id
 		// should be respected if it was specified.
-		keyvals["parent_job_id"] = parentTaskID
+		keyvals["parent_job_id"] = a.parentTaskID
 	}
 	return keyvals
 }
 
-func swarmingTags(cmd *worker.Command, conf *config.Config_SkylabWorker, params *test_platform.Request_Params) []string {
+func (a *argsGenerator) swarmingTags(cmd *worker.Command) []string {
 	tags := []string{
-		"luci_project:" + conf.LuciProject,
+		"luci_project:" + a.workerConfig.LuciProject,
 		"log_location:" + cmd.LogDogAnnotationURL,
 	}
-	if qa := params.GetScheduling().GetQuotaAccount(); qa != "" {
+	if qa := a.params.GetScheduling().GetQuotaAccount(); qa != "" {
 		tags = append(tags, "qs_account:"+qa)
 	}
 	// TODO(akeshet): Consider whether to ban qs_account, luci_project, log_location,
 	// and other "special tags" from being client-specified here.
-	tags = append(tags, params.GetDecorations().GetTags()...)
+	tags = append(tags, a.params.GetDecorations().GetTags()...)
 	return tags
 }
 
@@ -609,80 +691,6 @@ func (r *TaskSet) shouldRetry(tr *testRun) (bool, error) {
 
 func (r *TaskSet) globalRetriesRemaining() int32 {
 	return r.globalMaxRetries - r.retries
-}
-
-func toInventoryLabels(params *test_platform.Request_Params, deps []*build_api.AutotestTaskDependency) (*inventory.SchedulableLabels, error) {
-	flatDims := make([]string, len(deps))
-	for i, dep := range deps {
-		flatDims[i] = dep.Label
-	}
-
-	inv := labels.Revert(flatDims)
-
-	if params.GetSoftwareAttributes().GetBuildTarget() != nil {
-		*inv.Board = params.SoftwareAttributes.BuildTarget.Name
-	}
-	if params.GetHardwareAttributes().GetModel() != "" {
-		*inv.Model = params.HardwareAttributes.Model
-	}
-
-	if p := params.GetScheduling().GetPool(); p != nil {
-		switch v := p.(type) {
-		case *test_platform.Request_Params_Scheduling_ManagedPool_:
-			pool, ok := poolMap[v.ManagedPool]
-			if !ok {
-				return nil, errors.Reason("unknown managed pool %s", v.ManagedPool.String()).Err()
-			}
-			inv.CriticalPools = append(inv.CriticalPools, pool)
-		case *test_platform.Request_Params_Scheduling_UnmanagedPool:
-			inv.SelfServePools = append(inv.SelfServePools, v.UnmanagedPool)
-		case *test_platform.Request_Params_Scheduling_QuotaAccount:
-			inv.CriticalPools = append(inv.CriticalPools, inventory.SchedulableLabels_DUT_POOL_QUOTA)
-		default:
-			panic(fmt.Sprintf("unhandled scheduling type %#v", p))
-		}
-	}
-
-	return inv, nil
-}
-
-const (
-	// These prefixes are interpreted by autotest's provisioning behavior;
-	// they are defined in the autotest repo, at utils/labellib.py
-	prefixChromeOS   = "cros-version"
-	prefixFirmwareRO = "fwro-version"
-	prefixFirmwareRW = "fwrw-version"
-)
-
-func toProvisionableDimensions(deps []*test_platform.Request_Params_SoftwareDependency) ([]string, error) {
-	builds, err := common.ExtractBuilds(deps)
-	if err != nil {
-		return nil, errors.Annotate(err, "get provisionable dimensions").Err()
-	}
-
-	var dims []string
-	if b := builds.ChromeOS; b != "" {
-		dims = append(dims, "provisionable-"+prefixChromeOS+":"+b)
-	}
-	if b := builds.FirmwareRO; b != "" {
-		dims = append(dims, "provisionable-"+prefixFirmwareRO+":"+b)
-	}
-	if b := builds.FirmwareRW; b != "" {
-		dims = append(dims, "provisionable-"+prefixFirmwareRW+":"+b)
-	}
-
-	return dims, nil
-}
-
-func toTimeout(params *test_platform.Request_Params) (time.Duration, error) {
-	if params.Time == nil {
-		return 0, errors.Reason("get timeout: nil params.time").Err()
-	}
-	duration, err := ptypes.Duration(params.Time.MaximumDuration)
-	if err != nil {
-		return 0, errors.Annotate(err, "get timeout").Err()
-	}
-	return duration, nil
 }
 
 func unpackResult(results []*swarming_api.SwarmingRpcsTaskResult, taskID string) (*swarming_api.SwarmingRpcsTaskResult, error) {
