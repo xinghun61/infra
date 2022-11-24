4d7e7ab640993b23f24aee954eb13b06b23d0400
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-24 18:23:10 +0000

    Adding tarfile member sanitization to extractall()

--- infra/tools/dockerbuild/source.py
+++ infra/tools/dockerbuild/source.py
@@ -232,7 +232,26 @@ class Repository(object):
   @staticmethod
   def _unpack_tar_generic(path, dest_dir):
     with tarfile.open(path, 'r') as tf:
-      tf.extractall(dest_dir)
+      def is_within_directory(directory, target):
+          
+          abs_directory = os.path.abspath(directory)
+          abs_target = os.path.abspath(target)
+      
+          prefix = os.path.commonprefix([abs_directory, abs_target])
+          
+          return prefix == abs_directory
+      
+      def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+      
+          for member in tar.getmembers():
+              member_path = os.path.join(path, member.name)
+              if not is_within_directory(path, member_path):
+                  raise Exception("Attempted Path Traversal in Tar File")
+      
+          tar.extractall(path, members, numeric_owner=numeric_owner) 
+          
+      
+      safe_extract(tf, dest_dir)
 
   @staticmethod
   def _unpack_zip_generic(path, dest_dir):
--- recipes/recipe_modules/third_party_packages/resources/archive_util.py
+++ recipes/recipe_modules/third_party_packages/resources/archive_util.py
@@ -45,7 +45,26 @@ _UNPACK['.zip'] = _unpack_zip
 
 def _unpack_tar(archive, dest):
   with tarfile.open(archive, 'r') as tf:
-    tf.extractall(dest)
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(tf, dest)
 
 _UNPACK['.tar.gz'] = _unpack_tar
 
